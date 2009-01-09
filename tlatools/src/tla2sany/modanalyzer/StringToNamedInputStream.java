32047ee84dcb76359918a5d5099b6b68a3af6db2
simonzam <simonzam@76a6fc44-f60b-0410-a9a8-e67b0e8fc65c>
2009-01-09 22:46:38 +0000

    added svn:keyword property Id
    git-svn-id: svn+ssh://svn.msr-inria.inria.fr/var/lib/svn/repository/tla/trunk/tla2-inria@12511 76a6fc44-f60b-0410-a9a8-e67b0e8fc65c

--- tlatools/src/tla2sany/modanalyzer/StringToNamedInputStream.java
+++ tlatools/src/tla2sany/modanalyzer/StringToNamedInputStream.java
@@ -2,13 +2,12 @@
 // Portions Copyright (c) 2003 Microsoft Corporation.  All rights reserved.
 package tla2sany.modanalyzer;
 
-import java.io.*;
-
 /**
  * This interface contains a single method toIStream that maps a name to
  * a NamedInputStream.  (See the FrontEnd class's parseStream method for 
  * how it is used.)                                                      
  */
-public interface StringToNamedInputStream {
-  public NamedInputStream toIStream(String name);
+public interface StringToNamedInputStream
+{
+    public NamedInputStream toIStream(String name);
 }
