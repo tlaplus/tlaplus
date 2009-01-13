6418735b893131e1a8d50a50479020797b814655
simonzam <simonzam@76a6fc44-f60b-0410-a9a8-e67b0e8fc65c>
2009-01-13 04:31:44 +0000

    init
    git-svn-id: svn+ssh://svn.msr-inria.inria.fr/var/lib/svn/repository/tla/trunk/tla2-inria@12551 76a6fc44-f60b-0410-a9a8-e67b0e8fc65c

---
+++ org.lamport.tla.toolbox/src/org/lamport/tla/toolbox/ui/view/ProblemMarkerView.java
@@ -0,0 +1,21 @@
+package org.lamport.tla.toolbox.ui.view;
+
+import org.eclipse.ui.views.markers.MarkerSupportView;
+
+/**
+ * Represents the problems
+ * @author Simon Zambrovski
+ * @version $Id$
+ */
+public class ProblemMarkerView extends MarkerSupportView
+{
+    
+    public final static String ID = "toolbox.view.ProblemMarkerView";
+    public final static String CONTENT_GENERATOR_ID = "toolbox.markers.contentGenerator"; 
+    
+    public ProblemMarkerView()
+    {
+        super(CONTENT_GENERATOR_ID);
+    }
+
+}
