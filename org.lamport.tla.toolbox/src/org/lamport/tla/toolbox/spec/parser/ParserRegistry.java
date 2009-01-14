8072ef4d69df11c7b4ffcc04ac7840c19e6ffe46
simonzam <simonzam@76a6fc44-f60b-0410-a9a8-e67b0e8fc65c>
2009-01-14 02:36:35 +0000

    propagates now status change only
    git-svn-id: svn+ssh://svn.msr-inria.inria.fr/var/lib/svn/repository/tla/trunk/tla2-inria@12562 76a6fc44-f60b-0410-a9a8-e67b0e8fc65c

--- org.lamport.tla.toolbox/src/org/lamport/tla/toolbox/spec/parser/ParserRegistry.java
+++ org.lamport.tla.toolbox/src/org/lamport/tla/toolbox/spec/parser/ParserRegistry.java
@@ -5,17 +5,19 @@ import java.util.Vector;
 /**
  * Default parser registry, that is used for the listers interested in parse result changes 
  * @author zambrovski
+ * @version $Id$
  */
 public class ParserRegistry implements IParserRegistry
 {
     private Vector listeners = new Vector();
-    
+    private int status = IParseConstants.UNKNOWN;
+
     /* (non-Javadoc)
      * @see org.lamport.tla.toolbox.spec.parser.IParserRegistry#addParseResultListener(org.lamport.tla.toolbox.spec.parser.IParseResultListner)
      */
     public void addParseResultListener(IParseResultListner listener)
     {
-        if (!listeners.contains(listener)) 
+        if (!listeners.contains(listener))
         {
             listeners.add(listener);
         }
@@ -24,8 +26,22 @@ public class ParserRegistry implements IParserRegistry
     /* (non-Javadoc)
      * @see org.lamport.tla.toolbox.spec.parser.IParserRegistry#parseResultChanged(int)
      */
-    public void parseResultChanged(int parseStatus)
+    public synchronized void parseResultChanged(int parseStatus)
     {
+        // only react ro real status changes
+        if (status == parseStatus)
+        {
+            return;
+        }
+
+        // MODIFIED status can only replace status that is "greater" (see the constant values)
+        // it will not replace UNPARSED and UNKNOWN
+        if (parseStatus == IParseConstants.MODIFIED && status < parseStatus)
+        {
+            return;
+        }
+
+        this.status = parseStatus;
         for (int i = 0; i < listeners.size(); i++)
         {
             ((IParseResultListner) listeners.elementAt(i)).parseResultChanged(parseStatus);
