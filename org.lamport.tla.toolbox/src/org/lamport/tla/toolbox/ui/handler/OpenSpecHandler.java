8e6561b450a29b4350cdcb0cca76d566d89dc4e7
simonzam <simonzam@76a6fc44-f60b-0410-a9a8-e67b0e8fc65c>
2009-01-14 02:18:38 +0000

    dirty editor listener added
    git-svn-id: svn+ssh://svn.msr-inria.inria.fr/var/lib/svn/repository/tla/trunk/tla2-inria@12556 76a6fc44-f60b-0410-a9a8-e67b0e8fc65c

--- org.lamport.tla.toolbox/src/org/lamport/tla/toolbox/ui/handler/OpenSpecHandler.java
+++ org.lamport.tla.toolbox/src/org/lamport/tla/toolbox/ui/handler/OpenSpecHandler.java
@@ -5,15 +5,24 @@ import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.commands.IHandler;
 import org.eclipse.core.resources.IFile;
+import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.IPropertyListener;
+import org.eclipse.ui.IWorkbenchPartConstants;
 import org.eclipse.ui.part.FileEditorInput;
 import org.lamport.tla.toolbox.Activator;
 import org.lamport.tla.toolbox.spec.Spec;
+import org.lamport.tla.toolbox.spec.parser.IParseConstants;
 import org.lamport.tla.toolbox.ui.perspective.InitialPerspective;
 import org.lamport.tla.toolbox.ui.perspective.SpecLoadedPerspective;
 import org.lamport.tla.toolbox.util.PreferenceStoreHelper;
 import org.lamport.tla.toolbox.util.ResourceHelper;
 import org.lamport.tla.toolbox.util.UIHelper;
 
+/**
+ * Handles the open-spec command
+ * @author Simon Zambrovski
+ * @version $Id$
+ */
 public class OpenSpecHandler extends AbstractHandler implements IHandler
 {
     public static final String TLA_EDITOR = "org.lamport.tla.toolbox.editor.basic.TLAEditor";
@@ -38,9 +47,9 @@ public class OpenSpecHandler extends AbstractHandler implements IHandler
         UIHelper.switchPerspective(SpecLoadedPerspective.ID);
         // close the initial perspective
         UIHelper.closeWindow(InitialPerspective.ID);
-       
 
         String[] editors = PreferenceStoreHelper.getOpenedEditors(spec.getProject());
+        IEditorPart part = null;
         if (editors.length != 0)
         {
 
@@ -51,20 +60,28 @@ public class OpenSpecHandler extends AbstractHandler implements IHandler
                 IFile file = ResourceHelper.getLinkedFile(spec.getProject(), editors[i]);
 
                 // open the editor
-                UIHelper.openEditor(TLA_EDITOR, new FileEditorInput(file));
+                part = UIHelper.openEditor(TLA_EDITOR, new FileEditorInput(file));
 
             }
         } else
         {
             // open the editor
-            UIHelper.openEditor(TLA_EDITOR, new FileEditorInput(spec.getRootFile()));
+            part = UIHelper.openEditor(TLA_EDITOR, new FileEditorInput(spec.getRootFile()));
+            part.addPropertyListener(new IPropertyListener() {
+
+                public void propertyChanged(Object source, int propId)
+                {
+                    if (IWorkbenchPartConstants.PROP_DIRTY == propId)
+                    {
+                        Activator.getParserRegistry().parseResultChanged(IParseConstants.MODIFIED);
+                    }
+                }
+            });
         }
-
-
 
         // store information about opened spec in the spec manager
         Activator.getSpecManager().setSpecLoaded(spec);
-        
+
         return null;
     }
 
