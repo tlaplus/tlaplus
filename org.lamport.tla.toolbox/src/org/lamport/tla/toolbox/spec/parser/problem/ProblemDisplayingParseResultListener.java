5587713047c9aefc1e834b7909319c10f49be076
simonzam <simonzam@76a6fc44-f60b-0410-a9a8-e67b0e8fc65c>
2009-01-15 02:51:32 +0000

    changed behavior, now looking consulting the preference before raising the window
    git-svn-id: svn+ssh://svn.msr-inria.inria.fr/var/lib/svn/repository/tla/trunk/tla2-inria@12577 76a6fc44-f60b-0410-a9a8-e67b0e8fc65c

--- org.lamport.tla.toolbox/src/org/lamport/tla/toolbox/spec/parser/problem/ProblemDisplayingParseResultListener.java
+++ org.lamport.tla.toolbox/src/org/lamport/tla/toolbox/spec/parser/problem/ProblemDisplayingParseResultListener.java
@@ -1,37 +1,47 @@
 package org.lamport.tla.toolbox.spec.parser.problem;
 
+import org.lamport.tla.toolbox.spec.parser.IParseConstants;
 import org.lamport.tla.toolbox.spec.parser.IParseResultListner;
 import org.lamport.tla.toolbox.ui.perspective.ProblemsPerspective;
 import org.lamport.tla.toolbox.util.AdapterFactory;
 import org.lamport.tla.toolbox.util.TLAMarkerHelper;
 import org.lamport.tla.toolbox.util.UIHelper;
+import org.lamport.tla.toolbox.util.pref.IPreferenceConstants;
+import org.lamport.tla.toolbox.util.pref.PreferenceStoreHelper;
 
 /**
- * React to changes with UI display change 
- * @author zambrovski
+ * React to re-parse 
+ * @author Simon Zambrovski
  * @version $Id$
  */
 public class ProblemDisplayingParseResultListener implements IParseResultListner
 {
 
-    /* (non-Javadoc)
+    /**
      * @see org.lamport.tla.toolbox.spec.parser.IParseResultListner#parseResultChanged(int)
      */
     public void parseResultChanged(int parseStatus)
     {
- 
-        UIHelper.closeWindow(ProblemsPerspective.ID);
-
-        // there were problems -> open the problem view
-        if (AdapterFactory.isProblemStatus(parseStatus))
+        // ignore any changes to the MODIFIED state
+        if (IParseConstants.MODIFIED == parseStatus) 
         {
-            // UIHelper.openView(ProblemMarkerView.ID);
-            UIHelper.openPerspectiveInWindowRight(ProblemsPerspective.ID, null, 400);
+            return;
         }
 
+        // look up the preference for raising windows on errors
+        boolean showErrors = PreferenceStoreHelper.getInstancePreferenceStore().getBoolean(IPreferenceConstants.P_PARSER_POPUP_ERRORS);
+        if (showErrors) 
+        {
+            UIHelper.closeWindow(ProblemsPerspective.ID);
+            // there were problems -> open the problem view
+            if (AdapterFactory.isProblemStatus(parseStatus))
+            {
+                UIHelper.openPerspectiveInWindowRight(ProblemsPerspective.ID, null, ProblemsPerspective.WIDTH);
+            }
+        }
+        
         // update problem markers
         TLAMarkerHelper.updateProblemInformation();
-
     }
 
 }
