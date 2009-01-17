a428fef93177892a07d92065c9e91567cf5d0cef
simonzam <simonzam@76a6fc44-f60b-0410-a9a8-e67b0e8fc65c>
2009-01-17 07:39:54 +0000

    changed to usual contribution item
    git-svn-id: svn+ssh://svn.msr-inria.inria.fr/var/lib/svn/repository/tla/trunk/tla2-inria@12587 76a6fc44-f60b-0410-a9a8-e67b0e8fc65c

---
+++ org.lamport.tla.toolbox/src/org/lamport/tla/toolbox/ui/contribution/ParseStatusContributionItem.java
@@ -0,0 +1,64 @@
+package org.lamport.tla.toolbox.ui.contribution;
+
+import org.eclipse.jface.action.ContributionItem;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Label;
+import org.lamport.tla.toolbox.Activator;
+import org.lamport.tla.toolbox.spec.Spec;
+import org.lamport.tla.toolbox.util.AdapterFactory;
+
+/**
+ * A widget placed to the status line that shows the parse status of the root module 
+ * @version $Id$
+ * @author Simon Zambrovski
+ */
+public class ParseStatusContributionItem extends ContributionItem
+{
+    private Label statusLabel = null;
+
+    public ParseStatusContributionItem()
+    {
+
+    }
+
+    public ParseStatusContributionItem(String id)
+    {
+        super(id);
+    }
+    /**
+     * Creates the control 
+     */
+    public void fill(Composite parent)
+    {
+        // Create a composite to place the label in
+        Composite comp = new Composite(parent, SWT.NONE);
+
+        // Give some room around the control
+        FillLayout layout = new FillLayout();
+        layout.marginHeight = 2;
+        layout.marginWidth = 2;
+        comp.setLayout(layout);
+
+        // Create a label for the trim.
+        statusLabel = new Label(comp, SWT.BORDER | SWT.CENTER);
+        statusLabel.setForeground(parent.getDisplay().getSystemColor(SWT.COLOR_BLACK));
+        statusLabel.setToolTipText("Parse status");
+        
+        // display status
+        this.updateStatus();
+    }
+
+
+    /**
+     * Updates status from the specification currently loaded in the SpecManager
+     */
+    public void updateStatus()
+    {
+        Spec spec = Activator.getSpecManager().getSpecLoaded();
+        statusLabel.setText(AdapterFactory.getStatusAsString(spec));
+        statusLabel.setBackground(statusLabel.getDisplay().getSystemColor(AdapterFactory.getStatusAsSWTColor(spec)));
+        statusLabel.redraw();
+    }
+}
