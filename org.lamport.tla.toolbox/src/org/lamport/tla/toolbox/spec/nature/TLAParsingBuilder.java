97f26c8393ac09c2065440f9d5257d8792ca2a45
simonzam <simonzam@76a6fc44-f60b-0410-a9a8-e67b0e8fc65c>
2009-01-17 07:51:04 +0000

    progress
    git-svn-id: svn+ssh://svn.msr-inria.inria.fr/var/lib/svn/repository/tla/trunk/tla2-inria@12598 76a6fc44-f60b-0410-a9a8-e67b0e8fc65c

--- org.lamport.tla.toolbox/src/org/lamport/tla/toolbox/spec/nature/TLAParsingBuilder.java
+++ org.lamport.tla.toolbox/src/org/lamport/tla/toolbox/spec/nature/TLAParsingBuilder.java
@@ -13,6 +13,7 @@ import org.lamport.tla.toolbox.Activator;
 import org.lamport.tla.toolbox.spec.Spec;
 import org.lamport.tla.toolbox.spec.parser.IParserLauncher;
 import org.lamport.tla.toolbox.spec.parser.StreamInterpretingParserLauncher;
+import org.lamport.tla.toolbox.util.AdapterFactory;
 import org.lamport.tla.toolbox.util.TLAMarkerHelper;
 import org.lamport.tla.toolbox.util.pref.IPreferenceConstants;
 import org.lamport.tla.toolbox.util.pref.PreferenceStoreHelper;
@@ -26,14 +27,13 @@ public class TLAParsingBuilder extends IncrementalProjectBuilder
 {
     public static final String BUILDER_ID = "toolbox.builder.TLAParserBuilder";
 
-    private IParserLauncher launcher = new StreamInterpretingParserLauncher();
-    
-    
-    
-    
+    private final IParserLauncher launcher = new StreamInterpretingParserLauncher();
+
     protected void clean(IProgressMonitor monitor) throws CoreException
     {
-        super.clean(monitor);
+        System.out.println("Clean has been invoked");
+        Spec spec = Activator.getSpecManager().getSpecLoaded();
+        // TLAMarkerHelper.deleteMarkers(spec, monitor);
     }
 
     /**
@@ -41,43 +41,19 @@ public class TLAParsingBuilder extends IncrementalProjectBuilder
      */
     protected IProject[] build(int kind, Map args, IProgressMonitor monitor) throws CoreException
     {
-        /*
-        System.out.println("Build has been invoked");
-        */
-        switch (kind)
-        {
-        case INCREMENTAL_BUILD:
-            System.out.println("Incremental build");
-            break;
-        case FULL_BUILD:
-            System.out.println("Full build (e.G. on load)");
-            break;
-        case CLEAN_BUILD:
-            System.out.println("Clean build (explicit invocation)");
-            break;
-        case AUTO_BUILD:
-            System.out.println("Auto build (e.G. on saved modification)");
-            break;
-        default:
-            System.out.println("BUG. Unknown build type :" + kind + ".");
-        }
 
         if (PreferenceStoreHelper.getInstancePreferenceStore().getBoolean(
                 IPreferenceConstants.P_PARSER_RUN_ON_MODIFICATION))
         {
-            if (AUTO_BUILD == kind || FULL_BUILD == kind)
-            {
-                System.out.println("Running a " + ((kind == AUTO_BUILD) ? "Auto-Build" : "Full-Build"));
-                runBuild(monitor);
-            }
+            System.out.println("Running a Build (on): " + kind);
         } else
         {
-            if (CLEAN_BUILD == kind)
-            {
-                System.out.println("Running a Clean-Build");
-                runBuild(monitor);
-            }
+            System.out.println("Running a Build (off): " + kind);
         }
+
+        runBuild(monitor);
+
+        forgetLastBuiltState();
 
         // since no deltas should be recalculated, return null.
         // this could change in the future
@@ -89,31 +65,16 @@ public class TLAParsingBuilder extends IncrementalProjectBuilder
      */
     private void runBuild(IProgressMonitor monitor)
     {
-        //TODO handle the monitor
-        
-        
-        final Spec spec = Activator.getSpecManager().getSpecLoaded();
+        // TODO handle the monitor
+
         try
         {
-            TLAMarkerHelper.deleteMarkers(spec, monitor);
             getProject().accept(new SpecificationRootFileParsingVisitor());
-            TLAMarkerHelper.setupProblemInformation(spec, monitor);
-            
-            IWorkspaceRunnable runnable = new IWorkspaceRunnable() {
-
-                public void run(IProgressMonitor monitor) throws CoreException
-                {
-
-                    Activator.getParserRegistry().parseResultChanged(spec.getStatus());
-                }
-            };
-            getProject().getWorkspace().run(runnable, monitor);
-            
 
         } catch (CoreException e)
         {
             // TODO
-            
+            e.printStackTrace();
         }
     }
 
@@ -124,19 +85,34 @@ public class TLAParsingBuilder extends IncrementalProjectBuilder
      */
     public class SpecificationRootFileParsingVisitor implements IResourceVisitor
     {
+        IProgressMonitor monitor = null;
+        
         /**
          * @see org.eclipse.core.resources.IResourceVisitor#visit(org.eclipse.core.resources.IResource)
          */
         public boolean visit(IResource resource) throws CoreException
         {
+            
             if (resource instanceof IProject && ((IProject) resource).hasNature(TLANature.ID))
             {
+                System.out.println("Build Invoked");
                 // TODO improve this...
                 // better: lookup in the project property for the spec name
                 // and search a spec by name
-                Spec spec = Activator.getSpecManager().getSpecLoaded();
-                
-                launcher.parseSpecification(spec);
+
+                final Spec spec = Activator.getSpecManager().getSpecLoaded();
+                IWorkspaceRunnable run = new IWorkspaceRunnable(){
+
+                    public void run(IProgressMonitor monitor) throws CoreException
+                    {
+                        TLAMarkerHelper.deleteMarkers(spec, monitor);
+                        launcher.parseSpecification(spec);
+                        TLAMarkerHelper.setupProblemInformation(spec, monitor);
+                        System.out.println("Resulting status is: " + AdapterFactory.getStatusAsString(spec));
+                    }
+                };
+                resource.getWorkspace().run(run, monitor);
+
             }
 
             // invoke only on the IProject, never on the single module files
