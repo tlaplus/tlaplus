e59e51f70fec1c63f974002972d8d63b5f05dbbb
simonzam <simonzam@76a6fc44-f60b-0410-a9a8-e67b0e8fc65c>
2009-01-17 07:45:34 +0000

    switched to platform builder API
    git-svn-id: svn+ssh://svn.msr-inria.inria.fr/var/lib/svn/repository/tla/trunk/tla2-inria@12589 76a6fc44-f60b-0410-a9a8-e67b0e8fc65c

--- org.lamport.tla.toolbox/src/org/lamport/tla/toolbox/ui/handler/ParseHandler.java
+++ org.lamport.tla.toolbox/src/org/lamport/tla/toolbox/ui/handler/ParseHandler.java
@@ -6,24 +6,29 @@ import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.commands.IHandler;
+import org.eclipse.core.resources.IncrementalProjectBuilder;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.IWorkbenchWindow;
 import org.lamport.tla.toolbox.Activator;
 import org.lamport.tla.toolbox.spec.Spec;
-import org.lamport.tla.toolbox.spec.parser.IParserLauncher;
-import org.lamport.tla.toolbox.spec.parser.StreamInterpretingParserLauncher;
 import org.lamport.tla.toolbox.util.UIHelper;
 
 /**
  * Triggers parsing of specification's root file
  * 
- * @author zambrovski
+ * @author Simon Zambrovski
+ * @version $Id$
  */
 public class ParseHandler extends AbstractHandler implements IHandler
 {
+    //TODO provide the monitor
 
-    IParserLauncher launcher = new StreamInterpretingParserLauncher();
-
-    /*
-     * non-JavaDoc
+    private IProgressMonitor monitor = null;
+    
+    /**
      * @see IHandler#execute(ExecutionEvent event)
      */
     public Object execute(ExecutionEvent event) throws ExecutionException
@@ -32,22 +37,54 @@ public class ParseHandler extends AbstractHandler implements IHandler
         for (int i =0; i < references.size(); i++) 
         {
             // save resources
+            // TODO!!!!
             // ((EditorReference)references.get(i)).getEditor(false);
+            // saveDirtyEditors();
         }
         Spec spec = Activator.getSpecManager().getSpecLoaded();
 
         if (spec != null)
         {
-            int status = launcher.parseSpecification(spec);
+            try
+            {
+                
+                // this only cleans up
+                spec.getProject().build(IncrementalProjectBuilder.CLEAN_BUILD, monitor);
+                // this starts the build process
+                spec.getProject().build(IncrementalProjectBuilder.FULL_BUILD, monitor);
+            } catch (CoreException e)
+            {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
 
-            Activator.getParserRegistry().parseResultChanged(status);
-
+            
         } else
         {
             // TODO handle this case (could it occur?, declare in plugin.xml to activate the parse button on active
             // editor only)
         }
         return null;
+    }
+
+    // TODO refine this...
+    private void saveDirtyEditors()
+    {
+        Display display = Display.getCurrent();
+        if (display == null)
+            display = Display.getDefault();
+        display.syncExec(new Runnable() {
+            public void run()
+            {
+                IWorkbenchWindow[] windows = Activator.getDefault().getWorkbench().getWorkbenchWindows();
+                for (int i = 0; i < windows.length; i++)
+                {
+                    IWorkbenchPage[] pages = windows[i].getPages();
+                    for (int j = 0; j < pages.length; j++)
+                        pages[j].saveAllEditors(false);
+                }
+            }
+        });
     }
 
 }
