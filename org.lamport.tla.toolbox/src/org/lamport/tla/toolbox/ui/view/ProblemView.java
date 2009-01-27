d52e4e169215ec8da94c6ce853c8b7c81ae0c791
simonzam <simonzam@76a6fc44-f60b-0410-a9a8-e67b0e8fc65c>
2009-01-27 02:04:46 +0000

    changed representation of errors to styled text
    git-svn-id: svn+ssh://svn.msr-inria.inria.fr/var/lib/svn/repository/tla/trunk/tla2-inria@12606 76a6fc44-f60b-0410-a9a8-e67b0e8fc65c

--- org.lamport.tla.toolbox/src/org/lamport/tla/toolbox/ui/view/ProblemView.java
+++ org.lamport.tla.toolbox/src/org/lamport/tla/toolbox/ui/view/ProblemView.java
@@ -3,55 +3,48 @@ package org.lamport.tla.toolbox.ui.view;
 import java.util.Iterator;
 import java.util.List;
 
-import org.eclipse.jface.viewers.TableViewer;
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.custom.StyleRange;
+import org.eclipse.swt.custom.StyledText;
 import org.eclipse.swt.layout.RowLayout;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Event;
 import org.eclipse.swt.widgets.ExpandBar;
 import org.eclipse.swt.widgets.ExpandItem;
-import org.eclipse.swt.widgets.Label;
-import org.eclipse.ui.model.BaseWorkbenchContentProvider;
-import org.eclipse.ui.model.WorkbenchLabelProvider;
+import org.eclipse.swt.widgets.Listener;
 import org.eclipse.ui.part.ViewPart;
 import org.lamport.tla.toolbox.Activator;
 import org.lamport.tla.toolbox.spec.Spec;
 import org.lamport.tla.toolbox.spec.parser.problem.Problem;
 import org.lamport.tla.toolbox.spec.parser.problem.ProblemContainer;
+import org.lamport.tla.toolbox.util.AdapterFactory;
 
 /**
  * Shows parse problems
- * 
- * @author zambrovski
+ * @version $Id$
+ * @author Simon Zambrovski
  */
 public class ProblemView extends ViewPart
 {
-    public static final String ID  = "toolbox.view.ProblemView";
-    private ExpandBar          bar = null;
+    public static final String ID = "toolbox.view.ProblemView";
+    private ExpandBar bar = null;
 
     public ProblemView()
     {
     }
 
-    public void createPartControlTable(Composite parent)
-    {
-
-        TableViewer viewer = new TableViewer(parent, SWT.BORDER | SWT.MULTI | SWT.V_SCROLL);
-        getSite().setSelectionProvider(viewer);
-        viewer.getColumnProperties();
-        viewer.setLabelProvider(new WorkbenchLabelProvider());
-        viewer.setContentProvider(new BaseWorkbenchContentProvider());
-
-        viewer.setInput(getProblemContainer());
-
-    }
-
+    /**
+     * Creates the layout and fill it with data 
+     */
     public void createPartControl(Composite parent)
     {
-        parent.getShell().setText("Parse Problems");
+        parent.getShell().setText("TLA+ Parse Problems");
+
         bar = new ExpandBar(parent, SWT.V_SCROLL | SWT.BORDER);
         bar.setSpacing(8);
 
+
+        
         ProblemContainer container = getProblemContainer();
         if (container != null)
         {
@@ -60,54 +53,69 @@ public class ProblemView extends ViewPart
 
             for (; pIterator.hasNext();)
             {
-                Problem problem = (Problem) pIterator.next();
+                final Problem problem = (Problem) pIterator.next();
 
+                // listener
+                Listener listener = new Listener() {
+
+                    public void handleEvent(Event event)
+                    {
+                        System.out.println("Down on " + problem.location.moduleName);
+/*
+                        IEditorPart part = UIHelper.openEditor(OpenSpecHandler.TLA_EDITOR, new FileEditorInput((IFile) Activator.getSpecManager().getSpecLoaded().findModule(problem.location.moduleName)));
+                        if (part instanceof IGotoMarker) 
+                        {
+                            // ((IGotoMarker)part).gotoMarker(null);
+                        }
+*/                        
+                    }
+                };
+
+                // contents of the item
                 Composite problemItem = new Composite(bar, SWT.LINE_SOLID);
                 problemItem.setLayout(new RowLayout(SWT.VERTICAL));
+                problemItem.addListener(SWT.MouseDown, listener);
 
                 String[] lines = problem.message.split("\n");
                 for (int i = 0; i < lines.length; i++)
                 {
+                    StyledText styledText = new StyledText(problemItem, SWT.INHERIT_DEFAULT);
+                    styledText.setEditable(false);
+                    styledText.setCursor(styledText.getDisplay().getSystemCursor(SWT.CURSOR_HAND));
+                    styledText.setText(lines[i]);
+                    styledText.addListener(SWT.MouseDown, listener);
+
+                    if (isErrorLine(lines[i], problem))
+                    {
+                        StyleRange range = new StyleRange();
+                        range.underline = true;
+                        range.foreground = styledText.getDisplay().getSystemColor(SWT.COLOR_RED);
+                        range.start = 0;
+                        range.length = lines[i].length();
+                        styledText.setStyleRange(range);
+                    }
+
+                    /*
                     Label label = new Label(problemItem, SWT.INHERIT_DEFAULT);
                     label.setText(lines[i]);
+                    */
                 }
 
                 ExpandItem item = new ExpandItem(bar, SWT.NONE, 0);
                 item.setExpanded(true);
-                item.setText("");
+                item.setText(AdapterFactory.getProblemTypeAsText(problem));
                 item.setHeight(problemItem.computeSize(SWT.DEFAULT, SWT.DEFAULT).y);
                 item.setControl(problemItem);
-
+                item.addListener(SWT.MouseDown, listener);
             }
         } // else no errors to display
     }
 
-    public void createPartControlSWT(Composite parent)
+    private boolean isErrorLine(String line, Problem problem)
     {
-        Composite container;
-        container = new Composite(parent, SWT.BORDER | SWT.V_SCROLL);
-        container.setLayout(new GridLayout(1, true));
-        container.setBackground(parent.getDisplay().getSystemColor(SWT.COLOR_WHITE));
-
-        List problems = Activator.getSpecManager().getSpecLoaded().getParseProblems().getProblems(Problem.ALL);
-        Iterator pIterator = problems.iterator();
-
-        for (; pIterator.hasNext();)
-        {
-            Problem problem = (Problem) pIterator.next();
-
-            Composite problemItem = new Composite(container, SWT.LINE_SOLID);
-            problemItem.setLayout(new RowLayout(SWT.VERTICAL));
-
-            String[] lines = problem.message.split("\n");
-            for (int i = 0; i < lines.length; i++)
-            {
-                Label label = new Label(problemItem, SWT.INHERIT_DEFAULT);
-                label.setText(lines[i]);
-            }
-        }
-
+        return line.indexOf("module " + problem.location.moduleName) != -1;
     }
+
 
     /*
      * (non-Javadoc)
@@ -131,7 +139,11 @@ public class ProblemView extends ViewPart
             return null;
         } else
         {
+            
+            
             return spec.getParseProblems();
+            
+            
         }
 
     }
