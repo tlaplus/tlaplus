006e6814e061f81c6afc27774ed9f50a113c5fa1
simonzam <simonzam@76a6fc44-f60b-0410-a9a8-e67b0e8fc65c>
2009-01-28 05:37:47 +0000

    IProgressMonitor added
    git-svn-id: svn+ssh://svn.msr-inria.inria.fr/var/lib/svn/repository/tla/trunk/tla2-inria@12620 76a6fc44-f60b-0410-a9a8-e67b0e8fc65c

--- org.lamport.tla.toolbox/src/org/lamport/tla/toolbox/spec/parser/IParserLauncher.java
+++ org.lamport.tla.toolbox/src/org/lamport/tla/toolbox/spec/parser/IParserLauncher.java
@@ -1,17 +1,20 @@
 package org.lamport.tla.toolbox.spec.parser;
 
+import org.eclipse.core.runtime.IProgressMonitor;
 import org.lamport.tla.toolbox.spec.Spec;
 
 /**
  * Basic parser launcher interface
- * @author zambrovski
+ * @author Simon Zambrovski
+ * @version $Id$
  */
 public interface IParserLauncher
 {
     /**
      * Launches the spec parsing
      * @param spec specification handle
+     * @param monitor the monitor to report progress
      * @return the parse status (which should also be saved in the spec)
      */
-    public int parseSpecification(Spec spec);
+    public int parseSpecification(Spec spec, IProgressMonitor monitor);
 }
