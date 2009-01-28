bcdce4d8c8d0f7106d0ae8b823571cbb5fb59223
simonzam <simonzam@76a6fc44-f60b-0410-a9a8-e67b0e8fc65c>
2009-01-28 05:40:47 +0000

    constant change
    git-svn-id: svn+ssh://svn.msr-inria.inria.fr/var/lib/svn/repository/tla/trunk/tla2-inria@12630 76a6fc44-f60b-0410-a9a8-e67b0e8fc65c

--- org.lamport.tla.toolbox/src/org/lamport/tla/toolbox/util/pref/IPreferenceConstants.java
+++ org.lamport.tla.toolbox/src/org/lamport/tla/toolbox/util/pref/IPreferenceConstants.java
@@ -11,8 +11,9 @@ public interface IPreferenceConstants
     public static final String P_PROJECT_OPENED_MODULES = "ProjectOpenedModules";
     public static final String DEFAULT_NOT_SET                = "not set";
     
-    
-    public static final String P_PARSER_RUN_ON_MODIFICATION = "parserRunOnModify";
     public static final String P_PARSER_POPUP_ERRORS        = "parserPopupErrors";
+    
+    public static final String I_RESTORE_LAST_SPEC          = "restoreLastSpec";
+    public static final String I_SPEC_LOADED                = "specLoadedName";
 
 }
