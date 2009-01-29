553466df02d7d047a2b36e8791a0b1307bd2d379
simonzam <simonzam@76a6fc44-f60b-0410-a9a8-e67b0e8fc65c>
2009-01-29 05:05:47 +0000

    storage of opened editors removed
    git-svn-id: svn+ssh://svn.msr-inria.inria.fr/var/lib/svn/repository/tla/trunk/tla2-inria@12647 76a6fc44-f60b-0410-a9a8-e67b0e8fc65c

--- org.lamport.tla.toolbox/src/org/lamport/tla/toolbox/ui/handler/CloseSpecHandler.java
+++ org.lamport.tla.toolbox/src/org/lamport/tla/toolbox/ui/handler/CloseSpecHandler.java
@@ -5,7 +5,6 @@ import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.commands.IHandler;
 import org.lamport.tla.toolbox.Activator;
-import org.lamport.tla.toolbox.spec.Spec;
 import org.lamport.tla.toolbox.ui.perspective.InitialPerspective;
 import org.lamport.tla.toolbox.util.UIHelper;
 
@@ -19,13 +18,6 @@ public class CloseSpecHandler extends AbstractHandler implements IHandler
 
     public Object execute(ExecutionEvent event) throws ExecutionException
     {
-        Spec currentSpec =  Activator.getSpecManager().getSpecLoaded();
-        
-        String[] openedResources = UIHelper.getOpenedResources();
-        if (currentSpec != null) 
-        {
-            currentSpec.setOpenedModules(openedResources);
-        }
         UIHelper.getActivePage().closeAllEditors(true);
         UIHelper.switchPerspective(InitialPerspective.ID);
         Activator.getSpecManager().setSpecLoaded(null);
