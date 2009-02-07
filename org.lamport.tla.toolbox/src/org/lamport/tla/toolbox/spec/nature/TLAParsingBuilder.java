c9ed06f38c57df2f381f1f285307706cca2005cd
simonzam <simonzam@76a6fc44-f60b-0410-a9a8-e67b0e8fc65c>
2009-02-07 03:06:18 +0000

    rebuilt
    git-svn-id: svn+ssh://svn.msr-inria.inria.fr/var/lib/svn/repository/tla/trunk/tla2-inria@12693 76a6fc44-f60b-0410-a9a8-e67b0e8fc65c

--- org.lamport.tla.toolbox/src/org/lamport/tla/toolbox/spec/nature/TLAParsingBuilder.java
+++ org.lamport.tla.toolbox/src/org/lamport/tla/toolbox/spec/nature/TLAParsingBuilder.java
@@ -1,14 +1,16 @@
 package org.lamport.tla.toolbox.spec.nature;
 
+import java.util.List;
 import java.util.Map;
+import java.util.Vector;
 
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.IResourceDelta;
 import org.eclipse.core.resources.IResourceDeltaVisitor;
-import org.eclipse.core.resources.IResourceVisitor;
 import org.eclipse.core.resources.IWorkspaceRunnable;
 import org.eclipse.core.resources.IncrementalProjectBuilder;
+import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.lamport.tla.toolbox.Activator;
@@ -16,6 +18,7 @@ import org.lamport.tla.toolbox.spec.Spec;
 import org.lamport.tla.toolbox.spec.parser.ModuleParserLauncher;
 import org.lamport.tla.toolbox.spec.parser.SpecificationParserLauncher;
 import org.lamport.tla.toolbox.util.AdapterFactory;
+import org.lamport.tla.toolbox.util.ResourceHelper;
 import org.lamport.tla.toolbox.util.TLAMarkerHelper;
 
 /**
@@ -44,54 +47,44 @@ public class TLAParsingBuilder extends IncrementalProjectBuilder
 
     }
 
-    /**
-     * @see org.eclipse.core.resources.IncrementalProjectBuilder#build(int, java.util.Map, org.eclipse.core.runtime.IProgressMonitor)
-     */
     protected IProject[] build(int kind, Map args, IProgressMonitor monitor) throws CoreException
     {
-
-        /*
-        if (ResourcesPlugin.getWorkspace().isAutoBuilding())
+        if (getProject() != Activator.getSpecManager().getSpecLoaded().getProject())
         {
-            System.out.println("Running a Build (auto-builing is on): " + kind);
-        } else
-        {
-            System.out.println("Running a Build (auto-builing is off): " + kind);
+            // skip the build calls on wrong projects (which are in WS, but not a current spec)
+            return null;
         }
-        */
+        
+        System.out.println("----\n");
+        // TODO !!! look how the monitors should be treated !!!
+        // and then invoke monitor.begin
 
         if (IncrementalProjectBuilder.FULL_BUILD == kind)
         {
-            /* 
-             * a full build
-             */
-            /*
-            System.out.println("Full build started");
-            */
-            runFullBuild(monitor);
-
+            // explicit full build
+            // on workspace start
+            // or after clean
+            rebuildSpec(monitor);
         } else
         {
-
-            /*
-            if (IncrementalProjectBuilder.AUTO_BUILD == kind)
-            {
-                System.out.println("Auto build started");
-            } else if (IncrementalProjectBuilder.INCREMENTAL_BUILD == kind)
-            {
-                System.out.println("Incremental build started");
-            }
-            */
-
-            /*
-             *  incremental build
-             */
+            // an incremental build
+            // triggered manually or by resource modification 
             IResourceDelta delta = getDelta(getProject());
             if (delta == null)
             {
-                runFullBuild(monitor);
+                // no increment found, run a full build
+                rebuildSpec(monitor);
+                // runFullBuild(monitor);
             } else
             {
+                /*
+                 * The delta is a tree, with a root element pointing to the containing project,
+                 * and children indicating the files
+                 * 
+                 * Run a visitor on it in order to find out the changed files
+                 */
+                ChangedModulesGatheringDeltaVisitor moduleFinder = new ChangedModulesGatheringDeltaVisitor(monitor);
+                delta.accept(moduleFinder);
 
                 IResource rootFile = null;
                 Spec spec = Activator.getSpecManager().getSpecLoaded();
@@ -99,134 +92,140 @@ public class TLAParsingBuilder extends IncrementalProjectBuilder
                 {
                     rootFile = spec.getRootFile();
                 }
-                // if the changed file is a root file - run full build
-                if (rootFile != null && (rootFile.equals(delta.getResource())))
-                    //|| rootFile.getProject().equals(delta.getResource().getProject())
+
+                for (int i = 0; i < moduleFinder.modules.size(); i++)
                 {
-                    runFullBuild(monitor);
-                } else {
-                    // run incremental build
-                    runIncrementalBuild(delta, monitor);
+                    String changedModule = (String) moduleFinder.modules.get(i);
+
+                    // call build on the changed resource
+                    // if the file is a Root file it will call buildSpec
+                    // otherwise buildModule is invoked
+                    build(changedModule, rootFile, monitor);
+                    
+                    List modulesToRebuild = Activator.getModuleDependencyStorage().getListOfModules(changedModule);
+                    // iterate over modules and rebuild them
+                    for (int j = 0; j < modulesToRebuild.size(); j++)
+                    {
+                        // call build on dependent resources
+                        // if the file is a Root file it will call buildSpec
+                        // otherwise buildModule is invoked
+                        build((String) modulesToRebuild.get(j), rootFile, monitor);
+                    }
                 }
             }
         }
 
-        // since no deltas should be recalculated, return null.
-        // this could change in the future
+        System.out.println("----\n");
+        // since every project is one spec and we do not want to touch other specs always return NULL!
         return null;
     }
+    
+    private void build(String moduleFileName, IResource rootFile, IProgressMonitor monitor)
+    {
+        // if the changed file is a root file - run the spec build
+        if (rootFile != null && (rootFile.getName().equals(moduleFileName)))
+        {
+            // this will rebuild the spec starting from root, and change the spec status
+            // still, we want to continue and keep the dependency information about other files
+            rebuildSpec(monitor);
+        } else
+        {
+            // retrieve a resource
+            final IResource moduleFile = ResourceHelper.getLinkedFile(getProject(), moduleFileName, false);
+            if (moduleFile == null)
+            {
+                throw new IllegalStateException("Resource not found during build");
+            }
+            // run the module build
+            rebuildModule(moduleFile, monitor);
+        }
+    }
 
     /**
-     * Runs a the project builder 
+     * @param changedModule
      * @param monitor
      */
-    private void runFullBuild(IProgressMonitor monitor)
+    private void rebuildModule(final IResource resource, IProgressMonitor monitor)
     {
-        // TODO handle the monitor
+        System.out.println("Module build invoked on " + resource.getName() + " ...");
+        IWorkspaceRunnable run = new IWorkspaceRunnable() {
+
+            public void run(IProgressMonitor monitor) throws CoreException
+            {
+
+                TLAMarkerHelper.removeProblemMarkers(resource, monitor);
+                int status = moduleParser.parseModule(resource, monitor);
+            }
+        };
         try
         {
-            getProject().accept(new SpecificationRootFileParsingVisitor());
+            resource.getWorkspace().run(run, monitor);
         } catch (CoreException e)
         {
-            // TODO
+            // TODO Auto-generated catch block
             e.printStackTrace();
         }
+        System.out.println("... build invocation finished.");
+    }
+
+    private void rebuildSpec(IProgressMonitor monitor)
+    {
+        // TODO improve this...
+        // better: lookup in the project property for the spec name
+        // and search a spec by name
+
+        final Spec spec = Activator.getSpecManager().getSpecLoaded();
+        System.out.println("Spec build invoked on " + spec.getName() + " ...");
+        IWorkspaceRunnable run = new IWorkspaceRunnable() {
+
+            public void run(IProgressMonitor monitor) throws CoreException
+            {
+
+                TLAMarkerHelper.removeProblemMarkers(spec.getProject(), monitor);
+                launcher.parseSpecification(spec, monitor);
+                System.out.println("Resulting status is: " + AdapterFactory.getStatusAsString(spec));
+            }
+        };
+        try
+        {
+            ResourcesPlugin.getWorkspace().run(run, monitor);
+        } catch (CoreException e)
+        {
+            e.printStackTrace();
+        }
+        System.out.println("... build invocation finished.");
     }
 
     /**
-     * Runs incremental builder
-     * @param delta - a resource delta
-     * @param monitor 
-     */
-    private void runIncrementalBuild(IResourceDelta delta, IProgressMonitor monitor)
-    {
-        // TODO handle the monitor
-        try
-        {
-            delta.accept(new ModuleParsingVisitor());
-        } catch (CoreException e)
-        {
-            // TODO
-            e.printStackTrace();
-        }
-    }
-
-    /**
-     * Incremental builder that parses specified TLA+ module
+     * Visitor to find out what files changed
      * @author Simon Zambrovski
      * @version $Id$
      */
-    public class ModuleParsingVisitor implements IResourceDeltaVisitor
+    public class ChangedModulesGatheringDeltaVisitor implements IResourceDeltaVisitor
     {
-        IProgressMonitor monitor = null; // TODO init the monitor
+        private final IProgressMonitor monitor;
+        Vector modules = new Vector();
 
+        public ChangedModulesGatheringDeltaVisitor(IProgressMonitor monitor)
+        {
+            this.monitor = monitor;
+        }
         /**
          * @see org.eclipse.core.resources.IResourceDeltaVisitor#visit(org.eclipse.core.resources.IResourceDelta)
          */
         public boolean visit(IResourceDelta delta) throws CoreException
         {
             final IResource resource = delta.getResource();
-            System.out.println("Incremental build invoked on " + resource.getName() + " ...");
-            IWorkspaceRunnable run = new IWorkspaceRunnable() {
-
-                public void run(IProgressMonitor monitor) throws CoreException
-                {
-
-                    TLAMarkerHelper.removeProblemMarkers(resource, monitor);
-                    int status = moduleParser.parseModule(resource, monitor);
-                    // System.out.println("Resulting status is: " + AdapterFactory.getStatusAsString(spec));
-                }
-            };
-            delta.getResource().getWorkspace().run(run, monitor);
-            System.out.println("... build invocation finished.");
-            return true;
-        }
-
-    }
-
-    /**
-     * Visits the project and parses the associated specification root module
-     * @author Simon Zambrovski
-     * @version $Id$
-     */
-    public class SpecificationRootFileParsingVisitor implements IResourceVisitor
-    {
-        IProgressMonitor monitor = null;
-
-        /**
-         * @see org.eclipse.core.resources.IResourceVisitor#visit(org.eclipse.core.resources.IResource)
-         */
-        public boolean visit(IResource resource) throws CoreException
-        {
-            if (resource instanceof IProject && ((IProject) resource).hasNature(TLANature.ID))
+            if (IResource.FILE == resource.getType())
             {
-                System.out.println("Full build invoked on " + resource.getName() + " ...");
-                // TODO improve this...
-                // better: lookup in the project property for the spec name
-                // and search a spec by name
-
-                final Spec spec = Activator.getSpecManager().getSpecLoaded();
-                if (spec != null && spec.getProject().equals(((IProject) resource)))
+                // a file found
+                if (ResourceHelper.isModule(resource))
                 {
-                    IWorkspaceRunnable run = new IWorkspaceRunnable() {
-
-                        public void run(IProgressMonitor monitor) throws CoreException
-                        {
-
-                            TLAMarkerHelper.removeProblemMarkers(spec.getProject(), monitor);
-                            launcher.parseSpecification(spec, monitor);
-                            System.out.println("Resulting status is: " + AdapterFactory.getStatusAsString(spec));
-                        }
-                    };
-                    resource.getWorkspace().run(run, monitor);
-                    System.out.println("... build invocation finished.");
-                } else {
-                    System.out.println("... build skipped.");
+                    modules.add(resource.getName());
                 }
             }
-
-            // invoke only on the IProject, never on the single module files
-            return false;
+            // we want the visitor to visit the whole tree
+            return true;
         }
     }
 
