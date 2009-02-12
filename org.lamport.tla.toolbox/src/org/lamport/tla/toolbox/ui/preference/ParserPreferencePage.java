51cb4ea296780d73a4b113079198f07ce5644755
simonzam <simonzam@76a6fc44-f60b-0410-a9a8-e67b0e8fc65c>
2009-02-12 01:04:52 +0000

    pref add
    git-svn-id: svn+ssh://svn.msr-inria.inria.fr/var/lib/svn/repository/tla/trunk/tla2-inria@12729 76a6fc44-f60b-0410-a9a8-e67b0e8fc65c

--- org.lamport.tla.toolbox/src/org/lamport/tla/toolbox/ui/preference/ParserPreferencePage.java
+++ org.lamport.tla.toolbox/src/org/lamport/tla/toolbox/ui/preference/ParserPreferencePage.java
@@ -1,5 +1,8 @@
 package org.lamport.tla.toolbox.ui.preference;
 
+import org.eclipse.core.resources.IWorkspaceDescription;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.CoreException;
 import org.eclipse.jface.preference.BooleanFieldEditor;
 import org.eclipse.jface.preference.FieldEditorPreferencePage;
 import org.eclipse.swt.widgets.Composite;
@@ -17,7 +20,6 @@ import org.lamport.tla.toolbox.util.pref.PreferenceStoreHelper;
  */
 public class ParserPreferencePage extends FieldEditorPreferencePage implements IWorkbenchPreferencePage
 {
-    
     /**
      * Constructor
      */
@@ -26,8 +28,6 @@ public class ParserPreferencePage extends FieldEditorPreferencePage implements I
         super(GRID);
         setPreferenceStore(PreferenceStoreHelper.getInstancePreferenceStore());
         setDescription("TLA+ Parser preferences");
-
-
     }
 
     protected Control createContents(Composite parent)
@@ -42,24 +42,66 @@ public class ParserPreferencePage extends FieldEditorPreferencePage implements I
      */
     protected void createFieldEditors()
     {
-        addField(new BooleanFieldEditor(IPreferenceConstants.I_RESTORE_LAST_SPEC, "&Remember last loaded specification beyond restart",
-                getFieldEditorParent()));
+        addField(new BooleanFieldEditor(IPreferenceConstants.I_RESTORE_LAST_SPEC,
+                "&Remember last loaded specification beyond restart", getFieldEditorParent()));
 
-        addField(new BooleanFieldEditor(IPreferenceConstants.P_PARSER_POPUP_ERRORS, "&Popup problem window on parse errors",
-                getFieldEditorParent()));
-    }
+        addField(new BooleanFieldEditor(IPreferenceConstants.P_PARSER_POPUP_ERRORS,
+                "&Popup problem window on parse errors", getFieldEditorParent()));
 
-
-    protected void initialize()
-    {
-        super.initialize();
+        addField(new BooleanFieldEditor(IPreferenceConstants.I_PARSE_MODULE_ON_MODIFY,
+                "&Automatic re-parse module", getFieldEditorParent()));
         
-        
+        addField(new BooleanFieldEditor(IPreferenceConstants.I_PARSE_SPEC_ON_MODIFY,
+                "&Automatic re-parse specification", getFieldEditorParent()));
     }
 
     public void init(IWorkbench workbench)
     {
 
+    }
+    
+    
+    protected void initialize()
+    {
+        // sync the auto-build
+        getPreferenceStore().setValue(IPreferenceConstants.I_PARSE_MODULE_ON_MODIFY, ResourcesPlugin.getWorkspace().isAutoBuilding());
+        super.initialize();
+    }
+
+    
+    
+    protected void performApply()
+    {
+        super.performApply();
+        this.setAutoBuilding();
+    }
+
+    public boolean performOk()
+    {
+        boolean result = super.performOk();
+        this.setAutoBuilding();
+        return result;
+    }
+
+    /**
+     * Sets the value of the workspace auto-build to the module auto-build 
+     */
+    private void setAutoBuilding()
+    {
+        boolean autoBuildModule = getPreferenceStore().getBoolean(IPreferenceConstants.I_PARSE_MODULE_ON_MODIFY);
+        
+        // set the workspace auto-build flag
+        IWorkspaceDescription description = ResourcesPlugin.getWorkspace()
+                .getDescription();
+        if (autoBuildModule != ResourcesPlugin.getWorkspace().isAutoBuilding()) {
+            try {
+                description.setAutoBuilding(autoBuildModule);
+                ResourcesPlugin.getWorkspace().setDescription(description);
+            } catch (CoreException e) {
+                // TODO
+                e.printStackTrace();
+            }
+        }
     }
 
 }
