3a8b688148197131d65cb6bd3af90a1c17d1e024
simonzam <simonzam@76a6fc44-f60b-0410-a9a8-e67b0e8fc65c>
2009-02-12 01:01:01 +0000

    added
    git-svn-id: svn+ssh://svn.msr-inria.inria.fr/var/lib/svn/repository/tla/trunk/tla2-inria@12718 76a6fc44-f60b-0410-a9a8-e67b0e8fc65c

--- org.lamport.tla.toolbox/src/org/lamport/tla/toolbox/util/pref/IPreferenceConstants.java
+++ org.lamport.tla.toolbox/src/org/lamport/tla/toolbox/util/pref/IPreferenceConstants.java
@@ -13,6 +13,8 @@ public interface IPreferenceConstants
     
     public static final String P_PARSER_POPUP_ERRORS        = "parserPopupErrors";
     
+    public static final String I_PARSE_MODULE_ON_MODIFY     = "autoParseModule";
+    public static final String I_PARSE_SPEC_ON_MODIFY       = "autoParseSpec";
     public static final String I_RESTORE_LAST_SPEC          = "restoreLastSpec";
     public static final String I_SPEC_LOADED                = "specLoadedName";
 
--- org.lamport.tla.toolbox/src/org/lamport/tla/toolbox/util/pref/PreferenceInitializer.java
+++ org.lamport.tla.toolbox/src/org/lamport/tla/toolbox/util/pref/PreferenceInitializer.java
@@ -6,18 +6,24 @@ import org.eclipse.jface.preference.IPreferenceStore;
 /**
  * Class used to initialize default preference values.
  */
-public class PreferenceInitializer extends AbstractPreferenceInitializer {
+public class PreferenceInitializer extends AbstractPreferenceInitializer
+{
 
-	/**
-	 * 
-	 * 
-	 * @see org.eclipse.core.runtime.preferences.AbstractPreferenceInitializer#initializeDefaultPreferences()
-	 */
-	public void initializeDefaultPreferences() {
-		
-	    IPreferenceStore store = PreferenceStoreHelper.getInstancePreferenceStore();
-		store.setDefault(IPreferenceConstants.P_PARSER_POPUP_ERRORS, false);
-		store.setDefault(IPreferenceConstants.I_RESTORE_LAST_SPEC, true);
-	}
+    /**
+     * 
+     * 
+     * @see org.eclipse.core.runtime.preferences.AbstractPreferenceInitializer#initializeDefaultPreferences()
+     */
+    public void initializeDefaultPreferences()
+    {
+
+        IPreferenceStore store = PreferenceStoreHelper.getInstancePreferenceStore();
+        store.setDefault(IPreferenceConstants.P_PARSER_POPUP_ERRORS, false);
+
+        // instance based properties
+        store.setDefault(IPreferenceConstants.I_RESTORE_LAST_SPEC, true);
+        store.setDefault(IPreferenceConstants.I_PARSE_MODULE_ON_MODIFY, true);
+        store.setDefault(IPreferenceConstants.I_PARSE_SPEC_ON_MODIFY, false);
+    }
 
 }
