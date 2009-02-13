28ec1dea0a7d6200c709db92a350c604aa6c99f2
simonzam <simonzam@76a6fc44-f60b-0410-a9a8-e67b0e8fc65c>
2009-02-13 22:56:20 +0000

    formatted
    git-svn-id: svn+ssh://svn.msr-inria.inria.fr/var/lib/svn/repository/tla/trunk/tla2-inria@12764 76a6fc44-f60b-0410-a9a8-e67b0e8fc65c

--- tlatools/src/pcal/PcalDebug.java
+++ tlatools/src/pcal/PcalDebug.java
@@ -1,186 +1,258 @@
-/***************************************************************************
-* CLASS PcalDebug                                                          *
-*                                                                          *
-* This class provides methods called when an error is encountered,         *
-* methods for printing debugging output, and methods for measuring and     *
-* reporting elapsed time.                                                  *
-***************************************************************************/
 package pcal;
-import java.util.Vector;
 
+import java.util.Vector;
 import util.ToolIO;
 
+/***************************************************************************
+* This class provides methods called when an error is encountered,         
+* methods for printing debugging output, and methods for measuring and     
+* reporting elapsed time.                                                  
+* @author Leslie Lamport
+* @version Id
+***************************************************************************/
 public class PcalDebug
-  { public static void ReportError(String msg)
-      /*********************************************************************
-      * This method is called to report an error and abort.                *
-      *********************************************************************/
-      { ToolIO.out.println("");
+{
+    public static void ReportError(String msg)
+    /*********************************************************************
+    * This method is called to report an error and abort.                *
+    *********************************************************************/
+    {
+        ToolIO.out.println("");
         ToolIO.out.println("Unrecoverable error:");
         ToolIO.out.println("");
         ToolIO.out.println(" -- " + msg + ".");
         ToolIO.out.println("");
         throw new PCalUnrecoverableErrorRuntimeException("Report error has been called");
-      };
+    };
 
-public static void ReportErrorAt(String msg, AST ast)
-      /*********************************************************************
-      * This method is called to report an error in the object ast and     *
-      * abort.                                                             *
-      *                                                                    *
-      * Corrected 4 Mar 2006 by LL to use AST.location.                    *
-      *********************************************************************/
-      { if (ast.line == 0) ReportError(msg) ;
-        ReportError(msg +
-                    "\n    at " + ast.location()) ;
-      };
+    public static void ReportErrorAt(String msg, AST ast)
+    /*********************************************************************
+    * This method is called to report an error in the object ast and     *
+    * abort.                                                             *
+    *                                                                    *
+    * Corrected 4 Mar 2006 by LL to use AST.location.                    *
+    *********************************************************************/
+    {
+        if (ast.line == 0)
+            ReportError(msg);
+        ReportError(msg + "\n    at " + ast.location());
+    };
 
-    public static void Assert(boolean val) 
-      /*********************************************************************
-      * Why doesn't java provide an Assert statement?                      *
-      *********************************************************************/
-      { if (! val) {ReportBug("Assertion failure");};
-      };
+    public static void Assert(boolean val)
+    /*********************************************************************
+    * Why doesn't java provide an Assert statement?                      *
+    *********************************************************************/
+    {
+        if (!val)
+        {
+            ReportBug("Assertion failure");
+        }
+        ;
+    };
 
-    public static void Assert(boolean val, String msg) 
-      /*********************************************************************
-      * An Assert with an error message.                                   *
-      *********************************************************************/
-      { if (! val) {ReportBug("Failure of assertion: " + msg);};
-      };
+    public static void Assert(boolean val, String msg)
+    /*********************************************************************
+    * An Assert with an error message.                                   *
+    *********************************************************************/
+    {
+        if (!val)
+        {
+            ReportBug("Failure of assertion: " + msg);
+        }
+        ;
+    };
 
     public static void ReportBug(String msg)
-      /*********************************************************************
-      * This method is called to report a bug in the program and abort.    *
-      *********************************************************************/
-      { ToolIO.out.println("");
+    /*********************************************************************
+    * This method is called to report a bug in the program and abort.    *
+    *********************************************************************/
+    {
+        ToolIO.out.println("");
         ToolIO.out.println("You have discovered a bug in pcal.trans.");
         ToolIO.out.println("Send the following information and the");
         ToolIO.out.println("input file to the current maintainer(s).");
         ToolIO.out.println("");
         ToolIO.out.println(" -- " + msg + ".");
         ToolIO.out.println("");
-        throw new Error() ;
-      };
-
+        throw new Error();
+    };
 
     public static void printObjectArray(Object[] array, String name)
-      /*********************************************************************
-      * This method prints to standard output the contents of the array    *
-      * argument, where name is the name of the array.                     *
-      *********************************************************************/
-      { if (array == null)
-          {ToolIO.out.println(name + " == null");
-           return ;} ;
-        int i = 0 ;
+    /*********************************************************************
+    * This method prints to standard output the contents of the array    *
+    * argument, where name is the name of the array.                     *
+    *********************************************************************/
+    {
+        if (array == null)
+        {
+            ToolIO.out.println(name + " == null");
+            return;
+        }
+        ;
+        int i = 0;
         while (i < array.length)
-          { if (array[i] == null)
-              { ToolIO.out.println(name + "[" + i + "] = null") ;}
-            else
-              { ToolIO.out.println(name + "[" + i + "] = " 
-                                        + array[i].toString()) ; }
-            i = i+1;
-          } ;
+        {
+            if (array[i] == null)
+            {
+                ToolIO.out.println(name + "[" + i + "] = null");
+            } else
+            {
+                ToolIO.out.println(name + "[" + i + "] = " + array[i].toString());
+            }
+            i = i + 1;
+        }
+        ;
         if (array.length == 0)
-          {ToolIO.out.println(name + " = zero-length array" ); } ;
-      } ;
+        {
+            ToolIO.out.println(name + " = zero-length array");
+        }
+        ;
+    };
 
     public static void printIntArray(int[] array, String name)
-      /*********************************************************************
-      * This method prints to standard output the contents of the array    *
-      * argument, where name is the name of the array.                     *
-      *********************************************************************/
-      { if (array == null)
-          {ToolIO.out.println(name + " == null");
-           return ;} ;
-        int i = 0 ;
+    /*********************************************************************
+    * This method prints to standard output the contents of the array    *
+    * argument, where name is the name of the array.                     *
+    *********************************************************************/
+    {
+        if (array == null)
+        {
+            ToolIO.out.println(name + " == null");
+            return;
+        }
+        ;
+        int i = 0;
         while (i < array.length)
-          { ToolIO.out.println(name + "[" + i + "] = " + array[i]); 
-            i = i+1;
-          } ;
+        {
+            ToolIO.out.println(name + "[" + i + "] = " + array[i]);
+            i = i + 1;
+        }
+        ;
         if (array.length == 0)
-          {ToolIO.out.println(name + " = zero-length array" ); } ;
-      } ;
+        {
+            ToolIO.out.println(name + " = zero-length array");
+        }
+        ;
+    };
 
     public static void print2DArray(Object[][] array, String name)
-      /*********************************************************************
-      * This method prints to standard output the contents of the array    *
-      * argument, where name is the name of the array.                     *
-      *********************************************************************/
-      { if (array == null)
-          {ToolIO.out.println(name + " == null");
-           return ;} ;
-        int i = 0 ;
+    /*********************************************************************
+    * This method prints to standard output the contents of the array    *
+    * argument, where name is the name of the array.                     *
+    *********************************************************************/
+    {
+        if (array == null)
+        {
+            ToolIO.out.println(name + " == null");
+            return;
+        }
+        ;
+        int i = 0;
         while (i < array.length)
-          { int j = 0 ;
+        {
+            int j = 0;
             while (j < array[i].length)
-             { ToolIO.out.println(name + "[" + i + "][" + j + "] = " 
-                                     + array[i][j].toString()) ;
-               j = j+1;
-             };
+            {
+                ToolIO.out.println(name + "[" + i + "][" + j + "] = " + array[i][j].toString());
+                j = j + 1;
+            }
+            ;
             if (array[i].length == 0)
-              {ToolIO.out.println(name + "[" + i + "] = null" ); } ;
-            i = i+1;
-          } ;
+            {
+                ToolIO.out.println(name + "[" + i + "] = null");
+            }
+            ;
+            i = i + 1;
+        }
+        ;
         if (array.length == 0)
-          {ToolIO.out.println(name + " = zero-length array" ); } ;
-      } ;
-    
+        {
+            ToolIO.out.println(name + " = zero-length array");
+        }
+        ;
+    };
+
     public static void printVector(Vector vec, String name)
-      /*********************************************************************
-      * This method prints to standard output the contents of the vector   *
-      * argument, where name is the name of the vector.                    *
-      *********************************************************************/
-      { if (vec == null)
-          {ToolIO.out.println(name + " == null");
-           return ;} ;
-        int i = 0 ;
+    /*********************************************************************
+    * This method prints to standard output the contents of the vector   *
+    * argument, where name is the name of the vector.                    *
+    *********************************************************************/
+    {
+        if (vec == null)
+        {
+            ToolIO.out.println(name + " == null");
+            return;
+        }
+        ;
+        int i = 0;
         while (i < vec.size())
-          { if (vec.elementAt(i) == null)
-              { ToolIO.out.println(name + "[" + i + "] = null") ;}
-            else
-              { ToolIO.out.println(name + "[" + i + "] = " 
-                                        + vec.elementAt(i).toString()) ; }
-            i = i+1;
-          } ;
+        {
+            if (vec.elementAt(i) == null)
+            {
+                ToolIO.out.println(name + "[" + i + "] = null");
+            } else
+            {
+                ToolIO.out.println(name + "[" + i + "] = " + vec.elementAt(i).toString());
+            }
+            i = i + 1;
+        }
+        ;
         if (vec.size() == 0)
-          {ToolIO.out.println(name + " = zero-length vec" ); } ;
-      } ;
+        {
+            ToolIO.out.println(name + " = zero-length vec");
+        }
+        ;
+    };
 
     public static void print2DVector(Vector vec, String name)
-      /*********************************************************************
-      * This method prints to standard output the contents of its vector   *
-      * argument, which is a vector of vectors, where name is the name of  *
-      * the vector.                                                        *
-      *********************************************************************/
-      { if (vec == null)
-          {ToolIO.out.println(name + " == null");
-           return ;} ;
-        int i = 0 ;
+    /*********************************************************************
+    * This method prints to standard output the contents of its vector   *
+    * argument, which is a vector of vectors, where name is the name of  *
+    * the vector.                                                        *
+    *********************************************************************/
+    {
+        if (vec == null)
+        {
+            ToolIO.out.println(name + " == null");
+            return;
+        }
+        ;
+        int i = 0;
         while (i < vec.size())
-          { if (vec.elementAt(i) == null)
-              { ToolIO.out.println(name + "[" + i + "] = null") ;}
-            else
-              { printVector((Vector) vec.elementAt(i), 
-                                     name + "[" + i + "]") ;
-              };
-            i = i+1;
-          } ;
+        {
+            if (vec.elementAt(i) == null)
+            {
+                ToolIO.out.println(name + "[" + i + "] = null");
+            } else
+            {
+                printVector((Vector) vec.elementAt(i), name + "[" + i + "]");
+            }
+            ;
+            i = i + 1;
+        }
+        ;
         if (vec.size() == 0)
-          {ToolIO.out.println(name + " = zero-length vec" ); } ;
-      } ;
+        {
+            ToolIO.out.println(name + " = zero-length vec");
+        }
+        ;
+    };
 
     public static String pair(int i, int j)
-      /*********************************************************************
-      * Just a convenient little function.                                 *
-      *********************************************************************/
-      { return "(" + i + ", " + j + ")"; };
+    /*********************************************************************
+    * Just a convenient little function.                                 *
+    *********************************************************************/
+    {
+        return "(" + i + ", " + j + ")";
+    };
 
     public static void printPair(int i, int j)
-      /*********************************************************************
-      * Just prints "(i, j)".                                              *
-      *********************************************************************/
-      { ToolIO.out.println(pair(i,j)); };
+    /*********************************************************************
+    * Just prints "(i, j)".                                              *
+    *********************************************************************/
+    {
+        ToolIO.out.println(pair(i, j));
+    };
 
-  }
+}
 /* last modified on Sat  4 Mar 2006 at 10:15:03 PST by lamport */
