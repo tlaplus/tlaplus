41e4ff73f2d6b6f96a1460c43a0b73d623432067
simonzam <simonzam@76a6fc44-f60b-0410-a9a8-e67b0e8fc65c>
2009-02-13 19:58:27 +0000

    init
    git-svn-id: svn+ssh://svn.msr-inria.inria.fr/var/lib/svn/repository/tla/trunk/tla2-inria@12755 76a6fc44-f60b-0410-a9a8-e67b0e8fc65c

---
+++ tlatools/src/pcal/TranslatorLauncher.java
@@ -0,0 +1,21 @@
+package pcal;
+
+/**
+ * Launcher for the PCal Translator
+ * @author Simon Zambrovski
+ * @version $Id$
+ */
+public class TranslatorLauncher
+{
+    /**
+     * delegates the call to the {@link trans#main()}
+     * @param args
+     */
+    public static int main(String[] args)
+    {
+        
+        
+        
+        return trans.main(args);
+    }
+}
