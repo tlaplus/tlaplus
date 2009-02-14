3d11412ae1eee1e85df895f07ce0a6a5b8a628fe
simonzam <simonzam@76a6fc44-f60b-0410-a9a8-e67b0e8fc65c>
2009-02-14 07:11:57 +0000

    simple error handling added
    git-svn-id: svn+ssh://svn.msr-inria.inria.fr/var/lib/svn/repository/tla/trunk/tla2-inria@12782 76a6fc44-f60b-0410-a9a8-e67b0e8fc65c

--- org.lamport.tla.toolbox/src/org/lamport/tla/toolbox/ui/handler/TranslateModuleHandler.java
+++ org.lamport.tla.toolbox/src/org/lamport/tla/toolbox/ui/handler/TranslateModuleHandler.java
@@ -1,17 +1,22 @@
 package org.lamport.tla.toolbox.ui.handler;
 
+import java.util.List;
+
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.commands.IHandler;
+import org.eclipse.core.resources.IMarker;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.ui.IEditorInput;
 import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.IFileEditorInput;
+import org.lamport.tla.toolbox.util.TLAMarkerHelper;
 import org.lamport.tla.toolbox.util.UIHelper;
 
 import pcal.TranslatorLauncher;
+
 /**
  * Triggers the PCal translation of the module
  * @author Simon Zambrovski
@@ -19,40 +24,62 @@ import pcal.TranslatorLauncher;
  */
 public class TranslateModuleHandler extends AbstractHandler implements IHandler
 {
+    TranslatorLauncher launcher = new TranslatorLauncher();
 
     public Object execute(ExecutionEvent event) throws ExecutionException
     {
         IEditorPart activeEditor = UIHelper.getActivePage().getActiveEditor();
-        
-        if (activeEditor.isDirty()) 
+
+        if (activeEditor.isDirty())
         {
             // editor is not saved
             // TODO react on this!
         }
-        
+
         IEditorInput editorInput = activeEditor.getEditorInput();
         if (editorInput instanceof IFileEditorInput)
         {
-            IResource fileToBuild = ((IFileEditorInput)editorInput).getFile();
+            IResource fileToBuild = ((IFileEditorInput) editorInput).getFile();
             System.out.println(fileToBuild.getLocation().toOSString());
-            
-            TranslatorLauncher.runPcalTranslation(new String[]{fileToBuild.getLocation().toOSString()});
-            
-            
-            try
-            {
-                fileToBuild.refreshLocal(IResource.DEPTH_ONE, null);
-            } catch (CoreException e)
-            {
-                // TODO
-                e.printStackTrace();
-            }
-            
-        } 
 
-        
-        
+            launcher.runTranslation(new String[] { fileToBuild.getLocation().toOSString() });
+
+            List errors = launcher.getErrorMessages();
+
+            for (int i = 0; i < errors.size(); i++)
+            {
+                String errorMessage = (String) errors.get(i);
+
+                TLAMarkerHelper.installProblemMarker(fileToBuild, fileToBuild.getName(), IMarker.SEVERITY_ERROR,
+                        detectLocation(errorMessage), errorMessage, null);
+            }
+
+            // only refresh on no errors
+            if (errors.size() == 0)
+            {
+                try
+                {
+                    fileToBuild.refreshLocal(IResource.DEPTH_ONE, null);
+                } catch (CoreException e)
+                {
+                    e.printStackTrace();
+                }
+            }
+        }
         return null;
+    }
+
+    private int[] detectLocation(String message)
+    {
+        int lineStarts = message.indexOf("line ");
+        int lineEnds = message.indexOf(" , column ");
+        if (lineStarts != -1 && lineEnds != -1)
+        {
+            String line = message.substring(lineStarts, lineEnds);
+            int lineNumber = Integer.parseInt(line);
+            return new int[] { lineNumber, -1, Integer.parseInt(line), -1 };
+        }
+        return new int[] { -1, -1, -1, -1 };
     }
 
 }
