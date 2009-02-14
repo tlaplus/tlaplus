fc574c224c4a881b13e4662497dd19e1e3e7ab85
simonzam <simonzam@76a6fc44-f60b-0410-a9a8-e67b0e8fc65c>
2009-02-14 07:14:27 +0000

    simple error detecting mechanism
    git-svn-id: svn+ssh://svn.msr-inria.inria.fr/var/lib/svn/repository/tla/trunk/tla2-inria@12786 76a6fc44-f60b-0410-a9a8-e67b0e8fc65c

--- tlatools/src/pcal/TranslatorLauncher.java
+++ tlatools/src/pcal/TranslatorLauncher.java
@@ -1,5 +1,8 @@
 package pcal;
 
+import java.util.List;
+import java.util.Vector;
+
 import util.ToolIO;
 
 /**
@@ -10,30 +13,49 @@ import util.ToolIO;
 public class TranslatorLauncher
 {
     /**
-     * delegates the call to the {@link trans#main()}
-     * @param args
+     * Resets the Tool IO
      */
-    public static int runPcalTranslation(String[] args)
+    public void init()
     {
         ToolIO.reset();
         ToolIO.setMode(ToolIO.TOOL);
-        
-        int status = -1;
-        
-        try {
-            status = trans.runMe(args);
-            
-        } catch (PCalUnrecoverableErrorRuntimeException e) 
-        {
-            e.printStackTrace();
-        }
+    }
 
+    /**
+     * delegates the call to the {@link trans#main()}
+     * @param args
+     */
+    public int runTranslation(String[] args)
+    {
+        init();
+        int status = trans.runMe(args);
+        return status;
+    }
+
+    /**
+     * Retrieves the errors recorded during the execution
+     * @return
+     */
+    public List getErrorMessages()
+    {
         String[] messages = ToolIO.getAllMessages();
+        Vector errorMessages = new Vector();
+        System.out.println("Found " + messages.length + " messages");
+        int position;
+        String cleanMessage = null;
         for (int i = 0; i < messages.length; i++)
         {
-            System.out.println(messages[i]);
+            position = messages[i].indexOf(PcalDebug.UNRECOVERABLE_ERROR);
+            if (position != -1)
+            {
+                cleanMessage = messages[i].substring(position, messages[i].length() - PcalDebug.ERROR_POSTFIX.length());
+                errorMessages.add(cleanMessage);
+            } else
+            {
+                // unknown error format
+                System.out.println(cleanMessage);
+            }
         }
-        
-        return status;
+        return errorMessages;
     }
 }
