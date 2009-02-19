1bd0cb94501c1ac6d9b6c3f966476de2bb986784
simonzam <simonzam@76a6fc44-f60b-0410-a9a8-e67b0e8fc65c>
2009-02-19 01:36:49 +0000

    comments
    git-svn-id: svn+ssh://svn.msr-inria.inria.fr/var/lib/svn/repository/tla/trunk/tla2-inria@12832 76a6fc44-f60b-0410-a9a8-e67b0e8fc65c

--- org.lamport.tla.toolbox/src/org/lamport/tla/toolbox/util/pref/IPreferenceConstants.java
+++ org.lamport.tla.toolbox/src/org/lamport/tla/toolbox/util/pref/IPreferenceConstants.java
@@ -13,10 +13,16 @@ public interface IPreferenceConstants
     
     public static final String P_PARSER_POPUP_ERRORS        = "parserPopupErrors";
     
+    /** 
+     * restore the same state of the specification after a restart
+     *  
+     */
+    public static final String I_RESTORE_LAST_SPEC          = "restoreLastSpec";
+    
     public static final String I_PARSE_FILES_ON_MODIFY      = "autoParseTopModules";
     public static final String I_PARSE_MODULE_ON_MODIFY     = "autoParseModule";
     public static final String I_PARSE_SPEC_ON_MODIFY       = "autoParseSpec";
-    public static final String I_RESTORE_LAST_SPEC          = "restoreLastSpec";
+    
     public static final String I_SPEC_LOADED                = "specLoadedName";
 
     /** Resource persistent property for sticking info about the pcal algorithm */
