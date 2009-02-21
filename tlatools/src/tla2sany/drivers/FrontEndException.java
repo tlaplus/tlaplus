e62f13890ec048dda6cd5a8f4740c0ed9f3ce25c
simonzam <simonzam@76a6fc44-f60b-0410-a9a8-e67b0e8fc65c>
2009-02-21 01:16:46 +0000

    exception chaining added and visibility cvhanged
    git-svn-id: svn+ssh://svn.msr-inria.inria.fr/var/lib/svn/repository/tla/trunk/tla2-inria@12885 76a6fc44-f60b-0410-a9a8-e67b0e8fc65c

--- tlatools/src/tla2sany/drivers/FrontEndException.java
+++ tlatools/src/tla2sany/drivers/FrontEndException.java
@@ -1,11 +1,19 @@
 // Copyright (c) 2003 Compaq Corporation.  All rights reserved.
 package tla2sany.drivers;
 
+/**
+ * Exception thrown by SANY on errors
+ * @author Leslie Lamport
+ * @version $Id$
+ */
+public class FrontEndException extends Exception {
 
-class FrontEndException extends Exception {
-
-  Exception ex;
-
-  public FrontEndException(Exception e) { ex = e; }
-
+    // SZ Feb 20, 2009: fixed the exception chaining
+    // instead of having the cause as a member, the 
+    // exception now has it in trace
+    // also changed the visibility to public
+    public FrontEndException(Exception cause) 
+    { 
+        super(cause);
+    }
 }
