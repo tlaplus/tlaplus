af4c1df0319efe98de2b8141bf80caa4d3b5316f
simonzam <simonzam@76a6fc44-f60b-0410-a9a8-e67b0e8fc65c>
2009-02-21 00:36:41 +0000

    moved
    git-svn-id: svn+ssh://svn.msr-inria.inria.fr/var/lib/svn/repository/tla/trunk/tla2-inria@12849 76a6fc44-f60b-0410-a9a8-e67b0e8fc65c

---
+++ tlatools/src/tlc2/tool/other/CheckFP.java
@@ -0,0 +1,46 @@
+// Copyright (c) 2003 Compaq Corporation.  All rights reserved.
+// Portions Copyright (c) 2003 Microsoft Corporation.  All rights reserved.
+
+package tlc2.tool.other;
+
+import java.io.IOException;
+
+import tlc2.util.BufferedRandomAccessFile;
+
+/**
+ * Andrei Broder told us that
+ *  1. We are probably in trouble if any two fingerprints have the
+ *     same high order 56 bits.
+ *  2. With a lot of hand waving, the probability of collision is
+ *     about the max of 1/|fi - fj|.
+ * This program does these sanity checkings.
+ * @deprecated (SZ February 19, 2009)
+ */
+public class CheckFP {
+  public static void main(String args[]) {
+    try {
+      BufferedRandomAccessFile raf = new BufferedRandomAccessFile(args[0], "r");
+      long fileLen = raf.length();
+      long dis = Long.MAX_VALUE;
+      int cnt = 0;
+      long x = raf.readLong();
+      while (raf.getFilePointer() < fileLen) {
+	long y = raf.readLong();
+	if ((x >> 8) == (y >> 8))
+	  System.err.println("bad: " + x + " and " + y);
+	dis = Math.min(dis, y-x);
+	x = y;
+	cnt++;
+	if ((cnt & 0xFFFF) == 0)
+	  System.err.println("the number of states checked: " + cnt);
+      }
+      System.err.println("the number of states checked: " + cnt);
+      System.err.println("the probability of collision: " + 1.0/dis);
+    }
+    catch (IOException e) {
+      System.err.println("Error: " + e.getMessage());
+      System.exit(1);
+    }
+  }
+
+}
---
+++ tlatools/src/tlc2/tool/other/FileClassLoader.java
@@ -0,0 +1,67 @@
+// Copyright (c) 2003 Compaq Corporation.  All rights reserved.
+// Portions Copyright (c) 2003 Microsoft Corporation.  All rights reserved.
+// Last modified on Mon 30 Apr 2007 at  9:27:29 PST by lamport
+//      modified on Wed Jan 10 00:11:43 PST 2001 by yuanyu
+
+package tlc2.tool.other;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+/**
+ * This class is not used from anywhere from the project
+ * it seems to be a helper utility used during the development 
+ * @deprecated according to the paths it is not used (SZ February 19, 2009) 
+ */
+public class FileClassLoader extends ClassLoader {
+  /* Load a class from a file. */
+  private String dir;
+
+  public FileClassLoader(String dir) {
+    this.dir = dir;
+    if (dir.length() != 0 &&
+	!dir.endsWith(File.separator)) {
+      this.dir += File.separator;
+    }
+  }
+    
+  private byte[] loadClassData(String name) {
+    byte[] bytes = null;
+    String fileName = name + ".class";
+    try {
+      FileInputStream fis = new FileInputStream(this.dir + fileName);
+      int size = fis.available();
+      bytes = new byte[size];
+      fis.read(bytes);
+      fis.close();
+    }
+    catch (IOException e) { bytes = null; }
+    return bytes;
+  }
+
+  public synchronized Class loadClass(String name, boolean resolve) {
+    Class c = null;
+    byte[] data = loadClassData(name);
+    if (data != null) {
+      c = defineClass(name, data, 0, data.length);
+      if (resolve) resolveClass(c);
+    }
+    return c;
+  }
+
+  public static void main(String argv[]) {
+    FileClassLoader fcl = new FileClassLoader("/udir/yuanyu/proj/tlc/module");
+    try {
+      Class c = fcl.loadClass("Strings", true);  // must set CLASSPATH correctly
+      System.err.println(c);
+    }
+    catch (Exception e) {
+      // Assert.printStack();
+      System.err.println("Error: " + e.getMessage());
+    }
+  }
+
+}
+
+
