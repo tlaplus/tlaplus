ba8d113ac5f7fd5d6799415be699a9616f2e4e5c
simonzam <simonzam@76a6fc44-f60b-0410-a9a8-e67b0e8fc65c>
2009-02-21 01:56:50 +0000

    git-svn-id: svn+ssh://svn.msr-inria.inria.fr/var/lib/svn/repository/tla/trunk/tla2-inria@12890 76a6fc44-f60b-0410-a9a8-e67b0e8fc65c

--- org.lamport.tla.toolbox.tool.tlc/src/org/lamport/tla/toolbox/tool/tlc/handlers/TLCLaunchHandler.java
+++ org.lamport.tla.toolbox.tool.tlc/src/org/lamport/tla/toolbox/tool/tlc/handlers/TLCLaunchHandler.java
@@ -11,41 +11,44 @@ import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.handlers.HandlerUtil;
 import org.lamport.tla.toolbox.tool.ToolboxHandle;
+import org.lamport.tla.toolbox.util.RCPNameToFileIStream;
+import org.lamport.tla.toolbox.util.ResourceHelper;
 
 import tlc2.TLC;
+import util.ToolIO;
 
 /**
  * Our sample handler extends AbstractHandler, an IHandler base class.
  * @see org.eclipse.core.commands.IHandler
  * @see org.eclipse.core.commands.AbstractHandler
  */
-public class TLCLaunchHandler extends AbstractHandler {
-	/**
-	 * The constructor.
-	 */
-	public TLCLaunchHandler() {
-	}
+public class TLCLaunchHandler extends AbstractHandler
+{
+    /**
+     * The constructor.
+     */
+    public TLCLaunchHandler()
+    {
+    }
 
-	/**
-	 * the command has been executed, so extract extract the needed information
-	 * from the application context.
-	 */
-	public Object execute(ExecutionEvent event) throws ExecutionException {
-		IWorkbenchWindow window = HandlerUtil.getActiveWorkbenchWindowChecked(event);
-		MessageDialog.openInformation(
-				window.getShell(),
-				"TLA+ Toolbox TLC",
-				"TLC Launch executed");
-		
-		
-		IFile rootModule = ToolboxHandle.getRootModule();
-		
-		// TODO HACK
-		String cfgPath = rootModule.getLocation().removeFileExtension().addFileExtension("cfg").toOSString();
-		File cfgFile = new File(cfgPath);
-		if (!cfgFile.exists())
-		{
-		    try
+    /**
+     * the command has been executed, so extract extract the needed information
+     * from the application context.
+     */
+    public Object execute(ExecutionEvent event) throws ExecutionException
+    {
+        IWorkbenchWindow window = HandlerUtil.getActiveWorkbenchWindowChecked(event);
+        MessageDialog.openInformation(window.getShell(), "TLA+ Toolbox TLC", "TLC Launch executed");
+
+        IFile rootModule = ToolboxHandle.getRootModule();
+
+        String rootFilename = rootModule.getLocation().toOSString();
+        // TODO HACK
+        String cfgPath = rootModule.getLocation().removeFileExtension().addFileExtension("cfg").toOSString();
+        File cfgFile = new File(cfgPath);
+        if (!cfgFile.exists())
+        {
+            try
             {
                 cfgFile.createNewFile();
             } catch (IOException e)
@@ -53,12 +56,36 @@ public class TLCLaunchHandler extends AbstractHandler {
                 // TODO Auto-generated catch block
                 e.printStackTrace();
             }
-		}
-		
-		if (rootModule != null) 
-		{
-		    TLC.main(new String[]{"-config", cfgFile.getPath(), rootModule.toString()});
-		}
-		return null;
-	}
+        }
+
+        if (rootModule != null)
+        {
+            // Reset the tool output messages.
+            ToolIO.reset();
+            ToolIO.setMode(ToolIO.TOOL);
+
+            ToolIO.setUserDir(ResourceHelper.getParentDir(rootModule.getLocation().toOSString()));
+
+            // create a TLC instance
+            TLC tlc = new TLC();
+
+            // setup name resolver
+            tlc.setResolver(new RCPNameToFileIStream(null));
+
+            // handle parameters
+            // tlc.handleParameters(new String[]{"-config", cfgFile.getPath(),
+            // ResourceHelper.getModuleName(rootFilename)});
+            tlc.handleParameters(new String[] { ResourceHelper.getModuleName(rootFilename) });
+
+            // call the actual processing method
+            tlc.process();
+
+            String[] messages = ToolIO.getAllMessages();
+            for (int i = 0; i < messages.length; i++)
+            {
+                System.out.println(messages[i]);
+            }
+        }
+        return null;
+    }
 }
