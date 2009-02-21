dbb91a9e9cf442672570254dff6da02f111ac73a
simonzam <simonzam@76a6fc44-f60b-0410-a9a8-e67b0e8fc65c>
2009-02-21 01:00:28 +0000

    reformated
    git-svn-id: svn+ssh://svn.msr-inria.inria.fr/var/lib/svn/repository/tla/trunk/tla2-inria@12878 76a6fc44-f60b-0410-a9a8-e67b0e8fc65c

--- tlatools/src/tlc2/util/FileUtil.java
+++ tlatools/src/tlc2/util/FileUtil.java
@@ -14,83 +14,121 @@ import java.io.OutputStream;
 import java.util.zip.GZIPInputStream;
 import java.util.zip.GZIPOutputStream;
 
-public class FileUtil {
+import util.ToolIO;
 
-  /**
-   * deletes the file or directory. Returns true iff the deletion
-   * succeeds. The argument recurse forces the deletion of non-empty
-   * directory.
-   */
-  public static boolean deleteDir(File file, boolean recurse) {
-    if (file.exists()) {
-      if (file.isFile() || !recurse) {
-	return file.delete();
-      }
-      // must be a directory:
-      String[] fnames = file.list();
-      for (int i = 0; i < fnames.length; i++) {
-	if (!deleteDir(new File(file, fnames[i]), recurse)) {
-	  return false;
-	}
-      }
-      return file.delete();
-    }
-    return true;
-  }
+/**
+ * Utilities for file modifications
+ * @author Simon Zambrovski
+ * @version $Id$
+ */
+public class FileUtil
+{
 
-  public static InputStream newBZFileInputStream(String file, 
-						 boolean useGzip, 
-						 boolean useIBuffers,
-						 int buffersize)
-  throws IOException, FileNotFoundException {
-    if (useGzip) {
-      return new GZIPInputStream(new FileInputStream(file), buffersize);
+    /**
+     * Deletes the file or directory. Returns true iff the deletion
+     * succeeds. The argument recurse forces the deletion of non-empty
+     * directory.
+     */
+    public static boolean deleteDir(File file, boolean recurse)
+    {
+        if (file.exists())
+        {
+            if (file.isFile() || !recurse)
+            {
+                return file.delete();
+            }
+            // must be a directory:
+            String[] fnames = file.list();
+            for (int i = 0; i < fnames.length; i++)
+            {
+                if (!deleteDir(new File(file, fnames[i]), recurse))
+                {
+                    return false;
+                }
+            }
+            return file.delete();
+        }
+        return true;
     }
-    else if (useIBuffers) {
-      return new BufferedInputStream (new FileInputStream(file), buffersize);
-    }
-    else {
-      return new FileInputStream(file);
-    }
-  }
 
-  public static OutputStream newBZFileOutputStream(String file,
-						   boolean useGzip, 
-						   boolean useOBuffers,
-						   int buffersize)
-  throws IOException {
-    if (useGzip) {
-      return new GZIPOutputStream(new FileOutputStream(file), buffersize);
+    /**
+     * Constructs a input stream from the file
+     * @param file
+     * @param useGzip
+     * @param useIBuffers
+     * @param buffersize
+     * @return
+     * @throws IOException
+     * SZ Feb 20, 2009: FileNotFoundException removed
+     */
+    public static InputStream newBZFileInputStream(String file, boolean useGzip, boolean useIBuffers, int buffersize)
+            throws IOException
+    {
+        if (useGzip)
+        {
+            return new GZIPInputStream(new FileInputStream(file), buffersize);
+        } else if (useIBuffers)
+        {
+            return new BufferedInputStream(new FileInputStream(file), buffersize);
+        } else
+        {
+            return new FileInputStream(file);
+        }
     }
-    else if (useOBuffers) {
-      return new BufferedOutputStream (new FileOutputStream(file), buffersize);
-    }
-    else {
-      return new FileOutputStream(file);
-    }
-  }
 
-  public static OutputStream newBZFileOutputStream(String file,
-						   boolean useGzip, 
-						   boolean useOBuffers,
-						   int buffersize, boolean app)
-  throws IOException {
-    if (useGzip) {
-      return new GZIPOutputStream(new FileOutputStream(file, app), buffersize);
+    /**
+     * Constructs an output stream to a file
+     * @param file
+     * @param useGzip
+     * @param useOBuffers
+     * @param buffersize
+     * @return
+     * @throws IOException
+     */
+    public static OutputStream newBZFileOutputStream(String file, boolean useGzip, boolean useOBuffers, int buffersize)
+            throws IOException
+    {
+        return newBZFileOutputStream(file, useGzip, useOBuffers, buffersize, false);
     }
-    else if (useOBuffers) {
-      return new BufferedOutputStream (new FileOutputStream(file, app), buffersize);
-    }
-    else {
-      return new FileOutputStream(file, app);
-    }
-  }
 
-  public static void printArrayOfBigInts(InputStream in) throws IOException {
-    BigInt[] A = ByteUtils.readSizeArrayOfSizeBigInts(in);
-    for (int i = 0; i < A.length; i++) {
-      System.out.println(A[i]);
+    /**
+     * Constructs an output stream to a file
+     * @param file
+     * @param useGzip
+     * @param useOBuffers
+     * @param buffersize
+     * @param append
+     * 
+     * @return
+     * @throws IOException
+     */
+    public static OutputStream newBZFileOutputStream(String file, boolean useGzip, boolean useOBuffers, int buffersize,
+            boolean app) throws IOException
+    {
+        if (useGzip)
+        {
+            return new GZIPOutputStream(new FileOutputStream(file, app), buffersize);
+        } else if (useOBuffers)
+        {
+            return new BufferedOutputStream(new FileOutputStream(file, app), buffersize);
+        } else
+        {
+            return new FileOutputStream(file, app);
+        }
     }
-  }
-  
+
+    /**
+     * Print array of big integers read from a input stream  
+     * @param in
+     * @throws IOException
+     */
+    public static void printArrayOfBigInts(InputStream in) throws IOException
+    {
+        BigInt[] A = ByteUtils.readSizeArrayOfSizeBigInts(in);
+        for (int i = 0; i < A.length; i++)
+        {
+            ToolIO.out.println(A[i]);
+        }
+    }
+
 }
