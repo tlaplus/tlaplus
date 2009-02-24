f9e2ea1e19ae7fef7d6327be33a0e0eb41c5fc00
simonzam <simonzam@76a6fc44-f60b-0410-a9a8-e67b0e8fc65c>
2009-02-24 22:11:39 +0000

    methods added
    git-svn-id: svn+ssh://svn.msr-inria.inria.fr/var/lib/svn/repository/tla/trunk/tla2-inria@12967 76a6fc44-f60b-0410-a9a8-e67b0e8fc65c

--- tlatools/src/util/FileUtil.java
+++ tlatools/src/util/FileUtil.java
@@ -4,12 +4,18 @@ package util;
 
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.io.OutputStream;
+import java.math.BigInteger;
 import java.util.zip.GZIPInputStream;
 import java.util.zip.GZIPOutputStream;
 
@@ -23,32 +29,28 @@ import tlc2.util.ByteUtils;
  */
 public class FileUtil
 {
+    public static final char separatorChar = File.separatorChar;
+    public static final String separator = File.separator;
+    public static final String pathSeparator = File.pathSeparator;
 
     /**
      * Deletes the file or directory. Returns true iff the deletion
      * succeeds. The argument recurse forces the deletion of non-empty
      * directory.
-     * @deprecated use {@link FileUtil#deleteDir(String, boolean)}
      */
     public static boolean deleteDir(File file, boolean recurse)
     {
-        return doDeleteDir(file, recurse, ToolIO.getResolver());
+        return doDeleteDir(file, recurse);
     }
-    
+
     /**
-     * @deprecated don't use it
-     * It is a bad implementation 
+     * Convenience method 
      */
     public static boolean deleteDir(String filename, boolean recurse)
     {
-        return deleteDir(filename, recurse, ToolIO.getResolver());
+        return doDeleteDir(new File(filename), recurse);
     }
 
-    public static boolean deleteDir(String filename, boolean recurse, FilenameToStream resolver)
-    {
-        return doDeleteDir(new File(filename), recurse, resolver);
-    }
-    
     /**
      * Implementation of the file deletion
      * @param file
@@ -56,9 +58,9 @@ public class FileUtil
      * @param resolver
      * @return
      */
-    private static boolean doDeleteDir(File file, boolean recurse, FilenameToStream resolver)
+    private static boolean doDeleteDir(File file, boolean recurse)
     {
-        if (file.exists())
+        if (file !=null && file.exists())
         {
             if (file.isFile() || !recurse)
             {
@@ -69,15 +71,9 @@ public class FileUtil
             File child = null;
             for (int i = 0; i < fnames.length; i++)
             {
-                
-                if (resolver == null) 
-                {
-                    child = new File(file, fnames[i]);
-                } else {
-                    // TODO
-                }
-                
-                if (!doDeleteDir(child, recurse, resolver))
+                child = new File(file, fnames[i]);
+
+                if (!doDeleteDir(child, recurse))
                 {
                     return false;
                 }
@@ -86,8 +82,6 @@ public class FileUtil
         }
         return true;
     }
-    
-
 
     /**
      * Constructs a input stream from the file
@@ -112,6 +106,11 @@ public class FileUtil
         {
             return new FileInputStream(file);
         }
+    }
+    
+    public static InputStream newZFileInputStream(String file) throws FileNotFoundException, IOException
+    {
+        return new GZIPInputStream(new FileInputStream(file));
     }
 
     /**
@@ -169,6 +168,14 @@ public class FileUtil
         }
     }
     
+    public static void printSizeArrayOfSizeBigIntegers(InputStream in) throws IOException {
+        BigInteger[] A = ByteUtils.readSizeArrayOfSizeBigInts(in);
+        for (int i = 0; i < A.length; i++) 
+        {
+            ToolIO.out.println(A[i]);
+        }
+    }
+
     public static void copyFile(String fromName, String toName) throws IOException
     {
         // REFACTOR
@@ -183,6 +190,224 @@ public class FileUtil
         fis.close();
         fos.close();
     }
+
+    public static NamedInputStream createNamedInputStream(String name, FilenameToStream resolver)
+    {
+        // Strip off one NEWLINE and anything after it, if it is there
+        int n;
+        n = name.indexOf( '\n' );
+        if ( n >= 0 ) {
+            // SZ Feb 20, 2009: the message adjusted to what is actually done 
+            ToolIO.out.println("*** Warning: module name '" + name + "' contained NEWLINE; "
+                    + "Only the part before NEWLINE is considered.");
+            name = name.substring( 0, n );     // Strip off the newline
+        }
+
+        
+        String sourceFileName;
+        String sourceModuleName;
+        
+        
+        // consider name=/foo/bar/somemod.tla
+        // or name=/foo/bar/somemod
+        
+        // Make sure the file name ends with ".tla".
+        if (name.toLowerCase().endsWith(".tla")) 
+        {
+            name = name.substring(0, name.length() - 4);
+        }
+        
+        // now name=/foo/bar/somemod
+        
+        // filename is a path ending with .tla
+        // sourceFilename=/foo/bar/somemod
+        sourceFileName = name + ".tla";
+
+        // module name is =somemod
+        sourceModuleName = name.substring(name.lastIndexOf(FileUtil.separator) + 1);
+
+        File sourceFile = resolver.resolve(sourceFileName, true);
+        if (sourceFile != null && sourceFile.exists())
+        {
+            try
+            {
+                NamedInputStream nis = new NamedInputStream(sourceFileName, sourceModuleName, sourceFile);
+                return nis;
+            } catch (FileNotFoundException e)
+            {
+                ToolIO.out.println("***Internal error: Unable to create NamedInputStream" + " in toIStream method");
+            }
+        }
+        return null;
+    }
+
+    public static FileInputStream newFIS(File file)
+    {
+        if (file != null && file.exists())
+        {
+            try
+            {
+                FileInputStream fis = new FileInputStream(file);
+                return fis;
+            } catch (FileNotFoundException e)
+            {
+                ToolIO.out.println("***Internal error: Unable to create FileInputStream");
+            }
+        }
+        return null;
+    }
+    public static FileOutputStream newFOS(File file)
+    {
+        if (file != null && file.exists())
+        {
+            try
+            {
+                FileOutputStream fos = new FileOutputStream(file);
+                return fos;
+            } catch (FileNotFoundException e)
+            {
+                ToolIO.out.println("***Internal error: Unable to create FileOutStream");
+            }
+        }
+        return null;
+    }
+
+    
+    /**
+     * retrieves a new buffered file output stream
+     * @param name
+     * @return
+     */
+    public static OutputStream newBFOS(String name)
+    {
+        File file = new File(name);
+        if (file != null && file.exists())
+        {
+            try
+            {
+                FileOutputStream fos = new FileOutputStream(file);
+                return fos;
+            } catch (FileNotFoundException e)
+            {
+                ToolIO.out.println("***Internal error: Unable to create FileOutStream");
+            }
+        }
+        return null;
+    }
+    
+    public static BufferedDataInputStream newBdFIS(boolean useGZIP, File file) throws IOException
+    {
+        if (useGZIP) 
+        {
+            return new BufferedDataInputStream(new GZIPInputStream(new FileInputStream(file)));   
+        } else {
+            return new BufferedDataInputStream(new FileInputStream(file));    
+        }
+    }
+
+    /**
+     * @param useGZIP
+     * @param string
+     * @return
+     * @throws IOException 
+     */
+    public static BufferedDataInputStream newBdFIS(boolean useGZIP, String filename) throws IOException
+    {
+        return newBdFIS(useGZIP, new File(filename));
+    }
+
+    /**
+     * @param b
+     * @param poolFile
+     * @return
+     * @throws IOException 
+     * @throws FileNotFoundException 
+     */
+    public static BufferedDataOutputStream newBdFOS(boolean useGZIP, File file) throws FileNotFoundException, IOException
+    {
+        if (useGZIP) 
+        {
+            return new BufferedDataOutputStream(new GZIPOutputStream(new FileOutputStream(file)));   
+        } else {
+            return new BufferedDataOutputStream(new FileOutputStream(file));    
+        }
+    }
+
+    /**
+     * @param useGZIP
+     * @param string
+     * @return
+     * @throws IOException 
+     */
+    public static BufferedDataOutputStream newBdFOS(boolean useGZIP, String filename) throws IOException
+    {
+        return newBdFOS(useGZIP, new File(filename));
+    }
+
+    
+    public static ObjectInputStream newOBFIS(File file) throws FileNotFoundException, IOException
+    {
+        return new ObjectInputStream(new BufferedInputStream(new FileInputStream(file)));
+    }
+
+    /**
+     * @param chkptfile
+     * @return
+     * @throws IOException 
+     * @throws FileNotFoundException 
+     */
+    public static ObjectInputStream newOBFIS(String filename) throws FileNotFoundException, IOException
+    {
+        return newOBFIS(new File(filename));
+    }
+
+    
+    /**
+     * @param poolFile
+     * @return
+     * @throws IOException 
+     * @throws FileNotFoundException 
+     */
+    public static ObjectOutputStream newOBFOS(File file) throws FileNotFoundException, IOException
+    {
+        return new ObjectOutputStream(new BufferedOutputStream(new FileOutputStream(file)));
+    }
+
+    /**
+     * @param tmpfile
+     * @return
+     * @throws IOException 
+     * @throws FileNotFoundException 
+     */
+    public static ObjectOutputStream newOBFOS(String filename) throws FileNotFoundException, IOException
+    {
+        return newOBFOS(new File(filename));
+    }
+
+    /**
+     * @param b
+     * @param chkptName
+     * @return
+     * @throws FileNotFoundException 
+     */
+    public static DataInputStream newDFIS(String filename) throws FileNotFoundException
+    {
+        return new DataInputStream(new FileInputStream(new File(filename)));
+    }
+
+    /**
+     * @param chkptName
+     * @return
+     * @throws FileNotFoundException 
+     */
+    public static DataOutputStream newDFOS(String filename) throws FileNotFoundException
+    {
+        return new DataOutputStream(new FileOutputStream(new File(filename)));
+    }
+
+
+
+
 
 
 }
