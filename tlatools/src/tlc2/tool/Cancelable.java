e0e2c941f1687794af3ce0cad92d295c43f6d852
simonzam <simonzam@76a6fc44-f60b-0410-a9a8-e67b0e8fc65c>
2009-02-24 02:04:26 +0000

    init
    git-svn-id: svn+ssh://svn.msr-inria.inria.fr/var/lib/svn/repository/tla/trunk/tla2-inria@12901 76a6fc44-f60b-0410-a9a8-e67b0e8fc65c

---
+++ tlatools/src/tlc2/tool/Cancelable.java
@@ -0,0 +1,15 @@
+package tlc2.tool;
+
+/**
+ * 
+ * @author Simon Zambrovski
+ * @version $Id$
+ */
+public interface Cancelable
+{
+    /**
+     * Sets the flag to cancel the instance
+     * @param flag cancellation flag
+     */
+    public void setCancelFlag(boolean flag);
+}
