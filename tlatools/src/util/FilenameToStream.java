e7384ff35dde7a9d9aaca89de0cd0589cdc590a5
simonzam <simonzam@76a6fc44-f60b-0410-a9a8-e67b0e8fc65c>
2009-02-24 22:11:26 +0000

    refactored
    git-svn-id: svn+ssh://svn.msr-inria.inria.fr/var/lib/svn/repository/tla/trunk/tla2-inria@12966 76a6fc44-f60b-0410-a9a8-e67b0e8fc65c

--- tlatools/src/util/FilenameToStream.java
+++ tlatools/src/util/FilenameToStream.java
@@ -2,46 +2,23 @@
 // Portions Copyright (c) 2003 Microsoft Corporation.  All rights reserved.
 package util;
 
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
+import java.io.File;
 
 
 /**
- * Resolver for the name to Input Stream
- * (See the FrontEnd class's parseStream method for 
- * how it is used.)
+ * Resolver for the name to file handle
  * @author Leslie Lamport
  * @author Simon Zambrovski 
  * @version $Id$                                                      
  */
 public interface FilenameToStream
 {
-    /**
-     * Retrieves an input stream from the module name
-     * @param name logical name
-     * @return FIS or null, if file not found
-     */
-    public NamedInputStream toNIStream(String name);
-
-    /**
-     * Retrieves an input stream from the file name
-     * @param name logical name
-     * @param isModule, if true, looks for TLA+ module
-     * @return Input Stream or null, if file <code>not</code> found
-     */
-    public NamedInputStream toNIStream(String name, boolean isModule);
     
     /**
-     * retrieves a file input from the given name
-     * @param name
+     * This method resolves the logical name to the OS-resource 
+     * @param filename
+     * @param isModule
      * @return
      */
-    public FileInputStream toIStream(String name);
-    
-    /**
-     * Retrieves file output stream from the given name 
-     * @param name
-     * @return
-     */
-    public FileOutputStream toOStream(String name);
+    public File resolve(String filename, boolean isModule);
 }
