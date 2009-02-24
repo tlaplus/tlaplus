ee59ab4653b15dd0d927c6c2d3d01833f54884f8
simonzam <simonzam@76a6fc44-f60b-0410-a9a8-e67b0e8fc65c>
2009-02-24 02:09:42 +0000

    git-svn-id: svn+ssh://svn.msr-inria.inria.fr/var/lib/svn/repository/tla/trunk/tla2-inria@12918 76a6fc44-f60b-0410-a9a8-e67b0e8fc65c

--- org.lamport.tla.toolbox.tool.tlc/src/org/lamport/tla/toolbox/tool/tlc/handlers/TLCLaunchHandler.java
+++ org.lamport.tla.toolbox.tool.tlc/src/org/lamport/tla/toolbox/tool/tlc/handlers/TLCLaunchHandler.java
@@ -1,21 +1,12 @@
 package org.lamport.tla.toolbox.tool.tlc.handlers;
 
-import java.io.File;
-import java.io.IOException;
-
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
-import org.eclipse.core.resources.IFile;
-import org.eclipse.jface.dialogs.MessageDialog;
-import org.eclipse.ui.IWorkbenchWindow;
-import org.eclipse.ui.handlers.HandlerUtil;
-import org.lamport.tla.toolbox.tool.ToolboxHandle;
-import org.lamport.tla.toolbox.util.RCPNameToFileIStream;
-import org.lamport.tla.toolbox.util.ResourceHelper;
-
-import tlc2.TLC;
-import util.ToolIO;
+import org.eclipse.core.runtime.jobs.IJobChangeEvent;
+import org.eclipse.core.runtime.jobs.JobChangeAdapter;
+import org.lamport.tla.toolbox.tool.tlc.job.AbstractJob;
+import org.lamport.tla.toolbox.tool.tlc.job.TLCJob;
 
 /**
  * Our sample handler extends AbstractHandler, an IHandler base class.
@@ -37,55 +28,24 @@ public class TLCLaunchHandler extends AbstractHandler
      */
     public Object execute(ExecutionEvent event) throws ExecutionException
     {
-        IWorkbenchWindow window = HandlerUtil.getActiveWorkbenchWindowChecked(event);
-        MessageDialog.openInformation(window.getShell(), "TLA+ Toolbox TLC", "TLC Launch executed");
 
-        IFile rootModule = ToolboxHandle.getRootModule();
-
-        String rootFilename = rootModule.getLocation().toOSString();
-        // TODO HACK
-        String cfgPath = rootModule.getLocation().removeFileExtension().addFileExtension("cfg").toOSString();
-        File cfgFile = new File(cfgPath);
-        if (!cfgFile.exists())
+        AbstractJob job = new TLCJob();
+        job.addJobChangeListener(new JobChangeAdapter() 
         {
-            try
+
+            public void done(IJobChangeEvent event)
             {
-                cfgFile.createNewFile();
-            } catch (IOException e)
-            {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
+                if (event.getResult().isOK())
+                {
+                    System.out.println("JOB Change Listener ------------ { Done }");
+                } else
+                {
+                    System.out.println("JOB Change Listener ------------ { Cancelled }");
+                }
             }
-        }
-
-        if (rootModule != null)
-        {
-            // Reset the tool output messages.
-            ToolIO.reset();
-            ToolIO.setMode(ToolIO.TOOL);
-
-            ToolIO.setUserDir(ResourceHelper.getParentDir(rootModule.getLocation().toOSString()));
-
-            // create a TLC instance
-            TLC tlc = new TLC();
-
-            // setup name resolver
-            tlc.setResolver(new RCPNameToFileIStream(null));
-
-            // handle parameters
-            // tlc.handleParameters(new String[]{"-config", cfgFile.getPath(),
-            // ResourceHelper.getModuleName(rootFilename)});
-            tlc.handleParameters(new String[] { ResourceHelper.getModuleName(rootFilename) });
-
-            // call the actual processing method
-            tlc.process();
-
-            String[] messages = ToolIO.getAllMessages();
-            for (int i = 0; i < messages.length; i++)
-            {
-                System.out.println(messages[i]);
-            }
-        }
+        });
+        job.setUser(true);
+        job.schedule();
         return null;
     }
 }
