c761a4f66c0a40e384754aad6d3977dca3bfab79
simonzam <simonzam@76a6fc44-f60b-0410-a9a8-e67b0e8fc65c>
2009-02-24 02:11:36 +0000

    config file allocation added
    git-svn-id: svn+ssh://svn.msr-inria.inria.fr/var/lib/svn/repository/tla/trunk/tla2-inria@12924 76a6fc44-f60b-0410-a9a8-e67b0e8fc65c

--- org.lamport.tla.toolbox/src/org/lamport/tla/toolbox/tool/ToolboxHandle.java
+++ org.lamport.tla.toolbox/src/org/lamport/tla/toolbox/tool/ToolboxHandle.java
@@ -1,10 +1,16 @@
 package org.lamport.tla.toolbox.tool;
 
+import java.io.File;
+import java.io.IOException;
+
 import org.eclipse.core.resources.IFile;
 import org.eclipse.jface.preference.IPreferenceStore;
 import org.lamport.tla.toolbox.Activator;
+import org.lamport.tla.toolbox.spec.Spec;
 import org.lamport.tla.toolbox.util.pref.IPreferenceConstants;
 import org.lamport.tla.toolbox.util.pref.PreferenceStoreHelper;
+
+import tla2sany.modanalyzer.SpecObj;
 
 /**
  * Provides shortcuts to the internal toolbox methods, that should be made accessible to the other tools
@@ -23,6 +29,38 @@ public class ToolboxHandle
     {
         return ((Activator.getSpecManager().getSpecLoaded() == null) ? null : Activator.getSpecManager().getSpecLoaded().getRootFile()); 
     }
+
+
+    /**
+     * Retrieves the filename of the config file
+     * TODO HACK 
+     */
+    public static String getConfigFilename()
+    {
+        IFile rootModule = getRootModule();
+
+        String cfgPath = rootModule.getLocation().removeFileExtension().addFileExtension("cfg").toOSString();
+        
+        File cfgFile = new File(cfgPath);
+        if (!cfgFile.exists())
+        {
+            try
+            {
+                cfgFile.createNewFile();
+            } catch (IOException e)
+            {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+        if (cfgFile != null && cfgFile.exists())
+        {
+            return cfgFile.getName();
+        } else {
+            return null;
+        }
+        
+    }
     
     /**
      * Returns the instance preference store
@@ -31,4 +69,20 @@ public class ToolboxHandle
     {
         return PreferenceStoreHelper.getInstancePreferenceStore();
     }
+    
+    /**
+     * Returns the SpecObj of the current loaded specification
+     * @return SpecObj produced by the previous SANY run, or null
+     */
+    public static SpecObj getSpecObj()
+    {
+        Spec spec = Activator.getSpecManager().getSpecLoaded();
+        if (spec != null) 
+        {
+            return spec.getValidRootModule();
+        }
+        return null;
+    }
+    
+    
 }
