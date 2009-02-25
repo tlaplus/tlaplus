ce0abedba54667528729ef11bc1a79bc46b4b0c5
simonzam <simonzam@76a6fc44-f60b-0410-a9a8-e67b0e8fc65c>
2009-02-25 00:43:13 +0000

    init
    git-svn-id: svn+ssh://svn.msr-inria.inria.fr/var/lib/svn/repository/tla/trunk/tla2-inria@12978 76a6fc44-f60b-0410-a9a8-e67b0e8fc65c

---
+++ org.lamport.tla.toolbox/src/org/lamport/tla/toolbox/util/IHelpConstants.java
@@ -0,0 +1,15 @@
+package org.lamport.tla.toolbox.util;
+
+/**
+ * COnstnats for help system
+ * @author Simon Zambrovski
+ * @version $Id$
+ */
+public interface IHelpConstants
+{
+
+    public static final String PARSER_PREFERENCE_PAGE = "ParserPreferencePage";
+    public static final String MODULE_PROPERTY_PAGE = "ModulePropertyPage";
+    public static final String SPEC_PROPERTY_PAGE = "SpecPropertyPage";
+
+}
