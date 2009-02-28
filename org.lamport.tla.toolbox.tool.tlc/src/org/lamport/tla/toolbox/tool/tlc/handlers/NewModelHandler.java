3a9c97ee2a856c61fc447dd4f90ad88bce67662b
simonzam <simonzam@76a6fc44-f60b-0410-a9a8-e67b0e8fc65c>
2009-02-28 03:18:20 +0000

    init
    git-svn-id: svn+ssh://svn.msr-inria.inria.fr/var/lib/svn/repository/tla/trunk/tla2-inria@13000 76a6fc44-f60b-0410-a9a8-e67b0e8fc65c

---
+++ org.lamport.tla.toolbox.tool.tlc/src/org/lamport/tla/toolbox/tool/tlc/handlers/NewModelHandler.java
@@ -0,0 +1,100 @@
+package org.lamport.tla.toolbox.tool.tlc.handlers;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.debug.core.DebugPlugin;
+import org.eclipse.debug.core.ILaunchConfiguration;
+import org.eclipse.debug.core.ILaunchConfigurationType;
+import org.eclipse.debug.core.ILaunchConfigurationWorkingCopy;
+import org.eclipse.debug.core.ILaunchManager;
+import org.eclipse.debug.ui.DebugUITools;
+import org.eclipse.jface.viewers.StructuredSelection;
+import org.eclipse.jface.window.Window;
+import org.lamport.tla.toolbox.tool.ToolboxHandle;
+import org.lamport.tla.toolbox.tool.tlc.launch.TLCModelLaunchDelegate;
+import org.lamport.tla.toolbox.tool.tlc.launch.ui.IConfigurationConstants;
+import org.lamport.tla.toolbox.tool.tlc.util.ModelHelper;
+import org.lamport.tla.toolbox.util.UIHelper;
+
+/**
+ * Our sample handler extends AbstractHandler, an IHandler base class.
+ * @see org.eclipse.core.commands.IHandler
+ * @see org.eclipse.core.commands.AbstractHandler
+ */
+public class NewModelHandler extends AbstractHandler implements IConfigurationConstants
+{
+    public static final Object PARAM_MODEL_NAME = "modelName";
+
+    public static final String COMMAND_ID = null;
+
+    /**
+     * The constructor.
+     */
+    public NewModelHandler()
+    {
+    }
+
+    public Object execute(ExecutionEvent event) throws ExecutionException
+    {
+        // root file
+        IResource rootModule = ToolboxHandle.getRootModule();
+
+        ILaunchManager launchManager = DebugPlugin.getDefault().getLaunchManager();
+        ILaunchConfigurationType launchConfigurationType = launchManager
+                .getLaunchConfigurationType(TLCModelLaunchDelegate.LAUNCH_ID);
+
+        String modelName = ModelHelper.constructModelName(rootModule.getProject(), ToolboxHandle.getCurrentSpec()
+                .getName());
+        IResource modelRoot = ModelHelper.getModelRootFile(ToolboxHandle.getRootModule(), modelName);
+        IResource config = ModelHelper.getConfigFile(ToolboxHandle.getRootModule());
+        
+
+        try
+        {
+
+            // create new launch instance
+            ILaunchConfigurationWorkingCopy launchCopy = launchConfigurationType.newInstance(rootModule.getProject(),
+                    modelName);
+
+            launchCopy.setAttribute(SPEC_NAME, ToolboxHandle.getCurrentSpec().getName());
+            launchCopy.setAttribute(SPEC_ROOT_FILE, ToolboxHandle.getRootModule().getLocation().toOSString());
+            
+            launchCopy.setAttribute(MODEL_ROOT_FILE, modelRoot.getLocation().toOSString());
+            launchCopy.setAttribute(CONFIG_FILE, config.getLocation().toOSString());
+
+            ILaunchConfiguration launchSaved = launchCopy.doSave();
+
+            openLaunchDialog(launchSaved);
+
+        } catch (CoreException e)
+        {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    /**
+     * 
+     */
+    public static void openLaunchDialog(ILaunchConfiguration configCopy)
+    {
+        int dialogResponse = DebugUITools.openLaunchConfigurationDialogOnGroup(UIHelper.getShellProvider().getShell(),
+                new StructuredSelection(configCopy), "org.lamport.tla.toolbox.tool.tlc.launchGroup.modelcheck");
+        if (Window.CANCEL == dialogResponse)
+        {
+            // cancel pressed
+        } else if (Window.OK == dialogResponse)
+        {
+            // ok pressed
+
+        } else
+        {
+            System.out.println("BUG: Window/Dialog API changed");
+        }
+    }
+}
---
+++ org.lamport.tla.toolbox.tool.tlc/src/org/lamport/tla/toolbox/tool/tlc/handlers/OpenModelHandler.java
@@ -0,0 +1,67 @@
+package org.lamport.tla.toolbox.tool.tlc.handlers;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.debug.core.DebugPlugin;
+import org.eclipse.debug.core.ILaunchConfiguration;
+import org.eclipse.debug.core.ILaunchConfigurationType;
+import org.eclipse.debug.core.ILaunchManager;
+import org.lamport.tla.toolbox.tool.tlc.launch.TLCModelLaunchDelegate;
+import org.lamport.tla.toolbox.tool.tlc.launch.ui.IConfigurationConstants;
+
+/**
+ * Opens a model configuration
+ * @author Simon Zambrovski 
+ * @version $Id$
+ * @see org.eclipse.core.commands.IHandler
+ * @see org.eclipse.core.commands.AbstractHandler
+ */
+public class OpenModelHandler extends AbstractHandler implements IConfigurationConstants
+{
+    public static final String COMMAND_ID = "org.lamport.tla.toolbox.tool.tlc.commands.model.open";
+    public static final Object PARAM_MODEL_NAME = "org.lamport.tla.toolbox.tool.tlc.commands.model.open.param";
+
+
+    /**
+     * The constructor.
+     */
+    public OpenModelHandler()
+    {
+    }
+
+    public Object execute(ExecutionEvent event) throws ExecutionException
+    {
+        String modelName = event.getParameter((String) PARAM_MODEL_NAME);
+    
+        System.out.println("Open handler invoked on " +  modelName);
+        
+        ILaunchManager launchManager = DebugPlugin.getDefault().getLaunchManager();
+        ILaunchConfigurationType launchConfigurationType = launchManager
+        .getLaunchConfigurationType(TLCModelLaunchDelegate.LAUNCH_ID);
+
+        try
+        {
+            ILaunchConfiguration[] launchConfigurations = launchManager.getLaunchConfigurations(launchConfigurationType);
+            for (int i = 0; i < launchConfigurations.length; i++)
+            {
+                if (launchConfigurations[i].getName().equals(modelName)) 
+                {
+                    NewModelHandler.openLaunchDialog(launchConfigurations[i]);
+                    break;
+                }
+            }
+            
+            
+        } catch (CoreException e)
+        {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        
+        System.out.println("Finished open handler");
+        
+        return null;
+    }
+}
