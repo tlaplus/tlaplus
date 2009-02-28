7eaaa603e0430880d6041f4c0384fb6ccfee410b
simonzam <simonzam@76a6fc44-f60b-0410-a9a8-e67b0e8fc65c>
2009-02-28 03:12:13 +0000

    trnasfered to a job
    git-svn-id: svn+ssh://svn.msr-inria.inria.fr/var/lib/svn/repository/tla/trunk/tla2-inria@12991 76a6fc44-f60b-0410-a9a8-e67b0e8fc65c

--- org.lamport.tla.toolbox/src/org/lamport/tla/toolbox/ui/handler/TranslateModuleHandler.java
+++ org.lamport.tla.toolbox/src/org/lamport/tla/toolbox/ui/handler/TranslateModuleHandler.java
@@ -1,24 +1,23 @@
 package org.lamport.tla.toolbox.ui.handler;
 
-import java.util.List;
-import java.util.Vector;
-
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.commands.IHandler;
-import org.eclipse.core.resources.IMarker;
 import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.IResourceRuleFactory;
+import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.core.runtime.jobs.IJobChangeEvent;
+import org.eclipse.core.runtime.jobs.ISchedulingRule;
+import org.eclipse.core.runtime.jobs.JobChangeAdapter;
+import org.eclipse.core.runtime.jobs.MultiRule;
 import org.eclipse.ui.IEditorInput;
 import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.IFileEditorInput;
-import org.lamport.tla.toolbox.util.ResourceHelper;
-import org.lamport.tla.toolbox.util.TLAMarkerHelper;
+import org.lamport.tla.toolbox.job.TranslatorJob;
 import org.lamport.tla.toolbox.util.UIHelper;
-import org.lamport.tla.toolbox.util.pref.IPreferenceConstants;
-
-import pcal.Translator;
 
 /**
  * Triggers the PCal translation of the module
@@ -27,105 +26,68 @@ import pcal.Translator;
  */
 public class TranslateModuleHandler extends AbstractHandler implements IHandler
 {
-    Translator translator = new Translator();
 
     public Object execute(ExecutionEvent event) throws ExecutionException
     {
         IEditorPart activeEditor = UIHelper.getActivePage().getActiveEditor();
 
         if (activeEditor.isDirty())
-        {
+        { 
             // editor is not saved
             // TODO react on this!
         }
+        
 
         IEditorInput editorInput = activeEditor.getEditorInput();
         if (editorInput instanceof IFileEditorInput)
         {
-            IResource fileToBuild = ((IFileEditorInput) editorInput).getFile();
-            System.out.println("Translating " + fileToBuild.getLocation().toOSString());
-
-            boolean hasPcalAlg = false;
-            String[] params;
-
-            try
-            {
-                hasPcalAlg = ((Boolean) fileToBuild.getSessionProperty(ResourceHelper
-                        .getQName(IPreferenceConstants.CONTAINS_PCAL_ALGORITHM))).booleanValue();
-                params = ((String) fileToBuild.getPersistentProperty(ResourceHelper
-                        .getQName(IPreferenceConstants.PCAL_CAL_PARAMS))).split(" ");
-
-            } catch (CoreException e1)
-            {
-                e1.printStackTrace();
-                params = new String[0];
-            }
-
-            if (!hasPcalAlg)
-            {
-                // no algorithm detected
-                System.out.println("No algorithm found");
-            } else {
-                System.out.println("Algorithm found");
-            }
-
-            // add params from the resource setting
-            Vector callParams = new Vector();
-            for (int i=0; i < params.length; i++)
-            {
-                if (params[i] != null && !params[i].equals("")) 
-                {
-                    callParams.add(params[i]);
-                }
-            }
-            callParams.add(fileToBuild.getLocation().toOSString());
+            final IResource fileToBuild = ((IFileEditorInput) editorInput).getFile();
+            TranslatorJob job = new TranslatorJob(fileToBuild);
+            job.setUser(true);
+            // TODO config file is also changed
+            job.setRule(getModifyRule(new IResource[]{fileToBuild}));
             
-
-            StringBuffer buffer = new StringBuffer();
-            for (int i = 0; i < callParams.size(); i++)
-            {
-                buffer.append(" " + callParams.elementAt(i));
-            }
-            System.out.println("Translator invoked with params: '" + buffer.toString()+ "'");
-
-            translator.runTranslation((String[]) callParams.toArray(new String[callParams.size()]));
-
-            List errors = translator.getErrorMessages();
-
-            for (int i = 0; i < errors.size(); i++)
-            {
-                String errorMessage = (String) errors.get(i);
-
-                TLAMarkerHelper.installProblemMarker(fileToBuild, fileToBuild.getName(), IMarker.SEVERITY_ERROR,
-                        detectLocation(errorMessage), errorMessage, null);
-            }
-
-            // only refresh on no errors
-            if (errors.size() == 0)
-            {
-                try
+            job.addJobChangeListener(new JobChangeAdapter(){
+                public void done(IJobChangeEvent event)
                 {
-                    fileToBuild.refreshLocal(IResource.DEPTH_ONE, null);
-                } catch (CoreException e)
-                {
-                    e.printStackTrace();
+                    if (Status.OK_STATUS.equals(event.getResult()))
+                    {
+                        try
+                        {
+                            fileToBuild.refreshLocal(IResource.DEPTH_ONE, null);
+                        } catch (CoreException e)
+                        {
+                            // TODO
+                            e.printStackTrace();
+                        }
+                        
+                    }
                 }
-            }
+
+            });
+
+            
+            job.schedule();
         }
         return null;
     }
 
-    private int[] detectLocation(String message)
+    
+    public ISchedulingRule getModifyRule(IResource[] resources)
     {
-        int lineStarts = message.indexOf("line ");
-        int lineEnds = message.indexOf(" , column ");
-        if (lineStarts != -1 && lineEnds != -1)
+        if (resources == null)
         {
-            String line = message.substring(lineStarts, lineEnds);
-            int lineNumber = Integer.parseInt(line);
-            return new int[] { lineNumber, -1, Integer.parseInt(line), -1 };
+            return null;
         }
-        return new int[] { -1, -1, -1, -1 };
+        ISchedulingRule combinedRule = null;
+        
+        IResourceRuleFactory ruleFactory = ResourcesPlugin.getWorkspace().getRuleFactory();
+        for (int i=0; i < resources.length; i++)
+        {
+           ISchedulingRule rule = ruleFactory.modifyRule(resources[i]); 
+           combinedRule = MultiRule.combine(rule, combinedRule);
+        }
+        return combinedRule;
     }
 
 }
