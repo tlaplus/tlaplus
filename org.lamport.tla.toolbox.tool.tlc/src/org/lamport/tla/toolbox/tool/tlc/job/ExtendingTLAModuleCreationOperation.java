a45ee311325d9fb518417531f8a04b52b0587ebc
simonzam <simonzam@76a6fc44-f60b-0410-a9a8-e67b0e8fc65c>
2009-03-05 03:27:27 +0000

    init
    git-svn-id: svn+ssh://svn.msr-inria.inria.fr/var/lib/svn/repository/tla/trunk/tla2-inria@13011 76a6fc44-f60b-0410-a9a8-e67b0e8fc65c

---
+++ org.lamport.tla.toolbox.tool.tlc/src/org/lamport/tla/toolbox/tool/tlc/job/ExtendingTLAModuleCreationOperation.java
@@ -0,0 +1,66 @@
+package org.lamport.tla.toolbox.tool.tlc.job;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import org.eclipse.core.resources.IWorkspaceRunnable;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.Status;
+import org.lamport.tla.toolbox.tool.tlc.TLCActivator;
+import org.lamport.tla.toolbox.util.ResourceHelper;
+
+/**
+ * Creates the stub for modelecking, extending the specification root module
+ * @author Simon Zambrovski
+ * @version $Id$
+ */
+public class ExtendingTLAModuleCreationOperation implements IWorkspaceRunnable
+{
+
+    private String modelName;
+    private IPath modelRootPath;
+
+    /**
+     * @param modelName 
+     * @param modelRootPath 
+     * @param name
+     */
+    public ExtendingTLAModuleCreationOperation(IPath modelRootPath, String modelName)
+    {
+        this.modelRootPath = modelRootPath;
+        this.modelName = modelName;
+    }
+
+    /* (non-Javadoc)
+     * @see org.eclipse.core.resources.IWorkspaceRunnable#run(org.eclipse.core.runtime.IProgressMonitor)
+     */
+    public void run(IProgressMonitor monitor) throws CoreException
+    {
+        String moduleFileName = modelRootPath.lastSegment();
+
+        byte[] content = ResourceHelper.getExtendingModuleContent(moduleFileName, modelName);
+        try
+        {
+            // create file
+            File file = new File(modelRootPath.toOSString());
+            if (file.createNewFile())
+            {
+                // successfully created
+                FileOutputStream fos = new FileOutputStream(file);
+                fos.write(content);
+                fos.flush();
+                fos.close();
+            } else
+            {
+                throw new RuntimeException("Error creating a file");
+            }
+        } catch (IOException e)
+        {
+            throw new CoreException( new Status(Status.ERROR, TLCActivator.PLUGIN_ID, "Error creating TLA+ file", e));
+        }
+    }
+
+}
