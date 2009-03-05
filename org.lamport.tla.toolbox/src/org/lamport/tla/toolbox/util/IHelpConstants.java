970862b0f029c2356384c58c2df2de7ccc693271
simonzam <simonzam@76a6fc44-f60b-0410-a9a8-e67b0e8fc65c>
2009-03-05 03:29:37 +0000

    addings
    git-svn-id: svn+ssh://svn.msr-inria.inria.fr/var/lib/svn/repository/tla/trunk/tla2-inria@13017 76a6fc44-f60b-0410-a9a8-e67b0e8fc65c

--- org.lamport.tla.toolbox/src/org/lamport/tla/toolbox/util/IHelpConstants.java
+++ org.lamport.tla.toolbox/src/org/lamport/tla/toolbox/util/IHelpConstants.java
@@ -11,5 +11,13 @@ public interface IHelpConstants
     public static final String PARSER_PREFERENCE_PAGE = "ParserPreferencePage";
     public static final String MODULE_PROPERTY_PAGE = "ModulePropertyPage";
     public static final String SPEC_PROPERTY_PAGE = "SpecPropertyPage";
+    
+    
+    public static final String BEHAVIOR_FORMULA_MODEL_PAGE = "TLCModelSummaryPage";
+    public static final String GENERAL_MODEL_PAGE = "";
+    public static final String LAUNCH_MODEL_PAGE = "";
+    public static final String MODEL_VALUE_MODEL_PAGE = "";
+    public static final String PARAMETERS_MODEL_PAGE = "";
+    public static final String CORRECTNESS_MODEL_PAGE = "";
 
 }
