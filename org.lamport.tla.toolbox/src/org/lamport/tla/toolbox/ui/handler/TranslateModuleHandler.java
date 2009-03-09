d1366ce5c264462c5e981aab563d69fcf0b323f2
simonzam <simonzam@76a6fc44-f60b-0410-a9a8-e67b0e8fc65c>
2009-03-09 22:01:20 +0000

    changed to blocking dialog
    git-svn-id: svn+ssh://svn.msr-inria.inria.fr/var/lib/svn/repository/tla/trunk/tla2-inria@13055 76a6fc44-f60b-0410-a9a8-e67b0e8fc65c

--- org.lamport.tla.toolbox/src/org/lamport/tla/toolbox/ui/handler/TranslateModuleHandler.java
+++ org.lamport.tla.toolbox/src/org/lamport/tla/toolbox/ui/handler/TranslateModuleHandler.java
@@ -1,5 +1,7 @@
 package org.lamport.tla.toolbox.ui.handler;
 
+import java.lang.reflect.InvocationTargetException;
+
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
@@ -8,14 +10,15 @@ import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.IResourceRuleFactory;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
-import org.eclipse.core.runtime.Status;
-import org.eclipse.core.runtime.jobs.IJobChangeEvent;
 import org.eclipse.core.runtime.jobs.ISchedulingRule;
-import org.eclipse.core.runtime.jobs.JobChangeAdapter;
 import org.eclipse.core.runtime.jobs.MultiRule;
+import org.eclipse.jface.dialogs.ProgressMonitorDialog;
+import org.eclipse.jface.operation.IRunnableWithProgress;
+import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.IEditorInput;
 import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.IFileEditorInput;
+import org.eclipse.ui.IWorkbenchWindow;
 import org.lamport.tla.toolbox.job.TranslatorJob;
 import org.lamport.tla.toolbox.util.UIHelper;
 
@@ -42,11 +45,34 @@ public class TranslateModuleHandler extends AbstractHandler implements IHandler
         if (editorInput instanceof IFileEditorInput)
         {
             final IResource fileToBuild = ((IFileEditorInput) editorInput).getFile();
+
+            IRunnableWithProgress translatorOperation = TranslatorJob.getAsRunnableWithProgress(fileToBuild);
+            IWorkbenchWindow window = UIHelper.getActiveWindow();
+            Shell shell = window != null ? window.getShell() : null;
+            try
+            {
+                new ProgressMonitorDialog(shell).run(true, false, translatorOperation);
+                fileToBuild.refreshLocal(IResource.DEPTH_ONE, null);
+                
+            } catch (InvocationTargetException e)
+            {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            } catch (InterruptedException e)
+            {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            } catch (CoreException e)
+            {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+
+/*
             TranslatorJob job = new TranslatorJob(fileToBuild);
             job.setUser(true);
             // TODO config file is also changed
             job.setRule(getModifyRule(new IResource[]{fileToBuild}));
-            
             job.addJobChangeListener(new JobChangeAdapter(){
                 public void done(IJobChangeEvent event)
                 {
@@ -57,17 +83,15 @@ public class TranslateModuleHandler extends AbstractHandler implements IHandler
                             fileToBuild.refreshLocal(IResource.DEPTH_ONE, null);
                         } catch (CoreException e)
                         {
-                            // TODO
                             e.printStackTrace();
                         }
-                        
                     }
                 }
-
             });
-
-            
             job.schedule();
+*/        
+            
+        
         }
         return null;
     }
