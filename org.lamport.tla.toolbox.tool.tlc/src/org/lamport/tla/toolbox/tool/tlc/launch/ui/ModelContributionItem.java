ffb32cc10554f4c0fb8a423ccdbc93d15968578d
simonzam <simonzam@76a6fc44-f60b-0410-a9a8-e67b0e8fc65c>
2009-04-04 03:53:34 +0000

    scoped launches to the selected spec
    git-svn-id: svn+ssh://svn.msr-inria.inria.fr/var/lib/svn/repository/tla/trunk/tla2-inria@13118 76a6fc44-f60b-0410-a9a8-e67b0e8fc65c

--- org.lamport.tla.toolbox.tool.tlc/src/org/lamport/tla/toolbox/tool/tlc/launch/ui/ModelContributionItem.java
+++ org.lamport.tla.toolbox.tool.tlc/src/org/lamport/tla/toolbox/tool/tlc/launch/ui/ModelContributionItem.java
@@ -3,6 +3,7 @@ package org.lamport.tla.toolbox.tool.tlc.launch.ui;
 import java.util.HashMap;
 import java.util.Vector;
 
+import org.eclipse.core.resources.IProject;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.debug.core.DebugPlugin;
 import org.eclipse.debug.core.ILaunchConfiguration;
@@ -13,6 +14,7 @@ import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.ui.actions.CompoundContributionItem;
 import org.eclipse.ui.menus.CommandContributionItem;
 import org.eclipse.ui.menus.CommandContributionItemParameter;
+import org.lamport.tla.toolbox.tool.ToolboxHandle;
 import org.lamport.tla.toolbox.tool.tlc.TLCActivator;
 import org.lamport.tla.toolbox.tool.tlc.handlers.OpenModelHandler;
 import org.lamport.tla.toolbox.tool.tlc.launch.TLCModelLaunchDelegate;
@@ -26,7 +28,7 @@ import org.lamport.tla.toolbox.util.UIHelper;
 public class ModelContributionItem extends CompoundContributionItem
 {
     private ImageDescriptor modelIcon = TLCActivator.getImageDescriptor("icons/full/choice_sc_obj.gif");
-    
+
     /**
      * @see org.eclipse.ui.actions.CompoundContributionItem#getContributionItems()
      */
@@ -39,35 +41,41 @@ public class ModelContributionItem extends CompoundContributionItem
 
         Vector modelContributions = new Vector();
 
+        IProject specProject = ToolboxHandle.getCurrentSpec().getProject();
         try
         {
-            ILaunchConfiguration[] launchConfigurations = launchManager.getLaunchConfigurations(launchConfigurationType);
+            ILaunchConfiguration[] launchConfigurations = launchManager
+                    .getLaunchConfigurations(launchConfigurationType);
             for (int i = 0; i < launchConfigurations.length; i++)
             {
                 String modelName = launchConfigurations[i].getName();
-                
+
+                // skip launches from other specs
+                if (!specProject.equals(launchConfigurations[i].getFile().getProject()))
+                {
+                    continue;
+                }
+
                 HashMap parameters = new HashMap();
-                
-                
+
                 // fill the model name for the handler
                 parameters.put(OpenModelHandler.PARAM_MODEL_NAME, modelName);
 
                 // create the contribution item
-                CommandContributionItemParameter param = new CommandContributionItemParameter(UIHelper.getActiveWindow(),
-                        "toolbox.command.model.open." + modelName, OpenModelHandler.COMMAND_ID, parameters, modelIcon, null, null,
-                        modelName, null, "Opens " + modelName, CommandContributionItem.STYLE_PUSH, null,
-                        true);
-                
+                CommandContributionItemParameter param = new CommandContributionItemParameter(UIHelper
+                        .getActiveWindow(), "toolbox.command.model.open." + modelName, OpenModelHandler.COMMAND_ID,
+                        parameters, modelIcon, null, null, modelName, null, "Opens " + modelName,
+                        CommandContributionItem.STYLE_PUSH, null, true);
+
                 // add contribution item to the list
                 modelContributions.add(new CommandContributionItem(param));
             }
-            
-        } catch (CoreException e) {
-            
+
+        } catch (CoreException e)
+        {
+
         }
         return (IContributionItem[]) modelContributions.toArray(new IContributionItem[modelContributions.size()]);
     }
-
-
 
 }
