22362952bd16af68784eb41ecef89d3eecf428af
simonzam <simonzam@76a6fc44-f60b-0410-a9a8-e67b0e8fc65c>
2009-04-17 18:42:39 +0000

    git-svn-id: svn+ssh://svn.msr-inria.inria.fr/var/lib/svn/repository/tla/trunk/tla2-inria@13180 76a6fc44-f60b-0410-a9a8-e67b0e8fc65c

--- tlatools/test/tlc2/TestDriver2.java
+++ tlatools/test/tlc2/TestDriver2.java
@@ -13,6 +13,7 @@ public class TestDriver2
      * @version $Id: TLCJob.java 638 2009-04-10 04:08:14Z simonzam $
      */
     private static final long TIMEOUT = 1000 * 5;
+    private static final int REPEATS = 10;
 
     private String rootModule;
     private String cfgFile;
@@ -29,12 +30,12 @@ public class TestDriver2
         if (args.length < 6 ) 
         {
             // -workers 2 -config AtomicBakery_MC_1.cfg -metadir C:\org.zambrovski\download\AtomicBakery_MC_1.toolbox AtomicBakery_MC_1
-            System.out.println("Call with: -workers 2 -config AtomicBakery_MC_1.cfg -metadir C:\\org.zambrovski\\download\\AtomicBakery_MC_1.toolbox AtomicBakery_MC_1");
+            System.out.println("Call with: -workers <num> -config <name-of-config.cfg> -metadir <dir-to-put-states> <name-of-module>");
         }
         TestDriver2 testDriver2 = new TestDriver2(args[6], args[3], args[5]);
         testDriver2.setWorkers(Integer.parseInt(args[1]));
 
-        for (int i = 0 ; i < 10; i++)
+        for (int i = 0 ; i < REPEATS; i++)
         {
             testDriver2.reported = 0;
             testDriver2.run();
@@ -69,7 +70,7 @@ public class TestDriver2
     {
         report("entering run");
 
-        // setup tool io
+        // setup tool IO
         // Reset the tool output messages.
         ToolIO.reset();
         ToolIO.setMode(ToolIO.TOOL);
@@ -78,18 +79,25 @@ public class TestDriver2
         // create a TLC instance
         TLC tlc = new TLC();
         report("tlc created " + tlc.toString());
-        // setup name resolver
         
-        //tlc.setResolver(new RCPNameToFileIStream(null));
+        // setup name resolver
+        // in RCP FS model use:
+        // tlc.setResolver(new RCPNameToFileIStream(null));
+
+        // for simple FS model:
         tlc.setResolver(new SimpleFilenameToStream());
+
         // setup SpecObj from parser
         // SpecObj specObj = ToolboxHandle.getSpecObj();
         // tlc.setSpecObject(specObj);
 
         // handle parameters
-        String[] params = new String[] { "-config", cfgFile,
-        // "-coverage", "0.1",
-                "-workers", "" + workers, "-metadir", projectDir, rootModule };
+        String[] params = new String[] { 
+                "-config", cfgFile,
+                // "-coverage", "0.1",
+                "-workers", "" + workers, 
+                "-metadir", projectDir, 
+                rootModule };
         boolean status = tlc.handleParameters(params);
 
         // report errors in parameters
@@ -110,9 +118,8 @@ public class TestDriver2
         while (this.checkAndSleep())
         {
             // report the messages created since last reporting
-
             // check the cancellation status
-            if (false)
+            if (isCancelled())
             {
                 // cancel the TLC
                 tlc.setCanceledFlag(true);
@@ -131,13 +138,22 @@ public class TestDriver2
         return 1;
     }
 
-    // decrement the number and sleep
+    /**
+     * Method for external cancellation
+     */
+    private boolean isCancelled()
+    {
+        return false;
+    }
+    
+    
+    // check if TLC is ready and sleep
     private boolean checkAndSleep()
     {
         report("entering checkAndSleep()");
         try
         {
-            report("Sleep \t" + System.currentTimeMillis());
+            report("Go sleep \t" + System.currentTimeMillis());
             // go sleep
             Thread.sleep(TIMEOUT);
 
@@ -149,22 +165,18 @@ public class TestDriver2
             e.printStackTrace();
         }
 
-        boolean result = tlcThread.isRunning();
-
         // return true if the tlc is still calculating
+        boolean result = tlcThread.isRunning();
         report("leaving checkAndSleep() with " + result);
         return result;
     }
 
     /**
      * Report progress to the monitor 
-     * @param monitor
-     * TODO 
      */
     protected void reportProgress()
     {
         // report progress
-
         String[] messages = ToolIO.getAllMessages();
         for (; reported < messages.length; reported++)
         {
@@ -189,7 +201,7 @@ public class TestDriver2
         {
             synchronized (this)
             {
-                report("TLC Thread: ------------ {START}");
+                report("TLC Thread: {STARTED}");
                 isRunning = true;
             }
             // start TLC
@@ -198,7 +210,7 @@ public class TestDriver2
             synchronized (this)
             {
                 isRunning = false;
-                report("TLC Thread: ------------ {FINISHED}");
+                report("TLC Thread: {FINISHED}");
             }
         }
 
