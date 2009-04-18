11e98c6da2f7b2e479953231e74146abe69194b7
simonzam <simonzam@76a6fc44-f60b-0410-a9a8-e67b0e8fc65c>
2009-04-18 01:51:07 +0000

    console place-holder added
    git-svn-id: svn+ssh://svn.msr-inria.inria.fr/var/lib/svn/repository/tla/trunk/tla2-inria@13194 76a6fc44-f60b-0410-a9a8-e67b0e8fc65c

--- org.lamport.tla.toolbox/src/org/lamport/tla/toolbox/ui/perspective/SpecLoadedPerspective.java
+++ org.lamport.tla.toolbox/src/org/lamport/tla/toolbox/ui/perspective/SpecLoadedPerspective.java
@@ -13,13 +13,17 @@ public class SpecLoadedPerspective implements IPerspectiveFactory
 {
 
     public final static String ID = "org.lamport.tla.toolbox.ui.perspective.specLoaded";
+    public final static String CONSOLE_VIEW_ID = "org.eclipse.ui.console.ConsoleView";
 
     public void createInitialLayout(IPageLayout layout)
     {
         layout.setEditorAreaVisible(true);
         String editorArea = layout.getEditorArea();
         
-        layout.addPlaceholder(ProblemView.ID, IPageLayout.LEFT, 0.75f, editorArea);
-        
+        layout.addPlaceholder(ProblemView.ID, IPageLayout.RIGHT, 0.75f, editorArea);
+
+        // even if there is no dependency to the console plugin, we specify the possible location of the console
+        // this might be useful for tools
+        layout.addPlaceholder(CONSOLE_VIEW_ID, IPageLayout.BOTTOM, 0.75f, editorArea);
     }
 }
