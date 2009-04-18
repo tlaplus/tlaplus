42df936982767594bca4e7a44c518240ce50feed
simonzam <simonzam@76a6fc44-f60b-0410-a9a8-e67b0e8fc65c>
2009-04-18 01:52:43 +0000

    methods moved to ResourceHelper
    git-svn-id: svn+ssh://svn.msr-inria.inria.fr/var/lib/svn/repository/tla/trunk/tla2-inria@13198 76a6fc44-f60b-0410-a9a8-e67b0e8fc65c

--- org.lamport.tla.toolbox/src/org/lamport/tla/toolbox/job/DeleteOutOfSyncJob.java
+++ org.lamport.tla.toolbox/src/org/lamport/tla/toolbox/job/DeleteOutOfSyncJob.java
@@ -3,16 +3,11 @@ package org.lamport.tla.toolbox.job;
 import java.util.List;
 
 import org.eclipse.core.resources.IFile;
-import org.eclipse.core.resources.IResource;
-import org.eclipse.core.resources.IResourceRuleFactory;
-import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.resources.WorkspaceJob;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
-import org.eclipse.core.runtime.jobs.ISchedulingRule;
-import org.eclipse.core.runtime.jobs.MultiRule;
 
 /**
  * Delete files out of sync
@@ -43,20 +38,4 @@ public class DeleteOutOfSyncJob extends WorkspaceJob
         }
         return Status.OK_STATUS;
     }
-    
-    
-    public static ISchedulingRule getDeleteFilesRule(List files)
-    {
-        ISchedulingRule combinedRule = null;
-        IResourceRuleFactory ruleFactory = ResourcesPlugin.getWorkspace().getRuleFactory();
-
-        for (int i = 0; i < files.size(); i++)
-        {
-            ISchedulingRule rule = ruleFactory.deleteRule((IResource) files.get(i));
-            combinedRule = MultiRule.combine(rule, combinedRule);
-        }
-        return combinedRule;
-    }
-    
-
 }
