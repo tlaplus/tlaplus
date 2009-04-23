b3f340796f331da1e9c0fc5c8ea49964eca72bd0
simonzam <simonzam@76a6fc44-f60b-0410-a9a8-e67b0e8fc65c>
2009-04-23 06:06:31 +0000

    help methods
    git-svn-id: svn+ssh://svn.msr-inria.inria.fr/var/lib/svn/repository/tla/trunk/tla2-inria@13243 76a6fc44-f60b-0410-a9a8-e67b0e8fc65c

--- org.lamport.tla.toolbox/src/org/lamport/tla/toolbox/tool/ToolboxHandle.java
+++ org.lamport.tla.toolbox/src/org/lamport/tla/toolbox/tool/ToolboxHandle.java
@@ -1,14 +1,21 @@
 package org.lamport.tla.toolbox.tool;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+
 import org.eclipse.core.resources.IFile;
+import org.eclipse.core.runtime.FileLocator;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.Path;
 import org.eclipse.jface.preference.IPreferenceStore;
 import org.lamport.tla.toolbox.Activator;
 import org.lamport.tla.toolbox.spec.Spec;
 import org.lamport.tla.toolbox.util.pref.IPreferenceConstants;
 import org.lamport.tla.toolbox.util.pref.PreferenceStoreHelper;
+import org.osgi.framework.Bundle;
 
 import tla2sany.modanalyzer.SpecObj;
-import util.UniqueString;
 
 /**
  * Provides shortcuts to the internal toolbox methods, that should be made accessible to the other tools
@@ -18,14 +25,14 @@ import util.UniqueString;
 public class ToolboxHandle
 {
     public static String I_RESTORE_LAST_SPEC = IPreferenceConstants.I_RESTORE_LAST_SPEC;
-    
-    
+
     /**
      * Retrieves the root file of the loaded spec or <code>null</code> if no spec selected
      */
     public static IFile getRootModule()
     {
-        return ((Activator.getSpecManager().getSpecLoaded() == null) ? null : Activator.getSpecManager().getSpecLoaded().getRootFile()); 
+        return ((Activator.getSpecManager().getSpecLoaded() == null) ? null : Activator.getSpecManager()
+                .getSpecLoaded().getRootFile());
     }
 
     /**
@@ -35,7 +42,7 @@ public class ToolboxHandle
     {
         return PreferenceStoreHelper.getInstancePreferenceStore();
     }
-    
+
     /**
      * Returns the SpecObj of the current loaded specification
      * @return SpecObj produced by the previous SANY run, or null
@@ -43,21 +50,20 @@ public class ToolboxHandle
     public static SpecObj getSpecObj()
     {
         Spec spec = null;
-        try 
+        try
         {
             spec = Activator.getSpecManager().getSpecLoaded();
-        } catch (IllegalStateException e) 
+        } catch (IllegalStateException e)
         {
             // this happens is the workspace is closed
             // just return null for e.G. JUnit tests
         }
-        if (spec != null) 
+        if (spec != null)
         {
             return spec.getValidRootModule();
         }
         return null;
     }
-
 
     /**
      * @return
@@ -73,12 +79,60 @@ public class ToolboxHandle
      */
     public static boolean isUserModule(String name)
     {
-        if (name == null || name.isEmpty()) 
+        if (name == null || name.isEmpty())
         {
             return false;
         }
         return Activator.getModuleDependencyStorage().hasModule(name);
     }
+
+    /**
+     * @return
+     */
+    public static IPath getTLAToolsClasspath()
+    {
+        IPath bundleBase = getBundleLocation();
+        
+        if (bundleBase != null)
+        {
+            IPath tlaToolsLocation = bundleBase.append("tla2tools.jar"); //$NON-NLS-1$
+            return tlaToolsLocation;
+        }
+        return null;
+    }
+
+    /**
+     * 
+     */
+    public static IPath getModulesClasspath()
+    {
+        IPath bundleBase = getBundleLocation();
+        
+        if (bundleBase != null)
+        {
+            IPath tlaToolsLocation = bundleBase.append("StandardModules/"); //$NON-NLS-1$
+            return tlaToolsLocation;
+        }
+        return null;
+    }
+
     
-    
+    public static IPath getBundleLocation()
+    {
+        Bundle bundle = Activator.getDefault().getBundle();
+        if (bundle == null)
+            return null;
+
+        URL local = null;
+        try
+        {
+            local = FileLocator.toFileURL(bundle.getEntry("/")); //$NON-NLS-1$
+        } catch (IOException e)
+        {
+            return null;
+        }
+        String fullPath = new File(local.getPath()).getAbsolutePath();
+        return Path.fromOSString(fullPath);
+    }
+
 }
