ba27f8a4ad5a323714187bc357ac3137806df2a3
simonzam <simonzam@76a6fc44-f60b-0410-a9a8-e67b0e8fc65c>
2009-04-30 22:59:47 +0000

    constant
    git-svn-id: svn+ssh://svn.msr-inria.inria.fr/var/lib/svn/repository/tla/trunk/tla2-inria@13253 76a6fc44-f60b-0410-a9a8-e67b0e8fc65c

--- org.lamport.tla.toolbox/src/org/lamport/tla/toolbox/ui/provider/SpecExplorer.java
+++ org.lamport.tla.toolbox/src/org/lamport/tla/toolbox/ui/provider/SpecExplorer.java
@@ -11,7 +11,11 @@ import org.lamport.tla.toolbox.Activator;
  */
 public class SpecExplorer extends CommonNavigator
 {
+    public final static String VIEW_ID = "toolbox.view.SpecView";
 
+    /**
+     * Override the method to deliver the root object for the NCE activation
+     */
     protected IAdaptable getInitialInput()
     {
         return Activator.getSpecManager();
