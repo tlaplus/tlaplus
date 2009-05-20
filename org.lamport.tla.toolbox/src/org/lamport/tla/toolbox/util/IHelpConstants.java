1188ae28f3a9a831b4977e75dffcdcd99bb92d59
simonzam <simonzam@76a6fc44-f60b-0410-a9a8-e67b0e8fc65c>
2009-05-20 15:28:40 +0000

    constant added
    git-svn-id: svn+ssh://svn.msr-inria.inria.fr/var/lib/svn/repository/tla/trunk/tla2-inria@13305 76a6fc44-f60b-0410-a9a8-e67b0e8fc65c

--- org.lamport.tla.toolbox/src/org/lamport/tla/toolbox/util/IHelpConstants.java
+++ org.lamport.tla.toolbox/src/org/lamport/tla/toolbox/util/IHelpConstants.java
@@ -9,7 +9,8 @@ public interface IHelpConstants
 {
 
     public static final String PARSER_PREFERENCE_PAGE = "ParserPreferencePage";
-    public static final String MODULE_PROPERTY_PAGE = "ModulePropertyPage";
+    public static final String GENERAL_PREFERENCE_PAGE = "GeneralPreferencePage";
+    // public static final String MODULE_PROPERTY_PAGE = "ModulePropertyPage";
     public static final String SPEC_PROPERTY_PAGE = "SpecPropertyPage";
 
     public static final String BEHAVIOR_FORMULA_MODEL_PAGE = "TLCModelSummaryPage";
@@ -17,10 +18,10 @@ public interface IHelpConstants
     public static final String MAIN_MODEL_PAGE = "mainModelPage";
     public static final String ADVANCED_MODEL_PAGE = "advancedModelPage";
 
-    public static final String GENERAL_MODEL_PAGE = "";
-    public static final String LAUNCH_MODEL_PAGE = "";
-    public static final String MODEL_VALUE_MODEL_PAGE = "";
-    public static final String PARAMETERS_MODEL_PAGE = "";
-    public static final String CORRECTNESS_MODEL_PAGE = "";
+    // public static final String GENERAL_MODEL_PAGE = "";
+    // public static final String LAUNCH_MODEL_PAGE = "";
+    // public static final String MODEL_VALUE_MODEL_PAGE = "";
+    // public static final String PARAMETERS_MODEL_PAGE = "";
+    // public static final String CORRECTNESS_MODEL_PAGE = "";
 
 }
