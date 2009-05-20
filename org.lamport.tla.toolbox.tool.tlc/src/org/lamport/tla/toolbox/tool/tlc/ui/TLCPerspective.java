4eb57d3c012396ff742d128a37a8f8c6c31621c9
simonzam <simonzam@76a6fc44-f60b-0410-a9a8-e67b0e8fc65c>
2009-05-20 15:51:56 +0000

    switched to declarative implementation
    git-svn-id: svn+ssh://svn.msr-inria.inria.fr/var/lib/svn/repository/tla/trunk/tla2-inria@13319 76a6fc44-f60b-0410-a9a8-e67b0e8fc65c

--- org.lamport.tla.toolbox.tool.tlc/src/org/lamport/tla/toolbox/tool/tlc/ui/TLCPerspective.java
+++ org.lamport.tla.toolbox.tool.tlc/src/org/lamport/tla/toolbox/tool/tlc/ui/TLCPerspective.java
@@ -2,10 +2,6 @@ package org.lamport.tla.toolbox.tool.tlc.ui;
 
 import org.eclipse.ui.IPageLayout;
 import org.eclipse.ui.IPerspectiveFactory;
-import org.eclipse.ui.console.IConsoleConstants;
-import org.lamport.tla.toolbox.tool.tlc.launch.ui.ModelExplorer;
-import org.lamport.tla.toolbox.ui.provider.SpecExplorer;
-import org.lamport.tla.toolbox.ui.view.ProblemView;
 
 /**
  * Perspective for TLC tool
@@ -22,17 +18,5 @@ public class TLCPerspective implements IPerspectiveFactory
 
     public void createInitialLayout(IPageLayout layout)
     {
-        layout.setEditorAreaVisible(true);
-        String editorArea = layout.getEditorArea();
-        
-        layout.addPlaceholder(ProblemView.ID, IPageLayout.RIGHT, 0.75f, editorArea);
-        
-        layout.addFastView(SpecExplorer.VIEW_ID, 0.25f);
-        layout.getViewLayout(SpecExplorer.VIEW_ID).setCloseable(false);
-
-        layout.addFastView(ModelExplorer.VIEW_ID, 0.25f);
-        layout.getViewLayout(ModelExplorer.VIEW_ID).setCloseable(false);
-        
-        layout.addPlaceholder(IConsoleConstants.ID_CONSOLE_VIEW, IPageLayout.BOTTOM, 0.75f, editorArea);
     }
 }
