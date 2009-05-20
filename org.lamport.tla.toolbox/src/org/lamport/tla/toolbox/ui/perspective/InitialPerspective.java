75a8df7c47241e19997aa8181fd98546c3a51f41
simonzam <simonzam@76a6fc44-f60b-0410-a9a8-e67b0e8fc65c>
2009-05-20 15:26:11 +0000

    replaced with declarative perspectiveExtension in plugin.xml
    git-svn-id: svn+ssh://svn.msr-inria.inria.fr/var/lib/svn/repository/tla/trunk/tla2-inria@13301 76a6fc44-f60b-0410-a9a8-e67b0e8fc65c

--- org.lamport.tla.toolbox/src/org/lamport/tla/toolbox/ui/perspective/InitialPerspective.java
+++ org.lamport.tla.toolbox/src/org/lamport/tla/toolbox/ui/perspective/InitialPerspective.java
@@ -2,30 +2,19 @@ package org.lamport.tla.toolbox.ui.perspective;
 
 import org.eclipse.ui.IPageLayout;
 import org.eclipse.ui.IPerspectiveFactory;
-import org.lamport.tla.toolbox.ui.provider.SpecExplorer;
 
 /**
  * The initial perspective with actions for the spec manager and the welcome view
  * 
- * @author zambrovski
+ * @author Simon Zambrovski
+ * @version $Id$
  */
 public class InitialPerspective implements IPerspectiveFactory
 {
 
-    /**
-     * TODO refactor
-     */
-    public static final String INTRO_VIEW_ID = "org.eclipse.ui.internal.introview";
     public static final String ID = "org.lamport.tla.toolbox.ui.perspective.initial";
 
     public void createInitialLayout(IPageLayout layout)
     {
-        String editorArea = layout.getEditorArea();
-        layout.setEditorAreaVisible(false);
-        layout.addStandaloneView(INTRO_VIEW_ID, false, IPageLayout.LEFT, 0.9f, editorArea);
-        layout.getViewLayout(INTRO_VIEW_ID).setCloseable(false);
-        
-        layout.addFastView(SpecExplorer.VIEW_ID, 0.25f);
-        layout.getViewLayout(SpecExplorer.VIEW_ID).setCloseable(false);
     }
 }
--- org.lamport.tla.toolbox/src/org/lamport/tla/toolbox/ui/perspective/SpecLoadedPerspective.java
+++ org.lamport.tla.toolbox/src/org/lamport/tla/toolbox/ui/perspective/SpecLoadedPerspective.java
@@ -2,8 +2,6 @@ package org.lamport.tla.toolbox.ui.perspective;
 
 import org.eclipse.ui.IPageLayout;
 import org.eclipse.ui.IPerspectiveFactory;
-import org.lamport.tla.toolbox.ui.provider.SpecExplorer;
-import org.lamport.tla.toolbox.ui.view.ProblemView;
 
 /**
  * Spec loaded perspective containing the editor and spec actions
@@ -15,20 +13,7 @@ public class SpecLoadedPerspective implements IPerspectiveFactory
 
     public final static String ID = "org.lamport.tla.toolbox.ui.perspective.specLoaded";
 
-    // move
-    // public final static String CONSOLE_VIEW_ID = "org.eclipse.ui.console.ConsoleView";
-    
-
     public void createInitialLayout(IPageLayout layout)
     {
-        layout.setEditorAreaVisible(true);
-        String editorArea = layout.getEditorArea();
-        
-        layout.addPlaceholder(ProblemView.ID, IPageLayout.RIGHT, 0.75f, editorArea);
-        // even if there is no dependency to the console plugin, we specify the possible location of the console
-        // this might be useful for tools
-        // layout.addPlaceholder(CONSOLE_VIEW_ID, IPageLayout.BOTTOM, 0.75f, editorArea);
-        layout.addFastView(SpecExplorer.VIEW_ID, 0.25f);
-        layout.getViewLayout(SpecExplorer.VIEW_ID).setCloseable(false);
     }
 }
