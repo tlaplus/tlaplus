403ceb6551bee775679900112ef60ca6465bd20e
simonzam <simonzam@76a6fc44-f60b-0410-a9a8-e67b0e8fc65c>
2009-05-20 15:28:23 +0000

    property changed
    git-svn-id: svn+ssh://svn.msr-inria.inria.fr/var/lib/svn/repository/tla/trunk/tla2-inria@13304 76a6fc44-f60b-0410-a9a8-e67b0e8fc65c

--- org.lamport.tla.toolbox/src/org/lamport/tla/toolbox/ui/property/SpecPropertyPage.java
+++ org.lamport.tla.toolbox/src/org/lamport/tla/toolbox/ui/property/SpecPropertyPage.java
@@ -17,20 +17,30 @@ import org.lamport.tla.toolbox.util.pref.PreferenceStoreHelper;
  */
 public class SpecPropertyPage extends GenericFieldEditorPropertyPage
 {
-
+    private StringFieldEditor pcalParamEditor;
     
     protected Control createContents(Composite parent)
     {
         Control control = super.createContents(parent);
+        
+        // ensure the page has no special buttons
+        noDefaultAndApplyButton();
+        
         UIHelper.setHelp(control, IHelpConstants.SPEC_PROPERTY_PAGE);
         return control;
     }
 
     public void createFieldEditors(Composite composite)
     {
+        // TODO change root file
         StringFieldEditor rootFileEditor = new StringFieldEditor(IPreferenceConstants.P_PROJECT_ROOT_FILE,
                 "Specification root module", composite);
         addEditor(rootFileEditor);
+        rootFileEditor.getTextControl(composite).setEditable(false);
+        
+        pcalParamEditor = new StringFieldEditor(IPreferenceConstants.PCAL_CAL_PARAMS, "PCal call arguments", composite);
+        addEditor(pcalParamEditor);
+        
     }
    
     
