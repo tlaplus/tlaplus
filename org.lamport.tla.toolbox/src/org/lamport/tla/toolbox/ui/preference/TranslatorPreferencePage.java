52eee814a0a1b17e6eab45e51cd6186bb7b7190d
simonzam <simonzam@76a6fc44-f60b-0410-a9a8-e67b0e8fc65c>
2009-06-10 02:00:34 +0000

    init
    git-svn-id: svn+ssh://svn.msr-inria.inria.fr/var/lib/svn/repository/tla/trunk/tla2-inria@13333 76a6fc44-f60b-0410-a9a8-e67b0e8fc65c

---
+++ org.lamport.tla.toolbox/src/org/lamport/tla/toolbox/ui/preference/TranslatorPreferencePage.java
@@ -0,0 +1,59 @@
+package org.lamport.tla.toolbox.ui.preference;
+
+import org.eclipse.jface.preference.BooleanFieldEditor;
+import org.eclipse.jface.preference.FieldEditorPreferencePage;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.IWorkbenchPreferencePage;
+import org.lamport.tla.toolbox.util.IHelpConstants;
+import org.lamport.tla.toolbox.util.UIHelper;
+import org.lamport.tla.toolbox.util.pref.IPreferenceConstants;
+import org.lamport.tla.toolbox.util.pref.PreferenceStoreHelper;
+
+public class TranslatorPreferencePage extends FieldEditorPreferencePage implements IWorkbenchPreferencePage
+{
+    
+    /**
+     * Constructor
+     */
+    public TranslatorPreferencePage()
+    {
+        super(GRID);
+        setPreferenceStore(PreferenceStoreHelper.getInstancePreferenceStore());
+        setDescription("PCal Translator preferences");
+    }
+
+    protected Control createContents(Composite parent)
+    {
+        Control pageControl = super.createContents(parent);
+        UIHelper.setHelp(pageControl, IHelpConstants.TRANSLATOR_PREFERENCE_PAGE);
+        return pageControl;
+    }
+
+    /**
+     * Create field editors
+     */
+    protected void createFieldEditors()
+    {
+
+        addField(new BooleanFieldEditor(IPreferenceConstants.P_TRANSLATE_POPUP_ERRORS,
+                "&Popup problem window on translator errors", getFieldEditorParent()));
+
+        addField(new BooleanFieldEditor(IPreferenceConstants.I_TRANSLATE_MODULE_ON_MODIFY,
+                "&Automatic re-translate module", getFieldEditorParent()));
+    }
+
+    public void init(IWorkbench workbench)
+    {
+
+    }
+    
+    
+    protected void initialize()
+    {
+        // sync the auto-build
+        // getPreferenceStore().setValue(IPreferenceConstants.I_PARSE_MODULE_ON_MODIFY, ResourcesPlugin.getWorkspace().isAutoBuilding());
+        super.initialize();
+    }
+}
