46bd1c43d2b3a3950ea1afd35909111c1a2ba51b
simonzam <simonzam@76a6fc44-f60b-0410-a9a8-e67b0e8fc65c>
2009-07-01 05:47:54 +0000

    removed strings and comments, since they are partitions
    git-svn-id: svn+ssh://svn.msr-inria.inria.fr/var/lib/svn/repository/tla/trunk/tla2-inria@13370 76a6fc44-f60b-0410-a9a8-e67b0e8fc65c

--- org.lamport.tla.toolbox.editor.basic/src/org/lamport/tla/toolbox/editor/basic/tla/TLACodeScanner.java
+++ org.lamport.tla.toolbox.editor.basic/src/org/lamport/tla/toolbox/editor/basic/tla/TLACodeScanner.java
@@ -4,13 +4,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.eclipse.jface.text.TextAttribute;
-import org.eclipse.jface.text.rules.EndOfLineRule;
 import org.eclipse.jface.text.rules.IRule;
 import org.eclipse.jface.text.rules.IToken;
 import org.eclipse.jface.text.rules.RuleBasedScanner;
-import org.eclipse.jface.text.rules.SingleLineRule;
 import org.eclipse.jface.text.rules.Token;
-import org.eclipse.jface.text.rules.WhitespaceRule;
 import org.eclipse.jface.text.rules.WordRule;
 import org.eclipse.swt.SWT;
 import org.lamport.tla.toolbox.editor.basic.TLAColorProvider;
@@ -25,15 +22,6 @@ import org.lamport.tla.toolbox.editor.basic.util.DocumentHelper;
 public class TLACodeScanner extends RuleBasedScanner
 {
     /**
-     * String quotes
-     */
-    public static final String STRING_QUOTES = "\"";
-    /**
-     * Single line comment
-     */
-    public static final String COMMENT_START_SEQUENCE = "\\*";
-
-    /**
      * Construct the rules
      */
     public TLACodeScanner()
@@ -41,20 +29,13 @@ public class TLACodeScanner extends RuleBasedScanner
         TLAColorProvider provider = TLAEditorActivator.getDefault().getTLAColorProvider();
 
         IToken keyword = new Token(new TextAttribute(provider.getColor(TLAColorProvider.TLA_KEYWORD), null, SWT.BOLD));
-        IToken comment = new Token(new TextAttribute(provider.getColor(TLAColorProvider.COMMENT)));
         IToken value = new Token(new TextAttribute(provider.getColor(TLAColorProvider.TLA_VALUE)));
         IToken other = new Token(new TextAttribute(provider.getColor(TLAColorProvider.TLA_DEFAULT)));
 
         List rules = new ArrayList();
 
-        // Add rule for single line comments.
-        rules.add(new EndOfLineRule(COMMENT_START_SEQUENCE, comment)); //$NON-NLS-1$
-
-        // Add rule for strings.
-        rules.add(new SingleLineRule(STRING_QUOTES, STRING_QUOTES, value, '\\')); //$NON-NLS-2$ //$NON-NLS-1$
-
         // Add generic whitespace rule.
-        rules.add(new WhitespaceRule(DocumentHelper.getDefaultWhitespaceDetector()));
+        // rules.add(new WhitespaceRule(DocumentHelper.getDefaultWhitespaceDetector()));
 
         // Add word rule for standard words
         WordRule wordRule = new WordRule(DocumentHelper.getDefaultWordDetector(), other);
