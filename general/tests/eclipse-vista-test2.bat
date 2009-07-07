41f6b11bdf10c95a5a40681f152c664ca58b4fb3
lamport <lamport@76a6fc44-f60b-0410-a9a8-e67b0e8fc65c>
2009-07-07 17:58:24 +0000

    added scripts
    git-svn-id: svn+ssh://svn.msr-inria.inria.fr/var/lib/svn/repository/tla/trunk/tla2-inria@13397 76a6fc44-f60b-0410-a9a8-e67b0e8fc65c

---
+++ general/tests/eclipse-vista-test2.bat
@@ -0,0 +1,80 @@
+echo "STARTING"
+java -cp "c:/lamport/tla/tla2/tlatools/class"  -mx100m tlc2.TLC -cleanup  test1
+java -cp "c:/lamport/tla/tla2/tlatools/class"  -mx100m tlc2.TLC -cleanup  test2
+java -cp "c:/lamport/tla/tla2/tlatools/class"  -mx100m tlc2.TLC -cleanup  test3
+java -cp "c:/lamport/tla/tla2/tlatools/class"  -mx100m tlc2.TLC -cleanup  test4
+java -cp "c:/lamport/tla/tla2/tlatools/class"  -mx100m tlc2.TLC -cleanup  test5
+java -cp "c:/lamport/tla/tla2/tlatools/class"  -mx100m tlc2.TLC -cleanup  test6
+java -cp "c:/lamport/tla/tla2/tlatools/class"  -mx100m tlc2.TLC -cleanup  test7
+java -cp "c:/lamport/tla/tla2/tlatools/class"  -mx100m tlc2.TLC -cleanup  test8
+java -cp "c:/lamport/tla/tla2/tlatools/class"  -mx100m tlc2.TLC -cleanup  test9
+java -cp "c:/lamport/tla/tla2/tlatools/class"  -mx100m tlc2.TLC -cleanup  test10
+java -cp "c:/lamport/tla/tla2/tlatools/class"  -mx100m tlc2.TLC -cleanup  test11
+java -cp "c:/lamport/tla/tla2/tlatools/class"  -mx100m tlc2.TLC -cleanup  test12
+java -cp "c:/lamport/tla/tla2/tlatools/class"  -mx100m tlc2.TLC -cleanup  test13
+java -cp "c:/lamport/tla/tla2/tlatools/class"  -mx100m tlc2.TLC -cleanup  test14
+java -cp "c:/lamport/tla/tla2/tlatools/class"  -mx100m tlc2.TLC -cleanup  test15
+java -cp "c:/lamport/tla/tla2/tlatools/class"  -mx100m tlc2.TLC -cleanup  test16
+java -cp "c:/lamport/tla/tla2/tlatools/class"  -mx100m tlc2.TLC -cleanup  test17
+java -cp "c:/lamport/tla/tla2/tlatools/class"  -mx100m tlc2.TLC -cleanup  test18
+java -cp "c:/lamport/tla/tla2/tlatools/class"  -mx100m tlc2.TLC -cleanup  test19
+java -cp "c:/lamport/tla/tla2/tlatools/class"  -mx100m tlc2.TLC -cleanup  test20
+java -cp "c:/lamport/tla/tla2/tlatools/class"  -mx100m tlc2.TLC -cleanup  test21
+java -cp "c:/lamport/tla/tla2/tlatools/class"  -mx100m tlc2.TLC -cleanup  test22
+java -cp "c:/lamport/tla/tla2/tlatools/class"  -mx100m tlc2.TLC -cleanup  test23
+java -cp "c:/lamport/tla/tla2/tlatools/class"  -mx100m tlc2.TLC -cleanup  test24
+java -cp "c:/lamport/tla/tla2/tlatools/class"  -mx100m tlc2.TLC -cleanup  test25
+java -cp "c:/lamport/tla/tla2/tlatools/class"  -mx100m tlc2.TLC -cleanup  test26
+java -cp "c:/lamport/tla/tla2/tlatools/class"  -mx100m tlc2.TLC -cleanup  test27
+java -cp "c:/lamport/tla/tla2/tlatools/class"  -mx100m tlc2.TLC -cleanup  test28
+java -cp "c:/lamport/tla/tla2/tlatools/class"  -mx100m tlc2.TLC -cleanup  test29
+java -cp "c:/lamport/tla/tla2/tlatools/class"  -mx100m tlc2.TLC -cleanup  test30
+java -cp "c:/lamport/tla/tla2/tlatools/class"  -mx100m tlc2.TLC -cleanup  test31
+java -cp "c:/lamport/tla/tla2/tlatools/class"  -mx100m tlc2.TLC -cleanup  test32
+java -cp "c:/lamport/tla/tla2/tlatools/class"  -mx100m tlc2.TLC -cleanup  test33
+java -cp "c:/lamport/tla/tla2/tlatools/class"  -mx100m tlc2.TLC -cleanup  test34
+java -cp "c:/lamport/tla/tla2/tlatools/class"  -mx100m tlc2.TLC -cleanup  test35
+java -cp "c:/lamport/tla/tla2/tlatools/class"  -mx100m tlc2.TLC -cleanup  test36
+java -cp "c:/lamport/tla/tla2/tlatools/class"  -mx100m tlc2.TLC -cleanup  test37
+java -cp "c:/lamport/tla/tla2/tlatools/class"  -mx100m tlc2.TLC -cleanup  test38
+java -cp "c:/lamport/tla/tla2/tlatools/class"  -mx100m tlc2.TLC -cleanup  test39
+java -cp "c:/lamport/tla/tla2/tlatools/class"  -mx100m tlc2.TLC -cleanup  test40
+java -cp "c:/lamport/tla/tla2/tlatools/class"  -mx100m tlc2.TLC -cleanup  test41
+java -cp "c:/lamport/tla/tla2/tlatools/class"  -mx100m tlc2.TLC -cleanup  test42
+java -cp "c:/lamport/tla/tla2/tlatools/class"  -mx100m tlc2.TLC -cleanup  test43
+java -cp "c:/lamport/tla/tla2/tlatools/class"  -mx100m tlc2.TLC -cleanup  test44
+java -cp "c:/lamport/tla/tla2/tlatools/class"  -mx100m tlc2.TLC -cleanup  test45
+java -cp "c:/lamport/tla/tla2/tlatools/class"  -mx100m tlc2.TLC -cleanup  test46
+java -cp "c:/lamport/tla/tla2/tlatools/class"  -mx100m tlc2.TLC -cleanup  test47
+java -cp "c:/lamport/tla/tla2/tlatools/class"  -mx100m tlc2.TLC -cleanup  test48
+java -cp "c:/lamport/tla/tla2/tlatools/class"  -mx100m tlc2.TLC -cleanup  test49
+java -cp "c:/lamport/tla/tla2/tlatools/class"  -mx100m tlc2.TLC -cleanup  test50
+java -cp "c:/lamport/tla/tla2/tlatools/class"  -mx100m tlc2.TLC -cleanup  test51
+java -cp "c:/lamport/tla/tla2/tlatools/class"  -mx100m tlc2.TLC -cleanup  test52
+java -cp "c:/lamport/tla/tla2/tlatools/class"  -mx100m tlc2.TLC -cleanup  test53
+java -cp "c:/lamport/tla/tla2/tlatools/class"  -mx100m tlc2.TLC -cleanup  test54
+java -cp "c:/lamport/tla/tla2/tlatools/class"  -mx100m tlc2.TLC -cleanup  test55
+java -cp "c:/lamport/tla/tla2/tlatools/class"  -mx100m tlc2.TLC -cleanup  test56
+java -cp "c:/lamport/tla/tla2/tlatools/class"  -mx100m tlc2.TLC -cleanup  test57
+java -cp "c:/lamport/tla/tla2/tlatools/class"  -mx100m tlc2.TLC -cleanup  test58
+java -cp "c:/lamport/tla/tla2/tlatools/class"  -mx100m tlc2.TLC -cleanup  test59
+java -cp "c:/lamport/tla/tla2/tlatools/class"  -mx100m tlc2.TLC -cleanup  test60
+java -cp "c:/lamport/tla/tla2/tlatools/class"  -mx100m tlc2.TLC -cleanup  test61
+java -cp "c:/lamport/tla/tla2/tlatools/class"  -mx100m tlc2.TLC -cleanup  test62
+java -cp "c:/lamport/tla/tla2/tlatools/class"  -mx100m tlc2.TLC -cleanup  test63
+java -cp "c:/lamport/tla/tla2/tlatools/class"  -mx100m tlc2.TLC -cleanup  test201
+java -cp "c:/lamport/tla/tla2/tlatools/class"  -mx100m tlc2.TLC -cleanup  test202
+java -cp "c:/lamport/tla/tla2/tlatools/class"  -mx100m tlc2.TLC -cleanup  test203
+java -cp "c:/lamport/tla/tla2/tlatools/class"  -mx100m tlc2.TLC -cleanup  test204
+java -cp "c:/lamport/tla/tla2/tlatools/class"  -mx100m tlc2.TLC -cleanup  test205
+java -cp "c:/lamport/tla/tla2/tlatools/class"  -mx100m tlc2.TLC -cleanup  test206
+java -cp "c:/lamport/tla/tla2/tlatools/class"  -mx100m tlc2.TLC -cleanup  test207
+java -cp "c:/lamport/tla/tla2/tlatools/class"  -mx100m tlc2.TLC -cleanup  test208
+java -cp "c:/lamport/tla/tla2/tlatools/class"  -mx100m tlc2.TLC -cleanup  test209
+java -cp "c:/lamport/tla/tla2/tlatools/class"  tla2sany.SANY test210
+java -cp "c:/lamport/tla/tla2/tlatools/class"  tla2sany.SANY test211
+java -cp "c:/lamport/tla/tla2/tlatools/class"  tla2sany.SANY test212
+java -cp "c:/lamport/tla/tla2/tlatools/class"  tla2sany.SANY test213
+java -cp "c:/lamport/tla/tla2/tlatools/class"  tla2sany.SANY test214
+java -cp "c:/lamport/tla/tla2/tlatools/class"  tla2sany.SANY test215
+echo "DONE"
