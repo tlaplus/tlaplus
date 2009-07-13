a2d896bf3b11c83f95d5c53bec5a14e84173c372
simonzam <simonzam@76a6fc44-f60b-0410-a9a8-e67b0e8fc65c>
2009-07-13 21:24:36 +0000

    git-svn-id: svn+ssh://svn.msr-inria.inria.fr/var/lib/svn/repository/tla/trunk/tla2-inria@13460 76a6fc44-f60b-0410-a9a8-e67b0e8fc65c

--- tlatools/src/tla2sany/utilities/Assert.java
+++ tlatools/src/tla2sany/utilities/Assert.java
@@ -3,22 +3,25 @@ package tla2sany.utilities;
 
 import util.ToolIO;
 
+/**
+ * Implementation of assertion
+ * @author Simon Zambrovski
+ * @version $Id$
+ */
 public class Assert {
 
+  // fishy way to exit the program
   public final static void assertion(boolean b) {
     if (!b) {
       ToolIO.err.println("assertion failed:");
-      Throwable e = new Throwable();
-      e.printStackTrace();
       System.exit(1);
     }
   }
 
-  public final static void fail(String msg) {
-    ToolIO.err.println("Error: " + msg);
-    Throwable e = new Throwable();
-    e.printStackTrace();
-    System.exit(1);
-  }
+// SZ Jul 13, 2009: method not used
+//  public final static void fail(String msg) {
+//    ToolIO.err.println("Error: " + msg);
+//    System.exit(1);
+//  }
   
 }
