6721b0e36467bdc343f7b329bbab6371b3a33d52
simonzam <simonzam@76a6fc44-f60b-0410-a9a8-e67b0e8fc65c>
2009-07-14 03:01:34 +0000

    refactoring
    git-svn-id: svn+ssh://svn.msr-inria.inria.fr/var/lib/svn/repository/tla/trunk/tla2-inria@13467 76a6fc44-f60b-0410-a9a8-e67b0e8fc65c

--- tlatools/src/util/Assert.java
+++ tlatools/src/util/Assert.java
@@ -4,50 +4,34 @@
 
 package util;
 
-public class Assert extends RuntimeException {
-
-  /* The set of warned source locations. */
-  private static Set msgSet = new Set();
-
-  public Assert(String msg) { super(msg); }
-  
-  public static void fail() {
-    throw new RuntimeException();
-  }
-
-  public static void fail(String msg) {
-    throw new RuntimeException(msg);
-  }
-
-  public static void check(boolean b) {
-    if (!b) throw new RuntimeException();
-  }
-
-  public static void check(boolean b, String msg) {
-    if (!b) throw new RuntimeException(msg);
-  }
-
-  public static void warn(String msg) {
-    throw new Assert(msg);
-  }
-
-  public static void printWarning(boolean warn, String msg) {
-    if (warn && msgSet.put(msg) == null) {
-      ToolIO.err.println("Warning: " + msg);
-      ToolIO.err.println("(Use the -nowarning option to disable this warning.)");
+/**
+ * A toolkit for checking conditions and throwing unchecked exceptions if they are not met.
+ * 
+ * @author Yuan Yu, Simon Zambrovski
+ * @version $Id$
+ */
+public class Assert
+{
+    /**
+     * Unconditioned way to throw an exception
+     * @param reason the explaining message to be enclosed into the exception
+     */
+    public static void fail(String reason) throws RuntimeException
+    {
+        throw new RuntimeException(reason);
     }
-  }
 
-  public static void printStack(boolean b) {
-    if (b) {
-      Exception e = new Exception();
-      e.printStackTrace();
+    /**
+     * Checks whether the condition is true. Throws an unchecked exception if otherwise
+     * @param condition the condition to check
+     * @param reason the message explaining the exception
+     * @throws RuntimeException
+     */
+    public static void check(boolean condition, String reason) throws RuntimeException
+    {
+        if (!condition) 
+        {
+            throw new RuntimeException(reason);
+        }
     }
-  }
-
-  public static void printStack(Throwable e) {
-    System.gc();
-    e.printStackTrace();
-  }
-  
 }
