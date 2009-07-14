bfc18fd2ab0392fbdfc79f178740feae0fdbb947
simonzam <simonzam@76a6fc44-f60b-0410-a9a8-e67b0e8fc65c>
2009-07-14 03:00:32 +0000

    not used anymore
    git-svn-id: svn+ssh://svn.msr-inria.inria.fr/var/lib/svn/repository/tla/trunk/tla2-inria@13465 76a6fc44-f60b-0410-a9a8-e67b0e8fc65c

--- tlatools/src/tla2sany/utilities/Assert.java
+++ tlatools/src/tla2sany/utilities/Assert.java
@@ -1,27 +1,37 @@
 // Copyright (c) 2003 Compaq Corporation.  All rights reserved.
 package tla2sany.utilities;
 
-import util.ToolIO;
-
 /**
- * Implementation of assertion
+ * Implementation of assertion for SANY. 
+ * 
  * @author Simon Zambrovski
  * @version $Id$
+ * 
+ * @deprecated Use {@linkplain util.Assert} instead of this class
  */
-public class Assert {
+public class Assert
+{
 
-  // fishy way to exit the program
-  public final static void assertion(boolean b) {
-    if (!b) {
-      ToolIO.err.println("assertion failed:");
-      System.exit(1);
+    // SZ Jul 13, 2009: fishy way to exit the program
+    /**
+     * @deprecated Use {@linkplain util.Assert#check(boolean, String)} instead of this method
+     */
+    public final static void assertion(boolean b)
+    {
+        // if (!b) {
+        // ToolIO.err.println("assertion failed:");
+        // System.exit(1);
+        // }
     }
-  }
 
-// SZ Jul 13, 2009: method not used
-//  public final static void fail(String msg) {
-//    ToolIO.err.println("Error: " + msg);
-//    System.exit(1);
-//  }
-  
+    // SZ Jul 13, 2009: method not used
+    /**
+     * @deprecated Use {@linkplain util.Assert#fail(String)} instead of this method
+     */
+    public final static void fail(String msg)
+    {
+        // ToolIO.err.println("Error: " + msg);
+        // System.exit(1);
+    }
+
 }
