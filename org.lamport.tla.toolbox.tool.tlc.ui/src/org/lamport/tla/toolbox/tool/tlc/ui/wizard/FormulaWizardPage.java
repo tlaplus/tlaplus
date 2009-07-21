4f1ab20afa735b538f51e732de6d5b7ff5bb4239
simonzam <simonzam@76a6fc44-f60b-0410-a9a8-e67b0e8fc65c>
2009-07-21 20:03:21 +0000

    wrong font in sourceviewer fixed
    git-svn-id: svn+ssh://svn.msr-inria.inria.fr/var/lib/svn/repository/tla/trunk/tla2-inria@13494 76a6fc44-f60b-0410-a9a8-e67b0e8fc65c

--- org.lamport.tla.toolbox.tool.tlc.ui/src/org/lamport/tla/toolbox/tool/tlc/ui/wizard/FormulaWizardPage.java
+++ org.lamport.tla.toolbox.tool.tlc.ui/src/org/lamport/tla/toolbox/tool/tlc/ui/wizard/FormulaWizardPage.java
@@ -2,13 +2,13 @@ package org.lamport.tla.toolbox.tool.tlc.ui.wizard;
 
 import org.eclipse.jface.text.Document;
 import org.eclipse.jface.text.source.SourceViewer;
-import org.eclipse.jface.text.source.SourceViewerConfiguration;
 import org.eclipse.jface.wizard.WizardPage;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.custom.StyledText;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
+import org.lamport.tla.toolbox.tool.tlc.ui.util.FormHelper;
 
 /**
  * A page with a simple field for formula editing
@@ -36,10 +36,9 @@ public class FormulaWizardPage extends WizardPage
         GridLayout layout = new GridLayout(1, false);
         container.setLayout(layout);
 
-        sourceViewer = new SourceViewer(container, null, null, false, SWT.V_SCROLL | SWT.H_SCROLL | SWT.BORDER);
-        SourceViewerConfiguration configuration = new SourceViewerConfiguration();
-        sourceViewer.configure(configuration);
         
+        sourceViewer = FormHelper.createSourceViewer(container, SWT.BORDER | SWT.MULTI | SWT.H_SCROLL | SWT.V_SCROLL);
+       
         GridData gd = new GridData(SWT.CENTER, SWT.CENTER, true, true);
         gd.heightHint = 200;
         gd.widthHint = 400;
