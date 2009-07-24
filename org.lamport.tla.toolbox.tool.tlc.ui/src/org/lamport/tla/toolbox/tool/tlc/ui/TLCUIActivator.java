15e65447faeebc402913b4fe944606826262e881
simonzam <simonzam@76a6fc44-f60b-0410-a9a8-e67b0e8fc65c>
2009-07-24 07:16:16 +0000

    perspective switching implemented
    git-svn-id: svn+ssh://svn.msr-inria.inria.fr/var/lib/svn/repository/tla/trunk/tla2-inria@13538 76a6fc44-f60b-0410-a9a8-e67b0e8fc65c

--- org.lamport.tla.toolbox.tool.tlc.ui/src/org/lamport/tla/toolbox/tool/tlc/ui/TLCUIActivator.java
+++ org.lamport.tla.toolbox.tool.tlc.ui/src/org/lamport/tla/toolbox/tool/tlc/ui/TLCUIActivator.java
@@ -4,7 +4,15 @@ import org.eclipse.core.runtime.Status;
 import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.graphics.Font;
+import org.eclipse.ui.IPerspectiveDescriptor;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.PerspectiveAdapter;
+import org.eclipse.ui.navigator.INavigatorContentService;
+import org.eclipse.ui.navigator.NavigatorContentServiceFactory;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.lamport.tla.toolbox.tool.tlc.ui.modelexplorer.ModelContentProvider;
+import org.lamport.tla.toolbox.ui.provider.ToolboxExplorer;
 import org.lamport.tla.toolbox.util.UIHelper;
 import org.osgi.framework.BundleContext;
 
@@ -17,11 +25,45 @@ public class TLCUIActivator extends AbstractUIPlugin
     // The plug-in ID
     public static final String PLUGIN_ID = "org.lamport.tla.toolbox.tool.tlc.ui";
 
-
     // The shared instance
     private static TLCUIActivator plugin;
+    private Font courierFont;
+    private PerspectiveAdapter perspectiveAdapter = new PerspectiveAdapter() {
 
-    private Font cFont;
+        public void perspectiveActivated(IWorkbenchPage page, IPerspectiveDescriptor perspective)
+        {
+            System.out.println("A" + perspective.getId());
+            INavigatorContentService contentService = NavigatorContentServiceFactory.INSTANCE
+                    .createContentService(ToolboxExplorer.VIEW_ID);
+
+            if (TLCPerspective.ID.equals(perspective.getId())
+                    && !contentService.getActivationService().isNavigatorExtensionActive(
+                            ModelContentProvider.TLC_NCE))
+            {
+                contentService.getActivationService().activateExtensions(
+                        new String[] { ModelContentProvider.TLC_NCE }, false);
+                contentService.getActivationService().persistExtensionActivations();
+            }
+        }
+
+        public void perspectiveDeactivated(IWorkbenchPage page, IPerspectiveDescriptor perspective)
+        {
+            System.out.println("D" + perspective.getId());
+            INavigatorContentService contentService = NavigatorContentServiceFactory.INSTANCE
+                    .createContentService(ToolboxExplorer.VIEW_ID);
+
+            if (TLCPerspective.ID.equals(perspective.getId())
+                    && contentService.getActivationService().isNavigatorExtensionActive(
+                            ModelContentProvider.TLC_NCE))
+            {
+                System.out.println("was active: deactivated");
+                contentService.getActivationService().deactivateExtensions(
+                        new String[] { ModelContentProvider.TLC_NCE }, false);
+                contentService.getActivationService().persistExtensionActivations();
+            }
+
+        }
+    };
 
     /**
      * The constructor
@@ -38,6 +80,12 @@ public class TLCUIActivator extends AbstractUIPlugin
     {
         super.start(context);
         plugin = this;
+
+        IWorkbenchWindow window = UIHelper.getActiveWindow();
+        if (window != null)
+        {
+            // window.addPerspectiveListener(perspectiveAdapter);
+        }
     }
 
     /*
@@ -46,8 +94,12 @@ public class TLCUIActivator extends AbstractUIPlugin
      */
     public void stop(BundleContext context) throws Exception
     {
+        IWorkbenchWindow window = UIHelper.getActiveWindow();
+        if (window != null)
+        {
+            window.removePerspectiveListener(perspectiveAdapter);
+        }
         plugin = null;
-
         super.stop(context);
     }
 
@@ -63,13 +115,13 @@ public class TLCUIActivator extends AbstractUIPlugin
 
     public synchronized Font getCourierFont()
     {
-        if (cFont == null)
+        if (courierFont == null)
         {
-            cFont = new Font(UIHelper.getShellProvider().getShell().getDisplay(), "Courier New", 11, SWT.NORMAL);
+            courierFont = new Font(UIHelper.getShellProvider().getShell().getDisplay(), "Courier New", 11, SWT.NORMAL);
         }
-        return cFont;
+        return courierFont;
     }
-    
+
     /**
      * Returns an image descriptor for the image file at the given
      * plug-in relative path
@@ -77,10 +129,10 @@ public class TLCUIActivator extends AbstractUIPlugin
      * @param path the path
      * @return the image descriptor
      */
-    public static ImageDescriptor getImageDescriptor(String path) {
+    public static ImageDescriptor getImageDescriptor(String path)
+    {
         return imageDescriptorFromPlugin(PLUGIN_ID, path);
     }
-
 
     /**
      * Logs an error
@@ -90,8 +142,7 @@ public class TLCUIActivator extends AbstractUIPlugin
     public static void logError(String message, Throwable e)
     {
         getDefault().getLog().log(new Status(Status.ERROR, TLCUIActivator.PLUGIN_ID, message, e));
-        
+
     }
-    
-    
+
 }
