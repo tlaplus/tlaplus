da0324d5e73a44cc4c80cfa72a93be1b4226009e
simonzam <simonzam@76a6fc44-f60b-0410-a9a8-e67b0e8fc65c>
2009-07-30 02:39:57 +0000

    comments
    git-svn-id: svn+ssh://svn.msr-inria.inria.fr/var/lib/svn/repository/tla/trunk/tla2-inria@13595 76a6fc44-f60b-0410-a9a8-e67b0e8fc65c

--- org.lamport.tla.toolbox/src/org/lamport/tla/toolbox/tool/SpecLifecycleParticipant.java
+++ org.lamport.tla.toolbox/src/org/lamport/tla/toolbox/tool/SpecLifecycleParticipant.java
@@ -1,18 +1,35 @@
 package org.lamport.tla.toolbox.tool;
 
 /**
+ * A superclass for Spec Lifecycle Participant implementations used for 
+ * registration on the <code>org.lamport.tla.toolbox.spec</code> extension point.<br />
+ * This class should be overridden by the extensions.<br>
+ * The overrides must provide an 0-argument constructor.
+ * 
+ * @see {@link SpecEvent}
  * @author Simon Zambrovski
  * @version $Id$
  */
 public abstract class SpecLifecycleParticipant
 {
+    /**
+     * Marks the point in time from which on, the participant starts to receive events
+     */
     public void initialize()
     {
     }
 
+    /**
+     * Is called on every spec event
+     * @param event the event containing the information about what is happening
+     * @return a boolean value indicating the reaction of the participant to the event. 
+     * Reserved for future use.
+     */
     public abstract boolean eventOccured(SpecEvent event);
-    
 
+    /**
+     * Marks the point in time from which on, the participant stops to receive events
+     */
     public void terminate()
     {
     }
