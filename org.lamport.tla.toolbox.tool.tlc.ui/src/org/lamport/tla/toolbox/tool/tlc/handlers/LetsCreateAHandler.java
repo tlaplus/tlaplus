410a87050ead27aefe6a5f5de4887622bb4aa5c4
simon <simon@76a6fc44-f60b-0410-a9a8-e67b0e8fc65c>
2009-10-02 14:59:10 +0000

    git-svn-id: svn+ssh://svn.msr-inria.inria.fr/var/lib/svn/repository/tla/trunk/tla2-inria@14208 76a6fc44-f60b-0410-a9a8-e67b0e8fc65c

--- org.lamport.tla.toolbox.tool.tlc.ui/src/org/lamport/tla/toolbox/tool/tlc/handlers/LetsCreateAHandler.java
+++ org.lamport.tla.toolbox.tool.tlc.ui/src/org/lamport/tla/toolbox/tool/tlc/handlers/LetsCreateAHandler.java
@@ -4,12 +4,17 @@ import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 
+/**
+ * TODO LL says he deletes this file as soon as he does not need it anymore
+ * @author Leslie Lamport
+ *
+ */
 public class LetsCreateAHandler extends AbstractHandler
 {
 
     public Object execute(ExecutionEvent event) throws ExecutionException
     {
-System.out.println("Let's create a command class handler blah.");
+        // System.out.println("Let's create a command class handler blah.");
        // TODO Auto-generated method stub
         return null;
     }
@@ -22,7 +27,7 @@ System.out.println("Let's create a command class handler blah.");
 //    
     public boolean isEnabled() {
 //        super.setEnabled(this); 
-        System.out.println("LetsCreateAHandler.isEnabled() called.");
+//        System.out.println("LetsCreateAHandler.isEnabled() called.");
         return false;
     }
 }
