7d08fdf7f73005e1813f7330ed07cddcbef66fda
ricketts <ricketts@76a6fc44-f60b-0410-a9a8-e67b0e8fc65c>
2009-10-02 10:12:58 +0000

    Init
    git-svn-id: svn+ssh://svn.msr-inria.inria.fr/var/lib/svn/repository/tla/trunk/tla2-inria@14196 76a6fc44-f60b-0410-a9a8-e67b0e8fc65c

---
+++ org.lamport.tla.toolbox.tool.tla2tex/src/org/lamport/tla/toolbox/tool/tla2tex/handler/ProducePDFHandler.java
@@ -0,0 +1,21 @@
+package org.lamport.tla.toolbox.tool.tla2tex.handler;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+
+/**
+ * 
+ * @author Daniel Ricketts
+ * @version $Id$
+ */
+public class ProducePDFHandler extends AbstractHandler
+{
+
+    public Object execute(ExecutionEvent event)
+    {
+        System.out.println("PDF Handler executed.");
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
