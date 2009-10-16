ac24dc3473e40f9ea5d19ec2fc9187a176e80d42
lamport <lamport@76a6fc44-f60b-0410-a9a8-e67b0e8fc65c>
2009-10-16 17:49:20 +0000

    Adding help.
    git-svn-id: svn+ssh://svn.msr-inria.inria.fr/var/lib/svn/repository/tla/trunk/tla2-inria@14531 76a6fc44-f60b-0410-a9a8-e67b0e8fc65c

--- tlatools/src/tlc2/output/EC.java
+++ tlatools/src/tlc2/output/EC.java
@@ -72,7 +72,7 @@ public interface EC
 
     public static final int TLC_ERROR_STATE = 2120;
     public static final int TLC_BEHAVIOR_UP_TO_THIS_POINT = 2121;
-    public static final int TLC_BACK_TO_STATE = 2122;
+    public static final int TLC_BACK_TO_STATE = 2122;  // The "back to state" message from a liveness-error trace.
     public static final int TLC_FAILED_TO_RECOVER_INIT = 2123;
     public static final int TLC_REPORTER_DIED = 2124;
 
@@ -192,7 +192,7 @@ public interface EC
     // state printing
     public static final int TLC_STATE_PRINT1 = 2216;
     public static final int TLC_STATE_PRINT2 = 2217;
-    public static final int TLC_STATE_PRINT3 = 2218;
+    public static final int TLC_STATE_PRINT3 = 2218;  // This seems to be a "Stuttering" message from a liveness-error trace 
     public static final int TLC_SANY_END = 2219;
     public static final int TLC_SANY_START = 2220;
     public static final int TLC_COVERAGE_VALUE = 2221;
