e7cfb7b6c7f3b899317bc6a97a4d87cf636a6297
ricketts <ricketts@76a6fc44-f60b-0410-a9a8-e67b0e8fc65c>
2010-02-01 14:08:19 +0000

    Added "Spec Status : " to spec status composite in status toolbar.
    git-svn-id: svn+ssh://svn.msr-inria.inria.fr/var/lib/svn/repository/tla/trunk/tla2-inria@15812 76a6fc44-f60b-0410-a9a8-e67b0e8fc65c

--- org.lamport.tla.toolbox/src/org/lamport/tla/toolbox/ui/contribution/ParseStatusContributionItem.java
+++ org.lamport.tla.toolbox/src/org/lamport/tla/toolbox/ui/contribution/ParseStatusContributionItem.java
@@ -19,70 +19,81 @@ import org.lamport.tla.toolbox.util.AdapterFactory;
  * @version $Id: ParseStatusContributionItem.java 13736 2009-08-28 23:51:29Z
  *          lamport $
  */
-public class ParseStatusContributionItem extends
-		WorkbenchWindowControlContribution {
-	// the element
-	private Composite composite;
-	// label to display status
-	private Label statusLabel;
+public class ParseStatusContributionItem extends WorkbenchWindowControlContribution
+{
+    // the element
+    private Composite composite;
+    // label to display status
+    private Label statusLabel;
 
-	public ParseStatusContributionItem() {
-		super("specParseStatusState");
-		Activator.getDefault().setParseStatusContribution(this);
-	}
+    public ParseStatusContributionItem()
+    {
+        super("specParseStatusState");
+        Activator.getDefault().setParseStatusContribution(this);
+    }
 
-	protected Control createControl(Composite parent) {
-		// If the composite is good just return it.
-		if (composite != null && !composite.isDisposed()) {
-			return composite;
-		}
+    protected Control createControl(Composite parent)
+    {
+        // If the composite is good just return it.
+        if (composite != null && !composite.isDisposed())
+        {
+            return composite;
+        }
 
-		// Create a composite to place the label in
-		composite = new Composite(parent, SWT.NONE);
-		// composite.setData(this);
+        // Create a composite to place the label in
+        composite = new Composite(parent, SWT.NONE);
+        // composite.setData(this);
 
-		// Give some room around the control
-		FillLayout layout = new FillLayout();
-		layout.marginHeight = 2;
-		layout.marginWidth = 2;
-		composite.setLayout(layout);
+        // Give some room around the control
+        FillLayout layout = new FillLayout();
+        layout.marginHeight = 2;
+        layout.marginWidth = 2;
+        composite.setLayout(layout);
 
-		// Create label inside composite.
-		statusLabel = new Label(composite, SWT.BORDER | SWT.CENTER);
-		statusLabel.setToolTipText("Specification Parse Status");
-		statusLabel.setSize(100, 20);
+        // label informing the user that this reflects the parse status of the spec
+        Label description = new Label(composite, SWT.NONE);
+        description.setText("Spec Status : ");
+        description.setSize(70, 20);
 
-		// update status
-		updateStatus();
-		return composite;
-	}
+        // Create label inside composite.
+        statusLabel = new Label(composite, SWT.BORDER | SWT.CENTER);
+        statusLabel.setToolTipText("Specification Parse Status");
+        statusLabel.setSize(100, 20);
 
-	// Updates status from the specification currently loaded in the SpecManager
-	public void updateStatus() {
-		if (statusLabel == null || statusLabel.isDisposed()) {
-			return;
-		}
+        // update status
+        updateStatus();
+        return composite;
+    }
 
-		Spec spec = Activator.getSpecManager().getSpecLoaded();
-		if (spec == null) {
-			composite.setVisible(false);
-			return;
-		}
-		statusLabel.setText(AdapterFactory.getStatusAsString(spec));
-		statusLabel.setBackground(statusLabel.getDisplay().getSystemColor(
-				AdapterFactory.getStatusAsSWTBGColor(spec)));
-		statusLabel.setForeground(statusLabel.getDisplay().getSystemColor(
-				AdapterFactory.getStatusAsSWTFGColor(spec)));
-		statusLabel.redraw();
-		composite.setVisible(true);
-	}
+    // Updates status from the specification currently loaded in the SpecManager
+    public void updateStatus()
+    {
+        if (statusLabel == null || statusLabel.isDisposed())
+        {
+            return;
+        }
 
-	public void update() {
-		updateStatus();
-	}
+        Spec spec = Activator.getSpecManager().getSpecLoaded();
+        if (spec == null)
+        {
+            composite.setVisible(false);
+            return;
+        }
+        statusLabel.setText(AdapterFactory.getStatusAsString(spec));
+        statusLabel.setBackground(statusLabel.getDisplay().getSystemColor(AdapterFactory.getStatusAsSWTBGColor(spec)));
+        statusLabel.setForeground(statusLabel.getDisplay().getSystemColor(AdapterFactory.getStatusAsSWTFGColor(spec)));
+        statusLabel.redraw();
+        composite.setVisible(true);
+    }
 
-	protected int computeWidth(Control control) {
-		return 100;
-	}
+    public void update()
+    {
+        updateStatus();
+    }
+
+    protected int computeWidth(Control control)
+    {
+        return 170;
+    }
 
 }
