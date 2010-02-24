0bc87b8ee613999afdd45ba63510be549bc5e8c7
lamport <lamport@76a6fc44-f60b-0410-a9a8-e67b0e8fc65c>
2010-02-24 18:09:06 +0000

    Changed version and modification date.
    git-svn-id: svn+ssh://svn.msr-inria.inria.fr/var/lib/svn/repository/tla/trunk/tla2-inria@16290 76a6fc44-f60b-0410-a9a8-e67b0e8fc65c

--- tlatools/src/tla2sany/drivers/SANY.java
+++ tlatools/src/tla2sany/drivers/SANY.java
@@ -42,7 +42,7 @@ public class SANY {
     * The following string is inserted by an Emacs macro when a new        *
     * version is saved.                                                    *
     ***********************************************************************/
-    "last modified on Mon  9 November 2009 at 12:33:20 PST by lamport";
+    "last modified on Wed 24 February 2010 at 10:07:37 PST by lamport";
 
   private static String modDate = 
                 lastModified.substring(21, lastModified.indexOf(" at"));
@@ -51,7 +51,7 @@ public class SANY {
     ***********************************************************************/
 
   public static String version = 
-    "Version .99 created " + modDate ; 
+    "Version 2.0 created " + modDate ; 
 
   private static boolean doParsing          = true;  
     // true <=> parsing should be done
