6352d77ede832537054d29bd5291b2609364dfeb
ricketts <ricketts@76a6fc44-f60b-0410-a9a8-e67b0e8fc65c>
2010-04-22 15:40:04 +0000

    Preliminary support for coloring.
    git-svn-id: svn+ssh://svn.msr-inria.inria.fr/var/lib/svn/repository/tla/trunk/tla2-inria@17064 76a6fc44-f60b-0410-a9a8-e67b0e8fc65c

---
+++ org.lamport.tla.toolbox.editor.basic/src/org/lamport/tla/toolbox/editor/basic/prover/TLAPMColoringOutputListener.java
@@ -0,0 +1,87 @@
+package org.lamport.tla.toolbox.editor.basic.prover;
+
+import java.util.HashMap;
+
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.jface.text.BadLocationException;
+import org.eclipse.jface.text.IRegion;
+import org.eclipse.jface.text.Position;
+import org.eclipse.jface.text.source.Annotation;
+import org.eclipse.ui.part.FileEditorInput;
+import org.lamport.tla.toolbox.editor.basic.TLAEditor;
+import org.lamport.tla.toolbox.editor.basic.util.DocumentHelper;
+import org.lamport.tla.toolbox.tool.prover.ui.output.data.ObligationStatus;
+import org.lamport.tla.toolbox.tool.prover.ui.output.data.TLAPMData;
+import org.lamport.tla.toolbox.tool.prover.ui.output.source.ITLAPMOutputSourceListener;
+import org.lamport.tla.toolbox.tool.prover.ui.output.source.TLAPMOutputSourceRegistry;
+
+import tla2sany.st.Location;
+
+/**
+ * Performs the status coloring of proofs.
+ * 
+ * @author Daniel Ricketts
+ *
+ */
+public class TLAPMColoringOutputListener implements ITLAPMOutputSourceListener
+{
+
+    public static final String VERIFIED_TYPE = "org.lamport.tla.toolbox.editor.basic.verifiedProofStep";
+    public static final String REJECTED_TYPE = "org.lamport.tla.toolbox.editor.basic.rejectedProofStep";
+    private TLAEditor editor;
+
+    public TLAPMColoringOutputListener(TLAEditor editor)
+    {
+        this.editor = editor;
+
+        TLAPMOutputSourceRegistry.getInstance().addListener(this);
+    }
+
+    public IPath getFullModulePath()
+    {
+        return ((FileEditorInput) editor.getEditorInput()).getFile().getLocation();
+    }
+
+    public void newData(TLAPMData data)
+    {
+        if (data instanceof ObligationStatus)
+        {
+            ObligationStatus obRegion = (ObligationStatus) data;
+
+            String type = null;
+            if (obRegion.getStatus() == ObligationStatus.STATUS_VERIFIED)
+            {
+                type = VERIFIED_TYPE;
+            } else if (obRegion.getStatus() == ObligationStatus.STATUS_REJECTED)
+            {
+                type = REJECTED_TYPE;
+            }
+            if (type != null)
+            {
+                Location loc = obRegion.getLocation();
+                try
+                {
+
+                    IRegion locRegion = DocumentHelper.locationToRegion(editor.getDocumentProvider().getDocument(
+                            editor.getEditorInput()), loc);
+                    HashMap newAnnotations = new HashMap();
+                    newAnnotations.put(new Annotation(type, true, ""), new Position(locRegion.getOffset(), locRegion
+                            .getLength()));
+                    editor.modifyRegularAnnotations(null, newAnnotations, null);
+                } catch (BadLocationException e)
+                {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    /**
+     * Removes itself as a listener from the {@link TLAPMOutputSourceRegistry}.
+     */
+    public void dispose()
+    {
+        TLAPMOutputSourceRegistry.getInstance().removeListener(this);
+    }
+}
