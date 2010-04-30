dc006e76016107923d465bdece15c0a6dc47b294
ricketts <ricketts@76a6fc44-f60b-0410-a9a8-e67b0e8fc65c>
2010-04-30 07:32:47 +0000

    Marker indicating the prover is running on a module.
    git-svn-id: svn+ssh://svn.msr-inria.inria.fr/var/lib/svn/repository/tla/trunk/tla2-inria@17144 76a6fc44-f60b-0410-a9a8-e67b0e8fc65c

--- org.lamport.tla.toolbox.tool.prover/src/org/lamport/tla/toolbox/tool/prover/launch/ProverLaunchDelegate.java
+++ org.lamport.tla.toolbox.tool.prover/src/org/lamport/tla/toolbox/tool/prover/launch/ProverLaunchDelegate.java
@@ -1,16 +1,21 @@
 package org.lamport.tla.toolbox.tool.prover.launch;
 
+import org.eclipse.core.resources.IResource;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.core.runtime.Path;
+import org.eclipse.core.runtime.jobs.IJobChangeEvent;
+import org.eclipse.core.runtime.jobs.IJobChangeListener;
 import org.eclipse.debug.core.ILaunch;
 import org.eclipse.debug.core.ILaunchConfiguration;
 import org.eclipse.debug.core.model.LaunchConfigurationDelegate;
 import org.lamport.tla.toolbox.spec.parser.ModuleParserLauncher;
 import org.lamport.tla.toolbox.spec.parser.ParseResult;
+import org.lamport.tla.toolbox.tool.prover.ProverActivator;
 import org.lamport.tla.toolbox.tool.prover.job.ProverJob;
+import org.lamport.tla.toolbox.tool.prover.util.ProverHelper;
 import org.lamport.tla.toolbox.util.ResourceHelper;
 
 public class ProverLaunchDelegate extends LaunchConfigurationDelegate implements IProverConfigurationConstants
@@ -21,6 +26,7 @@ public class ProverLaunchDelegate extends LaunchConfigurationDelegate implements
 
     IPath modulePath;
     int lineNumber;
+    IResource module;
 
     public boolean finalLaunchCheck(ILaunchConfiguration configuration, String mode, IProgressMonitor monitor)
             throws CoreException
@@ -49,12 +55,27 @@ public class ProverLaunchDelegate extends LaunchConfigurationDelegate implements
         // use document provider
 
         /*
+         * Get the IResource pointing to the module.
+         * 
+         * If the following method returns null, then
+         * no module exists by the name given by modulePath
+         * in the currently open spec. In that case, this
+         * method should return false. The launch should not
+         * proceed.
+         */
+        module = ResourceHelper.getResourceByName(modulePath.lastSegment());
+
+        if (module == null)
+        {
+            return false;
+        }
+
+        /*
          * Parse module.
          * 
          * If parsing is unsuccessful, return false.
          */
-        ParseResult result = (new ModuleParserLauncher()).parseModule(ResourceHelper.getResourceByName(modulePath
-                .lastSegment()), new NullProgressMonitor());
+        ParseResult result = (new ModuleParserLauncher()).parseModule(module, new NullProgressMonitor());
         if (!result.getDetectedErrors().isEmpty())
         {
             // MessageDialog.openError(UIHelper.getShellProvider().getShell(), "Module not parsed.",
@@ -69,13 +90,88 @@ public class ProverLaunchDelegate extends LaunchConfigurationDelegate implements
             throws CoreException
     {
 
+        /*
+         * Set a flag on the module indicating that the prover is running.
+         * 
+         * This flag is set to false by a job change listener when the job finishes.
+         * 
+         * It is currently only possible to run the prover on a module in the open spec.
+         */
+        ProverHelper.setProverRunning(module, true);
+
+        /*
+         * Launch the prover.
+         * 
+         * For now, we pass in null for the path to cygwin and to the tlapm executable. Eventually, there
+         * may be a user preference for these paths, so these arguments may not be null in the future.
+         */
         ProverJob job = new ProverJob("Prover Job", modulePath, null /*new Path("C:/cygwin/usr/local/bin/tlapm")*/,
                 null /*new Path(
                      "C:/cygwin/bin")*/, launch);
 
         // set the job progress to appear in a dialog in the UI
         job.setUser(true);
+
+        job.addJobChangeListener(new ProverJobChangeListener());
+
         job.schedule();
+
+    }
+
+    /**
+     * A job change listener for the prover.
+     * 
+     * Currently, this only sets the flag
+     * on the module to indicate that the prover
+     * is no longer running on that module when
+     * the prover finishes.
+     * 
+     * @author Daniel Ricketts
+     *
+     */
+    class ProverJobChangeListener implements IJobChangeListener
+    {
+
+        public void aboutToRun(IJobChangeEvent event)
+        {
+            // TODO Auto-generated method stub
+
+        }
+
+        public void awake(IJobChangeEvent event)
+        {
+            // TODO Auto-generated method stub
+
+        }
+
+        public void done(IJobChangeEvent event)
+        {
+            try
+            {
+                ProverHelper.setProverRunning(module, false);
+            } catch (CoreException e)
+            {
+                ProverActivator.logError("Error setting flag on module to indicate the prover is not running.", e);
+            }
+        }
+
+        public void running(IJobChangeEvent event)
+        {
+            // TODO Auto-generated method stub
+
+        }
+
+        public void scheduled(IJobChangeEvent event)
+        {
+            // TODO Auto-generated method stub
+
+        }
+
+        public void sleeping(IJobChangeEvent event)
+        {
+            // TODO Auto-generated method stub
+
+        }
 
     }
 
--- org.lamport.tla.toolbox.tool.prover/src/org/lamport/tla/toolbox/tool/prover/util/ProverHelper.java
+++ org.lamport.tla.toolbox.tool.prover/src/org/lamport/tla/toolbox/tool/prover/util/ProverHelper.java
@@ -1,6 +1,104 @@
 package org.lamport.tla.toolbox.tool.prover.util;
 
+import org.eclipse.core.resources.IMarker;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.runtime.CoreException;
+
+/**
+ * Helper methods for the launching of the prover.
+ * 
+ * @author Daniel Ricketts
+ *
+ */
 public class ProverHelper
 {
+
+    /**
+     * ID of the marker that contains a boolean attribute indicating if the prover is running
+     * on a module.
+     */
+    public static final String PROVER_RUNNING_MARKER = "org.lamport.tla.toolbox.tool.prover.proverRunning";
+    /**
+     * ID for the boolean attribute for the {@link ProverHelper#PROVER_RUNNING_MARKER} indicating if the
+     * prover is running on a module.
+     */
+    public static final String PROVER_IS_RUNNING_ATR = "org.lamport.tla.toolbox.tool.prover.isProverRunning";
+
+    /**
+     * Signals using a marker that the prover is or is not running
+     * on module.
+     * 
+     * @param module
+     * @param isRunning whether the prover is running or not on module
+     * @throws CoreException 
+     */
+    public static void setProverRunning(IResource module, boolean isRunning) throws CoreException
+    {
+        if (module.exists())
+        {
+            IMarker marker;
+            // Try to find any existing markers.
+            IMarker[] foundMarkers = module.findMarkers(PROVER_RUNNING_MARKER, false, IResource.DEPTH_ZERO);
+
+            // There should only be one such marker at most.
+            // In case there is more than one existing marker,
+            // remove extra markers.
+            if (foundMarkers.length > 0)
+            {
+                marker = foundMarkers[0];
+                // remove trash if any
+                for (int i = 1; i < foundMarkers.length; i++)
+                {
+                    foundMarkers[i].delete();
+                }
+            } else
+            {
+                // Create a new marker if no existing ones.
+                marker = module.createMarker(PROVER_RUNNING_MARKER);
+            }
+
+            // Set the boolean attribute to indicate if the marker is running.
+            marker.setAttribute(PROVER_IS_RUNNING_ATR, isRunning);
+        }
+
+    }
+
+    /**
+     * Returns whether or not the prover is running on the module.
+     * 
+     * @param module
+     * @return
+     * @throws CoreException 
+     */
+    public static boolean isProverRunning(IResource module) throws CoreException
+    {
+        if (module.exists())
+        {
+            IMarker marker;
+            // Try to find any existing markers.
+            IMarker[] foundMarkers = module.findMarkers(PROVER_RUNNING_MARKER, false, IResource.DEPTH_ZERO);
+
+            // There should only be one such marker at most.
+            // In case there is more than one existing marker,
+            // remove extra markers.
+            if (foundMarkers.length > 0)
+            {
+                marker = foundMarkers[0];
+                // remove trash if any
+                for (int i = 1; i < foundMarkers.length; i++)
+                {
+                    foundMarkers[i].delete();
+                }
+
+                return marker.getAttribute(PROVER_IS_RUNNING_ATR, false);
+            } else
+            {
+                return false;
+            }
+        } else
+        {
+            return false;
+        }
+    }
 
 }
