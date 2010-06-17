8c854a5efc7010a0bc02a5498c5c8572e15fdd1d
ricketts <ricketts@76a6fc44-f60b-0410-a9a8-e67b0e8fc65c>
2010-06-17 09:22:07 +0000

    Set enablement properties of check proof step handler.
    git-svn-id: svn+ssh://svn.msr-inria.inria.fr/var/lib/svn/repository/tla/trunk/tla2-inria@17734 76a6fc44-f60b-0410-a9a8-e67b0e8fc65c

--- org.lamport.tla.toolbox.tool.prover/src/org/lamport/tla/toolbox/tool/prover/ui/handler/CheckProofHandler.java
+++ org.lamport.tla.toolbox.tool.prover/src/org/lamport/tla/toolbox/tool/prover/ui/handler/CheckProofHandler.java
@@ -4,6 +4,7 @@ import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.commands.IHandler;
+import org.lamport.tla.toolbox.editor.basic.util.EditorUtil;
 import org.lamport.tla.toolbox.tool.prover.ui.util.ProverHelper;
 
 /**
@@ -29,5 +30,13 @@ public class CheckProofHandler extends AbstractHandler implements IHandler
         ProverHelper.runProverForActiveSelection(false);
 
         return null;
+    }
+
+    /**
+     * This handler is enabled if there is a TLA editor with focus.
+     */
+    public void setEnabled(Object context)
+    {
+        setBaseEnabled(EditorUtil.getTLAEditorWithFocus() != null);
     }
 }
