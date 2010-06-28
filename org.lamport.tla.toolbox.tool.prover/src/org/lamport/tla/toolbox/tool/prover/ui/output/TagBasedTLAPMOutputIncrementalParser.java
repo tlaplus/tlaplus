e79985713b0d80c3e8a8550bd4d3fb449115d91c
ricketts <ricketts@76a6fc44-f60b-0410-a9a8-e67b0e8fc65c>
2010-06-28 08:51:28 +0000

    Fixed bug in parsing of tlapm output that resulted in an assertion failed exception if a message was sent to the parser that ended in the middle of a tag.
    git-svn-id: svn+ssh://svn.msr-inria.inria.fr/var/lib/svn/repository/tla/trunk/tla2-inria@17938 76a6fc44-f60b-0410-a9a8-e67b0e8fc65c

--- org.lamport.tla.toolbox.tool.prover/src/org/lamport/tla/toolbox/tool/prover/ui/output/TagBasedTLAPMOutputIncrementalParser.java
+++ org.lamport.tla.toolbox.tool.prover/src/org/lamport/tla/toolbox/tool/prover/ui/output/TagBasedTLAPMOutputIncrementalParser.java
@@ -44,8 +44,14 @@ import org.lamport.tla.toolbox.tool.prover.ui.util.ProverHelper;
 public class TagBasedTLAPMOutputIncrementalParser implements IProverProcessOutputSink
 {
 
-    private StringBuilder currentMessageBuffer;
-    // private ITLAPMOutputSource source;
+    /**
+     * The buffer that holds the current string of text out of which
+     * messages can be parsed.
+     */
+    private StringBuilder currentSearchTextBuffer;
+    /**
+     * The module on which the prover was launched.
+     */
     private IFile moduleFile;
     /**
      * The description of the prover launch.
@@ -75,122 +81,164 @@ public class TagBasedTLAPMOutputIncrementalParser implements IProverProcessOutpu
      * Called to stream text into this parser.
      * 
      * This method creates instances of {@link TLAPMMessage} and
-     * process them according to their type.  
+     * process them according to their type.
+     * 
+     * See the comments within the method for the parsing algorithm.
      * 
      * @param text
      * @throws BadLocationException
      */
     public void appendText(String text)
     {
-        // System.out.println("New text : \n" + text);
         /*
          * The following sends each string between a
          * begin and end tag to be parsed into
          * an instance of TLAPMMessage.
          * 
-         * Once it encounters a start tag, it appends all new
-         * strings sent to this methods to a string builder. Once
-         * an end tag is encountered, it sends the current contents
-         * of the string builder to TLAPMMessage to be parsed and clears
-         * the string builder for the next start tag.
-         *    
+         * Every piece of text to this method is appended to the
+         * current search text buffer. This method executes
+         * the following algorithm while one of the following
+         * conditions is true:
+         * 
+         * A.) This parser has encountered a start tag without an end
+         * tag previously and there is at least one end tag in currentSearchTextBuffer.
+         * 
+         * B.) This parser has not encountered a start tag without an end
+         * tag previously and there is at least one start tag in currentSearchTextBuffer.
+         * 
+         * The loop:
+         * 
+         * 1.) If this parser has encountered a start tag without an end
+         * tag previously, then there is at least one end tag in the buffer because
+         * we are in this loop. Everything from the beginning of currentSearchTextBuffer
+         * to the beginning of the first end tag in currentSearchTextBuffer is
+         * a message. Parse that into an instance of TLAPMMessage, then remove
+         * everything from the beginning of currentSearchTextBuffer to the end
+         * of the first end tag. Set the flag indicating that we have NOT encountered
+         * a start tag without an end tag.
+         * 
+         * 2.) If this parser has not encountered a start tag without an end
+         * tag previously, then there is at least on start tag in this buffer
+         * because we are in this loop. Remove everything from the beginning
+         * of currentSearchTextBuffer to the end of the first start tag. Set
+         * the flag indicating that we have encountered a start
+         * tag without an end tag.
+         * 
+         * 3.) After either step 1 or step 2 has been completed, search for the next
+         * tag. If this parser has previously encountered a start tag without an end
+         * tag, search for an end tag. Else search for a start tag.
+         * 
+         * Note: the boolean inMessage is true iff this parser has previously encountered a
+         * start tag without a matching end tag.
          */
-        String searchText = text;
-        while (!searchText.isEmpty())
+        currentSearchTextBuffer.append(text);
+        /*
+         * tagIndex is the index of the tag we are looking for.
+         * If this parser has encountered a start tag without an end
+         * tag previously, then we are looking for an end tag. Else, we are looking
+         * for a start tag.
+         */
+        int tagIndex = -1;
+        if (inMessage)
+        {
+            tagIndex = currentSearchTextBuffer.indexOf(END_TAG);
+        } else
+        {
+            tagIndex = currentSearchTextBuffer.indexOf(START_TAG);
+        }
+
+        while (tagIndex != -1)
         {
             if (inMessage)
             {
-                int endTagIndex = searchText.indexOf(END_TAG);
-                if (endTagIndex != -1)
+                /* This parser has encountered a start tag without an end
+                 * tag previously, then there is at least one end tag in the buffer because
+                 * we are in this loop. Everything from the beginning of currentSearchTextBuffer
+                 * to the beginning of the first end tag in currentSearchTextBuffer is
+                 * a message. Parse that into an instance of TLAPMMessage, then remove
+                 * everything from the beginning of currentSearchTextBuffer to the end
+                 * of the first end tag. Set the flag indicating that we have NOT encountered
+                 * a start tag without an end tag.
+                 * 
+                 * Process the parsed message as indicated below.
+                 */
+
+                TLAPMMessage data = TLAPMMessage.parseMessage(currentSearchTextBuffer.substring(0, tagIndex),
+                        moduleFile.getLocation().removeFileExtension().lastSegment());
+
+                currentSearchTextBuffer.replace(0, tagIndex + END_TAG.length(), "");
+
+                inMessage = false;
+
+                if (data != null)
                 {
-                    inMessage = false;
+
                     /*
-                     * Everything from the beginning of the search string
-                     * to the beginning of the end tag is part of the message.
+                     * Create the appropriate marker for the
+                     * message. If the message is step status inform ProverHelper
+                     * using ProverHelper.newStepStatusMessage(). If this is a
+                     * launch for status checking, then the toolbox does not compute
+                     * step statuses itself, so the step status messages must be used
+                     * to update status markers. This means that the flag addMarker
+                     * in the method newStepStatusMessage should be set to true.
                      * 
-                     * The current message is complete. It can be parsed to a
-                     * TLAPMMessage.
+                     * If the message is an obligation
+                     * status, create an obligation status marker.
                      * 
-                     * Continue searching after the end tag for a new message.
+                     * If the message gives the number of obligations
+                     * to be checked, then update the progress monitor
+                     * for this parser to indicate this. If the message is an
+                     * ObligationStatusMessage that indicates that
+                     * the TLAPM is done processing the obligation in any
+                     * way, then the monitor will reflect this fact.
                      */
-                    currentMessageBuffer.append(searchText.substring(0, endTagIndex));
 
-                    TLAPMMessage data = TLAPMMessage.parseMessage(currentMessageBuffer.toString(), moduleFile
-                            .getLocation().removeFileExtension().lastSegment());
-
-                    if (data != null)
+                    if (data instanceof ObligationStatusMessage)
                     {
+                        ProverHelper.processObligationMessage((ObligationStatusMessage) data, description
+                                .getLevelNode());
 
-                        /*
-                         * Create the appropriate marker for the
-                         * message. If the message is step status inform ProverHelper
-                         * using ProverHelper.newStepStatusMessage(). If this is a
-                         * launch for status checking, then the toolbox does not compute
-                         * step statuses itself, so the step status messages must be used
-                         * to update status markers. This means that the flag addMarker
-                         * in the method newStepStatusMessage should be set to true.
-                         * 
-                         * If the message is an obligation
-                         * status, create an obligation status marker.
-                         * 
-                         * If the message gives the number of obligations
-                         * to be checked, then update the progress monitor
-                         * for this parser to indicate this. If the message is an
-                         * ObligationStatusMessage that indicates that
-                         * the TLAPM is done processing the obligation in any
-                         * way, then the monitor will reflect this fact.
-                         */
-
-                        if (data instanceof ObligationStatusMessage)
+                        if (ProverHelper.isObligationFinished((ObligationStatusMessage) data, description))
                         {
-                            ProverHelper.processObligationMessage((ObligationStatusMessage) data, description
-                                    .getLevelNode());
-
-                            if (ProverHelper.isObligationFinished((ObligationStatusMessage) data, description))
-                            {
-                                monitor.worked(1);
-                            }
-                        } else if (data instanceof ObligationNumberMessage)
-                        {
-                            ObligationNumberMessage numMessage = (ObligationNumberMessage) data;
-                            monitor.beginTask("Processing Obligations : " + numMessage.getCount() + " total.",
-                                    numMessage.getCount());
-                        } else if (data instanceof StepStatusMessage)
-                        {
-                            ProverHelper.newStepStatusMessage((StepStatusMessage) data, description.isStatusCheck());
+                            monitor.worked(1);
                         }
+                    } else if (data instanceof ObligationNumberMessage)
+                    {
+                        ObligationNumberMessage numMessage = (ObligationNumberMessage) data;
+                        monitor.beginTask("Processing Obligations : " + numMessage.getCount() + " total.", numMessage
+                                .getCount());
+                    } else if (data instanceof StepStatusMessage)
+                    {
+                        ProverHelper.newStepStatusMessage((StepStatusMessage) data, description.isStatusCheck());
                     }
-
-                    searchText = searchText.substring(endTagIndex + END_TAG.length(), searchText.length());
-                } else
-                {
-                    /*
-                     * Everything in the search text is part of the message.
-                     * Nothing more to search.
-                     */
-                    currentMessageBuffer.append(searchText);
-                    searchText = "";
                 }
+
             } else
             {
-                // Matcher matcher = START_TAG_PATTERN.matcher(searchText);
-                int startTagIndex = searchText.indexOf(START_TAG);
-                if (startTagIndex != -1)
-                {
-                    inMessage = true;
-                    /*
-                     * Search the remaining text for an end tag.
-                     */
-                    searchText = searchText.substring(startTagIndex + START_TAG.length(), searchText.length());
+                /*
+                 * This parser has not encountered a start tag without an end
+                 * tag previously, so there is at least on start tag in this buffer
+                 * because we are in this loop. Remove everything from the beginning
+                 * of currentSearchTextBuffer to the end of the first start tag. Set
+                 * the flag indicating that we have encountered a start
+                 * tag without an end tag.
+                 */
+                currentSearchTextBuffer.replace(0, tagIndex + START_TAG.length(), "");
+                inMessage = true;
+            }
 
-                    currentMessageBuffer = new StringBuilder();
-                } else
-                {
-                    /*
-                     * Nothing more to search.
-                     */
-                    searchText = "";
-                }
+            /*
+             * tagIndex is the index of the tag we are looking for.
+             * If this parser has encountered a start tag without an end
+             * tag previously, then we are looking for an end tag. Else, we are looking
+             * for a start tag.
+             */
+            if (inMessage)
+            {
+                tagIndex = currentSearchTextBuffer.indexOf(END_TAG);
+            } else
+            {
+                tagIndex = currentSearchTextBuffer.indexOf(START_TAG);
             }
         }
     }
@@ -207,7 +255,7 @@ public class TagBasedTLAPMOutputIncrementalParser implements IProverProcessOutpu
      */
     public void initializeSink(IFile moduleFile, ProverLaunchDescription description, IProgressMonitor monitor)
     {
-        currentMessageBuffer = new StringBuilder();
+        currentSearchTextBuffer = new StringBuilder();
 
         this.moduleFile = moduleFile;
         this.monitor = monitor;
