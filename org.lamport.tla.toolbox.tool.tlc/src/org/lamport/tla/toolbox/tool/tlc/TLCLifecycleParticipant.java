1b8c0197457d59f1538fd998b9171fcff789359e
kuppe <kuppe@76a6fc44-f60b-0410-a9a8-e67b0e8fc65c>
2011-06-07 07:36:47 +0000

    NEW - bug 113: Toolbox does not shut down gracefully with model checks still running http://bugzilla.tlaplus.net/show_bug.cgi?id=113 - Unify job family identifier for org.lamport jobs - Simplify cancellation
    git-svn-id: svn+ssh://svn.msr-inria.inria.fr/var/lib/svn/repository/tla/trunk/tla2-inria@21885 76a6fc44-f60b-0410-a9a8-e67b0e8fc65c

--- org.lamport.tla.toolbox.tool.tlc/src/org/lamport/tla/toolbox/tool/tlc/TLCLifecycleParticipant.java
+++ org.lamport.tla.toolbox.tool.tlc/src/org/lamport/tla/toolbox/tool/tlc/TLCLifecycleParticipant.java
@@ -6,34 +6,16 @@ import org.lamport.tla.toolbox.tool.ToolboxLifecycleParticipant;
 import org.lamport.tla.toolbox.tool.tlc.job.TLCJob;
 
 /**
- * Stop all running jobs on shutdown
+ * Stop all running org.lamport.tla.toolbox.tool.tlc jobs on shutdown
+ * 
  * @author Simon Zambrovski
- * @version $Id$
  */
-public class TLCLifecycleParticipant extends ToolboxLifecycleParticipant
-{
-    
+public class TLCLifecycleParticipant extends ToolboxLifecycleParticipant {
 
-    public TLCLifecycleParticipant()
-    {
-    }
-
-    
-    public void terminate() throws ToolboxLifecycleException
-    {
-        Job[] runningSpecJobs = Job.getJobManager().find(new TLCJob.AllJobMatcher());
-        for (int i = 0; i < runningSpecJobs.length; i++)
-        {
-            // send cancellations to all jobs...
-            runningSpecJobs[i].cancel();
-        }
-        // System.out.println("TLC: system is going down");
-    }
-
-
-    public void initialize() throws ToolboxLifecycleException
-    {
-        // System.out.println("TLC: system is going up");
-    }
-
+	/* (non-Javadoc)
+	 * @see org.lamport.tla.toolbox.tool.ToolboxLifecycleParticipant#terminate()
+	 */
+	public void terminate() throws ToolboxLifecycleException {
+		Job.getJobManager().cancel(TLCJob.AllJobsMatcher);
+	}
 }
