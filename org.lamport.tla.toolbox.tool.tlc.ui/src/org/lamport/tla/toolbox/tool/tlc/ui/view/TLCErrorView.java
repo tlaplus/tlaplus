ab5552e4b2573d5698eddd2e2a1e5f510bc46c09
kuppe <kuppe@76a6fc44-f60b-0410-a9a8-e67b0e8fc65c>
2011-06-29 18:07:16 +0000

    NEW - bug 151: org.lamport.tla.toolbox.tool.tlc.output.source.TagBasedTLCOutputIncrementalParser does not scale for large output http://bugzilla.tlaplus.net/show_bug.cgi?id=151 - Expand trace explorer by default (reverting previous change)
    git-svn-id: svn+ssh://svn.msr-inria.inria.fr/var/lib/svn/repository/tla/trunk/tla2-inria@22188 76a6fc44-f60b-0410-a9a8-e67b0e8fc65c

--- org.lamport.tla.toolbox.tool.tlc.ui/src/org/lamport/tla/toolbox/tool/tlc/ui/view/TLCErrorView.java
+++ org.lamport.tla.toolbox.tool.tlc.ui/src/org/lamport/tla/toolbox/tool/tlc/ui/view/TLCErrorView.java
@@ -230,6 +230,10 @@ public class TLCErrorView extends ViewPart
                 this.setTraceInput(states);
                 traceExplorerComposite.changeExploreEnablement(true);
             }
+            if (states != null && !states.isEmpty())
+            {
+                variableViewer.expandToLevel(2);
+            }
 
             this.form.setText(modelName);
 
