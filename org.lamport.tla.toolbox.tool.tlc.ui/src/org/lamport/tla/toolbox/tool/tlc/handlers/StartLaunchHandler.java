0e2482dd0fee1a0ba42878abe040bf4f3b9ff41d
kuppe <kuppe@76a6fc44-f60b-0410-a9a8-e67b0e8fc65c>
2011-07-06 07:51:51 +0000

    NEW - bug 152: Improve keyboard experience http://bugzilla.tlaplus.net/show_bug.cgi?id=152 - F11 starts model checking on current active model
    git-svn-id: svn+ssh://svn.msr-inria.inria.fr/var/lib/svn/repository/tla/trunk/tla2-inria@22243 76a6fc44-f60b-0410-a9a8-e67b0e8fc65c

--- org.lamport.tla.toolbox.tool.tlc.ui/src/org/lamport/tla/toolbox/tool/tlc/handlers/StartLaunchHandler.java
+++ org.lamport.tla.toolbox.tool.tlc.ui/src/org/lamport/tla/toolbox/tool/tlc/handlers/StartLaunchHandler.java
@@ -5,51 +5,84 @@ import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.NullProgressMonitor;
-import org.eclipse.debug.core.ILaunch;
 import org.eclipse.debug.core.ILaunchConfiguration;
-import org.eclipse.jface.viewers.ISelection;
-import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.handlers.HandlerUtil;
 import org.lamport.tla.toolbox.tool.tlc.launch.TLCModelLaunchDelegate;
-import org.lamport.tla.toolbox.tool.tlc.ui.TLCUIActivator;
+import org.lamport.tla.toolbox.tool.tlc.ui.editor.ModelEditor;
 import org.lamport.tla.toolbox.tool.tlc.util.ModelHelper;
 
 /**
- * Runs a model
- * This class should no longer be used because the run
- * menu item in the spec explorer pop-up menu has been removed.
- * @author Simon Zambrovski
- * @deprecated
- * @version $Id$
+ * Initiates a model checker run
  */
-public class StartLaunchHandler extends AbstractHandler
-{
+public class StartLaunchHandler extends AbstractHandler {
 
-    public Object execute(ExecutionEvent event) throws ExecutionException
-    {
-        ISelection selection = HandlerUtil.getCurrentSelection(event);
-        if (selection instanceof IStructuredSelection && !((IStructuredSelection) selection).isEmpty())
-        {
-            Object element = ((IStructuredSelection) selection).getFirstElement();
-            if (element instanceof ILaunchConfiguration)
-            {
-                ILaunchConfiguration config = (ILaunchConfiguration) element;
-                try
-                {
-                    if (ModelHelper.isModelRunning(config) || ModelHelper.isModelStale(config))
-                    {
-                        return null;
-                    }
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		final String activeEditorId = HandlerUtil.getActiveEditorId(event);
+		if (activeEditorId != null && activeEditorId.startsWith(ModelEditor.ID)) {
 
-                    ILaunch launch = config.launch(TLCModelLaunchDelegate.MODE_MODELCHECK, new NullProgressMonitor(),
-                            false);
-                    System.out.println("Launched " + launch + " is executed");
-                } catch (CoreException e)
-                {
-                    TLCUIActivator.logError("Error launching the model", e);
-                }
-            }
-        }
-        return null;
-    }
+			final ModelEditor modelEditor = (ModelEditor) HandlerUtil.getActiveEditor(event);
+			final ILaunchConfiguration config = modelEditor.getConfig().getOriginal();
+
+			// 0) model check already running for the given model
+			try {
+				if (ModelHelper.isModelRunning(config)) {
+					return null;
+				}
+			} catch (CoreException e1) {
+				// why oh why does isModelRunning throw an exception?!
+				return null;
+			}
+
+			// 1) if editor is dirty, ask if we should save
+			final Shell shell = HandlerUtil.getActiveShell(event);
+			if (modelEditor.isDirty()) {
+				boolean save = MessageDialog.openQuestion(shell, "Save model?",
+						"The current model has not been saved, should the model be saved prior to launching?");
+				if (save) {
+					// TODO decouple from ui thread
+					modelEditor.doSaveWithoutValidating(new NullProgressMonitor());
+				} else {
+					return null;
+				}
+			}
+
+			// 2) model might be locked
+			if (modelEditor.isModelLocked()) {
+				boolean unlock = MessageDialog
+						.openQuestion(shell, "Unlock model?",
+								"The current model is locked, but has to be unlocked prior to launching. Should the model be unlocked?");
+				if (unlock) {
+					try {
+						ModelHelper.setModelLocked(config, false);
+					} catch (CoreException e) {
+						throw new ExecutionException(e.getMessage(), e);
+					}
+				} else {
+					return null;
+				}
+			}
+
+			// 3) model might be stale
+			if (modelEditor.isModelStale()) {
+				boolean unlock = MessageDialog
+						.openQuestion(shell, "Repair model?",
+								"The current model is stale and has to be repaird prior to launching. Should the model be repaired onw?");
+				if (unlock) {
+					try {
+						ModelHelper.recoverModel(config);
+					} catch (CoreException e) {
+						throw new ExecutionException(e.getMessage(), e);
+					}
+				} else {
+					return null;
+				}
+			}
+
+			// finally launch (for some reason config.launch(..) does not work %)
+			modelEditor.launchModel(TLCModelLaunchDelegate.MODE_MODELCHECK, true);
+		}
+		return null;
+	}
 }
