ef97839fe75f4b4cd7cf747aea92961e80fe4624
kuppe <kuppe@76a6fc44-f60b-0410-a9a8-e67b0e8fc65c>
2011-10-18 09:15:11 +0000

    comments indicating callers added
    git-svn-id: svn+ssh://svn.msr-inria.inria.fr/var/lib/svn/repository/tla/trunk/tla2-inria@24212 76a6fc44-f60b-0410-a9a8-e67b0e8fc65c

--- tlatools/src/tlc2/tool/distributed/DistApp.java
+++ tlatools/src/tlc2/tool/distributed/DistApp.java
@@ -16,59 +16,86 @@ import tlc2.tool.WorkerException;
  */
 public abstract class DistApp implements TraceApp {
 
+	// TLCServer
 	public abstract Boolean getCheckDeadlock();
 
+	// TLCServer
 	public abstract Boolean getPreprocess();
 
+	// TLCServer
 	public abstract String getFileName();
 
+	// TLCServer
 	public abstract String getMetadir();
 
+	// TLCServer
 	public abstract boolean canRecover();
 
 	// Returns a list of initial states.
+	// TLCServer
 	public abstract TLCState[] getInitStates() throws WorkerException;
 
 	// Returns a list of successor states of the state s. /
+	// TLCServer
 	public abstract TLCState[] getNextStates(TLCState s) throws WorkerException;
 
 	// Checks if the state is a valid state.
+	// TLCServer, TLCWorker
 	public abstract void checkState(TLCState s1, TLCState s2)
 			throws WorkerException;
 
 	// Checks if the state satisfies the state constraints.
+	// TLCServer, TLCWorker
 	public abstract boolean isInModel(TLCState s);
 
 	// Checks if a pair of states satisfy the action constraints.
+	// TLCWorker
 	public abstract boolean isInActions(TLCState s1, TLCState s2);
 
 	// Reconstruct the initial state whose fingerprint is fp.
+	// TLCTrace
+	/* (non-Javadoc)
+	 * @see tlc2.tool.TraceApp#getState(long)
+	 */
 	public abstract TLCStateInfo getState(long fp);
 
 	// Reconstruct the next state of state s whose fingerprint is fp.
+	// TLCTrace
+	/* (non-Javadoc)
+	 * @see tlc2.tool.TraceApp#getState(long, tlc2.tool.TLCState)
+	 */
 	public abstract TLCStateInfo getState(long fp, TLCState s);
 
 	// Reconstruct the info for the transition from s to s1. /
+	// TLCTrace
+	/* (non-Javadoc)
+	 * @see tlc2.tool.TraceApp#getState(tlc2.tool.TLCState, tlc2.tool.TLCState)
+	 */
 	public abstract TLCStateInfo getState(TLCState s1, TLCState s);
 
 	// Enables call stack tracing.
+	// TLCServer
 	public abstract void setCallStack();
 
 	// Prints call stack.
+	// TLCServer
 	public abstract String printCallStack();
 
 	/**
 	 * @return The specification root directory
 	 */
+	// TLCServer
 	public abstract String getSpecDir();
 
 	/**
 	 * @return The (qualified) configuration file name
 	 */
+	// TLCServer
 	public abstract String getConfigName();
 
 	/**
 	 * @return Statistics showing how many states have been computed by this app
 	 */
+	// TLCWorker
 	public abstract long getStatesComputed();
 }
