4004a0aeff7a874e7f8a798f3fce003ce30def79
kuppe <kuppe@76a6fc44-f60b-0410-a9a8-e67b0e8fc65c>
2011-12-16 22:52:55 +0000

    Merge branch 'work'
    git-svn-id: svn+ssh://svn.msr-inria.inria.fr/var/lib/svn/repository/tla/trunk/tla2-inria@25074 76a6fc44-f60b-0410-a9a8-e67b0e8fc65c

--- org.lamport.tla.toolbox.tool.tlc.ui/src/org/lamport/tla/toolbox/tool/tlc/output/data/StateSpaceInformationItem.java
+++ org.lamport.tla.toolbox.tool.tlc.ui/src/org/lamport/tla/toolbox/tool/tlc/output/data/StateSpaceInformationItem.java
@@ -107,6 +107,15 @@ public class StateSpaceInformationItem
 	 */
 	// TODO support formats of SIMULATOR and DFID
 	public static StateSpaceInformationItem parse(String outputMessage) {
+		final String OB = "(";
+		final String AT = ") at ";
+		final String COLON = ": ";
+		final String GENERATED = " states generated (";
+		final String SPM = " s/min), ";
+		final String DISTINCT = " distinct states found (";
+		final String DISTINCT_SPM = " ds/min), ";
+		final String LEFT = " states left on queue.";
+
 		// validated output is properly formatted by trying to find the known
 		// parts at expected locations.
 		int[] i = { outputMessage.indexOf(OB), outputMessage.indexOf(AT),
@@ -117,10 +126,7 @@ public class StateSpaceInformationItem
 
 		for (int j = 0; j < i.length; j++) {
 			if (i[j] == -1) {
-				TLCUIActivator.logError("Error reading progress information",
-						new IllegalArgumentException(outputMessage
-								+ " is in wrong format"));
-				return null;
+				return parseOld(outputMessage);
 			}
 		}
 
@@ -155,17 +161,52 @@ public class StateSpaceInformationItem
 		}
 		return null;
 	}
+	
+    /**
+     * @param outputMessage
+     * @return
+     */
+	public static StateSpaceInformationItem parseOld(String outputMessage) {
+	    final String OB = "(";
+	    final String AT = ") at ";
+	    final String COLON = ": ";
+	    final String GENERATED = " states generated, ";
+	    final String DISTINCT = " distinct states found, ";
+	    final String LEFT = " states left on queue.";
+	    
+		// TODO support formats of SIMULATOR and DFID
+		// "Progress(23) at 2009-08-07 08:20:02: 1604317 states generated, 421523 distinct states found, 109513 states left on queue."
+		int[] i = { outputMessage.indexOf(OB), outputMessage.indexOf(AT),
+				outputMessage.indexOf(COLON), outputMessage.indexOf(GENERATED),
+				outputMessage.indexOf(DISTINCT), outputMessage.indexOf(LEFT) };
+
+		for (int j = 0; j < i.length; j++) {
+			if (i[j] == -1) {
+				TLCUIActivator.logError("Error reading progress information",
+						new IllegalArgumentException(outputMessage
+								+ " is in wrong format"));
+				return null;
+			}
+		}
+
+		try {
+			return new StateSpaceInformationItem(SDF.parse(outputMessage
+					.substring(i[1] + AT.length(), i[2])),
+					Long.parseLong(outputMessage.substring(i[0] + OB.length(),
+							i[1])), Long.parseLong(outputMessage.substring(i[2]
+							+ COLON.length(), i[3])),
+					Long.parseLong(outputMessage.substring(
+							i[3] + GENERATED.length(), i[4])),
+					Long.parseLong(outputMessage.substring(
+							i[4] + DISTINCT.length(), i[5])), 0, 0);
+		} catch (NumberFormatException e) {
+			TLCUIActivator.logError("Error reading progress information", e);
+		} catch (ParseException e) {
+			TLCUIActivator.logError("Error reading progress information", e);
+		}
+		return null;
+	}
 
 	public final static SimpleDateFormat SDF = new SimpleDateFormat(
 			"yyyy-MM-dd HH:mm:ss");
-
-	public final static String OB = "(";
-	public final static String AT = ") at ";
-	public final static String COLON = ": ";
-	public final static String GENERATED = " states generated (";
-	public final static String SPM = " s/min), ";
-	public final static String DISTINCT = " distinct states found (";
-	public final static String DISTINCT_SPM = " ds/min), ";
-	public final static String LEFT = " states left on queue.";
-
 }
