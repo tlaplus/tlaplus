737c997f83c02edadfec9a34c3d4aa7f061f11a1
lamport <lamport@76a6fc44-f60b-0410-a9a8-e67b0e8fc65c>
2011-12-18 22:51:52 +0000

    Errors in GotoPCalSourceHandler show where I'm working.
    git-svn-id: svn+ssh://svn.msr-inria.inria.fr/var/lib/svn/repository/tla/trunk/tla2-inria@25081 76a6fc44-f60b-0410-a9a8-e67b0e8fc65c

--- org.lamport.tla.toolbox.editor.basic/src/org/lamport/tla/toolbox/editor/basic/handlers/GotoPCalSourceHandler.java
+++ org.lamport.tla.toolbox.editor.basic/src/org/lamport/tla/toolbox/editor/basic/handlers/GotoPCalSourceHandler.java
@@ -10,6 +10,12 @@ import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.commands.IHandler;
 import org.eclipse.core.commands.IHandlerListener;
+import org.eclipse.jface.text.BadLocationException;
+import org.eclipse.jface.text.IDocument;
+import org.eclipse.jface.text.IRegion;
+import org.eclipse.jface.text.ITextSelection;
+import org.eclipse.jface.text.Region;
+import org.eclipse.jface.text.source.ISourceViewer;
 import org.lamport.tla.toolbox.editor.basic.TLAEditor;
 import org.lamport.tla.toolbox.editor.basic.util.EditorUtil;
 import org.lamport.tla.toolbox.spec.Spec;
@@ -57,6 +63,43 @@ public class GotoPCalSourceHandler extends AbstractHandler implements IHandler {
             return null;
         }
         MappingObject[][] map = mapping.mapping;
+        
+        // The following code copied with modifications from GotoNextUseHandler
+        // begin copy
+        ISourceViewer internalSourceViewer = tlaEditor.publicGetSourceViewer();
+        IDocument document = internalSourceViewer.getDocument();
+        ITextSelection selection = (ITextSelection) tlaEditor.getSelectionProvider().getSelection();
+        IRegion region = new Region(selection.getOffset(), selection.getLength());
+
+
+        // Sets currentLine to the line of text of the module containing the beginning of the
+        // current region, and sets currentPos to the position of the beginning of the
+        // current region in that line. The Document.getLineLength method seems to include
+        // the length of the line's delimeter, which should not be included in currentLine.
+        String currentLine;
+        int currentPos;
+        int offsetOfLine;
+        try
+        {
+            int lineNumber = document.getLineOfOffset(region.getOffset());
+            int lineDelimLength = 0;
+            String delim = document.getLineDelimiter(lineNumber);
+            if (delim != null)
+            {
+                lineDelimLength = delim.length();
+            }
+            ;
+            offsetOfLine = document.getLineOffset(lineNumber);
+            currentLine = document.get(offsetOfLine, document.getLineLength(lineNumber) - lineDelimLength);
+            currentPos = region.getOffset() - offsetOfLine;
+        } catch (BadLocationException e)
+        {
+            System.out.println("Exception thrown");
+            return null;
+        }
+        // end copy
+XXXXXXXX editing here
+        tlaEditor.selectAndReveal(0, 22);
 // For testing
 MappingObject.printMapping(map);
         return null;
@@ -66,9 +109,19 @@ MappingObject.printMapping(map);
      * @see org.eclipse.core.commands.IHandler#isEnabled()
      */
     public boolean isEnabled() {
-        // TODO Auto-generated method stub
-        return true;
+        Spec spec = ToolboxHandle.getCurrentSpec();
+        if (spec == null)
+        {
+            return false;
+        }
+        TLAEditor tlaEditor = EditorUtil.getTLAEditorWithFocus();
+        if (tlaEditor == null)
+        {
+            return false;
+        }
+        String moduleName = tlaEditor.getModuleName();
+       
+       return spec.getTpMapping(moduleName + ".tla") != null; 
     }
-
 
 }
