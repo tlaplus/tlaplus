0d80ed17995c0d95ccc9e15fc122e33fec5927c9
Markus Alexander Kuppe <tlaplus.net@lemmster.de>
2012-07-05 12:39:43 +0200

    Simplify unit tests and remove redundancy

--- tlatools/test/tlc2/tool/fp/iterator/TLCIteratorTest.java
+++ tlatools/test/tlc2/tool/fp/iterator/TLCIteratorTest.java
@@ -8,7 +8,7 @@ import tlc2.tool.fp.TLCIterator;
 
 import junit.framework.TestCase;
 
-public abstract class TLCIteratorTest extends TestCase {
+public class TLCIteratorTest extends TestCase {
 
 	private TLCIterator itr;
 
@@ -20,7 +20,18 @@ public abstract class TLCIteratorTest extends TestCase {
 		itr = new TLCIterator(getBuffer());
 	}
 	
-	protected abstract long[][] getBuffer();
+	protected long[][] getBuffer() {
+		final long[][] buff = new long[8][];
+		buff[0] = getArray(8, 1, 8);
+		buff[1] = getArray(8, 9, 6);
+		buff[2] = null;
+		buff[3] = null;
+		buff[4] = getArray(8, 15, 3);
+		buff[5] = null;
+		buff[6] = getArray(8, 18, 4);
+		buff[7] = null;
+		return buff;
+	}
 	
 	protected long getLast() {
 		return 21L;
@@ -58,6 +69,9 @@ public abstract class TLCIteratorTest extends TestCase {
 		assertEquals(i, itr.reads());
 	}
 	
+	/**
+	 * Test method for {@link tlc2.tool.fp.TLCIterator#next()}.
+	 */
 	public void testNoNext() {
 		// read up all real elements
 		while (itr.hasNext()) {
