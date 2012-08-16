78eedb79fdb73c7d7525c10559de5fbef9821855
Markus Alexander Kuppe <tlaplus.net@lemmster.de>
2012-08-16 15:28:36 +0200

    Check for non-existence of succeeding fingerprints

--- tlatools/test-long/tlc2/tool/fp/RecoveredOffHeapDiskFPSetTest.java
+++ tlatools/test-long/tlc2/tool/fp/RecoveredOffHeapDiskFPSetTest.java
@@ -60,9 +60,17 @@ public class RecoveredOffHeapDiskFPSetTest extends AbstractFPSetTest {
 		final Random rnd = new Random(RNG_SEED);
 		final FPSet fpSet = getFPSetInitialized();
 		
+		// FPSet has to contain fpset.size fingerprints
 		for (int i = 0; i < fpSet.size(); i++) {
 			long fp = rnd.nextLong();
 			assertTrue(fpSet.contains(fp));
+		}
+		
+		// The remaining fps coming out of the RNG must not be in fpset. We
+		// limit it to 10.000 though.
+		for (int i = 0; i < 10000; i++) {
+			long fp = rnd.nextLong();
+			assertFalse(fpSet.contains(fp));
 		}
 	}
 }
