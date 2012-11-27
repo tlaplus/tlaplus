733c27c142fc53b1ce79e83f430ce48ac7574c9b
lamport <lamport@76a6fc44-f60b-0410-a9a8-e67b0e8fc65c>
2012-11-27 00:36:06 +0000

    Committed some test code to try to figure out how to put up a window with what I want in it.
    git-svn-id: svn+ssh://svn.msr-inria.inria.fr/var/lib/svn/repository/tla/trunk/tla2-inria@29918 76a6fc44-f60b-0410-a9a8-e67b0e8fc65c

--- org.lamport.tla.toolbox.editor.basic/src/org/lamport/tla/toolbox/editor/basic/handlers/DecomposeProofHandler.java
+++ org.lamport.tla.toolbox.editor.basic/src/org/lamport/tla/toolbox/editor/basic/handlers/DecomposeProofHandler.java
@@ -4,6 +4,8 @@
  */
 package org.lamport.tla.toolbox.editor.basic.handlers;
 
+import java.awt.MenuBar;
+
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
@@ -13,7 +15,21 @@ import org.eclipse.jface.text.IDocument;
 import org.eclipse.jface.text.IRegion;
 import org.eclipse.jface.text.TextSelection;
 import org.eclipse.jface.viewers.ISelectionProvider;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.custom.ScrolledComposite;
+import org.eclipse.swt.graphics.Point;
+import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.layout.RowLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Menu;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Text;
 import org.lamport.tla.toolbox.editor.basic.TLAEditor;
+import org.lamport.tla.toolbox.editor.basic.handlers.ShowDeclarationsHandler.ShowDeclarationsPopupDialog;
 import org.lamport.tla.toolbox.editor.basic.util.EditorUtil;
 import org.lamport.tla.toolbox.util.ResourceHelper;
 import org.lamport.tla.toolbox.util.UIHelper;
@@ -156,17 +172,80 @@ public class DecomposeProofHandler extends AbstractHandler implements IHandler {
          * to see how to add things to a Composite nested inside other things.  
          * See also the ScrolledComposite method, which with luck will just be
          * a composite with scrollbars when needed.
-
-
-
          */
+        
         // for testing
-        ThmOrAssumpDefNode theoremName = step.getDef();
-        if (theoremName == null) {
-            System.out.println("Unnamed theorem");
-            return null;
-        }
-        System.out.println(theoremName.getName().toString());
+        Shell topshell = UIHelper.getShellProvider().getShell() ;
+          Shell parent = new Shell(topshell, SWT.SHELL_TRIM) ; // | SWT.H_SCROLL); // SWT.RESIZE) ; // | SWT.V_SCROLL | SWT.H_SCROLL) ;
+          parent.setText("This is set by setText") ;
+          Composite shell = new Composite(parent, SWT.NONE) ;
+        GridLayout gridLayout = new GridLayout(3, false);
+        shell.setLayout(gridLayout);
+        new Button(shell, SWT.PUSH).setText("B1");
+        new Button(shell, SWT.PUSH).setText("Wide Button 2");
+        new Button(shell, SWT.PUSH).setText("Button 3");
+        Button button4 = new Button(shell, SWT.PUSH) ;
+        button4.setText("B4eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee"); 
+        GridData gridData = new GridData() ;
+        gridData.verticalAlignment = SWT.TOP;
+        button4.setLayoutData(gridData);
+
+        Composite comp = new Composite(shell, SWT.NONE) ;
+        GridLayout gl = new GridLayout(1, false);
+        // no margin around the widget.
+        gl.marginWidth = 0;
+        gl.marginHeight = 0;
+        comp.setLayout(gl);
+        Label l1 = new Label(comp, SWT.NONE);
+        l1.setText("Label1");
+        Composite compInner = new Composite(comp, SWT.None) ;
+        GridLayout gin = new GridLayout(2, false) ;
+          gin.marginWidth = 0;
+          gin.marginHeight = 0 ;
+          compInner.setLayout(gin) ;
+          gridData = new GridData() ;
+          gridData.verticalAlignment = SWT.TOP;
+          compInner.setLayoutData(gridData) ;          
+          Button button7 = new Button(compInner, SWT.PUSH) ;
+          button7.setText("B7") ;
+          gridData = new GridData() ;
+          gridData.verticalAlignment = SWT.TOP;
+          button7.setLayoutData(gridData) ;       
+          
+        Label l2 = new Label(compInner, SWT.NONE);
+        l2.setText("Label22\n22222"  + "\n22222222222222222222222222222\n" + 
+                   "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx" 
+                );
+        gridData = new GridData() ;
+        gridData.verticalAlignment = SWT.TOP;
+        l2.setLayoutData(gridData) ; 
+        gridData = new GridData() ;
+//        gridData.horizontalAlignment = SWT.LEFT;
+//        gridData.grabExcessHorizontalSpace = true;
+        gridData.verticalAlignment = SWT.TOP;
+//        gridData.grabExcessVerticalSpace = true;
+        comp.setLayoutData(gridData);
+//        comp.setSize(comp.computeSize(SWT.DEFAULT, SWT.DEFAULT)) ;
+//        sc2.setSize(comp.computeSize(SWT.DEFAULT, SWT.DEFAULT));
+        
+        
+        
+        Label l3 = new Label(shell, SWT.NONE) ;
+        l3.setText("LabelLabel3");
+        gridData = new GridData() ;
+        gridData.verticalAlignment = SWT.FILL;
+        l3.setLayoutData(gridData);
+        shell.pack() ;
+
+        System.out.println("Shell.size = " + shell.getSize().toString());
+        System.out.println("parent.size = " + parent.getSize().toString());
+        Point shellSize = shell.getSize() ;;
+        parent.setSize(shellSize.x + 30, shellSize.y + 30) ;
+        parent.update();
+        parent.open();
+        
+        System.out.println("closed") ;
+
         return null;
     }
 }
