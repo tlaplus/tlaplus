748e203cf6058b1cc5efd610a80cf91563bfdcc4
Markus Alexander Kuppe <tlaplus.net@lemmster.de>
2015-03-18 22:43:01 +0100

    Fix F11 keyboard shortcut (launch model). It used to fail if the model to be launched is neither active (the selected editor) nor has it been launched previously (in this Toolbox instance).
    Technically the handler searches through all open editors (unless the
    active editor happens to be a model or a model has been launched
    before). The first model found in this search is launched with the
    assumption being that only one model editor is usually open.

--- org.lamport.tla.toolbox.tool.tlc.ui/src/org/lamport/tla/toolbox/tool/tlc/handlers/StartLaunchHandler.java
+++ org.lamport.tla.toolbox.tool.tlc.ui/src/org/lamport/tla/toolbox/tool/tlc/handlers/StartLaunchHandler.java
@@ -9,7 +9,9 @@ import org.eclipse.debug.core.ILaunchConfiguration;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.IEditorReference;
+import org.eclipse.ui.IWorkbenchPage;
 import org.eclipse.ui.IWorkbenchSite;
+import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.handlers.HandlerUtil;
 import org.lamport.tla.toolbox.tool.tlc.launch.TLCModelLaunchDelegate;
 import org.lamport.tla.toolbox.tool.tlc.ui.editor.ModelEditor;
@@ -42,16 +44,16 @@ public class StartLaunchHandler extends AbstractHandler {
 				return null;
 			}
 
-	        // 0.5) Ask and save _spec_ editor if it's dirty
+			// 0.5) Ask and save _spec_ editor if it's dirty
 			final Shell shell = HandlerUtil.getActiveShell(event);
 			final IWorkbenchSite site = HandlerUtil.getActiveSite(event);
-	        final IEditorReference[] editors = site.getPage().getEditorReferences();
-	        for (IEditorReference ref : editors) {
+			final IEditorReference[] editors = site.getPage().getEditorReferences();
+			for (IEditorReference ref : editors) {
 				if (OpenSpecHandler.TLA_EDITOR_CURRENT.equals(ref.getId())) {
 					if (ref.isDirty()) {
 						final String title = ref.getName();
-						boolean save = MessageDialog.openQuestion(shell, "Save " + title + " spec?",
-								"The spec " + title + " has not been saved, should the spec be saved prior to launching?");
+						boolean save = MessageDialog.openQuestion(shell, "Save " + title + " spec?", "The spec "
+								+ title + " has not been saved, should the spec be saved prior to launching?");
 						if (save) {
 							// TODO decouple from ui thread
 							ref.getEditor(true).doSave(new NullProgressMonitor());
@@ -61,7 +63,7 @@ public class StartLaunchHandler extends AbstractHandler {
 					}
 				}
 			}
-	        
+
 			// 1) if model editor is dirty, save it
 			if (modelEditor.isDirty()) {
 				// TODO decouple from ui thread
@@ -100,7 +102,8 @@ public class StartLaunchHandler extends AbstractHandler {
 				}
 			}
 
-			// finally launch (for some reason config.launch(..) does not work %)
+			// finally launch (for some reason config.launch(..) does not work
+			// %)
 			modelEditor.launchModel(TLCModelLaunchDelegate.MODE_MODELCHECK, true);
 		}
 		return null;
@@ -112,6 +115,37 @@ public class StartLaunchHandler extends AbstractHandler {
 		if (activeEditorId != null && activeEditorId.startsWith(ModelEditor.ID)) {
 			lastModelEditor = (ModelEditor) HandlerUtil.getActiveEditor(event);
 		}
+		// If lastModelEditor is still null, it means we haven't run the model
+		// checker yet AND the model editor view is *not* active. Lets search
+		// through all editors to find a model checker assuming only a single one
+		// is open right now. If more than one model editor is open, randomly
+		// select one. In case it's not the one intended to be run by the user,
+		// she has to activate the correct model editor manually.
+		//
+		// It is tempting to store the name of the lastModelEditor
+		// in e.g. an IDialogSetting to persistently store even across Toolbox
+		// restarts. However, the only way to identify a model editor here is by
+		// its name and almost all model editors carry the name "Model_1" (the
+		// default name). So we might end up using Model_1 which was the last
+		// model that ran for spec A, but right now spec B and two of its model
+		// editors are open ("Model_1" and "Model_2"). It would launch Model_1,
+		// even though Model_2 might be what the user wants.
+		if (lastModelEditor == null) {
+			final IWorkbenchWindow workbenchWindow = HandlerUtil.getActiveWorkbenchWindow(event);
+			final IWorkbenchPage[] pages = workbenchWindow.getPages();
+			for (final IWorkbenchPage page : pages) {
+				final IEditorReference[] editorReferences = page.getEditorReferences();
+				for (final IEditorReference editorRefs: editorReferences) {
+					if (editorRefs.getId().equals(ModelEditor.ID)) {
+						lastModelEditor = (ModelEditor) editorRefs.getEditor(true);
+						break;
+					}
+				}
+			}
+		}
+		
+		// If the previous two attempts to find a model editor have failed, lets
+		// return whatever we have... which might be null.
 		return lastModelEditor;
 	}
 }
