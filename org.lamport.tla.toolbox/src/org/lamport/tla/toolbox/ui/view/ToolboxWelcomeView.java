e9d611b8003d502544669edd18aa49ab935b151f
Markus Alexander Kuppe <tlaplus.net@lemmster.de>
2015-05-12 09:24:10 +0200

    Update dates to reflect last change in preparation of today's release

--- org.lamport.tla.toolbox/src/org/lamport/tla/toolbox/ui/view/ToolboxWelcomeView.java
+++ org.lamport.tla.toolbox/src/org/lamport/tla/toolbox/ui/view/ToolboxWelcomeView.java
@@ -201,7 +201,7 @@ public class ToolboxWelcomeView extends ViewPart
 		
 		final Label lblVersion = new Label(outerContainer, SWT.WRAP);
 		lblVersion.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, false, false, 2, 1));
-		lblVersion.setText("Version 1.5.0  of 2 May 2015");
+		lblVersion.setText("Version 1.5.0  of 11 May 2015");
 		lblVersion.setBackground(backgroundColor);
     }
 
