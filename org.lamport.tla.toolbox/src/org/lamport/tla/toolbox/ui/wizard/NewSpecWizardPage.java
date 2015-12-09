2f38e7131f1dcc7d4bf7fc920d7f1c8d06f21e73
Markus Alexander Kuppe <tlaplus.net@lemmster.de>
2015-12-09 11:20:22 +0100

    Fix typo in error message
    [Refactor][Toolbox]

--- org.lamport.tla.toolbox/src/org/lamport/tla/toolbox/ui/wizard/NewSpecWizardPage.java
+++ org.lamport.tla.toolbox/src/org/lamport/tla/toolbox/ui/wizard/NewSpecWizardPage.java
@@ -28,7 +28,6 @@ import org.lamport.tla.toolbox.util.UIHelper;
  * A wizard page input of the specification name and the location of the root file
  * 
  * @author Simon Zambrovski
- * @version $Id$
  */
 public class NewSpecWizardPage extends WizardPage
 {
@@ -297,7 +296,7 @@ public class NewSpecWizardPage extends WizardPage
                 Spec existingSpec = Activator.getSpecManager().getSpecByName(specName);
                 if (existingSpec != null)
                 {
-                    reportError("The specification with provided name is already exists \nand uses "
+                    reportError("The specification with provided name already exists \nand uses "
                             + existingSpec.getRootFilename() + " as root module.");
                     return;
                 }
@@ -327,7 +326,7 @@ public class NewSpecWizardPage extends WizardPage
             if (!importExisting.getSelection())
             {
                 reportError("The " + getSpecName() + ".toolbox directory already exists at the provided location."
-                        + "\nPlease select a different specification name or  root-module file.");
+                        + "\nPlease select a different specification name or root-module file.");
                 return;
             }
             importExisting.setEnabled(true);
