ca444092e12886917d4893d939bcc06facc9552c
Markus Alexander Kuppe <tlaplus.net@lemmster.de>
2016-02-24 11:10:06 +0100

    Use Ubuntu 14.04.4 Point Release to reduce startup time.
    Startup time depends on the number of pending security updates. A newer point release obviously requires less updates.
    
    [Refactor][Toolbox]

--- org.lamport.tla.toolbox.jclouds/src/org/lamport/tla/toolbox/jcloud/EC2CloudTLCInstanceParameters.java
+++ org.lamport.tla.toolbox.jclouds/src/org/lamport/tla/toolbox/jcloud/EC2CloudTLCInstanceParameters.java
@@ -50,12 +50,12 @@ public class EC2CloudTLCInstanceParameters extends CloudTLCInstanceParameters {
 
 	@Override
 	public String getImageId() {
-		// Ubuntu 64bit 14.04.3 Trusty paravirtual/instance-store release
+		// Ubuntu 64bit 14.04.4 Trusty paravirtual/instance-store release
 		// https://cloud-images.ubuntu.com/releases/14.04/release/
 		// or http://cloud-images.ubuntu.com/locator/ec2/
 		// See http://aws.amazon.com/amazon-linux-ami/instance-type-matrix/
 		// for paravirtual vs. hvm
-		return "us-east-1/ami-261f3a4c";
+		return "us-east-1/ami-c85163a2";
 	}
 
 	@Override
