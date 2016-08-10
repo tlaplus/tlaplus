af9e200203bda350d0f474b4b5d41d8abfff52a2
Markus Alexander Kuppe <tlaplus.net@lemmster.de>
2016-08-10 11:30:41 +0200

    Use all available memory for FPSet in FPSet tests.
    [Tests][TLC]

--- tlatools/test-concurrent/tlc2/tool/fp/MultiThreadedMSBDiskFPSetTest.java
+++ tlatools/test-concurrent/tlc2/tool/fp/MultiThreadedMSBDiskFPSetTest.java
@@ -10,6 +10,7 @@ public class MultiThreadedMSBDiskFPSetTest extends MultiThreadedFPSetTest {
 	 */
 	@Override
 	protected FPSet getFPSet(final FPSetConfiguration fpSetConfig) throws IOException {
+		fpSetConfig.setRatio(1.0d); // Tests can consume all of the available VM memory
 		return new MSBDiskFPSet(fpSetConfig);
 	}
 }
