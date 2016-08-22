276c016813c533790403c433d47778fcb70cfcaa
Markus Alexander Kuppe <tlaplus.net@lemmster.de>
2016-08-22 14:39:41 +0200

    Log TLC_MODULE_VALUE_JAVA_METHOD_OVERRIDE_LOADED as progress output instead of user output.
    [Bug][Toolbox]

--- org.lamport.tla.toolbox.tool.tlc.ui/src/org/lamport/tla/toolbox/tool/tlc/output/data/TLCModelLaunchDataProvider.java
+++ org.lamport.tla.toolbox.tool.tlc.ui/src/org/lamport/tla/toolbox/tool/tlc/output/data/TLCModelLaunchDataProvider.java
@@ -345,6 +345,7 @@ public class TLCModelLaunchDataProvider implements ITLCOutputListener
                 case EC.TLC_STATS_SIMU:
                 case EC.TLC_SEARCH_DEPTH:
                 case EC.TLC_LIVE_IMPLIED:
+                case EC.TLC_MODULE_VALUE_JAVA_METHOD_OVERRIDE_LOADED:
                     setDocumentText(this.progressOutput, outputMessage, true);
                     break;
                 case EC.TLC_SUCCESS:
