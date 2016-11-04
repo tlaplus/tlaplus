4b3749b0a53d734b153ab8e7f2e76ce3a7096e0a
pron <ron.pressler@gmail.com>
2016-11-04 15:50:07 +0200

    QuickDiff

--- org.lamport.tla.toolbox.editor.basic/src/org/lamport/tla/toolbox/editor/basic/TLALastSaveReferenceProvider.java
+++ org.lamport.tla.toolbox.editor.basic/src/org/lamport/tla/toolbox/editor/basic/TLALastSaveReferenceProvider.java
@@ -2,6 +2,7 @@ package org.lamport.tla.toolbox.editor.basic;
 
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.jface.text.AbstractDocument;
+import org.eclipse.jface.text.Document;
 import org.eclipse.jface.text.IDocument;
 import org.eclipse.ui.internal.editors.quickdiff.LastSaveReferenceProvider;
 import org.eclipse.ui.texteditor.ITextEditor;
@@ -9,17 +10,19 @@ import org.eclipse.ui.texteditor.ITextEditor;
 import tla2unicode.TLAUnicode;
 
 public class TLALastSaveReferenceProvider extends LastSaveReferenceProvider {
-	private TLAEditor fEditor;
+//	private TLAEditor fEditor;
     private long fModificationStamp = -1;
-	
+    
 	@Override
 	public IDocument getReference(IProgressMonitor monitor) {
-		final IDocument doc = super.getReference(monitor);
+		IDocument doc = super.getReference(monitor);
 		if (TLAUnicodeReplacer.isUnicode()) {
 			if (doc instanceof AbstractDocument) {
 				final long mod = ((AbstractDocument)doc).getModificationStamp();
 				if (mod != fModificationStamp) {
-					doc.set(TLAUnicode.convert(true, doc.get()));
+					// doc.set(fEditor.getAsciiDocument().get());
+//					doc.set(TLAUnicode.convert(true, doc.get()));
+					doc = new Document(TLAUnicode.convert(true, doc.get()));
 					this.fModificationStamp = ((AbstractDocument) doc).getModificationStamp();
 				}
 			}
@@ -27,10 +30,10 @@ public class TLALastSaveReferenceProvider extends LastSaveReferenceProvider {
 		return doc;
 	}
 
-	@Override
-	public void setActiveEditor(ITextEditor targetEditor) {
-		super.setActiveEditor(targetEditor);
-		if (targetEditor instanceof TLAEditor)
-			this.fEditor = (TLAEditor) targetEditor;
-	}
+//	@Override
+//	public void setActiveEditor(ITextEditor targetEditor) {
+//		super.setActiveEditor(targetEditor);
+//		if (targetEditor instanceof TLAEditor)
+//			this.fEditor = (TLAEditor) targetEditor;
+//	}
 }
