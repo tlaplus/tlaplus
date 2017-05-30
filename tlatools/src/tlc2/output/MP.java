30523332c8d164a426a341553e552a9d296d8b51
Ian Morris Nieves <imnieves@gmail.com>
2017-05-30 15:24:52 -0700

    fix bug that would cause error message EC.TLC_INVARIANT_EVALUATION_FAILED to output incorrectly
    [Bug][TLC]

--- tlatools/src/tlc2/output/MP.java
+++ tlatools/src/tlc2/output/MP.java
@@ -363,7 +363,12 @@ public class MP
             break;
 
         case EC.TLC_INVARIANT_EVALUATION_FAILED:
-            b.append("Evaluating invariant %1% failed.");
+            if (parameters.length == 1) {
+                b.append("Evaluating invariant %1% failed.");
+            }
+            else if (parameters.length == 2) {
+                b.append("Evaluating invariant %1% failed.\n%2%");
+            }
             break;
 
         case EC.TLC_ACTION_PROPERTY_VIOLATED_BEHAVIOR:
