589e6fc82ce182b0c49c4c1fb63bc0aae711cf5f
Markus Alexander Kuppe <tlaplus.net@lemmster.de>
2018-05-29 08:46:22 +0200

    Skip installation of azure-cli package because the manual shutdown hack is no longer needed.
    [Refactor][Toolbox]

--- org.lamport.tla.toolbox.jclouds/src/org/lamport/tla/toolbox/jcloud/AzureCloudTLCInstanceParameters.java
+++ org.lamport.tla.toolbox.jclouds/src/org/lamport/tla/toolbox/jcloud/AzureCloudTLCInstanceParameters.java
@@ -160,24 +160,6 @@ public class AzureCloudTLCInstanceParameters extends CloudTLCInstanceParameters 
 	public void mungeBuilder(ContextBuilder builder) {
 		builder.endpoint("https://management.core.windows.net/" + getSubscriptionId());
 	}
-
-	/* (non-Javadoc)
-	 * @see org.lamport.tla.toolbox.jcloud.CloudTLCInstanceParameters#getExtraRepositories()
-	 */
-	@Override
-	public String getExtraRepositories() {
-		return "echo \"deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ wheezy main\" | sudo tee /etc/apt/sources.list.d/azure-cli.list && apt-key adv --keyserver packages.microsoft.com --recv-keys 417A0893";
-	}
-
-	/* (non-Javadoc)
-	 * @see org.lamport.tla.toolbox.jcloud.CloudTLCInstanceParameters#getExtraPackages()
-	 */
-	@Override
-	public String getExtraPackages() {
-		// https://docs.microsoft.com/en-us/cli/azure/install-azure-cli?view=azure-cli-latest#install-on-debianubuntu-with-apt-get
-		// see getExtraRepositories too.
-		return "azure-cli";
-	}
 	
 	/* (non-Javadoc)
 	 * @see org.lamport.tla.toolbox.jcloud.CloudTLCInstanceParameters#getHostnameSetup()
@@ -193,48 +175,5 @@ public class AzureCloudTLCInstanceParameters extends CloudTLCInstanceParameters 
 		// It would also be nice for Azure to offer a public API to query the hostname
 		// (similar to EC2CloudTLCInstanceParameters#getHostnameSetup.
 		return "hostname \"$(hostname).cloudapp.net\" && echo \"$(curl -s ifconfig.co) $(hostname)\" >> /etc/hosts";
-	}
-
-	/* (non-Javadoc)
-	 * @see org.lamport.tla.toolbox.jcloud.CloudTLCInstanceParameters#getCleanup()
-	 */
-	@Override
-	public String getCloudAPIShutdown() {
-		final String servicePrincipal = System.getenv("AZURE_COMPUTE_SERVICE_PRINCIPAL");
-		final String password = System.getenv("AZURE_COMPUTE_SERVICE_PRINCIPAL_PASSWORD");
-		final String tenant = System.getenv("AZURE_COMPUTE_SERVICE_PRINCIPAL_TENANT");
-		if (servicePrincipal == null || password == null || tenant == null) {
-			// Missing credentials.
-			return super.getCloudAPIShutdown();
-		}
-		// What we try to accomplish is to purge the complete Azure Resource Group (a RG
-		// combines all Azure resources associated with the VM (storage, networking,
-		// ips, ...).
-		// The way we do it, is to use the azure CLI to deploy the template in
-		// /tmp/rg.json created by the printf statement under the same resource group
-		// identify we wish to purge. The trick is, that the template defines no
-		// resources whatsoever. This effectively purges the old resources in the
-		// resource group. Idea taken from
-		// http://www.codeisahighway.com/effective-ways-to-delete-resources-in-a-resource-group-on-azure/
-		// bash/screen/ssh apparently fiddle with quotes and which is why the json is base64 encoded 
-		// and decoded on the instance:
-		// { "$schema": "https://schema.management.azure.com/schemas/2015-01-01/deploymentTemplate.json#", "contentVersion": "1.0.0.0", "parameters": {}, "variables": {}, "resources": [], "outputs": {} } | base64
-		//
-		// Unfortunately, the azure CLI needs credentials to talk to the Azure API. For
-		// that, one manually creates a service principal once as described in
-		// https://docs.microsoft.com/en-us/cli/azure/create-an-azure-service-principal-azure-cli?view=azure-cli-latest
-		// and uses it to log into Azure as described in
-		// https://docs.microsoft.com/en-us/cli/azure/authenticate-azure-cli?view=azure-cli-latest.
-		// Using AZURE_COMPUTE_CREDENTIALS and AZURE_COMPUTE_IDENTITY to login with azure CLI
-		// would trigger a two-factor auth for Microsoft FTEs. Not something we want.
-		//
-		// An alternative might be to use an auth.properties file, but this doesn't seem
-		// supported by azure CLI yet. Read "File based authentication" at
-		// https://docs.microsoft.com/en-us/java/azure/java-sdk-azure-authenticate#mgmt-file
-		return "echo eyAiJHNjaGVtYSI6ICJodHRwczovL3NjaGVtYS5tYW5hZ2VtZW50LmF6dXJlLmNvbS9zY2hlbWFzLzIwMTUtMDEtMDEvZGVwbG95bWVudFRlbXBsYXRlLmpzb24jIiwgImNvbnRlbnRWZXJzaW9uIjogIjEuMC4wLjAiLCAicGFyYW1ldGVycyI6IHt9LCAidmFyaWFibGVzIjoge30sICJyZXNvdXJjZXMiOiBbXSwgIm91dHB1dHMiOiB7fSB9Cg== | base64 -d > /tmp/rg.json"
-				+ " && " + "az login --service-principal -u \"" + servicePrincipal + "\" -p " + password + " --tenant "
-				// $(hostname -s) only works iff hostname is correctly setup with getHostnameSetup() above.
-				+ tenant + " && " + "az group deployment create --resource-group $(hostname -s)"
-				+ " --template-file /tmp/rg.json --mode Complete";
 	}
 }
