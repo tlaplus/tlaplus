852eaa3e1ea367d9f0254dbf9c991831663478cd
Markus Alexander Kuppe <tlaplus.net@lemmster.de>
2018-06-26 14:23:45 +0200

    Add more parameter permutations to SubsetValueBenchmark#getRandomSetOfSubsets.
    [Tests][TLC]

--- tlatools/test-benchmark/tlc2/value/SubsetValueBenchmark.java
+++ tlatools/test-benchmark/tlc2/value/SubsetValueBenchmark.java
@@ -25,11 +25,8 @@
  ******************************************************************************/
 package tlc2.value;
 
-import java.io.File;
-
 import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.BenchmarkMode;
-import org.openjdk.jmh.annotations.Fork;
 import org.openjdk.jmh.annotations.Measurement;
 import org.openjdk.jmh.annotations.Mode;
 import org.openjdk.jmh.annotations.Scope;
@@ -47,95 +44,224 @@ public class SubsetValueBenchmark {
 	private static final SubsetValue subset200;
 	private static final SubsetValue subset300;
 	private static final SubsetValue subset400;
-	
+
 	private static final int k = 80000;
+	private static final double d = 0.1d;
+	private static final int k2 = 160000;
+	private static final double d2 = 0.2d;
 
 	static {
 		EnumerableValue.setRandom(15041980L);
 		EnumerableValue.resetRandom();
-		
+
 		FP64.Init();
 
 		subset35 = new SubsetValue(new IntervalValue(1, 35));
+		subset35.normalize();
 		subset60 = new SubsetValue(new IntervalValue(1, 60));
+		subset60.normalize();
 		subset100 = new SubsetValue(new IntervalValue(1, 100));
+		subset100.normalize();
 		subset200 = new SubsetValue(new IntervalValue(1, 200));
+		subset200.normalize();
 		subset300 = new SubsetValue(new IntervalValue(1, 300));
+		subset300.normalize();
 		subset400 = new SubsetValue(new IntervalValue(1, 400));
-		
-		new File("target" + File.separator + "probabilistic").mkdirs();
+		subset400.normalize();
 	}
 
 	@Benchmark
 	@Warmup(iterations = 3, time = 1)
 	@Measurement(iterations = 3, time = 1)
 	@BenchmarkMode(Mode.Throughput)
-//	@Fork(jvmArgsAppend = { "-XX:+UnlockCommercialFeatures", "-XX:+UnlockDiagnosticVMOptions",
-//			"-XX:+DebugNonSafepoints", "-XX:+FlightRecorder",
-//			"-XX:StartFlightRecording=maxage=2h,settings=./test-benchmark/heapstats.jfc,",
-//			"-XX:FlightRecorderOptions=defaultrecording=true,disk=false,repository=/tmp,dumponexit=true,dumponexitpath=./target/probabilisticN035" })
-	public EnumerableValue probabilisticN035() {
-		return subset35.getRandomSetOfSubsets(k, .1d);
+	public EnumerableValue probabilisticN035k80d01() {
+		return subset35.getRandomSetOfSubsets(k, d);
 	}
 
 	@Benchmark
 	@Warmup(iterations = 3, time = 1)
 	@Measurement(iterations = 3, time = 1)
 	@BenchmarkMode(Mode.Throughput)
-//	@Fork(jvmArgsAppend = { "-XX:+UnlockCommercialFeatures", "-XX:+UnlockDiagnosticVMOptions",
-//			"-XX:+DebugNonSafepoints", "-XX:+FlightRecorder",
-//			"-XX:StartFlightRecording=maxage=2h,settings=./test-benchmark/heapstats.jfc,",
-//			"-XX:FlightRecorderOptions=defaultrecording=true,disk=false,repository=/tmp,dumponexit=true,dumponexitpath=./target/probabilisticN060" })
-	public EnumerableValue probabilisticN060() {
-		return subset60.getRandomSetOfSubsets(k, .1d);
+	public EnumerableValue probabilisticN060k80d01() {
+		return subset60.getRandomSetOfSubsets(k, d);
 	}
 
 	@Benchmark
 	@Warmup(iterations = 3, time = 1)
 	@Measurement(iterations = 3, time = 1)
 	@BenchmarkMode(Mode.Throughput)
-//	@Fork(jvmArgsAppend = { "-XX:+UnlockCommercialFeatures", "-XX:+UnlockDiagnosticVMOptions",
-//			"-XX:+DebugNonSafepoints", "-XX:+FlightRecorder",
-//			"-XX:StartFlightRecording=maxage=2h,settings=./test-benchmark/heapstats.jfc,",
-//			"-XX:FlightRecorderOptions=defaultrecording=true,disk=false,repository=/tmp,dumponexit=true,dumponexitpath=./target/probabilisticN100" })
-	public EnumerableValue probabilisticN100() {
-		return subset100.getRandomSetOfSubsets(k, .1d);
+	public EnumerableValue probabilisticN100k80d01() {
+		return subset100.getRandomSetOfSubsets(k, d);
 	}
 
 	@Benchmark
 	@Warmup(iterations = 3, time = 1)
 	@Measurement(iterations = 3, time = 1)
 	@BenchmarkMode(Mode.Throughput)
-//	@Fork(jvmArgsAppend = { "-XX:+UnlockCommercialFeatures", "-XX:+UnlockDiagnosticVMOptions",
-//			"-XX:+DebugNonSafepoints", "-XX:+FlightRecorder",
-//			"-XX:StartFlightRecording=maxage=2h,settings=./test-benchmark/heapstats.jfc,",
-//			"-XX:FlightRecorderOptions=defaultrecording=true,disk=false,repository=/tmp,dumponexit=true,dumponexitpath=./target/probabilisticN200" })
-	public EnumerableValue probabilisticN200() {
-		return subset200.getRandomSetOfSubsets(k, .1d);
+	public EnumerableValue probabilisticN200k80d01() {
+		return subset200.getRandomSetOfSubsets(k, d);
 	}
 
 	@Benchmark
 	@Warmup(iterations = 3, time = 1)
 	@Measurement(iterations = 3, time = 1)
 	@BenchmarkMode(Mode.Throughput)
-//	@Fork(jvmArgsAppend = { "-XX:+UnlockCommercialFeatures", "-XX:+UnlockDiagnosticVMOptions",
-//			"-XX:+DebugNonSafepoints", "-XX:+FlightRecorder",
-//			"-XX:StartFlightRecording=maxage=2h,settings=./test-benchmark/heapstats.jfc,",
-//			"-XX:FlightRecorderOptions=defaultrecording=true,disk=false,repository=/tmp,dumponexit=true,dumponexitpath=./target/probabilisticN300" })
-	public EnumerableValue probabilisticN300() {
-		return subset300.getRandomSetOfSubsets(k, .1d);
+	public EnumerableValue probabilisticN300k80d01() {
+		return subset300.getRandomSetOfSubsets(k, d);
 	}
 
 	@Benchmark
 	@Warmup(iterations = 3, time = 1)
 	@Measurement(iterations = 3, time = 1)
 	@BenchmarkMode(Mode.Throughput)
-//	@Fork(jvmArgsAppend = { "-XX:+UnlockCommercialFeatures", "-XX:+UnlockDiagnosticVMOptions",
-//			"-XX:+DebugNonSafepoints", "-XX:+FlightRecorder",
-//			"-XX:StartFlightRecording=maxage=2h,settings=./test-benchmark/heapstats.jfc,",
-//			"-XX:FlightRecorderOptions=defaultrecording=true,disk=false,repository=/tmp,dumponexit=true,dumponexitpath=./target/probabilisticN400" })
-	public EnumerableValue probabilisticN400() {
-		return subset400.getRandomSetOfSubsets(k, .1d);
+	public EnumerableValue probabilisticN400k80d01() {
+		return subset400.getRandomSetOfSubsets(k, d);
+	}
+
+	@Benchmark
+	@Warmup(iterations = 3, time = 1)
+	@Measurement(iterations = 3, time = 1)
+	@BenchmarkMode(Mode.Throughput)
+	public EnumerableValue probabilisticN035k16d01() {
+		return subset35.getRandomSetOfSubsets(k2, d);
+	}
+
+	@Benchmark
+	@Warmup(iterations = 3, time = 1)
+	@Measurement(iterations = 3, time = 1)
+	@BenchmarkMode(Mode.Throughput)
+	public EnumerableValue probabilisticN060k16d01() {
+		return subset60.getRandomSetOfSubsets(k2, d);
+	}
+
+	@Benchmark
+	@Warmup(iterations = 3, time = 1)
+	@Measurement(iterations = 3, time = 1)
+	@BenchmarkMode(Mode.Throughput)
+	public EnumerableValue probabilisticN100k16d01() {
+		return subset100.getRandomSetOfSubsets(k2, d);
+	}
+
+	@Benchmark
+	@Warmup(iterations = 3, time = 1)
+	@Measurement(iterations = 3, time = 1)
+	@BenchmarkMode(Mode.Throughput)
+	public EnumerableValue probabilisticN200k16d01() {
+		return subset200.getRandomSetOfSubsets(k2, d);
+	}
+
+	@Benchmark
+	@Warmup(iterations = 3, time = 1)
+	@Measurement(iterations = 3, time = 1)
+	@BenchmarkMode(Mode.Throughput)
+	public EnumerableValue probabilisticN300k16d01() {
+		return subset300.getRandomSetOfSubsets(k2, d);
+	}
+
+	@Benchmark
+	@Warmup(iterations = 3, time = 1)
+	@Measurement(iterations = 3, time = 1)
+	@BenchmarkMode(Mode.Throughput)
+	public EnumerableValue probabilisticN400k16d01() {
+		return subset400.getRandomSetOfSubsets(k2, d);
+	}
+	
+	/* d2 */
+
+	@Benchmark
+	@Warmup(iterations = 3, time = 1)
+	@Measurement(iterations = 3, time = 1)
+	@BenchmarkMode(Mode.Throughput)
+	public EnumerableValue probabilisticN035k80d02() {
+		return subset35.getRandomSetOfSubsets(k, d2);
+	}
+
+	@Benchmark
+	@Warmup(iterations = 3, time = 1)
+	@Measurement(iterations = 3, time = 1)
+	@BenchmarkMode(Mode.Throughput)
+	public EnumerableValue probabilisticN060k80d02() {
+		return subset60.getRandomSetOfSubsets(k, d2);
+	}
+
+	@Benchmark
+	@Warmup(iterations = 3, time = 1)
+	@Measurement(iterations = 3, time = 1)
+	@BenchmarkMode(Mode.Throughput)
+	public EnumerableValue probabilisticN100k80d02() {
+		return subset100.getRandomSetOfSubsets(k, d2);
+	}
+
+	@Benchmark
+	@Warmup(iterations = 3, time = 1)
+	@Measurement(iterations = 3, time = 1)
+	@BenchmarkMode(Mode.Throughput)
+	public EnumerableValue probabilisticN200k80d02() {
+		return subset200.getRandomSetOfSubsets(k, d2);
+	}
+
+	@Benchmark
+	@Warmup(iterations = 3, time = 1)
+	@Measurement(iterations = 3, time = 1)
+	@BenchmarkMode(Mode.Throughput)
+	public EnumerableValue probabilisticN300k80d02() {
+		return subset300.getRandomSetOfSubsets(k, d2);
+	}
+
+	@Benchmark
+	@Warmup(iterations = 3, time = 1)
+	@Measurement(iterations = 3, time = 1)
+	@BenchmarkMode(Mode.Throughput)
+	public EnumerableValue probabilisticN400k80d02() {
+		return subset400.getRandomSetOfSubsets(k, d2);
+	}
+
+	@Benchmark
+	@Warmup(iterations = 3, time = 1)
+	@Measurement(iterations = 3, time = 1)
+	@BenchmarkMode(Mode.Throughput)
+	public EnumerableValue probabilisticN035k16d02() {
+		return subset35.getRandomSetOfSubsets(k2, d2);
+	}
+
+	@Benchmark
+	@Warmup(iterations = 3, time = 1)
+	@Measurement(iterations = 3, time = 1)
+	@BenchmarkMode(Mode.Throughput)
+	public EnumerableValue probabilisticN060k16d02() {
+		return subset60.getRandomSetOfSubsets(k2, d2);
+	}
+
+	@Benchmark
+	@Warmup(iterations = 3, time = 1)
+	@Measurement(iterations = 3, time = 1)
+	@BenchmarkMode(Mode.Throughput)
+	public EnumerableValue probabilisticN100k16d02() {
+		return subset100.getRandomSetOfSubsets(k2, d2);
+	}
+
+	@Benchmark
+	@Warmup(iterations = 3, time = 1)
+	@Measurement(iterations = 3, time = 1)
+	@BenchmarkMode(Mode.Throughput)
+	public EnumerableValue probabilisticN200k16d02() {
+		return subset200.getRandomSetOfSubsets(k2, d2);
+	}
+
+	@Benchmark
+	@Warmup(iterations = 3, time = 1)
+	@Measurement(iterations = 3, time = 1)
+	@BenchmarkMode(Mode.Throughput)
+	public EnumerableValue probabilisticN300k16d02() {
+		return subset300.getRandomSetOfSubsets(k2, d2);
+	}
+
+	@Benchmark
+	@Warmup(iterations = 3, time = 1)
+	@Measurement(iterations = 3, time = 1)
+	@BenchmarkMode(Mode.Throughput)
+	public EnumerableValue probabilisticN400k16d02() {
+		return subset400.getRandomSetOfSubsets(k2, d2);
 	}
 }
 
