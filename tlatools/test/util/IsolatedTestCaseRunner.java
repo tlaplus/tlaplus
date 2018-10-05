f4763d800472fd3f5c4ce1913ee01cf46bf2a6d4
Markus Alexander Kuppe <tlaplus.net@lemmster.de>
2018-10-05 09:03:36 -0700

    Run garbage collection prior to each test invocation to free up resources in a single VM run.
    [Tests]

--- tlatools/test/util/IsolatedTestCaseRunner.java
+++ tlatools/test/util/IsolatedTestCaseRunner.java
@@ -1,3 +1,28 @@
+/*******************************************************************************
+ * Copyright (c) 2018 Microsoft Research. All rights reserved. 
+ *
+ * The MIT License (MIT)
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy 
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is furnished to do
+ * so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software. 
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN
+ * AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ * Contributors:
+ *   Markus Alexander Kuppe - initial API and implementation
+ ******************************************************************************/
 package util;
 
 import java.lang.reflect.InvocationTargetException;
@@ -21,9 +46,16 @@ public class IsolatedTestCaseRunner extends Runner {
 			throws InitializationError, ClassNotFoundException, InstantiationException, IllegalAccessException,
 			IllegalArgumentException, InvocationTargetException, NoSuchMethodException, SecurityException {
 	
+		// Since IsolatedTestCaseRunner runs several isolated tests in a single VM, it
+		// is good practice to clean resources before each new test.
+		System.gc();
 		
 		ClassLoader classLoader = IsolatedTestCaseRunner.class.getClassLoader();
 		if (classLoader instanceof URLClassLoader) {
+			// When run within e.g. Eclipse, the classloader might not be of instance
+			// URLClassLoader. In this case, just use the provided class loader which won't
+			// isolate tests. A set of tests can thus only be run in a single VM from ant
+			// or maven which pass a URLClassLoader.
 			classLoader = new IsolatedTestCaseClassLoader((URLClassLoader) classLoader);
 		}
 		delegate = new JUnit4(classLoader.loadClass(testFileClass.getName()));
@@ -41,12 +73,13 @@ public class IsolatedTestCaseRunner extends Runner {
 	
 	private class IsolatedTestCaseClassLoader extends URLClassLoader {
 
-		private final Map<String, Object> cache = new HashMap<>();
+		private final Map<String, Class<?>> cache = new HashMap<>();
 		private final Set<String> packages = new HashSet<>();
 		
 		public IsolatedTestCaseClassLoader(URLClassLoader classLoader) {
 			super(classLoader.getURLs());
 			
+			// All of TLC's java packages. 
 			packages.add("tla2sany");
 			packages.add("pcal");
 			packages.add("util");
@@ -57,8 +90,9 @@ public class IsolatedTestCaseRunner extends Runner {
 		@Override
 		public Class<?> loadClass(String name) throws ClassNotFoundException {
 			if (cache.containsKey(name)) {
-				// Cache to not load classes twice which results in a LinkageError.
-				return (Class<?>) cache.get(name);
+				// Cache to not load classes twice for a single test case which results in a
+				// LinkageError.
+				return cache.get(name);
 			}
 			for (final String pkg : packages) {
 				if (name.startsWith(pkg)) {
