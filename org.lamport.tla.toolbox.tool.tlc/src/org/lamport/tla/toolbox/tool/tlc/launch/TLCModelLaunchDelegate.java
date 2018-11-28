tlaplus/tlaplus

commit 7827c730341378dbb948ca9c6baf4e0fcb22640b

Do not redefine behavior spec, init/next, and properties in MC.tla/.cfg unless needed.
This indirection causes an annoyance with the soon to be introduced cost
model, s.t. costs are reported for MC.tla.

[Feature][TLC]

org.lamport.tla.toolbox.tool.tlc/src/org/lamport/tla/toolbox/tool/tlc/launch/TLCModelLaunchDelegate.java
@@ -12,6 +12,7 @@ import java.util.Hashtable;
 import java.util.List;
 import java.util.Properties;
 import java.util.Vector;
+import java.util.stream.Collectors;
 
 import org.eclipse.core.internal.resources.ResourceException;
 import org.eclipse.core.resources.IFile;
@@ -58,8 +59,10 @@ import org.lamport.tla.toolbox.tool.tlc.job.ITLCJobStatus;
 import org.lamport.tla.toolbox.tool.tlc.job.TLCJobFactory;
 import org.lamport.tla.toolbox.tool.tlc.job.TLCProcessJob;
 import org.lamport.tla.toolbox.tool.tlc.model.Assignment;
+import org.lamport.tla.toolbox.tool.tlc.model.Formula;
 import org.lamport.tla.toolbox.tool.tlc.model.Model;
 import org.lamport.tla.toolbox.tool.tlc.model.ModelWriter;
+import org.lamport.tla.toolbox.tool.tlc.model.TLCSpec;
 import org.lamport.tla.toolbox.tool.tlc.model.TypedSet;
 import org.lamport.tla.toolbox.tool.tlc.output.IProcessOutputSink;
 import org.lamport.tla.toolbox.tool.tlc.util.ModelHelper;
@@ -450,16 +453,31 @@ public class TLCModelLaunchDelegate extends LaunchConfigurationDelegate implemen
             case MODEL_BEHAVIOR_TYPE_SPEC_CLOSED:
 
                 // the specification name-formula pair
-                writer.addFormulaList(ModelWriter.createSourceContent(MODEL_BEHAVIOR_CLOSED_SPECIFICATION,
-                        ModelWriter.SPEC_SCHEME, config), "SPECIFICATION", MODEL_BEHAVIOR_CLOSED_SPECIFICATION);
+            	final String spec = config.getAttribute(MODEL_BEHAVIOR_CLOSED_SPECIFICATION, EMPTY_STRING);
+    			if (model.getSpec().declares(spec)) {
+            		writer.addFormulaList(spec, "SPECIFICATION", MODEL_BEHAVIOR_CLOSED_SPECIFICATION);
+    			} else {
+    				writer.addFormulaList(ModelWriter.createSourceContent(MODEL_BEHAVIOR_CLOSED_SPECIFICATION,
+    						ModelWriter.SPEC_SCHEME, config), "SPECIFICATION", MODEL_BEHAVIOR_CLOSED_SPECIFICATION);
+    			}
                 break;
             case MODEL_BEHAVIOR_TYPE_SPEC_INIT_NEXT:
-
-                // the init and next formulas
-                writer.addFormulaList(ModelWriter.createSourceContent(MODEL_BEHAVIOR_SEPARATE_SPECIFICATION_INIT,
-                        ModelWriter.INIT_SCHEME, config), "INIT", MODEL_BEHAVIOR_SEPARATE_SPECIFICATION_INIT);
-                writer.addFormulaList(ModelWriter.createSourceContent(MODEL_BEHAVIOR_SEPARATE_SPECIFICATION_NEXT,
-                        ModelWriter.NEXT_SCHEME, config), "NEXT", MODEL_BEHAVIOR_SEPARATE_SPECIFICATION_NEXT);
+            	// the init and next formulas
+            	final String init = config.getAttribute(MODEL_BEHAVIOR_SEPARATE_SPECIFICATION_INIT, EMPTY_STRING);
+				if (model.getSpec().declares(init)) {
+            		writer.addFormulaList(init, "INIT", MODEL_BEHAVIOR_SEPARATE_SPECIFICATION_INIT);
+            	} else {
+            		writer.addFormulaList(ModelWriter.createSourceContent(MODEL_BEHAVIOR_SEPARATE_SPECIFICATION_INIT,
+            				ModelWriter.INIT_SCHEME, config), "INIT", MODEL_BEHAVIOR_SEPARATE_SPECIFICATION_INIT);
+            	}
+				
+            	final String next = config.getAttribute(MODEL_BEHAVIOR_SEPARATE_SPECIFICATION_NEXT, EMPTY_STRING);
+				if (model.getSpec().declares(next)) {
+	           		writer.addFormulaList(next, "NEXT", MODEL_BEHAVIOR_SEPARATE_SPECIFICATION_INIT);
+            	} else {
+	                writer.addFormulaList(ModelWriter.createSourceContent(MODEL_BEHAVIOR_SEPARATE_SPECIFICATION_NEXT,
+	                        ModelWriter.NEXT_SCHEME, config), "NEXT", MODEL_BEHAVIOR_SEPARATE_SPECIFICATION_NEXT);
+            	}
                 break;
             }
 
@@ -470,15 +488,16 @@ public class TLCModelLaunchDelegate extends LaunchConfigurationDelegate implemen
             // do not write invariants and properties if there is no behavior spec
             if (hasSpec)
             {
-                // invariants
-                writer.addFormulaList(ModelWriter.createFormulaListContent(config.getAttribute(
-                        MODEL_CORRECTNESS_INVARIANTS, new Vector<String>()), ModelWriter.INVARIANT_SCHEME), "INVARIANT",
-                        MODEL_CORRECTNESS_INVARIANTS);
+            	// invariants (separate those declared in the spec from those declared in the model).
+				final List<String> invariants = config.getAttribute(MODEL_CORRECTNESS_INVARIANTS, new Vector<String>());
+				writer.addFormulaList(
+						createProperties(writer, model.getSpec(), invariants, ModelWriter.INVARIANT_SCHEME),
+						"INVARIANT", MODEL_CORRECTNESS_INVARIANTS);
 
-                // properties
-                writer.addFormulaList(ModelWriter.createFormulaListContent(config.getAttribute(
-                        MODEL_CORRECTNESS_PROPERTIES, new Vector<String>()), ModelWriter.PROP_SCHEME), "PROPERTY",
-                        MODEL_CORRECTNESS_PROPERTIES);
+				// properties
+				final List<String> properties = config.getAttribute(MODEL_CORRECTNESS_PROPERTIES, new Vector<String>());
+				writer.addFormulaList(createProperties(writer, model.getSpec(), properties, ModelWriter.PROP_SCHEME),
+						"PROPERTY", MODEL_CORRECTNESS_PROPERTIES);
             }
 
             monitor.worked(STEP);
@@ -498,6 +517,28 @@ public class TLCModelLaunchDelegate extends LaunchConfigurationDelegate implemen
 
         // we don't want to rebuild the workspace
         return false;
+    }
+    
+	private static List<String[]> createProperties(final ModelWriter writer, final TLCSpec spec, final List<String> properties,
+			final String scheme) {
+		
+		// Convert list of strings such as {"1Inv", "0x \in Nat"} into a subset of
+		// checked formulas (prefixed with 1).
+		final List<Formula> checkedFormula = ModelHelper.deserializeFormulaList(properties);
+
+		// Collect those formula that are declared in the model.
+		final List<Formula> undeclaredFormula = checkedFormula.stream().filter(f -> !spec.declares(f.getFormula()))
+				.collect(Collectors.toList());
+
+		// Create the input for the model writer out of the undeclared formula...
+		final List<String[]> createFormulaListContent = ModelWriter.createFormulaListContentFormula(undeclaredFormula,
+				scheme);
+		
+		// ...and append the declared ones such that the model writer does not redeclare it.
+		checkedFormula.removeAll(undeclaredFormula);
+		checkedFormula.forEach(f -> createFormulaListContent.add(new String[] { f.getFormula(), EMPTY_STRING }));
+		
+		return createFormulaListContent;
     }
 
 	// Copy userModule overrides (see tlc2.tool.Spec.processSpec(SpecObj)) if

org.lamport.tla.toolbox.tool.tlc/src/org/lamport/tla/toolbox/tool/tlc/model/ModelWriter.java
@@ -25,6 +25,7 @@
  ******************************************************************************/
 package org.lamport.tla.toolbox.tool.tlc.model;
 
+import java.util.ArrayList;
 import java.util.Hashtable;
 import java.util.List;
 import java.util.Vector;
@@ -464,7 +465,12 @@ public class ModelWriter
             }
         }
     }
-
+    public void addFormulaList(String element, String keyword, String attributeName) {
+    	final List<String[]> elements = new ArrayList<>(1);
+    	elements.add(new String[] {element, EMPTY_STRING});
+    	addFormulaList(elements, keyword, attributeName);
+    }
+    
     /**
      * Puts (String[])element[0] to CFG file and element[1] to TLA_MC file
      * 
@@ -485,7 +491,7 @@ public class ModelWriter
         {
             String[] element = elements.get(i);
             cfgBuffer.append(element[0]).append(CR);
-            // when a definition in the root module is overriden as a model value
+            // when a definition in the root module is overridden as a model value
             // there is nothing to add to the MC.tla file so, we do not do the following
             if (!element[1].equals(EMPTY_STRING))
             {
@@ -560,7 +566,12 @@ public class ModelWriter
     public static List<String[]> createFormulaListContent(List<String> serializedFormulaList, String labelingScheme)
     {
         List<Formula> formulaList = ModelHelper.deserializeFormulaList(serializedFormulaList);
-        return (createListContent(formulaList, labelingScheme));
+        return createFormulaListContentFormula(formulaList, labelingScheme);
+    }
+    
+    public static List<String[]> createFormulaListContentFormula(List<Formula> serializedFormulaList, String labelingScheme)
+    {
+    	return (createListContent(serializedFormulaList, labelingScheme));
     }
 
     /**

org.lamport.tla.toolbox.tool.tlc/.classpath
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <classpath>
-	<classpathentry kind="con" path="org.eclipse.jdt.launching.JRE_CONTAINER/org.eclipse.jdt.internal.debug.ui.launcher.StandardVMType/JavaSE-1.7"/>
+	<classpathentry kind="con" path="org.eclipse.jdt.launching.JRE_CONTAINER/org.eclipse.jdt.internal.debug.ui.launcher.StandardVMType/JavaSE-1.8"/>
 	<classpathentry kind="con" path="org.eclipse.pde.core.requiredPlugins"/>
 	<classpathentry kind="src" path="src"/>
 	<classpathentry kind="output" path="bin"/>

org.lamport.tla.toolbox.tool.tlc/.settings/org.eclipse.jdt.core.prefs
@@ -1,7 +1,7 @@
 eclipse.preferences.version=1
 org.eclipse.jdt.core.compiler.codegen.inlineJsrBytecode=enabled
-org.eclipse.jdt.core.compiler.codegen.targetPlatform=1.7
-org.eclipse.jdt.core.compiler.compliance=1.7
+org.eclipse.jdt.core.compiler.codegen.targetPlatform=1.8
+org.eclipse.jdt.core.compiler.compliance=1.8
 org.eclipse.jdt.core.compiler.problem.assertIdentifier=error
 org.eclipse.jdt.core.compiler.problem.enumIdentifier=error
-org.eclipse.jdt.core.compiler.source=1.7
+org.eclipse.jdt.core.compiler.source=1.8

org.lamport.tla.toolbox.tool.tlc/src/org/lamport/tla/toolbox/tool/tlc/model/TLCSpec.java
@@ -40,6 +40,8 @@ import org.eclipse.debug.core.ILaunchManager;
 import org.lamport.tla.toolbox.spec.Spec;
 import org.lamport.tla.toolbox.tool.tlc.launch.TLCModelLaunchDelegate;
 
+import tla2sany.modanalyzer.SpecObj;
+
 /**
  * {@link TLCSpec} is the glue between {@link Spec} and {@link Model}. Why do we
  * need glue? The Toolbox is written in a modular style. Thus, it is
@@ -54,11 +56,19 @@ import org.lamport.tla.toolbox.tool.tlc.launch.TLCModelLaunchDelegate;
  */
 public class TLCSpec extends Spec {
 
+	private final Spec spec;
+
 	/**
 	 * Only supposed to be called by {@link TLCSpecFactory}
 	 */
 	TLCSpec(Spec spec) {
 		super(spec.getProject());
+		this.spec = spec;
+	}
+
+	@Override
+	public SpecObj getRootModule() {
+		return this.spec.getRootModule();
 	}
 
 	/**

org.lamport.tla.toolbox/src/org/lamport/tla/toolbox/spec/Spec.java
@@ -35,7 +35,10 @@ import org.lamport.tla.toolbox.util.pref.PreferenceStoreHelper;
 
 import pcal.TLAtoPCalMapping;
 import tla2sany.modanalyzer.SpecObj;
+import tla2sany.semantic.Context;
+import tla2sany.semantic.ExternalModuleTable;
 import util.SimpleFilenameToStream;
+import util.UniqueString;
 
 /**
  * Represents a specification handle in the toolbox
@@ -640,5 +643,20 @@ public class Spec implements IAdaptable {
 			modules.add(module);
 		}
 		return modules;
+	}
+
+	public boolean declares(final String str) {
+		return declares(UniqueString.uniqueStringOf(str));
+	}
+
+	public boolean declares(final UniqueString us) {
+		if (getRootModule() != null) {
+			final ExternalModuleTable externalModuleTable = getRootModule().getExternalModuleTable();
+			final Context context = externalModuleTable.getContextForRootModule();
+			if (context != null) {
+				return context.occurSymbol(us);
+			}
+		}
+		return false;
 	}
 }

tlatools/src/tla2sany/modanalyzer/SpecObj.java
@@ -30,7 +30,7 @@ public class SpecObj
     // The raw file name for the root (top) module, unprocessed by adding
     // ".tla" or by prepending the full file system path to it)
 
-    ExternalModuleTable externalModuleTable = new ExternalModuleTable();
+    final ExternalModuleTable externalModuleTable = new ExternalModuleTable();
     // This is the one ExternalModuleTable for the entire specification;
     // it includes ModuleNode's for the root module, and for all modules
     // that it depends on directly or indirectly by EXTENDS or INSTANCE

tlatools/src/tla2sany/semantic/ExternalModuleTable.java
@@ -108,6 +108,10 @@ public class ExternalModuleTable implements ExploreNode {
     return p.ctxt;
   }
 
+  public final Context getContextForRootModule() {
+	  return getContext(getRootModule().getName());
+  }
+  
   /**
    *  Returns a vector of ModuleNodes, one for each outer module (i.e. not
    *  inner modules) in the specification.  InnerModules can be obtained 

commit 5963d99a3f7449ade76287a73aeb948536d1dd32

Stable order for properties.
Regression caused by 7827c730341378dbb948ca9c6baf4e0fcb22640b

[Bug][Toolbox]

org.lamport.tla.toolbox.tool.tlc/src/org/lamport/tla/toolbox/tool/tlc/launch/TLCModelLaunchDelegate.java
@@ -526,18 +526,26 @@ public class TLCModelLaunchDelegate extends LaunchConfigurationDelegate implemen
 		// checked formulas (prefixed with 1).
 		final List<Formula> checkedFormula = ModelHelper.deserializeFormulaList(properties);
 
-		// Collect those formula that are declared in the model.
-		final List<Formula> undeclaredFormula = checkedFormula.stream().filter(f -> !spec.declares(f.getFormula()))
+		// Create the input for the model writer out of all formula...
+		final List<String[]> createFormulaListContent = ModelWriter.createFormulaListContentFormula(checkedFormula,
+				scheme);
+
+		// Collect those formula that are declared in the spec, not the MC.tla file.
+		final List<Formula> declaredFormula = checkedFormula.stream().filter(f -> spec.declares(f.getFormula()))
 				.collect(Collectors.toList());
 
-		// Create the input for the model writer out of the undeclared formula...
-		final List<String[]> createFormulaListContent = ModelWriter.createFormulaListContentFormula(undeclaredFormula,
-				scheme);
-		
-		// ...and append the declared ones such that the model writer does not redeclare it.
-		checkedFormula.removeAll(undeclaredFormula);
-		checkedFormula.forEach(f -> createFormulaListContent.add(new String[] { f.getFormula(), EMPTY_STRING }));
-		
+		// Override each declared formula with its original declaration. This will cause
+		// ModelWriter.addFormulaList to omit it from the MC.tla file and only add it to MC.cfg.
+		declaredFormula.forEach(f -> createFormulaListContent.set(checkedFormula.indexOf(f),
+				new String[] { f.getFormula(), EMPTY_STRING, String.valueOf(checkedFormula.indexOf(f)) }));
+
+		// createFormulaListContent has to be in the same order as checkedFormula.
+		// Otherwise, TLCModelLaunchDataProvider.createError(TLCRegion,
+		// String) would fail to match the violated invariant to the one stored in the
+		// model. This results in the Toolbox reporting the wrong invariant. As a second
+		// safeguard ModelWriter.createFormulaListContentFormula adds the formula's index
+		// to the String[] which - if present - is eventually used as the formula's index
+		// by ModelWriter.addFormulaList.
 		return createFormulaListContent;
     }
 

org.lamport.tla.toolbox.tool.tlc/src/org/lamport/tla/toolbox/tool/tlc/model/ModelWriter.java
@@ -472,7 +472,7 @@ public class ModelWriter
     }
     
     /**
-     * Puts (String[])element[0] to CFG file and element[1] to TLA_MC file
+     * Puts (String[])element[0] to CFG file and element[1] to TLA_MC file, if element[2] present, uses it as index.
      * 
      * @param elements a list of String[] elements
      * @param keyword the keyword to be used in the CFG file
@@ -496,7 +496,7 @@ public class ModelWriter
             if (!element[1].equals(EMPTY_STRING))
             {
                 tlaBuffer.append(COMMENT).append(keyword + " definition ").append(ATTRIBUTE).append(attributeName)
-                        .append(INDEX).append(i).append(CR);
+                        .append(INDEX).append(element.length > 2 ? element[2] : i).append(CR);
                 tlaBuffer.append(element[1]).append(CR).append(SEP).append(CR);
             }
         }
@@ -571,7 +571,7 @@ public class ModelWriter
     
     public static List<String[]> createFormulaListContentFormula(List<Formula> serializedFormulaList, String labelingScheme)
     {
-    	return (createListContent(serializedFormulaList, labelingScheme));
+    	return createListContent(serializedFormulaList, labelingScheme);
     }
 
     /**
@@ -730,7 +730,7 @@ public class ModelWriter
             // formulas
             // to .cfg : <id>
             // to _MC.tla : <id> == <expression>
-            content = new String[] { label, label + DEFINES_CR + formulaList.get(i).getFormula() };
+            content = new String[] { label, label + DEFINES_CR + formulaList.get(i).getFormula(), String.valueOf(i) };
             resultContent.add(content);
         }
         return resultContent;