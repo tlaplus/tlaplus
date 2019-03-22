e627b49b78cc6933b682d09af48026a07b8dd11f
Markus Alexander Kuppe <tlaplus.net@lemmster.de>
2019-03-22 11:14:16 -0700

    Visually more appealing "skipped" statement.
    [Refactor][Toolbox]

--- org.lamport.tla.toolbox.jclouds/src/org/lamport/tla/toolbox/jcloud/CloudDistributedTLCJob.java
+++ org.lamport.tla.toolbox.jclouds/src/org/lamport/tla/toolbox/jcloud/CloudDistributedTLCJob.java
@@ -199,8 +199,8 @@ public class CloudDistributedTLCJob extends Job {
 						"Lookup succeeded thus skipping provisioning steps 5 to 7",
 						nodes > 1 ? "" : "a ", nodes > 1 ? "s" : ""));
 				// skipped provisionNodes(...) which takes 35 steps.
-				monitor.subTask("(skipped)");
-				monitor.subTask("(skipped)");
+				monitor.subTask("--- skipped ---");
+				monitor.subTask("--- skipped ---");
 				monitor.worked(35);
 			}
 
