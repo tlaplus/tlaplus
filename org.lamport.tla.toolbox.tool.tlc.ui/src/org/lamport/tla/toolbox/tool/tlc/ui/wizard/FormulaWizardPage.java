5fe3a86e03b49fe7286c4bcba417ae138c4d76b3
Markus Alexander Kuppe <tlaplus.net@lemmster.de>
2019-03-27 23:33:09 -0700

    Add syntax highlighting and undo/redo support to Error Trace Exploration input boxes.
    Fixes GitHub issue #264
    https://github.com/tlaplus/tlaplus/issues/264
    
    [Feature][Toolbox][Changelog]

--- org.lamport.tla.toolbox.tool.tlc.ui/src/org/lamport/tla/toolbox/tool/tlc/ui/wizard/FormulaWizardPage.java
+++ org.lamport.tla.toolbox.tool.tlc.ui/src/org/lamport/tla/toolbox/tool/tlc/ui/wizard/FormulaWizardPage.java
@@ -1,15 +1,23 @@
 package org.lamport.tla.toolbox.tool.tlc.ui.wizard;
 
 import org.eclipse.jface.text.Document;
+import org.eclipse.jface.text.IDocumentPartitioner;
+import org.eclipse.jface.text.ITextOperationTarget;
 import org.eclipse.jface.text.source.SourceViewer;
 import org.eclipse.jface.wizard.WizardPage;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.custom.StyledText;
+import org.eclipse.swt.events.KeyEvent;
+import org.eclipse.swt.events.KeyListener;
 import org.eclipse.swt.events.ModifyEvent;
 import org.eclipse.swt.events.ModifyListener;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
+import org.lamport.tla.toolbox.editor.basic.TLAEditorActivator;
+import org.lamport.tla.toolbox.editor.basic.TLAFastPartitioner;
+import org.lamport.tla.toolbox.editor.basic.TLAPartitionScanner;
+import org.lamport.tla.toolbox.editor.basic.TLASourceViewerConfiguration;
 import org.lamport.tla.toolbox.tool.tlc.ui.util.FormHelper;
 
 /**
@@ -38,7 +46,30 @@ public class FormulaWizardPage extends WizardPage
         GridLayout layout = new GridLayout(1, false);
         container.setLayout(layout);
 
-        sourceViewer = FormHelper.createSourceViewer(container, SWT.BORDER | SWT.MULTI | SWT.H_SCROLL | SWT.V_SCROLL);
+		sourceViewer = FormHelper.createSourceViewer(container, SWT.BORDER | SWT.MULTI | SWT.H_SCROLL | SWT.V_SCROLL,
+				new TLASourceViewerConfiguration());
+		sourceViewer.getTextWidget().addKeyListener(new KeyListener() {
+			@Override
+			public void keyPressed(KeyEvent e) {
+				if (isUndoKeyPress(e)) {
+					sourceViewer.doOperation(ITextOperationTarget.UNDO);
+				} else if (isRedoKeyPress(e)) {
+					sourceViewer.doOperation(ITextOperationTarget.REDO);
+				}
+			}
+
+			private boolean isRedoKeyPress(KeyEvent e) {
+				return ((e.stateMask & SWT.CONTROL) > 0) && ((e.keyCode == 'y') || (e.keyCode == 'Y'));
+			}
+
+			private boolean isUndoKeyPress(KeyEvent e) {
+				return ((e.stateMask & SWT.CONTROL) > 0) && ((e.keyCode == 'z') || (e.keyCode =='Z'));
+			}
+
+			@Override
+			public void keyReleased(KeyEvent e) {
+			}
+		});
 
         // SWT.FILL causes the text field to expand and contract
         // with changes in size of the dialog window.
@@ -60,10 +91,16 @@ public class FormulaWizardPage extends WizardPage
         control.setEditable(true);
         control.setLayoutData(gd);
 
+    
+
         if (this.document == null)
         {
             this.document = new Document();
         }
+		final IDocumentPartitioner partitioner = new TLAFastPartitioner(
+				TLAEditorActivator.getDefault().getTLAPartitionScanner(), TLAPartitionScanner.TLA_PARTITION_TYPES);
+		document.setDocumentPartitioner(TLAPartitionScanner.TLA_PARTITIONING, partitioner);
+		partitioner.connect(document);
         sourceViewer.setDocument(this.getDocument());
         setControl(container);
     }
