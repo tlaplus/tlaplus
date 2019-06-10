tlaplus/tlaplus

commit ea70206be3be2846255a8fa6307e7e16127a9dc0

Print process id (pid) as part of TLC's startup banner with JVM version -gt 1.8.
[Feature][TLC]

tlatools/src/tlc2/TLC.java
@@ -880,6 +880,8 @@ public class TLC
     		final String osVersion = System.getProperty("os.version");
     		final String osArch = System.getProperty("os.arch");
     		
+    		final long pid = TLCRuntime.getInstance().pid();
+    		
     		final RandomGenerator rng = new RandomGenerator();
             // Start checking:
             if (isSimulate)
@@ -896,7 +898,8 @@ public class TLC
 				MP.printMessage(EC.TLC_MODE_SIMU,
 						new String[] { String.valueOf(seed), String.valueOf(TLCGlobals.getNumWorkers()),
 								TLCGlobals.getNumWorkers() == 1 ? "" : "s", cores, osName, osVersion, osArch, vendor,
-								version, arch, Long.toString(heapMemory), Long.toString(offHeapMemory) });
+								version, arch, Long.toString(heapMemory), Long.toString(offHeapMemory),
+								pid == -1 ? "" : String.valueOf(pid) });
 				Simulator simulator = new Simulator(mainFile, configFile, traceFile, deadlock, traceDepth, 
                         traceNum, rng, seed, true, resolver, specObj, TLCGlobals.getNumWorkers());
                 TLCGlobals.simulator = simulator;
@@ -914,7 +917,8 @@ public class TLC
 				final String[] parameters = new String[] { String.valueOf(TLCGlobals.getNumWorkers()),
 						TLCGlobals.getNumWorkers() == 1 ? "" : "s", cores, osName, osVersion, osArch, vendor, version,
 						arch, Long.toString(heapMemory), Long.toString(offHeapMemory),
-						Long.toString(EnumerableValue.getRandomSeed()), Integer.toString(fpIndex) };
+						Long.toString(EnumerableValue.getRandomSeed()), Integer.toString(fpIndex),
+						pid == -1 ? "" : String.valueOf(pid) };
 
             	// model checking
         		AbstractChecker mc = null;

tlatools/src/tlc2/output/MP.java
@@ -807,13 +807,25 @@ public class MP
             b.append("Finished in %1% at (").append(SDF.format(new Date())).append(")");
             break;
         case EC.TLC_MODE_MC:
-            b.append("Running breadth-first search Model-Checking with fp %13% and seed %12% with %1% worker%2% on %3% cores with %10%MB heap and %11%MB offheap memory (%4% %5% %6%, %7% %8% %9%).");
+            b.append("Running breadth-first search Model-Checking with fp %13% and seed %12% with %1% worker%2% on %3% cores with %10%MB heap and %11%MB offheap memory");
+            if (!"".equals(parameters[13])) {
+            	b.append(" [pid: %14%]");
+            }
+            b.append(" (%4% %5% %6%, %7% %8% %9%).");
             break;
         case EC.TLC_MODE_MC_DFS:
-            b.append("Running depth-first search Model-Checking with fp %13% and seed %12% with %1% worker%2% on %3% cores with %10%MB heap and %11%MB offheap memory (%4% %5% %6%, %7% %8% %9%).");
+            b.append("Running depth-first search Model-Checking with fp %13% and seed %12% with %1% worker%2% on %3% cores with %10%MB heap and %11%MB offheap memory");
+            if (!"".equals(parameters[13])) {
+            	b.append(" [pid: %14%]");
+            }
+     		b.append(" (%4% %5% %6%, %7% %8% %9%).");
             break;
         case EC.TLC_MODE_SIMU:
-            b.append("Running Random Simulation with seed %1% with %2% worker%3% on %4% cores with %11%MB heap and %12%MB offheap memory (%5% %6% %7%, %8% %9% %10%).");
+            b.append("Running Random Simulation with seed %1% with %2% worker%3% on %4% cores with %11%MB heap and %12%MB offheap memory");
+            if (!"".equals(parameters[12])) {
+            	b.append(" [pid: %13%]");
+            }
+            b.append(" (%5% %6% %7%, %8% %9% %10%).");
             break;
         case EC.TLC_COMPUTING_INIT:
             b.append("Computing initial states...");

tlatools/src/util/TLCRuntime.java
@@ -165,4 +165,22 @@ public class TLCRuntime {
 		}
 		return ARCH.x86;
 	}
+
+	// See java.lang.ProcessHandle.current().pid() or -1 when Java version -lt 9.
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	public long pid() {
+		// Once Java9 is minimum BREE, change to:
+        // return java.lang.ProcessHandle.current().pid();
+		try {
+			// Get class.
+			final ClassLoader classLoader = getClass().getClassLoader();
+	        final Class aClass = classLoader.loadClass("java.lang.ProcessHandle");
+	        // Execute static current()
+	        final Object o = aClass.getMethod("current").invoke(null, (Object[]) null);
+	        // Execute instance method pid()
+	        return (long) aClass.getMethod("pid").invoke(o, (Object[]) null);
+	    } catch (Exception e) {
+			return -1;
+		}
+	}
 }

commit fac937cb640931d784327f540f0238e0d1e279a4

Regression introduced with commit ea70206be3be2846255a8fa6307e7e16127a9dc0.
[Bug][Toolbox]

org.lamport.tla.toolbox.tool.tlc.ui/src/org/lamport/tla/toolbox/tool/tlc/output/data/TLCModelLaunchDataProvider.java
@@ -584,7 +584,7 @@ public class TLCModelLaunchDataProvider implements ITLCOutputListener
         }
     }
 
-	private int getFPIndex(final String startupMessage) {
+	static int getFPIndex(final String startupMessage) {
 		final Matcher matcher = startupMessagePattern.matcher(startupMessage);
 		if (matcher.find()) {
 			return Integer.parseInt(matcher.group(2));
@@ -889,9 +889,9 @@ public class TLCModelLaunchDataProvider implements ITLCOutputListener
     private static final String CR = "\n";
     private static final String EMPTY = "";
 
-	private static final Pattern startupMessagePattern = Pattern.compile(
-			"^Running (depth|breadth)-first search Model-Checking with fp ([0-9]+) and seed "
-			+ "[-0-9]+ with [0-9]+ workers on [0-9]+ cores with .*? heap and .*? offheap memory \\(.*\\).$");
+	private static final Pattern startupMessagePattern = Pattern
+			.compile("^Running (depth|breadth)-first search Model-Checking with fp ([0-9]+) and seed "
+					+ "[-0-9]+ with [0-9]+ worker[s]? on [0-9]+ cores with .*? heap and .*? offheap memory (\\[pid: [0-9]+\\])? \\(.*\\).$");
 
     /**
      * Sets text to a document

tlatools/src/tlc2/TLC.java
@@ -983,7 +983,7 @@ public class TLC
 		return TLCGlobals.DFIDMax == -1;
 	}
 
-	private static Map<String, String> getSimulationRuntime(final long seed) {
+	public static Map<String, String> getSimulationRuntime(final long seed) {
 		final Runtime runtime = Runtime.getRuntime();
 		final long heapMemory = runtime.maxMemory() / 1024L / 1024L;
 		
@@ -1008,7 +1008,7 @@ public class TLC
 		return result;
 	}
 
-	private static Map<String, String> getModelCheckingRuntime(final int fpIndex, final FPSetConfiguration fpSetConfig) {
+	public static Map<String, String> getModelCheckingRuntime(final int fpIndex, final FPSetConfiguration fpSetConfig) {
 		final Runtime runtime = Runtime.getRuntime();
 		final long heapMemory = runtime.maxMemory() / 1024L / 1024L;
 		

tlatools/src/tlc2/output/MP.java
@@ -166,7 +166,7 @@ public class MP
      * @param parameters string of parameters to be replaced in the message
      * @return the formatted message
      */
-    private synchronized static String getMessage(int messageClass, int messageCode, String[] parameters)
+    public synchronized static String getMessage(int messageClass, int messageCode, String[] parameters)
     {
 
         if (parameters == null)
@@ -817,6 +817,13 @@ public class MP
         case EC.TLC_FINISHED:
             b.append("Finished in %1% at (").append(now()).append(")");
             break;
+		/*
+		 * The two startup banners below are parsed by the Toolbox in
+		 * org.lamport.tla.toolbox.tool.tlc.output.data.TLCModelLaunchDataProvider.
+		 * startupMessagePattern.  Remember to update when the banners below 
+		 * get changed 
+		 * (see org.lamport.tla.toolbox.tool.tlc.output.data.TLCModelLaunchDataProviderTest)!!!
+		 */
         case EC.TLC_MODE_MC:
             b.append("Running breadth-first search Model-Checking with fp %13% and seed %12% with %1% worker%2% on %3% cores with %10%MB heap and %11%MB offheap memory");
             if (!"".equals(parameters[13])) {

org.lamport.tla.toolbox.tool.tlc.ui.test/src/org/lamport/tla/toolbox/tool/tlc/output/data/TLCModelLaunchDataProviderTest.java
@@ -1,0 +1,75 @@
+/*******************************************************************************
+ * Copyright (c) 2019 Microsoft Research. All rights reserved. 
+ *
+ * The MIT License (MIT)
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy 
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is furnished to do
+ * so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software. 
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN
+ * AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ * Contributors:
+ *   Markus Alexander Kuppe - initial API and implementation
+ ******************************************************************************/
+package org.lamport.tla.toolbox.tool.tlc.output.data;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import tlc2.TLC;
+import tlc2.TLCGlobals;
+import tlc2.output.EC;
+import tlc2.output.MP;
+import tlc2.tool.fp.FPSetConfiguration;
+
+public class TLCModelLaunchDataProviderTest {
+	
+	@Test
+	public void testDFSSingleWorker() {
+		TLCGlobals.setNumWorkers(1);
+		String startupMessage = MP.getMessage(MP.NONE, EC.TLC_MODE_MC_DFS,
+				TLC.getModelCheckingRuntime(23, new FPSetConfiguration()).values().toArray(new String[0]));		
+		
+		assertEquals(23, TLCModelLaunchDataProvider.getFPIndex(startupMessage));
+	}
+	
+	@Test
+	public void testDFSMultipleWorkers() {
+		TLCGlobals.setNumWorkers(3);
+		String startupMessage = MP.getMessage(MP.NONE, EC.TLC_MODE_MC_DFS,
+				TLC.getModelCheckingRuntime(4, new FPSetConfiguration()).values().toArray(new String[0]));		
+		
+		assertEquals(4, TLCModelLaunchDataProvider.getFPIndex(startupMessage));
+	}
+
+	@Test
+	public void testBFSSingleWorker() {
+		TLCGlobals.setNumWorkers(1);
+		String startupMessage = MP.getMessage(MP.NONE, EC.TLC_MODE_MC,
+				TLC.getModelCheckingRuntime(4711, new FPSetConfiguration()).values().toArray(new String[0]));		
+		
+		assertEquals(4711, TLCModelLaunchDataProvider.getFPIndex(startupMessage));
+	}
+
+	@Test
+	public void testBFSMultipleWorkers() {
+		TLCGlobals.setNumWorkers(2);
+		String startupMessage = MP.getMessage(MP.NONE, EC.TLC_MODE_MC,
+				TLC.getModelCheckingRuntime(42, new FPSetConfiguration()).values().toArray(new String[0]));		
+		
+		assertEquals(42, TLCModelLaunchDataProvider.getFPIndex(startupMessage));
+	}
+}