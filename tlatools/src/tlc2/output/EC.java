tlaplus/tlaplus

commit 67f49ca32c2ca4b03020b43f9a07a2ec8dd67c71

Report underlying TLCRuntimeException instead of generic error.
[Refactor][TLC]

tlatools/src/tlc2/output/MP.java
@@ -16,6 +16,7 @@ import util.Assert;
 import util.DebugPrinter;
 import util.Set;
 import util.ToolIO;
+import util.Assert.TLCRuntimeException;
 
 /**
  * This class is used in the following way to support the replacements of the
@@ -1459,6 +1460,15 @@ public class MP
         DebugPrinter.print("leaving printError(int, String[]) with errorCode "); //$NON-NLS-1$
     }
 
+    public static int printTLCRuntimeException(TLCRuntimeException tre) {
+    	recorder.record(tre.errorCode, (Object[]) new Object[] {tre});
+        DebugPrinter.print("entering printTLCRuntimeException(TLCRuntimeException) with errorCode " + tre.errorCode); //$NON-NLS-1$
+        // write the output
+        ToolIO.out.println(tre.getMessage());
+        DebugPrinter.print("leaving printTLCRuntimeException(TLCRuntimeException) with errorCode "); //$NON-NLS-1$
+        return tre.errorCode;
+    }
+    
     /** 
      * Prints the state
      * @param parameters

tlatools/src/tlc2/TLC.java
@@ -39,6 +39,7 @@ import tlc2.util.NoopStateWriter;
 import tlc2.util.RandomGenerator;
 import tlc2.util.StateWriter;
 import tlc2.value.RandomEnumerableValues;
+import util.Assert.TLCRuntimeException;
 import util.DebugPrinter;
 import util.ExecutionStatisticsCollector;
 import util.FileUtil;
@@ -947,6 +948,8 @@ public class TLC
             {
                 System.gc();
                 return MP.printError(EC.SYSTEM_OUT_OF_MEMORY, e);
+            } else if (e instanceof TLCRuntimeException) {
+            	return MP.printTLCRuntimeException((TLCRuntimeException) e);
             } else if (e instanceof RuntimeException) 
             {
                 // SZ 29.07.2009 

tlatools/test/tlc2/tool/TLCGetNamedUndefinedTest.java
@@ -43,6 +43,5 @@ public class TLCGetNamedUndefinedTest extends ModelCheckerTestCase {
 		assertTrue(recorder.recorded(EC.TLC_FINISHED));
 		assertTrue(recorder.recorded(EC.TLC_MODULE_TLCGET_UNDEFINED));
 		assertTrue(recorder.recorded(EC.TLC_CONFIG_SUBSTITUTION_NON_CONSTANT));
-		assertTrue(recorder.recorded(EC.GENERAL));
 	}
 }

tlatools/test/tlc2/tool/suite/ETest1.java
@@ -25,8 +25,6 @@
  ******************************************************************************/
 package tlc2.tool.suite;
 
-import static org.junit.Assert.assertTrue;
-
 import org.junit.Test;
 
 import tlc2.output.EC;
@@ -35,7 +33,6 @@ public class ETest1 extends SuiteETestCase {
 
 	@Test
 	public void testSpec() {
-		assertTrue(recorder.recorded(EC.GENERAL));
 		assertSubstring("*** Errors: 1\n\n" + "line 18, col 12 to line 18, col 17 of module etest1\n\n"
 				+ "The operator Foo requires 1 arguments.");
 	}

tlatools/test/tlc2/tool/suite/ETest16.java
@@ -25,8 +25,6 @@
  ******************************************************************************/
 package tlc2.tool.suite;
 
-import static org.junit.Assert.assertTrue;
-
 import org.junit.Test;
 
 import tlc2.output.EC;
@@ -35,7 +33,6 @@ public class ETest16 extends SuiteETestCase {
 
 	@Test
 	public void testSpec() {
-		assertTrue(recorder.recorded(EC.GENERAL));
 		assertSubstring("*** Errors: 2\n\n" + "line 5, col 27 to line 5, col 27 of module etest16\n\n"
 				+ "Non-unique fields in constructor.\n\n\n"
 				+ "line 7, col 27 to line 7, col 27 of module etest16\n\n"

tlatools/test/tlc2/tool/suite/ETest2.java
@@ -25,7 +25,7 @@
  ******************************************************************************/
 package tlc2.tool.suite;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
 
 import org.junit.Test;
 
@@ -35,7 +35,7 @@ public class ETest2 extends SuiteETestCase {
 
 	@Test
 	public void testSpec() {
-		assertTrue(recorder.recorded(EC.GENERAL));
+		assertFalse(recorder.recorded(EC.GENERAL));
 		assertSubstring("*** Errors: 1\n\n"
 				+ "line 18, col 12 to line 18, col 14 of module etest2\n\n"
 				+ "The operator Foo requires 2 arguments.");

tlatools/test/tlc2/tool/suite/ETest5.java
@@ -25,7 +25,7 @@
  ******************************************************************************/
 package tlc2.tool.suite;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
 
 import org.junit.Test;
 
@@ -35,6 +35,9 @@ public class ETest5 extends SuiteETestCase {
 
 	@Test
 	public void testSpec() {
-		assertTrue(recorder.recorded(EC.GENERAL));
+		assertFalse(recorder.recorded(EC.GENERAL));
+		assertSubstring("*** Errors: 1\n\n"
+				+ "line 13, col 17 to line 13, col 20 of module etest5\n\n"
+				+ "Unknown operator: `Init'.");
 	}
 }

tlatools/test/tlc2/tool/suite/ETest7.java
@@ -35,7 +35,7 @@ public class ETest7 extends SuiteETestCase {
 
 	@Test
 	public void testSpec() {
-		assertTrue(recorder.recordedWithSubStringValue(EC.GENERAL,
-				"The configuration file substitutes constant C with non-constant Foo."));
+		assertTrue(recorder.recordedWithStringValues(EC.TLC_CONFIG_SUBSTITUTION_NON_CONSTANT,
+				"C", "Foo"));
 	}
 }

commit bef007f1b9daf97278f81777910b5201aad383f9

Toolbox no longer raises an error for TLCRuntimeExecptions but simply prints to output:
See e.g. this spec

```tla
----- MODULE RealNumbers ----
EXTENDS Naturals
VARIABLE x
Init == x = 3
Next == x' = x - 1.5
====================
```

Regression introduced by 67f49ca32c2ca4b03020b43f9a07a2ec8dd67c71.

[Bug][TLC]

tlatools/src/tlc2/output/EC.java
@@ -386,7 +386,7 @@ public interface EC
 	        case TLC_CONFIG_PROPERTY_NOT_CORRECTLY_DEFINED:
 	        case TLC_CONFIG_OP_ARITY_INCONSISTENT:
 	        case TLC_CONFIG_NO_STATE_TYPE:
-	        case TLC_CANT_HANDLE_REAL_NUMBERS:
+	        case TLC_CANT_HANDLE_REAL_NUMBERS: // might also be in the spec
 	        case TLC_NO_MODULES:
 	        	return ERROR_CONFIG_PARSE;
 	        	

tlatools/src/tlc2/output/MP.java
@@ -1471,13 +1471,18 @@ public class MP
         DebugPrinter.print("leaving printError(int, String[]) with errorCode "); //$NON-NLS-1$
     }
 
-    public static int printTLCRuntimeException(TLCRuntimeException tre) {
-    	recorder.record(tre.errorCode, (Object[]) new Object[] {tre});
-        DebugPrinter.print("entering printTLCRuntimeException(TLCRuntimeException) with errorCode " + tre.errorCode); //$NON-NLS-1$
-        // write the output
-        ToolIO.out.println(tre.getMessage());
-        DebugPrinter.print("leaving printTLCRuntimeException(TLCRuntimeException) with errorCode "); //$NON-NLS-1$
-        return tre.errorCode;
+    public static int printTLCRuntimeException(final TLCRuntimeException tre) {
+    	if (tre.parameters != null) {
+    		recorder.record(tre.errorCode, (Object[]) new Object[] {tre});
+    		DebugPrinter.print("entering printTLCRuntimeException(TLCRuntimeException) with errorCode " + tre.errorCode); //$NON-NLS-1$
+    		// write the output
+    		ToolIO.out.println(getMessage(ERROR, tre.errorCode, tre.parameters));
+    		DebugPrinter.print("leaving printTLCRuntimeException(TLCRuntimeException) with errorCode "); //$NON-NLS-1$
+    	} else {
+    		// Legacy code path except actual errorCode instead of EC.General.
+    		printError(tre.errorCode, tre);
+    	}
+    	return tre.errorCode;
     }
     
     /** 

tlatools/src/util/Assert.java
@@ -29,7 +29,7 @@ public class Assert
      */
     public static void fail(int errorCode, String[] parameters)
     {
-        throw new TLCRuntimeException(errorCode, MP.getMessage(errorCode, parameters));
+        throw new TLCRuntimeException(errorCode, parameters, MP.getMessage(errorCode, parameters));
     }
     
     /**
@@ -39,7 +39,7 @@ public class Assert
      */
     public static void fail(int errorCode, String parameter)
     {
-        throw new TLCRuntimeException(errorCode, MP.getMessage(errorCode, parameter));
+        throw new TLCRuntimeException(errorCode, new String[] {parameter}, MP.getMessage(errorCode, parameter));
     }
 
     /**
@@ -72,7 +72,7 @@ public class Assert
     {
         if (!condition) 
         {
-            throw new TLCRuntimeException(errorCode, MP.getMessage(errorCode, parameters));
+            throw new TLCRuntimeException(errorCode, parameters, MP.getMessage(errorCode, parameters));
         }
     }
 
@@ -87,7 +87,7 @@ public class Assert
     {
         if (!condition) 
         {
-            throw new TLCRuntimeException(errorCode, MP.getMessage(errorCode, parameter));
+            throw new TLCRuntimeException(errorCode, new String[] {parameter}, MP.getMessage(errorCode, parameter));
         }
     }
 
@@ -131,6 +131,7 @@ public class Assert
 	public static class TLCRuntimeException extends RuntimeException {
 
 		public final int errorCode;
+		public String[] parameters = null;
 
 		public TLCRuntimeException(String errorMsg) {
 			super(errorMsg);
@@ -145,6 +146,11 @@ public class Assert
 		public TLCRuntimeException(int errorCode, String message, Throwable cause) {
 			super(message, cause);
 			this.errorCode = errorCode;
+		}
+
+		public TLCRuntimeException(int errorCode, String[] parameters, String message) {
+			this(errorCode, message);
+			this.parameters = parameters;
 		}
     }
 }