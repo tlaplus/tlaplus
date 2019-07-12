9607d4a497fbf29dd2b6649c2faf93fe05de73ae
Markus Alexander Kuppe <tlaplus.net@lemmster.de>
2019-07-11 19:38:41 -0700

    Cover other locale.
    [Bug][Tests]

--- tlatools/test/tlc2/output/MPTest.java
+++ tlatools/test/tlc2/output/MPTest.java
@@ -72,6 +72,9 @@ public class MPTest
         MP.printMessage(EC.TLC_PROGRESS_STATS, parameters);
         String[] allMessages = ToolIO.getAllMessages();
         assertEquals(1, allMessages.length);
-        assertTrue(allMessages[0], allMessages[0].contains("3,000,000 states generated (10,000 s/min), 5,000 distinct states found (1,234 ds/min), 1,222,333,444 states left on queue."));
+		assertTrue(allMessages[0], allMessages[0].contains(
+				"3,000,000 states generated (10,000 s/min), 5,000 distinct states found (1,234 ds/min), 1,222,333,444 states left on queue.")
+				|| allMessages[0].contains(
+						"3.000.000 states generated (10.000 s/min), 5.000 distinct states found (1.234 ds/min), 1.222.333.444 states left on queue."));
     }
 }
