c42fa5cdce1f7a7fce7a515a1b72128f2a432b50
Markus Alexander Kuppe <tlaplus.net@lemmster.de>
2019-11-12 17:01:08 -0800

    Prevent ClassCastException triggered by clicking the “Goto Obligations” button on a failed proof obligation.
    java.lang.ClassCastException: class
    org.eclipse.ui.ide.FileStoreEditorInput cannot be cast to class
    org.eclipse.ui.IFileEditorInput (org.eclipse.ui.ide.FileStoreEditorInput
    and org.eclipse.ui.IFileEditorInput are in unnamed module of loader
    org.eclipse.osgi.internal.loader.EquinoxClassLoader @462f6d27)
           at org.lamport.tla.toolbox.editor.basic.TLAEditor$2.resourceChanged(TLAEditor.java:243)
           at org.eclipse.core.internal.events.NotificationManager$1.run(NotificationManager.java:300)
           at org.eclipse.core.runtime.SafeRunner.run(SafeRunner.java:45)
           at org.eclipse.core.internal.events.NotificationManager.notify(NotificationManager.java:290)
           at org.eclipse.core.internal.events.NotificationManager.broadcastChanges(NotificationManager.java:153)
           at org.eclipse.core.internal.resources.Workspace.broadcastPostChange(Workspace.java:379)
           at org.eclipse.core.internal.resources.Workspace.endOperation(Workspace.java:1502)
           at org.eclipse.core.internal.resources.Marker.setAttribute(Marker.java:274)
           at org.eclipse.core.internal.resources.Marker.setAttribute(Marker.java:283)
           at org.lamport.tla.toolbox.editor.basic.util.EditorUtil.setReadOnly(EditorUtil.java:781)
           at org.lamport.tla.toolbox.tool.prover.job.ProverJob.run(ProverJob.java:560)
           at org.eclipse.core.internal.jobs.Worker.run(Worker.java:63)
    
    [Bug][Toolbox]

--- org.lamport.tla.toolbox.editor.basic/src/org/lamport/tla/toolbox/editor/basic/TLAEditor.java
+++ org.lamport.tla.toolbox.editor.basic/src/org/lamport/tla/toolbox/editor/basic/TLAEditor.java
@@ -80,7 +80,7 @@ import org.eclipse.ui.contexts.IContextService;
 import org.eclipse.ui.editors.text.EditorsUI;
 import org.eclipse.ui.editors.text.TextEditor;
 import org.eclipse.ui.handlers.HandlerUtil;
-import org.eclipse.ui.ide.IGotoMarker;
+import org.eclipse.ui.ide.FileStoreEditorInput;
 import org.eclipse.ui.part.FileEditorInput;
 import org.eclipse.ui.texteditor.ChainedPreferenceStore;
 import org.eclipse.ui.texteditor.IDocumentProvider;
@@ -240,16 +240,27 @@ public class TLAEditor extends TextEditor
 
                 for (int i = 0; i < markerChanges.length; i++)
                 {
-                    if (markerChanges[i].getResource().equals(((IFileEditorInput) getEditorInput()).getFile()))
-                    {
-                        UIHelper.runUIAsync(new Runnable() {
-
-                            public void run()
-                            {
-                                refresh();
-                            }
-                        });
-                    }
+                	if (getEditorInput() instanceof IFileEditorInput) {
+                		final IFileEditorInput iFileEditorInput = (IFileEditorInput) getEditorInput();
+                		if (markerChanges[i].getResource().equals(iFileEditorInput.getFile())) {
+                			UIHelper.runUIAsync(new Runnable() {
+                				public void run() {
+                					refresh();
+                				}
+                			});
+                		}
+                	} else if (getEditorInput() instanceof FileStoreEditorInput) {
+						// Leslie reported a ClassCastException triggered by clicking the “Goto
+						// Obligation” button on a failed proof obligation.
+                		final FileStoreEditorInput fsei = (FileStoreEditorInput) getEditorInput();
+                		if (markerChanges[i].getResource().getLocationURI().equals(fsei.getURI())) {
+                			UIHelper.runUIAsync(new Runnable() {
+                				public void run() {
+                					refresh();
+                				}
+                			});
+                		}
+                	}
                 }
             }
         };
