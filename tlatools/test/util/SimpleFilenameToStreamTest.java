c2376fdb889e2d717e93fd8861cab0431b46e62a
loki der quaeler <quaeler@gmail.com>
2020-02-20 14:44:35 -0800

    Accomodating Windows full paths including drive letter - #424
    . Adding a Windows unit test
    
    [Enhancement][Tools]

--- tlatools/test/util/SimpleFilenameToStreamTest.java
+++ tlatools/test/util/SimpleFilenameToStreamTest.java
@@ -20,4 +20,22 @@ public class SimpleFilenameToStreamTest {
 		assertNotNull(file);
 		assertTrue(file.getAbsolutePath() + " does not exist!", file.exists());
 	}
+	
+	/**
+	 * Test whether the fix for #424 still works
+	 */
+	@Test
+	public void testWindowsTLAFileCreation() {
+		if (System.getProperty("os.name").toLowerCase().indexOf("win") > -1) {
+			final String driveLetter = "X:";
+			final String parentDirectory = driveLetter + "\\Develop\\myspecs\\DecentSpec\\";
+			final String child = parentDirectory + "Fromage.tla";
+			final FilenameToStream.TLAFile file = new FilenameToStream.TLAFile(parentDirectory, child, null);
+			final int driveLetterCount = file.getAbsolutePath().split(driveLetter).length - 1;
+			
+			assertTrue("There should be 1 drive letter in the child's absolute path, but there are " + driveLetterCount,
+					   (1 == driveLetterCount));
+		}
+	}
+	
 }
