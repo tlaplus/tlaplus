0ec35c7fae688523deca1b5e7bc7c4488ee547fd
Markus Alexander Kuppe <tlaplus.net@lemmster.de>
2020-04-09 12:24:42 -0700

    Test case for EvaluatingValue that can be used to override TLA+ actions.
    The perfect tool to shoot yourself in the foot!
    
    [Tests][TLC]

---
+++ tlatools/org.lamport.tlatools/test-model/EvaluatingValueTest.tla
@@ -0,0 +1,16 @@
+------------------------ MODULE EvaluatingValueTest ------------------------
+EXTENDS Naturals
+
+VARIABLES x
+
+Init == x = 1
+
+A == x' = FALSE \* This action is replaced by the @Evaluation in EvaluatingValueTest.java
+
+Spec == (x = 1) /\ [][A]_x
+
+Inv == x \in Nat
+
+THEOREM Spec => []Inv
+
+============================================================================
---
+++ tlatools/org.lamport.tlatools/test/tlc2/tool/EvaluatingValueTest.java
@@ -0,0 +1,72 @@
+/*******************************************************************************
+ * Copyright (c) 2020 Microsoft Research. All rights reserved. 
+ *
+ * The MIT License (MIT)
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy 
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is furnished to do
+ * so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software. 
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN
+ * AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ * Contributors:
+ *   Markus Alexander Kuppe - initial API and implementation
+ ******************************************************************************/
+package tlc2.tool;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import tla2sany.semantic.ExprOrOpArgNode;
+import tlc2.output.EC;
+import tlc2.overrides.Evaluation;
+import tlc2.tool.coverage.CostModel;
+import tlc2.tool.impl.Tool;
+import tlc2.tool.liveness.ModelCheckerTestCase;
+import tlc2.util.Context;
+import tlc2.value.impl.BoolValue;
+import tlc2.value.impl.IntValue;
+import tlc2.value.impl.Value;
+import util.UniqueString;
+
+public class EvaluatingValueTest extends ModelCheckerTestCase {
+
+	public EvaluatingValueTest() {
+		super("EvaluatingValueTest");
+	}
+
+	@Test
+	public void testSpec() {
+		assertTrue(recorder.recorded(EC.TLC_FINISHED));
+		// Two states: x = 1 and x = 42
+		assertTrue(recorder.recordedWithStringValues(EC.TLC_STATS, "3", "2", "0"));
+		assertTrue(recorder.recordedWithStringValue(EC.TLC_SEARCH_DEPTH, "2"));
+
+		assertFalse(recorder.recorded(EC.GENERAL));
+
+		assertFalse(recorder.recorded(EC.TLC_TEMPORAL_PROPERTY_VIOLATED));
+		assertFalse(recorder.recorded(EC.TLC_COUNTER_EXAMPLE));
+	}
+
+	@Evaluation(definition = "A", module = "EvaluatingValueTest")
+	public synchronized static Value action(final Tool tool, final ExprOrOpArgNode[] args, final Context c,
+			final TLCState s0, final TLCState s1, final int control, final CostModel cm) {
+
+		// Set value of x variable of successor state to 42. 
+		s1.bind(UniqueString.uniqueStringOf("x"), IntValue.gen(42));
+		
+		return BoolValue.ValTrue;
+	}
+}
