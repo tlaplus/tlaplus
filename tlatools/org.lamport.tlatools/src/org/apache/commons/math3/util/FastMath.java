1d50b7201b0ef32192308a62b5561d796ed99dba
Markus Alexander Kuppe <tlaplus.net@lemmster.de>
2020-10-30 12:54:47 -0700

    Let's paraphrase TLAPS "do not reinvent math" with "do not reimplement math".  However, let's not drag all of (Apache Commons) math in, which is a whopping >2mb jar file.  We only need a few classes of commons-math 3.6.1 so far.
    The NOTICE file that accompanies the (binary) distribution has been
    added to be included by TLC too to comply with the Apache License.
    
    See https://commons.apache.org/proper/commons-math/ and
    https://github.com/apache/commons-math
    
    [Feature][TLC]

---
+++ tlatools/org.lamport.tlatools/src/org/apache/commons/math3/util/FastMath.java
@@ -0,0 +1,11 @@
+package org.apache.commons.math3.util;
+
+public class FastMath {
+
+	public static double sqrt(int d) {
+		// The original implementation of FastMath.java in Apache Commons Math delegates
+		// to Math.sqrt too. However, its FastMath has many more methods in that we drag
+		// in many dependencies. We don't want nor need those dependencies.
+		return Math.sqrt(d);
+	}
+}
