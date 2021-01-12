496db544fb2fff8aa9cc10ada9c442def077b8e0
Markus Alexander Kuppe <tlaplus.net@lemmster.de>
2021-01-12 10:37:15 -0800

    Override TLCState#noneAssigned in TLCStateMut with more efficient variant to speed-up TLC running in the debugger.
    [Feature][TLC][Debugger]

--- tlatools/org.lamport.tlatools/src/tlc2/tool/TLCStateMut.java
+++ tlatools/org.lamport.tlatools/src/tlc2/tool/TLCStateMut.java
@@ -277,7 +277,19 @@ public final class TLCStateMut extends TLCState implements Cloneable, Serializab
     }
     return true;
   }
-  
+
+    @Override
+	public boolean noneAssigned() {
+		int len = this.values.length;
+		for (int i = 0; i < len; i++) {
+			if (values[i] != null) {
+				return false;
+			}
+		}
+		return true;
+	}
+ 
+    @Override
 	public final Set<OpDeclNode> getUnassigned() {
 		// Return sorted set (lexicographical).
 		final Set<OpDeclNode> unassignedVars = new TreeSet<OpDeclNode>(new Comparator<OpDeclNode>() {
