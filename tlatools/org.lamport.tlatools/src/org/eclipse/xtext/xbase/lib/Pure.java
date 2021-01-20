a69804ae48561aa559c682a52edb8c027a902e28
Markus Alexander Kuppe <tlaplus.net@lemmster.de>
2021-01-19 17:35:02 -0800

    Add dummy classes for xtext @Pure and ToStringBuilder to prevent ClassNotFoundExceptions while debugging.  See https://github.com/eclipse/lsp4j/issues/494 for context.
    [Bug][Debugger]

---
+++ tlatools/org.lamport.tlatools/src/org/eclipse/xtext/xbase/lib/Pure.java
@@ -0,0 +1,16 @@
+package org.eclipse.xtext.xbase.lib;
+
+import static java.lang.annotation.ElementType.CONSTRUCTOR;
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+@Documented
+@Retention(RUNTIME)
+@Target({ METHOD, CONSTRUCTOR })
+public @interface Pure {
+
+}
---
+++ tlatools/org.lamport.tlatools/src/org/eclipse/xtext/xbase/lib/util/ToStringBuilder.java
@@ -0,0 +1,15 @@
+package org.eclipse.xtext.xbase.lib.util;
+
+public class ToStringBuilder {
+	
+	public ToStringBuilder(Object instance) {
+	}
+	
+	public ToStringBuilder add(Object value) {
+		return this;
+	}
+
+	public ToStringBuilder add(String fieldName, Object value) {
+		return this;
+	}
+}
