tlaplus/tlaplus

commit e183eec864f2612a6e5000dde5dd75b157c67d3f

Add INextStateFunctor that is applied to each successor state individually as part of evaluating the next-state relation.
Before, the next-state relation was evaluated (and the operations now
wrapped by the functor) executed as part of a loop over the set of
successor states.

[Feature][TLC]

tlatools/src/tlc2/tool/Worker.java
@@ -10,6 +10,7 @@ import java.io.DataOutputStream;
 import java.io.File;
 import java.io.IOException;
 
+import tlc2.TLCGlobals;
 import tlc2.output.EC;
 import tlc2.output.MP;
 import tlc2.tool.queue.IStateQueue;
@@ -19,10 +20,13 @@ import tlc2.util.IdThread;
 import tlc2.util.SetOfStates;
 import tlc2.util.statistics.FixedSizedBucketStatistics;
 import tlc2.util.statistics.IBucketStatistics;
+import util.Assert.TLCRuntimeException;
 import util.FileUtil;
+import util.WrongInvocationException;
 
-public final class Worker extends IdThread implements IWorker {
+public final class Worker extends IdThread implements IWorker, INextStateFunctor {
 
+	protected static final boolean coverage = TLCGlobals.isCoverageEnabled();
 	private static final int INITIAL_CAPACITY = 16;
 	
 	/**
@@ -31,10 +35,12 @@ public final class Worker extends IdThread implements IWorker {
 	 * We expect to get linear speedup with respect to the number of processors.
 	 */
 	private final ModelChecker tlc;
+	private final ITool tool;
 	private final IStateQueue squeue;
 	private final IBucketStatistics outDegree;
 	private final String filename;
 	private final BufferedRandomAccessFile raf;
+	private final boolean checkDeadlock;
 
 	private long lastPtr;
 	private long statesGenerated;
@@ -47,6 +53,9 @@ public final class Worker extends IdThread implements IWorker {
 		// SZ 12.04.2009: added thread name
 		this.setName("TLC Worker " + id);
 		this.tlc = (ModelChecker) tlc;
+		this.checkLiveness = this.tlc.checkLiveness;
+		this.checkDeadlock = this.tlc.checkDeadlock;
+		this.tool = this.tlc.tool;
 		this.squeue = this.tlc.theStateQueue;
 		this.outDegree = new FixedSizedBucketStatistics(this.getName(), 32); // maximum outdegree of 32 appears sufficient for now.
 		this.setName("TLCWorkerThread-" + String.format("%03d", id));
@@ -61,7 +70,6 @@ public final class Worker extends IdThread implements IWorker {
    * updates the state set and state queue.
 	 */
 	public void run() {
-		final boolean checkLiveness = this.tlc.checkLiveness;
 		TLCState curState = null;
 		try {
 			while (true) {
@@ -76,17 +84,28 @@ public final class Worker extends IdThread implements IWorker {
 				}
 				setCurrentState(curState);
 				
-				SetOfStates setOfStates = null;
-				if (checkLiveness) {
+				if (this.checkLiveness) {
+					// Allocate iff liveness is checked.
 					setOfStates = createSetOfStates();
 				}
 				
-				if (this.tlc.doNext(curState, setOfStates, this)) {
-					return;
+				final long preNext = this.statesGenerated;
+				try {
+					this.tool.getNextStates(this, curState);
+				} catch (TLCRuntimeException e) {
+					// The next-state relation couldn't be evaluated.
+					this.tlc.doNextFailed(curState, null, e);
+				}
+				
+				if (this.checkDeadlock && preNext == this.statesGenerated) {
+					// A deadlock is defined as a state without (seen or unseen) successor
+					// states. In other words, evaluating the next-state relation for a state
+					// yields no states.
+	                this.tlc.doNextSetErr(curState, null, false, EC.TLC_DEADLOCK_REACHED, null);
 				}
 				
 	            // Finally, add curState into the behavior graph for liveness checking:
-	            if (checkLiveness)
+	            if (this.checkLiveness)
 	            {
 					doNextCheckLiveness(curState, setOfStates);
 	            }
@@ -112,6 +131,10 @@ public final class Worker extends IdThread implements IWorker {
 	/* Liveness */
 	
 	private int multiplier = 1;
+
+	private SetOfStates setOfStates;
+
+	private final boolean checkLiveness;
 
 	private final void doNextCheckLiveness(TLCState curState, SetOfStates liveNextStates) throws IOException {
 		final long curStateFP = curState.fingerPrint();
@@ -280,5 +303,83 @@ public final class Worker extends IdThread implements IWorker {
 		public void close() throws IOException {
 			this.enumRaf.close();
 		}
+	}
+	
+	//**************************************************************//
+
+	@Override
+	public Object addElement(final TLCState state) {
+		throw new WrongInvocationException("tlc2.tool.Worker.addElement(TLCState) should not be called");
+	}
+
+	@Override
+	public Object addElement(final TLCState curState, final Action action, final TLCState succState) {
+	    if (coverage) { action.cm.incInvocations(); }
+		this.statesGenerated++;
+		
+		try {
+			if (!this.tool.isGoodState(succState)) {
+				this.tlc.doNextSetErr(curState, succState, action);
+				throw new InvariantViolatedException();
+			}
+			
+			// Check if state is excluded by a state or action constraint.
+			final boolean inModel = (this.tool.isInModel(succState) && this.tool.isInActions(curState, succState));
+			
+			// Check if state is new or has been seen earlier.
+			boolean unseen = true;
+			if (inModel) {
+				unseen = !isSeenState(curState, succState, action);
+			}
+			
+			// Check if succState violates any invariant:
+			if (unseen) {
+				if (this.tlc.doNextCheckInvariants(curState, succState)) {
+					throw new InvariantViolatedException();
+				}
+			}
+			
+			// Check if the state violates any implied action. We need to do it
+			// even if succState is not new.
+			if (this.tlc.doNextCheckImplied(curState, succState)) {
+				throw new InvariantViolatedException();
+			}
+			
+			if (inModel && unseen) {
+				// The state is inModel, unseen and neither invariants
+				// nor implied actions are violated. It is thus eligible
+				// for further processing by other workers.
+				this.squeue.sEnqueue(succState);
+			}
+			return this;
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	private final boolean isSeenState(final TLCState curState, final TLCState succState, final Action action)
+			throws IOException {
+		final long fp = succState.fingerPrint();
+		final boolean seen = this.tlc.theFPSet.put(fp);
+		// Write out succState when needed:
+		this.tlc.allStateWriter.writeState(curState, succState, !seen, action);
+		if (!seen) {
+			// Write succState to trace only if it satisfies the
+			// model constraints. Do not enqueue it yet, but wait
+			// for implied actions and invariants to be checked.
+			// Those checks - if violated - will cause model checking
+			// to terminate. Thus we cannot let concurrent workers start
+			// exploring this new state. Conversely, the state has to
+			// be in the trace in case either invariant or implied action
+			// checks want to print the trace.
+			this.writeState(curState, fp, succState);
+			if (coverage) {	action.cm.incSecondary(); }
+		}
+		// For liveness checking:
+		if (this.checkLiveness)
+		{
+			this.setOfStates.put(fp, succState);
+		}
+		return seen;
 	}
 }

tlatools/src/tlc2/tool/INextStateFunctor.java
@@ -1,0 +1,35 @@
+/*******************************************************************************
+ * Copyright (c) 2019 Microsoft Research. All rights reserved. 
+ *
+ * The MIT License (MIT)
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy 
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is furnished to do
+ * so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software. 
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN
+ * AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ * Contributors:
+ *   Markus Alexander Kuppe - initial API and implementation
+ ******************************************************************************/
+package tlc2.tool;
+
+public interface INextStateFunctor extends IStateFunctor {
+
+	Object addElement(final TLCState s, final Action a, final TLCState t);
+	
+	public static class InvariantViolatedException extends RuntimeException {
+		
+	}
+}

tlatools/src/tlc2/tool/ITool.java
@@ -77,6 +77,8 @@ public interface ITool extends TraceApp {
 	   * in the given state.
 	   */
 	StateVec getNextStates(Action action, TLCState state);
+	
+	boolean getNextStates(final INextStateFunctor functor, final TLCState state);
 
 	IValue eval(SemanticNode expr, Context c, TLCState s0);
 

tlatools/src/tlc2/tool/ModelChecker.java
@@ -494,7 +494,7 @@ public class ModelChecker extends AbstractChecker
 		return seen;
 	}
 
-	private final boolean doNextCheckInvariants(final TLCState curState, final TLCState succState) throws IOException, WorkerException, Exception {
+	final boolean doNextCheckInvariants(final TLCState curState, final TLCState succState) throws IOException, WorkerException, Exception {
         int k = 0;
 		try
         {
@@ -525,7 +525,7 @@ public class ModelChecker extends AbstractChecker
 		return false;
 	}
 
-	private final boolean doNextCheckImplied(final TLCState curState, final TLCState succState) throws IOException, WorkerException, Exception {
+	final boolean doNextCheckImplied(final TLCState curState, final TLCState succState) throws IOException, WorkerException, Exception {
 		int k = 0;
         try
         {
@@ -558,7 +558,7 @@ public class ModelChecker extends AbstractChecker
         return false;
 	}
 
-	private final boolean doNextSetErr(TLCState curState, TLCState succState, boolean keep, int ec, String param) throws IOException, WorkerException {
+	final boolean doNextSetErr(TLCState curState, TLCState succState, boolean keep, int ec, String param) throws IOException, WorkerException {
 		synchronized (this)
 		{
 		    if (this.setErrState(curState, succState, keep, ec))
@@ -576,7 +576,7 @@ public class ModelChecker extends AbstractChecker
 		return true;
 	}
 
-	private final boolean doNextSetErr(TLCState curState, TLCState succState, Action action) throws IOException, WorkerException {
+	final boolean doNextSetErr(TLCState curState, TLCState succState, Action action) throws IOException, WorkerException {
 		synchronized (this) {
 			final int errorCode = EC.TLC_STATE_NOT_COMPLETELY_SPECIFIED_NEXT;
 			if (this.setErrState(curState, succState, false, errorCode))
@@ -616,7 +616,7 @@ public class ModelChecker extends AbstractChecker
 		}
 	}
 
-	private final void doNextFailed(TLCState curState, TLCState succState, Throwable e)
+	final void doNextFailed(TLCState curState, TLCState succState, Throwable e)
 			throws IOException, WorkerException, Throwable {
 		// Assert.printStack(e);
 		final boolean keep = ((e instanceof StackOverflowError) || (e instanceof OutOfMemoryError)

tlatools/src/tlc2/tool/StateVec.java
@@ -15,7 +15,7 @@ import util.Assert;
  * updates are used for improved performance and reduced
  * allocation.
  */
-public final class StateVec implements IStateFunctor {
+public final class StateVec implements IStateFunctor, INextStateFunctor {
   private TLCState v[];
   private int size;
 
@@ -85,6 +85,11 @@ public final class StateVec implements IStateFunctor {
     return this;
   }
   
+  @Override
+  public final StateVec addElement(TLCState predecessor, Action action, TLCState state) {
+	  return addElement(state);
+  }
+ 
   public final StateVec addElements(StateVec s1) {
     StateVec s0 = this;
 

commit 4a2c40fd28d120987fa536072d4448c4b0651c78

Evaluation error (EvalException) in state constraint has no counter-example.
This is a regression that was introduced in git commit
e183eec864f2612a6e5000dde5dd75b157c67d3f

[Bug][Regression][TLC]

tlatools/org.lamport.tlatools/src/tlc2/tool/Worker.java
@@ -23,7 +23,6 @@ import tlc2.util.IdThread;
 import tlc2.util.SetOfStates;
 import tlc2.util.statistics.FixedSizedBucketStatistics;
 import tlc2.util.statistics.IBucketStatistics;
-import util.Assert.TLCRuntimeException;
 import util.FileUtil;
 import util.WrongInvocationException;
 
@@ -103,9 +102,12 @@ public final class Worker extends IdThread implements IWorker, INextStateFunctor
 				final long preNext = this.statesGenerated;
 				try {
 					this.tool.getNextStates(this, curState);
-				} catch (TLCRuntimeException | EvalException e) {
-					// The next-state relation couldn't be evaluated.
-					this.tlc.doNextFailed(curState, null, e);
+				} catch (final WrappingRuntimeException e) {
+					// The next-state relation couldn't be evaluated. If doNextFailed itself throws
+					// a Throwable, the catch block below will handle it.
+					this.tlc.doNextFailed(curState, e.unwrapState(), e.unwrapExp());
+				} catch (final Throwable notExpectedToHappen) {
+					this.tlc.doNextFailed(curState, null, notExpectedToHappen);
 				}
 				
 				if (this.checkDeadlock && preNext == this.statesGenerated) {
@@ -373,7 +375,32 @@ public final class Worker extends IdThread implements IWorker, INextStateFunctor
 			}
 			return this;
 		} catch (Exception e) {
-			throw new RuntimeException(e);
+			// We can't throw Exception here because it would violate the contract of
+			// tlc2.tool.INextStateFunctor.addElement(TLCState, Action, TLCState). Thus,
+			// wrap the exception regardless of whether it is a (unchecked) runtime
+			// exception or not. We expect the outer code in run(..) above to unwrap this
+			// exception.  As a bonus, we can attach succState and send it up the stack.
+			throw new WrappingRuntimeException(e, succState);
+		}
+	}
+	
+	@SuppressWarnings("serial")
+	private static class WrappingRuntimeException extends RuntimeException {
+
+		private final Exception e;
+		private final TLCState state;
+
+		public WrappingRuntimeException(final Exception e, final TLCState state) {
+			this.e = e;
+			this.state = state;
+		}
+		
+		public TLCState unwrapState() {
+			return state;
+		}
+
+		public Exception unwrapExp() {
+			return e;
 		}
 	}
 

tlatools/org.lamport.tlatools/test-model/DistBakery.tla
@@ -1,0 +1,215 @@
+----- CONFIG DistBakery ------
+
+CONSTANT
+    Nodes <- const_16137059216052000
+CONSTRAINT
+    constr_16137059216053000
+SPECIFICATION
+    Spec
+INVARIANT
+    TypeOK
+    Mutex
+PROPERTY
+    StarvationFree
+
+=============
+
+----------------------------- MODULE DistBakery -----------------------------
+EXTENDS Integers, Sequences, TLC
+
+CONSTANT Nodes
+ASSUME Nodes \subseteq Int
+
+MutexProcs == [node : Nodes, type : {"mutex"}] 
+MsgProcs   == [node : Nodes, type : {"msg"}]
+
+(***************************************************************************)
+(* \ll is the less-than relation for the lexicographical ordering of pairs *)
+(* of integers.  It is a total ordering on Int \X Int.                     *)
+(***************************************************************************)
+pr1 \ll pr2 == \/ pr1[1] < pr2[1]
+               \/ /\ pr1[1] = pr2[1]
+                  /\ pr1[2] < pr2[2]
+
+(***************************************************************************)
+(* For n a node and pr a <<number, node>> pair NextHigher(pr,n) is the     *)
+(* smallest integer nm such that pr \ll <<nm, n>>.                         *)
+(***************************************************************************)
+NextHigher(pr,n) == IF pr[2] < n  THEN pr[1] ELSE pr[1] + 1 
+
+(***************************************************************************)
+(* If PrSet is a nonempt set of pairs of integers, then MaxPr(PrSet) is    *)
+(* the largest element in PrSet under the \ll ordering.                    *)
+(***************************************************************************)
+MaxPr(PrSet) == CHOOSE max \in PrSet : \A pr \in PrSet \ {max} : pr \ll max
+
+(***************************************************************************)
+(* If num[n1] # 0 and <<num[n1], n1>> \prec <<num[n2], n2>>, then n1 has   *)
+(* priority over n2 in entering its critical section.                      *)
+(***************************************************************************)
+pr1 \prec pr2 == \/ pr2[1] = 0 
+                 \/ pr1 \ll pr2
+
+\* messages used in the algorithm
+Write(c) == [type |-> "write", num |-> c]
+Ack == [type |-> "ack"]
+
+Messages == [type : {"write"}, num : Nat] \cup {Ack}
+(*****
+--algorithm DistributedMutex {
+    variables num  = [n \in Nodes |-> 0] ,
+              rnum = [n \in Nodes |-> [m \in Nodes \ {n} |-> 0]],
+              acks = [n \in Nodes |-> {}],
+              net  = [i \in Nodes |-> [j \in Nodes \ {i} |-> << >>]]             ;
+             
+   macro Broadcast(msg) {
+     net[self.node] := [i \in Nodes \ {self.node} |-> Append(net[self.node][i], msg)]
+   } 
+   
+  macro Rcv(from) {
+    net[from][self.node] := Tail(net[from][self.node])
+  }
+   macro RcvAndReply(from, msg) {
+        net[from][self.node] := Tail(net[from][self.node])  
+     || net[self.node][from] := Append(net[self.node][from], msg)
+   }
+     
+   fair process(mu \in MutexProcs) {
+    ncs:- while (TRUE) {
+            skip;  \* non-critical section
+   enter:   with (nonZeros = {i \in Nodes \ {self.node} : rnum[self.node][i] # 0},
+                  competition = {<<rnum[i], i>> : i \in nonZeros} ) {
+              num[self.node] := IF competition = { } 
+                                  THEN 1
+                                  ELSE NextHigher(MaxPr(competition), self.node) ;
+\*              print <<num[self.node], self>>
+            } ;
+            acks[self.node] := {self.node};
+            Broadcast(Write(num[self.node])) ; 
+      e1:   await  /\ acks[self.node] = Nodes
+                   /\ \A n \in Nodes \ {self.node} : <<num[self.node],self.node>> \prec <<rnum[self.node][n],n>>;
+      cs:   skip;
+    exit:   \* num[self.node] := num[self.node] + 1;
+            Broadcast(Write(0)) ;
+          } \* end while
+   }
+
+
+   fair process(mg \in MsgProcs) {
+    a: while (TRUE) {
+          with (n \in Nodes \ {self.node}) {
+             await net[n][self.node] /= << >> ;
+             with (msg = Head(net[n][self.node]), 
+                   me = self.node) {             
+                if (msg.type = "write") {
+                    rnum[me][n] := msg.num;
+                    if (msg.num = 0) { Rcv(n) }
+                    else { RcvAndReply(n, Ack) } ;
+                }                                                                   
+                else{ acks[me] := acks[me] \cup {n} }
+             }  \* end with
+           }  \* end with
+        }  \* end while
+   }
+}
+***)
+\* BEGIN TRANSLATION (chksum(pcal) = "b2c38cfa" /\ chksum(tla) = "56f014e9")
+VARIABLES num, rnum, acks, net, pc
+
+vars == << num, rnum, acks, net, pc >>
+
+ProcSet == (MutexProcs) \cup (MsgProcs)
+
+Init == (* Global variables *)
+        /\ num = [n \in Nodes |-> 0]
+        /\ rnum = [n \in Nodes |-> [m \in Nodes \ {n} |-> 0]]
+        /\ acks = [n \in Nodes |-> {}]
+        /\ net = [i \in Nodes |-> [j \in Nodes \ {i} |-> << >>]]
+        /\ pc = [self \in ProcSet |-> CASE self \in MutexProcs -> "ncs"
+                                        [] self \in MsgProcs -> "a"]
+
+ncs(self) == /\ pc[self] = "ncs"
+             /\ TRUE
+             /\ pc' = [pc EXCEPT ![self] = "enter"]
+             /\ UNCHANGED << num, rnum, acks, net >>
+
+enter(self) == /\ pc[self] = "enter"
+               /\ LET nonZeros == {i \in Nodes \ {self.node} : rnum[self.node][i] # 0} IN
+                    LET competition == {<<rnum[i], i>> : i \in nonZeros} IN
+                      num' = [num EXCEPT ![self.node] = IF competition = { }
+                                                          THEN 1
+                                                          ELSE NextHigher(MaxPr(competition), self.node)]
+               /\ acks' = [acks EXCEPT ![self.node] = {self.node}]
+               /\ net' = [net EXCEPT ![self.node] = [i \in Nodes \ {self.node} |-> Append(net[self.node][i], (Write(num'[self.node])))]]
+               /\ pc' = [pc EXCEPT ![self] = "e1"]
+               /\ rnum' = rnum
+
+e1(self) == /\ pc[self] = "e1"
+            /\ /\ acks[self.node] = Nodes
+               /\ \A n \in Nodes \ {self.node} : <<num[self.node],self.node>> \prec <<rnum[self.node][n],n>>
+            /\ pc' = [pc EXCEPT ![self] = "cs"]
+            /\ UNCHANGED << num, rnum, acks, net >>
+
+cs(self) == /\ pc[self] = "cs"
+            /\ TRUE
+            /\ pc' = [pc EXCEPT ![self] = "exit"]
+            /\ UNCHANGED << num, rnum, acks, net >>
+
+exit(self) == /\ pc[self] = "exit"
+              /\ net' = [net EXCEPT ![self.node] = [i \in Nodes \ {self.node} |-> Append(net[self.node][i], (Write(0)))]]
+              /\ pc' = [pc EXCEPT ![self] = "ncs"]
+              /\ UNCHANGED << num, rnum, acks >>
+
+mu(self) == ncs(self) \/ enter(self) \/ e1(self) \/ cs(self) \/ exit(self)
+
+a(self) == /\ pc[self] = "a"
+           /\ \E n \in Nodes \ {self.node}:
+                /\ net[n][self.node] /= << >>
+                /\ LET msg == Head(net[n][self.node]) IN
+                     LET me == self.node IN
+                       IF msg.type = "write"
+                          THEN /\ rnum' = [rnum EXCEPT ![me][n] = msg.num]
+                               /\ IF msg.num = 0
+                                     THEN /\ net' = [net EXCEPT ![n][self.node] = Tail(net[n][self.node])]
+                                     ELSE /\ net' = [net EXCEPT ![n][self.node] = Tail(net[n][self.node]),
+                                                                ![self.node][n] = Append(net[self.node][n], Ack)]
+                               /\ acks' = acks
+                          ELSE /\ acks' = [acks EXCEPT ![me] = acks[me] \cup {n}]
+                               /\ UNCHANGED << rnum, net >>
+           /\ pc' = [pc EXCEPT ![self] = "a"]
+           /\ num' = num
+
+mg(self) == a(self)
+
+Next == (\E self \in MutexProcs: mu(self))
+           \/ (\E self \in MsgProcs: mg(self))
+
+Spec == /\ Init /\ [][Next]_vars
+        /\ \A self \in MutexProcs : WF_vars((pc[self] # "ncs") /\ mu(self))
+        /\ \A self \in MsgProcs : WF_vars(mg(self))
+
+\* END TRANSLATION 
+TypeOK == /\ num \in [Nodes -> Nat] 
+          /\ /\ (DOMAIN rnum) = Nodes    
+             /\ \A n \in Nodes : rnum[n] \in [Nodes \ {n} -> Nat]
+          /\ acks \in [Nodes -> SUBSET Nodes]
+          /\ /\ (DOMAIN net) = Nodes    
+             /\ \A n \in Nodes : net[n] \in [Nodes \ {n} -> Seq(Messages)]
+
+Mutex == \A m, n \in MutexProcs : (n # m) => ~(pc[n] = "cs" /\ pc[m] = "cs")
+
+StarvationFree == \A m \in MutexProcs : (pc[m]="enter") ~> (pc[m]="cs")
+
+------
+
+const_16137059216052000 == 
+    {-1, -2}
+
+constr_16137059216053000 ==
+    \A n \in Nodes : num[n] =< 4
+
+=============================================================================
+\* Modification History
+\* Last modified Thu Feb 18 17:54:45 PST 2021 by markus
+\* Last modified Thu Feb 18 17:18:09 PST 2021 by lamport
+\* Created Thu Feb 18 15:57:59 PST 2021 by lamport

tlatools/org.lamport.tlatools/test/tlc2/tool/EvalExceptionTest.java
@@ -1,0 +1,115 @@
+/*******************************************************************************
+ * Copyright (c) 2021 Microsoft Research. All rights reserved.
+ *
+ * The MIT License (MIT)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is furnished to do
+ * so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN
+ * AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ * Contributors:
+ *   Markus Alexander Kuppe - initial API and implementation
+ ******************************************************************************/
+package tlc2.tool;
+
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+
+import tlc2.output.EC;
+import tlc2.tool.liveness.ModelCheckerTestCase;
+
+public class EvalExceptionTest extends ModelCheckerTestCase {
+    public EvalExceptionTest() {
+        super("DistBakery", new String[] { "-config", "DistBakery.tla" }, EC.ExitStatus.ERROR);
+    }
+
+    @Override
+	protected boolean doCoverage() {
+		return false;
+	}
+
+	@Test
+    public void testSpec() {
+        assertTrue(recorder.recorded(EC.TLC_FINISHED));
+        assertTrue(recorder.recordedWithStringValues(EC.TLC_STATS, "24", "17", "5"));
+
+        // Error: The first argument of <= should be an integer, but instead it is:
+        // (-1 :> 0)
+		assertTrue(recorder.recordedWithStringValues(EC.TLC_MODULE_ARGUMENT_ERROR_AN,
+				"first", "<=", "integer", "(-1 :> 0)"));
+
+		final List<String> expectedTrace = new ArrayList<String>(6);
+		expectedTrace.add("/\\ num = (-2 :> 0 @@ -1 :> 0)\n"
+				+ "/\\ rnum = (-2 :> (-1 :> 0) @@ -1 :> (-2 :> 0))\n"
+				+ "/\\ net = (-2 :> (-1 :> <<>>) @@ -1 :> (-2 :> <<>>))\n"
+				+ "/\\ acks = (-2 :> {} @@ -1 :> {})\n"
+				+ "/\\ pc = ( [node |-> -2, type |-> \"msg\"] :> \"a\" @@\n"
+				+ "  [node |-> -2, type |-> \"mutex\"] :> \"ncs\" @@\n"
+				+ "  [node |-> -1, type |-> \"msg\"] :> \"a\" @@\n"
+				+ "  [node |-> -1, type |-> \"mutex\"] :> \"ncs\" )");
+		expectedTrace.add("/\\ num = (-2 :> 0 @@ -1 :> 0)\n"
+				+ "/\\ rnum = (-2 :> (-1 :> 0) @@ -1 :> (-2 :> 0))\n"
+				+ "/\\ net = (-2 :> (-1 :> <<>>) @@ -1 :> (-2 :> <<>>))\n"
+				+ "/\\ acks = (-2 :> {} @@ -1 :> {})\n"
+				+ "/\\ pc = ( [node |-> -2, type |-> \"msg\"] :> \"a\" @@\n"
+				+ "  [node |-> -2, type |-> \"mutex\"] :> \"enter\" @@\n"
+				+ "  [node |-> -1, type |-> \"msg\"] :> \"a\" @@\n"
+				+ "  [node |-> -1, type |-> \"mutex\"] :> \"ncs\" )");
+		expectedTrace.add("/\\ num = (-2 :> 1 @@ -1 :> 0)\n"
+				+ "/\\ rnum = (-2 :> (-1 :> 0) @@ -1 :> (-2 :> 0))\n"
+				+ "/\\ net = (-2 :> (-1 :> <<[type |-> \"write\", num |-> 1]>>) @@ -1 :> (-2 :> <<>>))\n"
+				+ "/\\ acks = (-2 :> {-2} @@ -1 :> {})\n"
+				+ "/\\ pc = ( [node |-> -2, type |-> \"msg\"] :> \"a\" @@\n"
+				+ "  [node |-> -2, type |-> \"mutex\"] :> \"e1\" @@\n"
+				+ "  [node |-> -1, type |-> \"msg\"] :> \"a\" @@\n"
+				+ "  [node |-> -1, type |-> \"mutex\"] :> \"ncs\" )");
+		expectedTrace.add("/\\ num = (-2 :> 1 @@ -1 :> 0)\n"
+				+ "/\\ rnum = (-2 :> (-1 :> 0) @@ -1 :> (-2 :> 0))\n"
+				+ "/\\ net = (-2 :> (-1 :> <<[type |-> \"write\", num |-> 1]>>) @@ -1 :> (-2 :> <<>>))\n"
+				+ "/\\ acks = (-2 :> {-2} @@ -1 :> {})\n"
+				+ "/\\ pc = ( [node |-> -2, type |-> \"msg\"] :> \"a\" @@\n"
+				+ "  [node |-> -2, type |-> \"mutex\"] :> \"e1\" @@\n"
+				+ "  [node |-> -1, type |-> \"msg\"] :> \"a\" @@\n"
+				+ "  [node |-> -1, type |-> \"mutex\"] :> \"enter\" )");
+		expectedTrace.add("/\\ num = (-2 :> 1 @@ -1 :> 0)\n"
+				+ "/\\ rnum = (-2 :> (-1 :> 0) @@ -1 :> (-2 :> 1))\n"
+				+ "/\\ net = (-2 :> (-1 :> <<>>) @@ -1 :> (-2 :> <<[type |-> \"ack\"]>>))\n"
+				+ "/\\ acks = (-2 :> {-2} @@ -1 :> {})\n"
+				+ "/\\ pc = ( [node |-> -2, type |-> \"msg\"] :> \"a\" @@\n"
+				+ "  [node |-> -2, type |-> \"mutex\"] :> \"e1\" @@\n"
+				+ "  [node |-> -1, type |-> \"msg\"] :> \"a\" @@\n"
+				+ "  [node |-> -1, type |-> \"mutex\"] :> \"enter\" )");
+		expectedTrace.add("/\\ num = (-2 :> 1 @@ -1 :> (-1 :> 0))\n"
+				+ "/\\ rnum = (-2 :> (-1 :> 0) @@ -1 :> (-2 :> 1))\n"
+				+ "/\\ net = ( -2 :> (-1 :> <<>>) @@\n"
+				+ "  -1 :> (-2 :> <<[type |-> \"ack\"], [type |-> \"write\", num |-> (-1 :> 0)]>>) )\n"
+				+ "/\\ acks = (-2 :> {-2} @@ -1 :> {-1})\n"
+				+ "/\\ pc = ( [node |-> -2, type |-> \"msg\"] :> \"a\" @@\n"
+				+ "  [node |-> -2, type |-> \"mutex\"] :> \"e1\" @@\n"
+				+ "  [node |-> -1, type |-> \"msg\"] :> \"a\" @@\n"
+				+ "  [node |-> -1, type |-> \"mutex\"] :> \"e1\" )");
+		assertTraceWith(recorder.getRecords(EC.TLC_STATE_PRINT2), expectedTrace);
+
+		assertTrue(recorder.recordedWithStringValues(EC.TLC_NESTED_EXPRESSION,
+				"0. Line 209, column 5 to line 209, column 32 in DistBakery\n"
+				+ "1. Line 209, column 22 to line 209, column 32 in DistBakery\n"
+				+ "\n"));
+	}
+}