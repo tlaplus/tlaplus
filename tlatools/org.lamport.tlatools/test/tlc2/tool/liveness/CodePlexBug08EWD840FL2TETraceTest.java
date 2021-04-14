tlaplus/tlaplus

commit 1025627b7f41e0bd977fdf9ce4d3b138547506ee

Add integration tests for TLC's trace expression evaluation/exploration feature.
The tests assert that the generated trace specs re-construct the same
error trace that the original TLC integrate tests constructed.

Squashed commit originating in Github PR #588
https://github.com/tlaplus/tlaplus/pull/588#

[Tests][TLC]

tlatools/org.lamport.tlatools/test/tlc2/TraceExpressionTestCase.java
@@ -1,0 +1,149 @@
+/*******************************************************************************
+ * Copyright (c) 2020 Microsoft Research. All rights reserved. 
+ *
+ * The MIT License (MIT)
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy 
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is furnished to do
+ * so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software. 
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN
+ * AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ * Contributors:
+ *   Markus Alexander Kuppe - initial API and implementation
+ ******************************************************************************/
+package tlc2;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Stream;
+
+import com.google.gson.Gson;
+
+import tlc2.tool.liveness.ModelCheckerTestCase;
+import util.TLAConstants;
+
+// TraceExpressionSpecTest is already taken, this is different because it runs
+// the spec using `TLCRunner` AND the TE spec.
+public abstract class TraceExpressionTestCase extends ModelCheckerTestCase {
+
+    public final String originalSpec;
+    public final String TESpec;
+    public final Map<String, Object> options;
+
+    public TraceExpressionTestCase(String spec, String path, final int exitStatus) {
+		this(spec, path, new String[] {}, exitStatus, new HashMap<String, Object>());
+	}
+
+    public TraceExpressionTestCase(String spec, String path, String[] extraArguments, final int exitStatus) {
+		this(spec, path, extraArguments, exitStatus, new HashMap<String, Object>());
+	}
+
+    public TraceExpressionTestCase(String spec, String path, String[] extraArguments, final int exitStatus, Map<String, Object> options) {
+		super(
+            spec + "_" + TLAConstants.TraceExplore.TRACE_EXPRESSION_MODULE_NAME + "_2000000000",
+            path,
+            extraArguments,
+            exitStatus
+        );
+        this.originalSpec = spec;
+        this.TESpec = spec + "_" + TLAConstants.TraceExplore.TRACE_EXPRESSION_MODULE_NAME + "_2000000000";
+        this.options = options;
+
+        List<String> defaultArguments = new ArrayList<String>();
+        if (!(this.options.containsKey("removeDeadlockConfig") && (Boolean)this.options.get("removeDeadlockConfig") == true)) {
+            defaultArguments.add("-deadlock");
+        }
+        if (!(this.options.containsKey("noRandomFPandSeed") && (Boolean)this.options.get("noRandomFPandSeed") == false)) {
+            defaultArguments.add("-fp");
+            defaultArguments.add("0");
+            // Deterministic simulation (order in which actions are explored).
+            defaultArguments.add("-seed");
+            defaultArguments.add("1");
+        }
+        this.extraArguments = Stream.of(this.extraArguments, defaultArguments.toArray()).flatMap(Stream::of).toArray(String[]::new);
+	}
+
+    @Override
+    protected void beforeSetUp() {
+        this.removeGeneratedFiles();
+        final File outFile = new File(BASE_PATH, "test" + TLAConstants.Files.OUTPUT_EXTENSION);
+
+        List<String> runnerArgs = new ArrayList<>(Arrays.asList(new String[] {BASE_PATH + this.path + File.separator + this.originalSpec}));
+        runnerArgs.addAll(Arrays.asList(this.extraArguments));
+
+        final TLCRunner tlcRunner = new TLCRunner(runnerArgs, outFile);
+        TLCRunner.JVM_ARGUMENTS.add("-DTLC_TRACE_EXPLORER_TIMESTAMP=2000000000");
+
+        if (this.options.containsKey("expectedJsonPath")) {
+            TLCRunner.JVM_ARGUMENTS.add("-DTLC_TRACE_EXPLORER_JSON_UNCOMMENTED=true");
+        }
+
+        if ((this.options.containsKey("LIVENESS_TESTING_IMPLEMENTATION") && (Boolean)this.options.get("LIVENESS_TESTING_IMPLEMENTATION") == true)) {
+            TLCRunner.JVM_ARGUMENTS.add("-Dtlc2.tool.liveness.ILiveCheck.testing=true");
+        }
+
+        try {
+            final int errorCode = tlcRunner.run();            
+            if(errorCode != this.expectedExitStatus) {
+                fail(String.format("The output of the original spec TLC run was not the expected exit status, it was %d instead.", errorCode));
+            }
+        } catch (IOException exception) {
+            fail(exception.getMessage());
+        }
+    }
+
+    private void testJson() {
+        try {
+            Gson gson = new Gson();
+            Object expectedJson = gson.fromJson(new FileReader(BASE_PATH + "GeneratedTESpecs" + File.separator + "EWD840_TTrace_JsonOutput.json"), Object.class);
+            Object jsonObject = gson.fromJson(new FileReader(System.getProperty("user.dir") + File.separator + this.TESpec + ".json"), Object.class);
+            assertTrue(expectedJson.equals(jsonObject));
+        } catch (FileNotFoundException exception) {
+            fail(exception.getMessage());
+        }
+    }
+
+    private void removeGeneratedFiles() {
+        // Remove generated files, if any.
+        new File(BASE_PATH + this.path + File.separator + this.TESpec + ".tla").delete();
+        new File(BASE_PATH + this.path + File.separator + this.TESpec + ".cfg").delete();
+        new File(System.getProperty("user.dir") + File.separator + this.TESpec + ".json").delete();
+    }
+
+    @Override
+    protected void beforeTearDown() {
+        if (this.options.containsKey("expectedJsonPath")) {
+            testJson();
+        }
+        //this.removeGeneratedFiles();
+    }
+
+    @Override
+	protected boolean doCoverage() {
+		// Do not do coverage or the code to avoid a bug,
+		// see https://github.com/tlaplus/tlaplus/pull/588#issuecomment-817371371.
+		return false;
+	}
+}

tlatools/org.lamport.tlatools/test/tlc2/tool/EWD840TESpecTest.java
@@ -1,0 +1,83 @@
+/*******************************************************************************
+ * Copyright (c) 2016 Microsoft Research. All rights reserved. 
+ *
+ * The MIT License (MIT)
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy 
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is furnished to do
+ * so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software. 
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN
+ * AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ * Contributors:
+ *   Markus Alexander Kuppe - initial API and implementation
+ ******************************************************************************/
+package tlc2.tool;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import org.junit.Test;
+
+import tlc2.TraceExpressionTestCase;
+import tlc2.output.EC;
+
+public class EWD840TESpecTest extends TraceExpressionTestCase {
+    
+	public EWD840TESpecTest() {
+        super("EWD840", "GeneratedTESpecs", new String[] {}, EC.ExitStatus.VIOLATION_LIVENESS, 
+            new HashMap<String, Object>() {{
+                put("expectedJsonPath", BASE_PATH + "GeneratedTESpecs" + File.separator + "EWD840_TTrace_JsonOutput.json");                
+            }});
+	}
+        
+    @Test
+	public void test() {
+		assertTrue(recorder.recorded(EC.TLC_FINISHED));
+		assertTrue(recorder.recordedWithStringValueAt(EC.TLC_STATS, "1", 1));
+		assertTrue(recorder.recordedWithStringValueAt(EC.TLC_STATS, "0", 2));
+		assertFalse(recorder.recorded(EC.GENERAL));
+
+        final List<String> expectedTrace = new ArrayList<String>();
+		expectedTrace.add(
+            "/\\ tpos = 0\n" + 
+            "/\\ active = ( 0 :> FALSE @@\n" +
+            "  1 :> FALSE @@\n" +
+            "  2 :> FALSE @@\n" +
+            "  3 :> FALSE @@\n" +
+            "  4 :> FALSE @@\n" +
+            "  5 :> FALSE @@\n" +
+            "  6 :> FALSE @@\n" +
+            "  7 :> FALSE @@\n" +
+            "  8 :> FALSE @@\n" +
+            "  9 :> FALSE )\n" +
+            "/\\ tcolor = \"black\"\n" +
+            "/\\ color = ( 0 :> \"white\" @@\n" +
+            "  1 :> \"white\" @@\n" +
+            "  2 :> \"white\" @@\n" +
+            "  3 :> \"white\" @@\n" +
+            "  4 :> \"white\" @@\n" +
+            "  5 :> \"white\" @@\n" +
+            "  6 :> \"white\" @@\n" +
+            "  7 :> \"white\" @@\n" +
+            "  8 :> \"white\" @@\n" +
+            "  9 :> \"white\" )");
+        assertTraceWith(recorder.getRecords(EC.TLC_STATE_PRINT2), expectedTrace);
+	}
+}

tlatools/org.lamport.tlatools/test/tlc2/tool/liveness/CodePlexBug08EWD840FL2TETraceTest.java
@@ -1,0 +1,109 @@
+/*******************************************************************************
+ * Copyright (c) 2015 Microsoft Research. All rights reserved. 
+ *
+ * The MIT License (MIT)
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy 
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is furnished to do
+ * so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software. 
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN
+ * AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ * Contributors:
+ *   Markus Alexander Kuppe - initial API and implementation
+ ******************************************************************************/
+
+package tlc2.tool.liveness;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+
+import tlc2.TraceExpressionTestCase;
+import tlc2.output.EC;
+import tlc2.output.EC.ExitStatus;
+
+/**
+ * see http://tlaplus.codeplex.com/workitem/8
+ */
+public class CodePlexBug08EWD840FL2TETraceTest extends TraceExpressionTestCase {
+
+	public CodePlexBug08EWD840FL2TETraceTest() {
+		super("EWD840MC2", "CodePlexBug08", ExitStatus.VIOLATION_LIVENESS);
+	}
+	
+	@Test
+	public void testSpec() {
+		// ModelChecker has finished and generated the expected amount of states
+		assertTrue(recorder.recorded(EC.TLC_FINISHED));
+		assertTrue(recorder.recordedWithStringValues(EC.TLC_STATS, "11", "10","0"));
+		assertFalse(recorder.recorded(EC.GENERAL));
+	
+		// Assert it has found the temporal violation and also a counter example
+		assertTrue(recorder.recorded(EC.TLC_TEMPORAL_PROPERTY_VIOLATED));
+		assertTrue(recorder.recorded(EC.TLC_COUNTER_EXAMPLE));
+
+		// Assert the error trace
+		assertTrue(recorder.recorded(EC.TLC_STATE_PRINT2));
+		final List<String> expectedTrace = new ArrayList<String>();
+		expectedTrace.add("/\\ tpos = 0\n"
+		                + "/\\ active = (0 :> FALSE @@ 1 :> FALSE @@ 2 :> FALSE @@ 3 :> TRUE)\n"
+		                + "/\\ tcolor = \"black\"\n"
+		                + "/\\ color = (0 :> \"white\" @@ 1 :> \"white\" @@ 2 :> \"white\" @@ 3 :> \"white\")");
+		expectedTrace.add("/\\ tpos = 3\n"
+		                + "/\\ active = (0 :> FALSE @@ 1 :> FALSE @@ 2 :> FALSE @@ 3 :> TRUE)\n"
+		                + "/\\ tcolor = \"white\"\n"
+		                + "/\\ color = (0 :> \"white\" @@ 1 :> \"white\" @@ 2 :> \"white\" @@ 3 :> \"white\")");
+		expectedTrace.add("/\\ tpos = 3\n"
+		                + "/\\ active = (0 :> FALSE @@ 1 :> FALSE @@ 2 :> TRUE @@ 3 :> TRUE)\n"
+		                + "/\\ tcolor = \"white\"\n"
+		                + "/\\ color = (0 :> \"white\" @@ 1 :> \"white\" @@ 2 :> \"white\" @@ 3 :> \"white\")");
+		expectedTrace.add("/\\ tpos = 3\n"
+		                + "/\\ active = (0 :> TRUE @@ 1 :> FALSE @@ 2 :> TRUE @@ 3 :> TRUE)\n"
+		                + "/\\ tcolor = \"white\"\n"
+		                + "/\\ color = (0 :> \"white\" @@ 1 :> \"white\" @@ 2 :> \"white\" @@ 3 :> \"white\")");
+		expectedTrace.add("/\\ tpos = 3\n"
+		                + "/\\ active = (0 :> TRUE @@ 1 :> FALSE @@ 2 :> TRUE @@ 3 :> FALSE)\n"
+		                + "/\\ tcolor = \"white\"\n"
+		                + "/\\ color = (0 :> \"white\" @@ 1 :> \"white\" @@ 2 :> \"white\" @@ 3 :> \"white\")");
+		expectedTrace.add("/\\ tpos = 3\n"
+		                + "/\\ active = (0 :> FALSE @@ 1 :> FALSE @@ 2 :> TRUE @@ 3 :> FALSE)\n"
+		                + "/\\ tcolor = \"white\"\n"
+		                + "/\\ color = (0 :> \"white\" @@ 1 :> \"white\" @@ 2 :> \"white\" @@ 3 :> \"white\")");
+		expectedTrace.add("/\\ tpos = 2\n"
+		                + "/\\ active = (0 :> FALSE @@ 1 :> FALSE @@ 2 :> TRUE @@ 3 :> FALSE)\n"
+		                + "/\\ tcolor = \"white\"\n"
+		                + "/\\ color = (0 :> \"white\" @@ 1 :> \"white\" @@ 2 :> \"white\" @@ 3 :> \"white\")");
+		expectedTrace.add("/\\ tpos = 2\n"
+		                + "/\\ active = (0 :> FALSE @@ 1 :> FALSE @@ 2 :> TRUE @@ 3 :> TRUE)\n"
+		                + "/\\ tcolor = \"white\"\n"
+		                + "/\\ color = (0 :> \"white\" @@ 1 :> \"white\" @@ 2 :> \"black\" @@ 3 :> \"white\")");
+		expectedTrace.add("/\\ tpos = 1\n"
+		                + "/\\ active = (0 :> FALSE @@ 1 :> FALSE @@ 2 :> TRUE @@ 3 :> TRUE)\n"
+		                + "/\\ tcolor = \"black\"\n"
+		                + "/\\ color = (0 :> \"white\" @@ 1 :> \"white\" @@ 2 :> \"white\" @@ 3 :> \"white\")");
+		expectedTrace.add("/\\ tpos = 1\n"
+		                + "/\\ active = (0 :> FALSE @@ 1 :> FALSE @@ 2 :> FALSE @@ 3 :> TRUE)\n"
+		                + "/\\ tcolor = \"black\"\n"
+		                + "/\\ color = (0 :> \"white\" @@ 1 :> \"white\" @@ 2 :> \"white\" @@ 3 :> \"white\")");
+		assertTraceWith(recorder.getRecords(EC.TLC_STATE_PRINT2), expectedTrace);
+		
+		// last state points back to state 1
+		assertBackToState(1);
+	}
+}

.gitignore
@@ -33,3 +33,8 @@ junit[0-9]*.properties
 junitvmwatcher[0-9]*.properties
 test.jfr
 *.jfr
+*_TTrace_2000000000.tla
+*_TTrace_2000000000.cfg
+*_TTrace_2000000000.json
+tlatools/org.lamport.tlatools/test-model/GeneratedTESpecs/
+tlatools/org.lamport.tlatools/test-model/test.out

tlatools/org.lamport.tlatools/customBuild.xml
@@ -417,6 +417,7 @@
 				<pathelement location="lib/junit-4.12.jar" />
 				<pathelement location="lib/hamcrest-core-1.3.jar" />
 				<pathelement location="lib/easymock-3.3.1.jar" />
+				<pathelement location="lib/gson/gson-2.8.6.jar" />
 				<pathelement location="lib/lsp/org.eclipse.lsp4j.jsonrpc-0.10.0.jar" />
 				<pathelement location="lib/lsp/org.eclipse.lsp4j.debug-0.10.0.jar" />
 				<pathelement path="${class.dir}" />
@@ -502,6 +503,7 @@
 					<exclude name="**/TLCDebuggerTestCase.java" />
 					<exclude name="**/PCalModelCheckerTestCase.java" />
 					<exclude name="**/TraceExpressionSpecTest.java" />
+					<exclude name="**/TraceExpressionTestCase.java" />
 					<exclude name="**/TraceExpressionSpecSafetyTest.java" />
 					<exclude name="**/SuiteTestCase.java" />
 					<exclude name="**/SuiteETestCase.java" />
@@ -679,6 +681,7 @@
 						<exclude name="**/TLCDebuggerTestCase.java" />
 						<exclude name="**/PCalModelCheckerTestCase.java" />
 						<exclude name="**/TraceExpressionSpecTest.java" />
+						<exclude name="**/TraceExpressionTestCase.java" />
 						<exclude name="**/TraceExpressionSpecSafetyTest.java" />
 						<exclude name="**/SuiteTestCase.java" />
 						<exclude name="**/SuiteETestCase.java" />

tlatools/org.lamport.tlatools/src/tlc2/TLCRunner.java
@@ -29,8 +29,8 @@ import tlc2.tool.fp.FPSetFactory;
  * 
  * In light of that, i've written this class to spin off a new JVM for the model check.
  */
-class TLCRunner {
-	static List<String> JVM_ARGUMENTS;
+public class TLCRunner {
+	public static List<String> JVM_ARGUMENTS;
 	private static final String TLC_CLASS = TLC.class.getName();
 	
 	static {
@@ -47,7 +47,7 @@ class TLCRunner {
 	
 	private boolean silenceStdOut;
 
-	TLCRunner(final List<String> tlcArguments, final File logfileDestination) {
+	public TLCRunner(final List<String> tlcArguments, final File logfileDestination) {
 		outputLogfile = logfileDestination;
 		outputOutputStream = null;
 		arguments = tlcArguments;
@@ -55,7 +55,7 @@ class TLCRunner {
 		silenceStdOut = false;
 	}
 
-	TLCRunner(final List<String> tlcArguments, final OutputStream logfileOutputStream) {
+	public TLCRunner(final List<String> tlcArguments, final OutputStream logfileOutputStream) {
 		outputLogfile = null;
 		outputOutputStream = logfileOutputStream;
 		arguments = tlcArguments;
@@ -75,7 +75,7 @@ class TLCRunner {
 	 * @return the exit value of the TLC process
 	 * @throws IOException
 	 */
-	int run() throws IOException {
+	public int run() throws IOException {
 		final ProcessBuilder processBuilder = createProcess();
 		final Process p = processBuilder.start();
 		final BufferedInputStream stdOutReader = new BufferedInputStream(p.getInputStream());

tlatools/org.lamport.tlatools/src/tlc2/TraceExplorationSpec.java
@@ -118,6 +118,10 @@ public class TraceExplorationSpec {
 	}
 	
 	public static String teModuleId(Date timestamp) {
+		String traceExplorerTimestamp = System.getProperty("TLC_TRACE_EXPLORER_TIMESTAMP");
+		if (traceExplorerTimestamp != null) {
+			return traceExplorerTimestamp;
+		}
 		final long secondsSinceEpoch = timestamp.getTime() / 1_000L;
 		return Long.toString(secondsSinceEpoch);
 	}

commit 298a0f33661c1dfda6f39b1ef509ed735e721916

Remove EWD840 test and test json using other spec test.
Fixes test failure introduced in previous commit 1025627b7f41e0bd977fdf9ce4d3b138547506ee

[Tests][TLC]

tlatools/org.lamport.tlatools/test/tlc2/TraceExpressionTestCase.java
@@ -115,9 +115,13 @@ public abstract class TraceExpressionTestCase extends ModelCheckerTestCase {
     }
 
     private void testJson() {
+        if (!this.options.containsKey("expectedJsonPath")) {
+            return;
+        }
+
         try {
             Gson gson = new Gson();
-            Object expectedJson = gson.fromJson(new FileReader(BASE_PATH + "GeneratedTESpecs" + File.separator + "EWD840_TTrace_JsonOutput.json"), Object.class);
+            Object expectedJson = gson.fromJson(new FileReader((String)this.options.get("expectedJsonPath")), Object.class);
             Object jsonObject = gson.fromJson(new FileReader(System.getProperty("user.dir") + File.separator + this.TESpec + ".json"), Object.class);
             assertTrue(expectedJson.equals(jsonObject));
         } catch (FileNotFoundException exception) {
@@ -134,10 +138,8 @@ public abstract class TraceExpressionTestCase extends ModelCheckerTestCase {
 
     @Override
     protected void beforeTearDown() {
-        if (this.options.containsKey("expectedJsonPath")) {
-            testJson();
-        }
-        //this.removeGeneratedFiles();
+        testJson();
+        this.removeGeneratedFiles();
     }
 
     @Override

tlatools/org.lamport.tlatools/test/tlc2/tool/EWD840TESpecTest.java
@@ -1,83 +1,0 @@
-/*******************************************************************************
- * Copyright (c) 2016 Microsoft Research. All rights reserved. 
- *
- * The MIT License (MIT)
- * 
- * Permission is hereby granted, free of charge, to any person obtaining a copy 
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
- * of the Software, and to permit persons to whom the Software is furnished to do
- * so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software. 
- * 
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
- * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
- * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN
- * AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
- * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- *
- * Contributors:
- *   Markus Alexander Kuppe - initial API and implementation
- ******************************************************************************/
-package tlc2.tool;
-
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
-
-import java.io.File;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-
-import org.junit.Test;
-
-import tlc2.TraceExpressionTestCase;
-import tlc2.output.EC;
-
-public class EWD840TESpecTest extends TraceExpressionTestCase {
-    
-	public EWD840TESpecTest() {
-        super("EWD840", "GeneratedTESpecs", new String[] {}, EC.ExitStatus.VIOLATION_LIVENESS, 
-            new HashMap<String, Object>() {{
-                put("expectedJsonPath", BASE_PATH + "GeneratedTESpecs" + File.separator + "EWD840_TTrace_JsonOutput.json");                
-            }});
-	}
-        
-    @Test
-	public void test() {
-		assertTrue(recorder.recorded(EC.TLC_FINISHED));
-		assertTrue(recorder.recordedWithStringValueAt(EC.TLC_STATS, "1", 1));
-		assertTrue(recorder.recordedWithStringValueAt(EC.TLC_STATS, "0", 2));
-		assertFalse(recorder.recorded(EC.GENERAL));
-
-        final List<String> expectedTrace = new ArrayList<String>();
-		expectedTrace.add(
-            "/\\ tpos = 0\n" + 
-            "/\\ active = ( 0 :> FALSE @@\n" +
-            "  1 :> FALSE @@\n" +
-            "  2 :> FALSE @@\n" +
-            "  3 :> FALSE @@\n" +
-            "  4 :> FALSE @@\n" +
-            "  5 :> FALSE @@\n" +
-            "  6 :> FALSE @@\n" +
-            "  7 :> FALSE @@\n" +
-            "  8 :> FALSE @@\n" +
-            "  9 :> FALSE )\n" +
-            "/\\ tcolor = \"black\"\n" +
-            "/\\ color = ( 0 :> \"white\" @@\n" +
-            "  1 :> \"white\" @@\n" +
-            "  2 :> \"white\" @@\n" +
-            "  3 :> \"white\" @@\n" +
-            "  4 :> \"white\" @@\n" +
-            "  5 :> \"white\" @@\n" +
-            "  6 :> \"white\" @@\n" +
-            "  7 :> \"white\" @@\n" +
-            "  8 :> \"white\" @@\n" +
-            "  9 :> \"white\" )");
-        assertTraceWith(recorder.getRecords(EC.TLC_STATE_PRINT2), expectedTrace);
-	}
-}

tlatools/org.lamport.tlatools/test/tlc2/tool/liveness/CodePlexBug08EWD840FL2TETraceTest.java
@@ -29,7 +29,9 @@ package tlc2.tool.liveness;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
+import java.io.File;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 import org.junit.Test;
@@ -44,7 +46,10 @@ import tlc2.output.EC.ExitStatus;
 public class CodePlexBug08EWD840FL2TETraceTest extends TraceExpressionTestCase {
 
 	public CodePlexBug08EWD840FL2TETraceTest() {
-		super("EWD840MC2", "CodePlexBug08", ExitStatus.VIOLATION_LIVENESS);
+		super("EWD840MC2", "CodePlexBug08", new String[] {}, ExitStatus.VIOLATION_LIVENESS,
+			new HashMap<String, Object>() {{
+				put("expectedJsonPath", BASE_PATH + "CodePlexBug08" + File.separator + "EWD840MC2_TTrace_2000000000_Expected.json");                
+			}});
 	}
 	
 	@Test