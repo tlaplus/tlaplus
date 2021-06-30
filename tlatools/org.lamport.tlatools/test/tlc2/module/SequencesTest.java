9c63802db38534825adad25f0ec7b4b49b63de52
Markus Alexander Kuppe <tlaplus.net@lemmster.de>
2021-06-30 14:39:08 -0700

    application and DOMAIN of strings in TLC
    Adresses comment #717602371 in Github issue #512
    https://github.com/tlaplus/tlaplus/issues/512#issuecomment-717602371
    
    [Regression][TLC]

--- tlatools/org.lamport.tlatools/test/tlc2/module/SequencesTest.java
+++ tlatools/org.lamport.tlatools/test/tlc2/module/SequencesTest.java
@@ -35,19 +35,38 @@ import tlc2.output.EC;
 import tlc2.tool.EvalException;
 import tlc2.value.impl.IntValue;
 import tlc2.value.impl.StringValue;
+import tlc2.value.impl.TupleValue;
 import tlc2.value.impl.Value;
+import util.UniqueString;
 
 public class SequencesTest {
 
+	// Tail("abc") = "bc"
+	// TLC can evaluate Tail("abc") because its value, "bc", can be written without
+	// explicitly writing any character (characters would be 'b' and 'c', which do 
+	// not exist in TLC).
+	// https://github.com/tlaplus/tlaplus/issues/512#issuecomment-717602371
+	
+	@Test
+	public void testTailString() {
+		Value v = Sequences.Tail(new StringValue("abc"));
+		assertTrue(v instanceof StringValue);
+		assertEquals(UniqueString.of("bc"), ((StringValue) v).val);
+	}
+
+	// Head("a") = "a"[1]  and TLC cannot evaluate "a"[1]:
+	// Error evaluating expression: '"abc"[1]'
+	// A non-function (a string) was applied as a function.
+	
 	@Test
 	public void testHeadString() {
-		Value a = Sequences.Head(new StringValue("a"));
-		assertTrue(a instanceof StringValue);
-		assertEquals("a", ((StringValue) a).val.toString());
-
-		a = Sequences.Head(new StringValue("abc"));
-		assertTrue(a instanceof StringValue);
-		assertEquals("a", ((StringValue) a).val.toString());
+		try {
+			Sequences.Head(new StringValue("a"));
+		} catch (EvalException e) {
+			assertEquals(EC.TLC_MODULE_ONE_ARGUMENT_ERROR, e.getErrorCode());
+			return;
+		}
+		fail();
 	}
 
 	@Test
@@ -55,7 +74,7 @@ public class SequencesTest {
 		try {
 			Sequences.Head(new StringValue(""));
 		} catch (EvalException e) {
-			assertEquals(EC.TLC_MODULE_APPLY_EMPTY_SEQ, e.getErrorCode());
+			assertEquals(EC.TLC_MODULE_ONE_ARGUMENT_ERROR, e.getErrorCode());
 			return;
 		}
 		fail();
@@ -63,15 +82,25 @@ public class SequencesTest {
 
 	@Test
 	public void testAppendString() {
-		Value a = Sequences.Append(new StringValue(""), new StringValue("a"));
-		assertTrue(a instanceof StringValue);
-		assertEquals("a", ((StringValue) a).val.toString());
-
-		a = Sequences.Append(new StringValue("abc"), new StringValue("d"));
-		assertTrue(a instanceof StringValue);
-		assertEquals("abcd", ((StringValue) a).val.toString());
+		try {
+			Sequences.Append(new StringValue(""), new StringValue("a"));
+		} catch (EvalException e) {
+			assertEquals(EC.TLC_MODULE_EVALUATING, e.getErrorCode());
+			return;
+		}
+		fail();
 	}
 
+	@Test
+	public void testAppendString2() {
+		try {
+			Sequences.Append(new StringValue("abc"), new StringValue("d"));
+		} catch (EvalException e) {
+			assertEquals(EC.TLC_MODULE_EVALUATING, e.getErrorCode());
+			return;
+		}
+		fail();
+	}
 	@Test
 	public void testAppendStringNonString() {
 		try {
@@ -81,5 +110,83 @@ public class SequencesTest {
 			return;
 		}
 		fail();
+	}
+
+	// "a" \o <<>> or <<>> \o "a" should produce an error. There is little value in
+	// implementing it because concat would only work for the empty sequence.
+	
+	@Test
+	public void testConcatStringToSeq() {
+		try {
+			Sequences.Concat(new TupleValue(new StringValue("abc")), new StringValue("d"));
+		} catch (EvalException e) {
+			assertEquals(EC.TLC_MODULE_EVALUATING, e.getErrorCode());
+			return;
+		}
+		fail();
+	}
+
+	@Test
+	public void testConcatSeqToString() {
+		try {
+			Sequences.Concat(new StringValue("abc"), new TupleValue(new StringValue("d")));
+		} catch (EvalException e) {
+			assertEquals(EC.TLC_MODULE_EVALUATING, e.getErrorCode());
+			return;
+		}
+		fail();
+	}
+
+	// "a" \o "b" = "ab"
+	
+	@Test
+	public void testConcatStringToString() {
+		Value v = Sequences.Concat(new StringValue("abc"), (new StringValue("d")));
+		assertTrue(v instanceof StringValue);
+		assertEquals(UniqueString.of("abcd"), ((StringValue) v).val);
+	}
+
+	@Test
+	public void testConcatIntToSeq() {
+		try {
+			Sequences.Concat(new TupleValue(new StringValue("abc")), IntValue.ValOne);
+		} catch (EvalException e) {
+			assertEquals(EC.TLC_MODULE_EVALUATING, e.getErrorCode());
+			return;
+		}
+		fail();
+	}
+
+	@Test
+	public void testConcatSeqToInt() {
+		try {
+			Sequences.Concat(IntValue.ValOne, new TupleValue(new StringValue("d")));
+		} catch (EvalException e) {
+			assertEquals(EC.TLC_MODULE_EVALUATING, e.getErrorCode());
+			return;
+		}
+		fail();
+	}
+
+	// 1 \o 1 # 11
+	
+	@Test
+	public void testConcatIntToInt() {
+		try {
+			Sequences.Concat(IntValue.ValOne, IntValue.ValOne);
+		} catch (EvalException e) {
+			assertEquals(EC.TLC_MODULE_EVALUATING, e.getErrorCode());
+			return;
+		}
+		fail();
+	}
+	
+	// SubSeq("abc", 1, 1) = "a"
+	
+	@Test
+	public void testSubseq() {
+		Value v = Sequences.SubSeq(new StringValue("abc"), IntValue.ValOne, IntValue.ValOne);
+		assertTrue(v instanceof StringValue);
+		assertEquals(UniqueString.of("a"), ((StringValue) v).val);
 	}
 }
