073dc019e4020bc7f3d7ab2f65d33a3872e5dc91
Eduardo Salinas <edus@microsoft.com>
2021-08-12 14:42:30 +0000

    ValueVec doesn't need to catch exception

--- tlatools/org.lamport.tlatools/src/tlc2/value/impl/ValueVec.java
+++ tlatools/org.lamport.tlatools/src/tlc2/value/impl/ValueVec.java
@@ -10,7 +10,6 @@ import java.util.Collection;
 
 import tlc2.TLCGlobals;
 import util.WrongInvocationException;
-import util.Assert.TLCTypeMismatchException;;
 
 public class ValueVec implements Cloneable, Serializable {
   private Value [] elementData;
@@ -153,26 +152,22 @@ public class ValueVec implements Cloneable, Serializable {
     if (sorted) {
       int cmp = 0, mid = 0, low = 0, high = this.elementCount;
       while (low < high) {
-        mid = (low + high) >> 1;
-        cmp = elem.compareTo(this.elementData[mid]);
-        if (cmp == 0) return true;
-        if (cmp < 0) {
-          high = mid;
-        }
-        else {
-          low = mid + 1;
-        }
+	mid = (low + high) >> 1;
+	cmp = elem.compareTo(this.elementData[mid]);
+	if (cmp == 0) return true;
+	if (cmp < 0) {
+	  high = mid;
+	}
+	else {
+	  low = mid + 1;
+	}
       }
     }
     else {
       for (int i = 0; i < this.elementCount; i++) {
-        try{
-          if (this.elementData[i].equals(elem)) {
-            return true;
-          }
-        }
-        // we ignore type mismatch -> treat as not equal
-        catch(TLCTypeMismatchException e) { }
+	if (this.elementData[i].equals(elem)) {
+	  return true;
+	}
       }
     }
     return false;
