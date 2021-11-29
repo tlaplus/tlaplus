581673be6d7948a4211e6b7de56b41432351a77d
Markus Alexander Kuppe <github.com@lemmster.de>
2021-11-28 16:34:12 -0800

    Thread safety concern due to "memory barrier" in FcnRcdValue#select.
    * Demonstrate problem with ModelValue#compareTo (old hash-based lookup
    hid the problem because it threw an exception lookup came up empty).
    
    Part of Github #439
    https://github.com/tlaplus/tlaplus/issues/439
    
    [Performance][TLC]

--- tlatools/org.lamport.tlatools/test/tlc2/value/impl/FcnRcdValueTest.java
+++ tlatools/org.lamport.tlatools/test/tlc2/value/impl/FcnRcdValueTest.java
@@ -28,6 +28,7 @@ package tlc2.value.impl;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
+import static org.junit.Assert.fail;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -35,8 +36,10 @@ import java.util.List;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import tlc2.module.TLCExt;
 import tlc2.util.FP64;
 import tlc2.value.RandomEnumerableValues;
+import util.Assert.TLCRuntimeException;
 
 public class FcnRcdValueTest {
 
@@ -102,6 +105,92 @@ public class FcnRcdValueTest {
 					}
 				}
 			}
+		}
+	}
+	
+	/*
+	 * #### Typed Model Values
+	 * 
+	 * One way that TLC finds bugs is by reporting an error if it tries to compare
+	 * two incomparable values&mdash;for example, a string and a set. The use of
+	 * model values can cause TLC to miss bugs because it will compare a model value
+	 * to any value without complaining (finding it unequal to anything but itself).
+	 * Typed model values have been introduced to solve this problem.
+	 * 
+	 * For any character &tau;, a model value whose name begins with the
+	 * two-character string "&tau;\_" is defined to have type &tau;. For example,
+	 * the model value _x\_1_ has type _x_. Any other model value is untyped. TLC
+	 * treats untyped model values as before, being willing to compare them to
+	 * anything. However it reports an error if it tries to compare a typed model
+	 * value to anything other than a model value of the same type or an untyped
+	 * model value. Thus, TLC will find the model value _x_1_ unequal to the model
+	 * values _x_ab2_ and _none_, but will report an error if it tries to compare
+	 * _x\_1_ to _a\_1_.
+	 */
+	
+	@Test
+	public void testSelectLinearSearchTypedMV() {
+		final String str = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+		final List<ModelValue> l = new ArrayList<>();
+		for (int i = 0; i < str.length(); i++) {
+			l.add((ModelValue) TLCExt.tlcModelValue(new StringValue("A_" + str.charAt(i))));
+		}
+		
+		final Value[] dom = l.toArray(ModelValue[]::new);
+		final Value[] rng = getInts(0, dom.length, 0);
+		final FcnRcdValue rcdValue = (FcnRcdValue) new FcnRcdValue(dom, rng, false).normalize();
+
+		try {
+			rcdValue.select(ModelValue.make("B_c"));
+			fail("Comparison to typed model value should fail");
+		} catch (TLCRuntimeException e) {
+			assertEquals("Attempted to check equality of the differently-typed model values A_A and B_c", e.getMessage());
+		}
+		try {
+			rcdValue.select(IntValue.ValNegOne);
+			fail("Comparison to typed model value should fail");
+		} catch (TLCRuntimeException e) {
+			assertEquals("Attempted to check equality of typed model value A_A and non-model value\n"
+					+ "-1", e.getMessage());
+		}
+		
+		for (int i = 0; i < dom.length; i++) {
+			IntValue val = (IntValue) rcdValue.select(dom[i]);
+			assertNotNull(val);
+			assertEquals(IntValue.gen(i), val);
+		}
+	}
+	
+	@Test
+	public void testSelectBinarySearchTypedMV() {
+		final String str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+		final List<ModelValue> l = new ArrayList<>();
+		for (int i = 0; i < str.length(); i++) {
+			l.add((ModelValue) TLCExt.tlcModelValue(new StringValue("A_" + str.charAt(i))));
+		}
+		
+		final Value[] dom = l.toArray(ModelValue[]::new);
+		final Value[] rng = getInts(0, dom.length, 0);
+		final FcnRcdValue rcdValue = (FcnRcdValue) new FcnRcdValue(dom, rng, false).normalize();
+
+		try {
+			rcdValue.select(ModelValue.make("B_c"));
+			fail("Comparison to typed model value should fail");
+		} catch (TLCRuntimeException e) {
+			assertEquals("Attempted to check equality of the differently-typed model values A_A and B_c", e.getMessage());
+		}
+		try {
+			rcdValue.select(IntValue.ValNegOne);
+			fail("Comparison to typed model value should fail");
+		} catch (TLCRuntimeException e) {
+			assertEquals("Attempted to check equality of typed model value A_A and non-model value\n"
+					+ "-1", e.getMessage());
+		}
+		
+		for (int i = 0; i < dom.length; i++) {
+			IntValue val = (IntValue) rcdValue.select(dom[i]);
+			assertNotNull(val);
+			assertEquals(IntValue.gen(i), val);
 		}
 	}
 }
