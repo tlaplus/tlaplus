f91c7b0dcf62ba03749c74588cfbd0ee0b032b21
Markus Alexander Kuppe <github.com@lemmster.de>
2021-12-23 16:58:48 -0800

    Rudimentary performance data analysis.
    Output appears in https://github.com/tlaplus/tlaplus/issues/648#issuecomment-1000603103
    
    [Tests][TLC]

--- general/performance/stats.R
+++ general/performance/stats.R
@@ -2,9 +2,48 @@
 library(here)
 data <- read.csv(header=TRUE, sep = "#", file = here("out_run-stats.csv"))
 
-## Aggregate generted per minute.
-data$GenMin <- data$Generated / data$Duration
-
 ## Convert epoch to date.
 library(anytime)
-data$date <- anytime(data$Timestamp)
+data$Date <- anytime(data$Timestamp)
+
+## Calculate Throughput on aggregated data.
+data$Throughput <- data$Generated / data$Duration
+  
+## Aggregate multiple runs.
+data <- aggregate(cbind(Generated,Duration) ~ Spec + RevTag + Workers, data = data, FUN = mean, na.rm = TRUE)
+
+## Calculate Throughput on aggregated data.
+data$Throughput <- data$Generated / data$Duration
+
+##################
+
+lma <- lm(Throughput ~ RevTag, data)
+line <- data.frame(slope = coef(lma)["RevTag"], intercept = coef(lma)["(Intercept)"], stroke = "red", stroke_width = 3, stroke_dasharray = "7,5")
+
+library(scatterD3)
+#library(htmlwidgets)
+s <- scatterD3(data = data, 
+               x = RevTag, y = Throughput, y_log=T,
+               xlab = "RevTag", ylab = "log(Time&Throughput)", 
+               col_var=Spec
+               )
+
+library(htmlwidgets)
+saveWidget(s, file="index.html")
+
+##################
+
+library(dplyr)
+trend <- data %>%
+  group_by(Spec, RevTag, Workers) %>% 
+  summarise(Throughput = mean(Throughput)) %>%
+  arrange(Spec, Workers) %>% 
+  group_by(Spec, Workers) %>%
+  summarise(
+    inc = (first(Throughput) - last(Throughput)) / first(Throughput), 
+    increase = scales::percent(inc)
+  )
+
+library(knitr)
+trend <- trend[order(-trend$inc),]
+kable(format = "markdown", trend[, c(1,2,4)], digits=2)
