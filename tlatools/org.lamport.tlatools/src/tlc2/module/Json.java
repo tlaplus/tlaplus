79dd4eadecf16513470dd805aae1bd2a22898810
Markus Alexander Kuppe <github.com@lemmster.de>
2022-02-04 15:24:32 -0800

    Explicitly convert input to TupleValue to handle cases such as when input is of type FcnRcdValue.
    [Bug][TLC]

--- tlatools/org.lamport.tlatools/src/tlc2/module/Json.java
+++ tlatools/org.lamport.tlatools/src/tlc2/module/Json.java
@@ -141,7 +141,8 @@ public class Json {
    * @return a boolean value indicating whether the serialization was successful
    */
   @TLAPlusOperator(identifier = "ndJsonSerialize", module = "Json", warn = false)
-  public synchronized static BoolValue ndSerialize(final StringValue path, final TupleValue value) throws IOException {
+  public synchronized static BoolValue ndSerialize(final StringValue path, final Value v) throws IOException {
+	TupleValue value = (TupleValue) v.toTuple();
     File file = new File(path.val.toString());
     if (file.getParentFile() != null) {file.getParentFile().mkdirs();} // Cannot create parent dir for relative path.
     try (BufferedWriter writer = new BufferedWriter(new FileWriter(new File(path.val.toString())))) {
@@ -160,7 +161,8 @@ public class Json {
    * @return a boolean value indicating whether the serialization was successful
    */
   @TLAPlusOperator(identifier = "JsonSerialize", module = "Json", warn = false)
-  public synchronized static BoolValue serialize(final StringValue path, final TupleValue value) throws IOException {
+  public synchronized static BoolValue serialize(final StringValue path, final Value v) throws IOException {
+	TupleValue value = (TupleValue) v.toTuple();
     File file = new File(path.val.toString());
     if (file.getParentFile() != null) {file.getParentFile().mkdirs();} // Cannot create parent dir for relative path.
     try (BufferedWriter writer = new BufferedWriter(new FileWriter(new File(path.val.toString())))) {
