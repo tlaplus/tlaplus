tlaplus/tlaplus

commit 58fdb46ea9597f037245123efc493a968cbd81c1

SyntaxTreeNode Filename

tlatools/org.lamport.tlatools/src/tla2sany/parser/TLAplusParser.java
@@ -23,7 +23,7 @@ public class TLAplusParser implements tla2sany.st.SyntaxTreeConstants, ParseTree
     return deps;
   }
   public TreeNode rootNode() { return ParseTree; }
-  public String moduleName() { return mn.toString(); }
+  public String moduleName() { return mn; }
 //  public tla2sany.st.ParseErrors getErrors() { return PErrors; } Unused, apparently
 // The front end can simply read the public PErrors.
 
@@ -34,7 +34,7 @@ public class TLAplusParser implements tla2sany.st.SyntaxTreeConstants, ParseTree
 
   public Vector dependencyList = new Vector( 20 );
 
-  private UniqueString mn = null;
+  private String mn = null;
      /**********************************************************************
      * The module name.                                                    *
      **********************************************************************/
@@ -1496,7 +1496,7 @@ Token t;
   t = getToken(1);
   if (isFieldNameToken( t )) t.kind = IDENTIFIER;
     lSTN[1] = Identifier();
-    if ( mn == null ) mn = UniqueString.of(lSTN[1].image);
+    if ( mn == null ) mn = lSTN[1].image;
     expecting = "----";
     t = jj_consume_token(SEPARATOR);
                       lSTN[2] =  new SyntaxTreeNode(mn, t );
@@ -6448,7 +6448,7 @@ SyntaxTreeNode tn;
     t = jj_consume_token(ARROW);
                 zn[1] = new SyntaxTreeNode(mn, t);
     zn[2] = Expression();
-    epa(); {if (true) return new SyntaxTreeNode( mn, N_CaseArm, zn );}
+    epa(); {if (true) return new SyntaxTreeNode(mn, N_CaseArm, zn );}
     throw new Error("Missing return statement in function");
   }
 
@@ -6461,7 +6461,7 @@ SyntaxTreeNode tn;
     t = jj_consume_token(ARROW);
                 zn[1] = new SyntaxTreeNode(mn, t);
     zn[2] = Expression();
-    epa(); {if (true) return new SyntaxTreeNode( mn, N_OtherArm, zn );}
+    epa(); {if (true) return new SyntaxTreeNode(mn, N_OtherArm, zn );}
     throw new Error("Missing return statement in function");
   }
 

tlatools/org.lamport.tlatools/src/tla2sany/parser/OperatorStack.java
@@ -213,7 +213,7 @@ What do left and right mean?????? What does shift mean????????
       } else {
 // System.out.println("postfix reduction : FcnOp");
         SyntaxTreeNode eSTN = ((OSelement) CurrentTop.elementAt( n-2)).getNode();
-        lSTN = new SyntaxTreeNode( eSTN.getFN(), N_FcnAppl, eSTN, (SyntaxTreeNode[]) (opNode.heirs()) );
+        lSTN = new SyntaxTreeNode( eSTN.getName(), N_FcnAppl, eSTN, (SyntaxTreeNode[]) (opNode.heirs()) );
       }
       CurrentTop.removeElementAt(n-1);
       CurrentTop.setElementAt(new OSelement(lSTN) , n-2);

tlatools/org.lamport.tlatools/src/tla2sany/parser/SyntaxTreeNode.java
@@ -115,7 +115,7 @@ public class SyntaxTreeNode implements TreeNode, SyntaxTreeConstants,
     * the values <<endLine, endColumn>> of its tokens.                     *
     ***********************************************************************/
     
-  private UniqueString            fileName    = null;
+  private String            fileName    = null;
     /***********************************************************************
     * fileName seems to be the name of the module containing this node.    *
     ***********************************************************************/
@@ -161,11 +161,11 @@ public class SyntaxTreeNode implements TreeNode, SyntaxTreeConstants,
     zero = nullArray;
     one = nullArray;
     location[0] = 0; location[1] = 0; location[2] = 0; location[3] = 0;
-    fileName = UniqueString.uniqueStringOf("--TLA+ BUILTINS--");
+    fileName = ("--TLA+ BUILTINS--");
   }
 
 
-  public SyntaxTreeNode(UniqueString fn, Token t) {
+  public SyntaxTreeNode(String fn, Token t) {
     this.kind = t.kind; 
     this.image = t.image;
     zero = nullArray; 
@@ -179,7 +179,7 @@ public class SyntaxTreeNode implements TreeNode, SyntaxTreeConstants,
   }
 
 
-  public SyntaxTreeNode(UniqueString fn, int kind, Token t) {
+  public SyntaxTreeNode(String fn, int kind, Token t) {
     this.kind = kind;
 //  this.image = SyntaxNodeImage[ kind ];
     /***********************************************************************
@@ -197,7 +197,7 @@ public class SyntaxTreeNode implements TreeNode, SyntaxTreeConstants,
   }
 
 
-  public SyntaxTreeNode(UniqueString fn, int kind, SyntaxTreeNode a[]) {
+  public SyntaxTreeNode(String fn, int kind, SyntaxTreeNode a[]) {
     this.kind = kind;
     image = SyntaxNodeImage[ kind ].toString();
     zero = a;
@@ -222,7 +222,7 @@ public class SyntaxTreeNode implements TreeNode, SyntaxTreeConstants,
   }
 
 
-  public SyntaxTreeNode(UniqueString fn, int kind, SyntaxTreeNode a, 
+  public SyntaxTreeNode(String fn, int kind, SyntaxTreeNode a, 
                         SyntaxTreeNode b[]) {
     this.kind = kind;
     image = SyntaxNodeImage[ kind ].toString();
@@ -236,7 +236,7 @@ public class SyntaxTreeNode implements TreeNode, SyntaxTreeConstants,
   }
 
 
-  public SyntaxTreeNode(UniqueString fn, int kind, SyntaxTreeNode a[], 
+  public SyntaxTreeNode(String fn, int kind, SyntaxTreeNode a[], 
                         SyntaxTreeNode b[]) {
     this.kind = kind;
     image = SyntaxNodeImage[ kind ].toString();
@@ -394,9 +394,10 @@ public class SyntaxTreeNode implements TreeNode, SyntaxTreeConstants,
   }
   
 
-  public final String         getFilename() {return fileName.toString(); }
+  public final String         getFilename() {return fileName; }
 
-  public final UniqueString   getFN() { return fileName; }
+  /** @deprecated */
+  public final UniqueString   getFN() { return UniqueString.of(fileName); }
 
   public final Location       getLocation( ) {
     return new Location( fileName, location[0], location[1], location[2], 

commit 64befd2e7941fd296160c2c07029578ac2bd9040

fixed bug introduced by 58fdb46ea9597f037245123efc493a968cbd81c1

tlatools/org.lamport.tlatools/src/tla2sany/parser/TLAplusParser.java
@@ -19,7 +19,7 @@ public class TLAplusParser implements tla2sany.st.SyntaxTreeConstants, ParseTree
     ***********************************************************************/
     String[]deps = new String[ dependencyList.size() ];
     for (int lvi =0; lvi < deps.length; lvi++)
-      deps[lvi] = ((UniqueString)dependencyList.elementAt(lvi)).toString();
+      deps[lvi] = dependencyList.elementAt(lvi);
     return deps;
   }
   public TreeNode rootNode() { return ParseTree; }
@@ -32,7 +32,7 @@ public class TLAplusParser implements tla2sany.st.SyntaxTreeConstants, ParseTree
     * The root node.                                                       *
     ***********************************************************************/
 
-  public Vector dependencyList = new Vector( 20 );
+  public Vector<String> dependencyList = new Vector( 20 );
 
   private String mn = null;
      /**********************************************************************
@@ -767,13 +767,8 @@ if (System.getProperty("TLA-StackTrace", "off").equals("on")) ToolIO.out.println
 //
 
   Stack internals = new Stack();
-
-  private final void addDependency( String s ) {
-	  addDependency(UniqueString.of(s));
-  }
 	  
-  /** @deprecated */
-  private final void addDependency( UniqueString s ) {
+  private final void addDependency(String s) {
     int lvi = internals.search( s );
     if ( lvi < 0 )
       dependencyList.addElement( s );
@@ -1466,7 +1461,7 @@ Token t;
   expecting = "==== or more Module body";
     lSTN[3] = EndModule();
   do { pop = internals.pop(); } while (pop != null );
-  internals.push( UniqueString.of(lSTN[0].zero[1].image) );
+  internals.push(lSTN[0].zero[1].image);
   epa(); {if (true) return new SyntaxTreeNode( mn, N_Module, lSTN );}
     throw new Error("Missing return statement in function");
   }