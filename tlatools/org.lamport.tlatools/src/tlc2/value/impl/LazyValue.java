9aee565dfdb9ca7306647734aadca327457f61fe
Calvin Loncaric <calvin.loncaric@oracle.com>
2023-03-17 17:35:58 -0700

    Refactor LazyValue to help prevent unsafe use of cached values
    MOTIVATION
    
    TLC has had several bugs involving misuse of LazyValue.val.  Instead of
    proving every use of that field correct, this change shifts the
    management of the field to the LazyValue class itself.  Now callers
    must specify the behavior and evaluation mode that they are evaluating
    with when reading the cached value.  The cached value is only returned
    if it was computed using the same behavior and evaluation mode.  If it
    wasn't, it must be recomputed.
    
    This does not GUARANTEE that LazyValue is always used correctly.
    Callers can still pass incorrect arguments to LazyValue.getValue() to
    cause bugs.  However, it should greatly reduce the likelihood of
    further latent LazyValue-related defects.
    
    Pros:
    
     - A lot of complexity can be removed without losing any potential
       benefits of LazyValue.
    
    Cons:
    
     - The LazyValue class now stores several more fields.  It is possible
       that this will worsen TLC's performance since many instances of this
       class are created during evaluation.
    
    DETAILS
    
    A key component of this change is the refactored definition of the
    condition for WHEN a LazyValue can be re-used in evaluation (see
    `LazyValue.getCachedValue`).
    
    First, when can a LazyValue be used with a different TLC state:
    
    A bit of debugging revealed that TLCState.equals() does not behave how
    I would expect.  Furthermore, such a check is too strict: TLC often
    evaluates a LazyValue in a state where `x=1 /\ y=null` before later
    re-evaluating it in a state where `x=1 /\ y=2`.
    
    TLC can use the cached value when it was computed in a superset of the
    requested state (see `TLCState.isSubset`).
    
    Second, when can a LazyValue be used with a different EvalControl:
    
    Debugging uncovered that LazyValues are often recomputed using similar
    EvalControl settings, such as
    
        Const | KeepLazy
    
    vs
    
        Const
    
    We have an opportunity to be more aggressive about caching by
    identifying EvalControl settings that are semantically equivalent
    (in the sense that they do not affect the meaning of the computed
    value).
    
    I have also been careful to handle exceptions due to incomparable
    values.
    
    Signed-off-by: Calvin Loncaric <calvin.loncaric@oracle.com>

--- tlatools/org.lamport.tlatools/src/tlc2/value/impl/LazyValue.java
+++ tlatools/org.lamport.tlatools/src/tlc2/value/impl/LazyValue.java
@@ -1,5 +1,6 @@
 // Copyright (c) 2003 Compaq Corporation.  All rights reserved.
 // Portions Copyright (c) 2003 Microsoft Corporation.  All rights reserved.
+// Copyright (c) 2023, Oracle and/or its affiliates.
 // Last modified on Wed 12 Jul 2017 at 16:10:00 PST by ian morris nieves
 //      modified on Mon 30 Apr 2007 at 15:30:08 PST by lamport
 //      modified on Thu Feb  8 21:23:55 PST 2001 by yuanyu
@@ -22,6 +23,52 @@ import tlc2.value.IValue;
 import util.Assert;
 import util.ToolIO;
 
+/**
+ * LazyValue is TLA+ expression that has not been reduced to a simple value.
+ *
+ * <h2>The Need for Laziness</h2>
+ *
+ * <p>TLC uses LazyValue when it "computes" the arguments to an operator or the bound value in a LET...IN expression.
+ * Because TLA+ has temporal operators, it cannot behave like a normal call-by-value interpreter in these cases.
+ *
+ * <p>For example, suppose a TLA+ spec contains the definition
+ * <pre>
+ *     F(x) == x'
+ * </pre>
+ * and TLC has to evaluate the expression <code>F(var)</code> in a behavior where <code>var = 1</code> and
+ * <code>var' = 2</code>.  The correct output is 2.  However, fully reducing the argument <code>var</code> before
+ * expanding <code>F</code> would lead to wrong output:
+ * <pre>
+ *     F(var) = F(1)   \* wrong!
+ *            = 1'
+ *            = 1
+ *
+ *     F(var) = var'   \* right!
+ *            = 2
+ * </pre>
+ * When expanding <code>F</code> TLC needs to keep the expression that was used as the <code>x</code> argument in order
+ * to get the right output.
+ *
+ * <h2>Caching and Performance</h2>
+ *
+ * <p>For performance, LazyValue can cache the reduced value when it is computed.  This enables TLC to have similar
+ * performance to a call-by-value language in many cases.  For example, if <code>G(x) == x + x</code>, then evaluating
+ * the expression <code>G(very_expensive_arg)</code> should only evaluate the very expensive argument once.
+ *
+ * <p>TLC has had multiple soundness bugs due to mishandling of cached LazyValues: see Github issues #113 and #798.  As
+ * a result, reads of the cached value are now guarded and callers must specify the tool, behavior, and evaluation mode
+ * they are using when reading it (see {@link #getCachedValue(Tool, TLCState, TLCState, int)}).
+ *
+ * <p>The performance benefits of caching values is generally debatable. The time
+ * it takes TLC to check a reasonable sized model of the PaxosCommit [1] spec is
+ * ~2h with, with limited caching due to the fix for issue 113 or without
+ * caching. There is no measurable performance difference even though the change
+ * for issue 113 reduces the cache hits from ~13 billion to ~4 billion. This was
+ * measured with an instrumented version of TLC.
+ * [1] general/performance/PaxosCommit/
+ *
+ * @see #getValue(Tool, TLCState, TLCState, int)
+ */
 public class LazyValue extends Value {
 	/**
 	 * Allow to completely disable LazyValue by passing a VM property/system
@@ -40,6 +87,10 @@ public class LazyValue extends Value {
 			ToolIO.out.println("LazyValue is disabled.");
 		}
 	}
+
+  public final SemanticNode expr;
+  public final Context con;
+
   /**
    * The field val is the result of evaluating expr in context con and
    * a pair of states.  If val is null, then the value has not been
@@ -47,10 +98,12 @@ public class LazyValue extends Value {
    * val. If val is ValUndef, then the value has not been computed,
    * and when computed, it can not be cached in the field val.
    */
-
-  public SemanticNode expr;
-  public Context con;
   private Value val;
+  private int toolID;
+  private TLCState s0;
+  private TLCState s1;
+  private int control;
+  private int cacheCount = 0;
 
   public LazyValue(SemanticNode expr, Context con, final CostModel cm) {
 	  this(expr, con, true, coverage ? cm.get(expr) : cm);
@@ -61,26 +114,74 @@ public class LazyValue extends Value {
     this.con = con;
     this.cm = coverage ? cm.get(expr) : cm;
     this.val = null;
-    // See comment on cachable's meager performance in Tool.java on line 1408.
-    // See other note about a bug that surfaced with LazyValue in Tool.java on line ~1385.
+    // See comment on cachable's meager performance in the docstring above.
+    // See other note about a bug that surfaced with LazyValue in the docstring above.
     if (LAZYEVAL_OFF || !cachable) {
     	this.val = UndefValue.ValUndef;
     }
   }
 
-  public final boolean isUncachable() { return this.val == UndefValue.ValUndef; }
+  private boolean isCachable() { return this.val != UndefValue.ValUndef; }
 
-  public final void setValue(final Value aValue) {
-	  assert !isUncachable();
-	  this.val = aValue;
+  /** For testing only. */
+  public int getNumTimesThatANewValueWasCached() {
+    return cacheCount;
   }
 
-  public Value getValue() {
-	  // cache hit on (this.val != null && !isUncachable)
-      // cache miss on (this.val == null)
-	  return this.val;
+  /**
+   * Equivalent to {@link #getValue(Tool, TLCState, TLCState, int)}, but does not
+   * compute a new value.  Instead, if a new value would need to be computed, this
+   * method returns null.
+   *
+   * @param tool a tool that evaluates expressions
+   * @param s0 the first state in the behavior
+   * @param s1 the next state in the behavior
+   * @param control see {@link EvalControl}
+   * @return a cached value for the given tool, behavior, and control; or null if
+   *         computing the requested value would require work
+   */
+  public Value getCachedValue(Tool tool, TLCState s0, TLCState s1, int control) {
+    if (val != null &&
+            isCachable() &&
+            tool.getId() == toolID &&
+            TLCState.isSubset(s0, this.s0).isDefinitely(true) &&
+            TLCState.isSubset(s1, this.s1).isDefinitely(true) &&
+            EvalControl.semanticallyEquivalent(control, this.control).isDefinitely(true)) {
+      return val;
+    }
+    return null;
   }
- 
+
+  /**
+   * Reduce this LazyValue to a fully-reduced value in the given behavior.
+   * The returned value might be cached to accelerate future calls.  LazyValue
+   * guarantees not to misuse the cached value; if this method is called later
+   * with a different tool, behavior, or control option that is incompatible
+   * with the ones used to compute the cached value, then the cached value will
+   * not be returned.
+   *
+   * @param tool a tool that evaluates expressions
+   * @param s0 the first state in the behavior
+   * @param s1 the next state in the behavior
+   * @param control see {@link EvalControl}
+   * @return a fully-reduced value
+   */
+  public Value getValue(Tool tool, TLCState s0, TLCState s1, int control) {
+    Value res = getCachedValue(tool, s0, s1, control);
+    if (res == null) {
+      res = tool.eval(this.expr, this.con, s0, s1, control, getCostModel());
+      if (isCachable()) {
+        this.val = res;
+        this.toolID = tool.getId();
+        this.s0 = s0 != null ? s0.copy() : null;
+        this.s1 = s1 != null ? s1.copy() : null;
+        this.control = control;
+        ++cacheCount;
+      }
+    }
+    return res;
+  }
+
   @Override
   public final byte getKind() { return LAZYVALUE; }
 
