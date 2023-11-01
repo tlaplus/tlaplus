40722f8b581666c59a7616e879a72e33e677bcdf
Calvin Loncaric <calvin.loncaric@oracle.com>
2023-11-01 16:16:20 -0700

    More comments and tests for `FilenameToStream`
    This commit also normalizes the whitespace in `FilenameToStream` to
    four spaces (instead of a mix of tabs and spaces).
    
    Signed-off-by: Calvin Loncaric <calvin.loncaric@oracle.com>

--- tlatools/org.lamport.tlatools/src/util/FilenameToStream.java
+++ tlatools/org.lamport.tlatools/src/util/FilenameToStream.java
@@ -1,5 +1,6 @@
 // Copyright (c) 2003 Compaq Corporation.  All rights reserved.
 // Portions Copyright (c) 2003 Microsoft Corporation.  All rights reserved.
+// Copyright (c) 2023, Oracle and/or its affiliates.
 package util;
 
 import java.io.File;
@@ -25,150 +26,151 @@ import java.util.regex.Pattern;
 public interface FilenameToStream
 {
 
-	/*
-	 * Higher layers of TLC (and the Toolbox) have to determine if a module was
-	 * loaded from a library location s.a. those defined by TLA_LIBRARY (see
-	 * SimpleFilenameToStream). Thus, capture this information at module load
-	 * time when it is known where a module was loaded from.
-	 */
-	@SuppressWarnings("serial")
-	public static class TLAFile extends File {
-		// The following regex is concerned with determining whether the provided 'parent' string to our
-		//	parent/child constructor looks like the start of a legal absolute file path potentially including
-		//	a drive letter.
-		private static final String ROOT_PATH_REGEX
-								= "^([a-zA-Z]+:)?" + ((File.separatorChar == '\\') ? "\\\\" : File.separator);
-		private static final Pattern ROOT_PATH_PATTERN = Pattern.compile(ROOT_PATH_REGEX);
-		
-		
-		private final boolean isLibraryModule;
-		private transient final FilenameToStream resolver;
-		// may be null.
-		private final URI libraryPath;
-
-		public TLAFile(String pathname, FilenameToStream fts) {
-			this(pathname, false, fts);
-		}
-
-		public TLAFile(String pathname, boolean isLibraryModule, FilenameToStream fts) {
-			super(pathname);
-			this.isLibraryModule = isLibraryModule;
-			this.libraryPath = this.exists() ? this.toURI() : null;
-			this.resolver = fts;
-		}
-
-		public TLAFile(Path path, boolean isLibraryModule, FilenameToStream fts) {
-			this(path.toFile().toString(), isLibraryModule, fts);
-		}
-
-		public TLAFile(String parent, String child, FilenameToStream fts) {
-			super(parent,
-				  ((ROOT_PATH_PATTERN.matcher(parent).find() && child.startsWith(parent))
-						  ? child.substring(parent.length())
-					      : child));
-			this.libraryPath = this.exists() ? this.toURI() : null;
-			this.isLibraryModule = false;
-			this.resolver = fts;
-		}
-
-		public TLAFile(String pathname, URL location, boolean isLibraryModule, FilenameToStream fts) {
-			super(pathname);
-			// Do not check exists here like in the other ctors because it returns false
-			// before the file is actually read in util.SimpleFilenameToStream.read(String,
-			// URL, InputStream). Instead, assume that the file exists if we get here. After
-			// all, we know that its inputstream exists.
-			// If the conversion from URL to URI fails, we continue with null an hope that
-			// everything goes fine.
-			this.libraryPath = toNullOrURI(location);
-			this.isLibraryModule = isLibraryModule;
-			this.resolver = fts;
-		}
-
-		public TLAFile(Path path, URL location, boolean isLibraryModule, FilenameToStream fts) {
-			this(path.toFile().toString(), location, isLibraryModule, fts);
-		}
-
-		private static URI toNullOrURI(URL location) {
-	  		try {
-	  			return location.toURI();
-	  		} catch (URISyntaxException e) {
-	  			return null;
-	        }
-		}
-
-		public boolean isLibraryModule() {
-			return isLibraryModule;
-		}
-
-		/**
-		 * @return null if no TLC module override for this module exists.
-		 */
-		public File getModuleOverride() {
-			final File moduleOverride = resolver.resolve(getName().replaceAll(".tla$", ".class"), false);
-			if (moduleOverride.exists()) {
-				// resolve(...) return a File instance even if the file doesn't exist.
-				return moduleOverride;
-			}
-			return null;
-		}
-
-		/**
-		 * This method enables us to keep track of the original path (or library) 
-		 * of the module.
-		 */
-		public URI getLibraryPath() {
-			return libraryPath;
-		}
-
-		public boolean hasLibraryPath() {
-			return libraryPath != null;
-		}
-	}
-	
     /**
-     * This method resolves the logical name to the OS-resource
-     * @param filename
-     * @param isModule
-     * @return
+     * Higher layers of TLC (and the Toolbox) have to determine if a module was
+     * loaded from a library location s.a. those defined by TLA_LIBRARY (see
+     * SimpleFilenameToStream). Thus, capture this information at module load
+     * time when it is known where a module was loaded from.
      */
-    public File resolve(String filename, boolean isModule);
+    @SuppressWarnings("serial")
+    public static class TLAFile extends File {
+        /**
+         * The following regex is concerned with determining whether the provided 'parent' string to our
+         *parent/child constructor looks like the start of a legal absolute file path potentially including
+         *a drive letter.
+         */
+        private static final String ROOT_PATH_REGEX
+                = "^([a-zA-Z]+:)?" + ((File.separatorChar == '\\') ? "\\\\" : File.separator);
+        private static final Pattern ROOT_PATH_PATTERN = Pattern.compile(ROOT_PATH_REGEX);
 
-      /**
-       * August 2014 - TL
-       * Added this method which returns all the path locations stored in the resolver
-      */
-    public String getFullPath();
+
+        private final boolean isLibraryModule;
+        private transient final FilenameToStream resolver;
+        // may be null.
+        private final URI libraryPath;
+
+        public TLAFile(String pathname, FilenameToStream fts) {
+            this(pathname, false, fts);
+        }
+
+        public TLAFile(String pathname, boolean isLibraryModule, FilenameToStream fts) {
+            super(pathname);
+            this.isLibraryModule = isLibraryModule;
+            this.libraryPath = this.exists() ? this.toURI() : null;
+            this.resolver = fts;
+        }
+
+        public TLAFile(Path path, boolean isLibraryModule, FilenameToStream fts) {
+            this(path.toFile().toString(), isLibraryModule, fts);
+        }
+
+        public TLAFile(String parent, String child, FilenameToStream fts) {
+            super(parent,
+                    ((ROOT_PATH_PATTERN.matcher(parent).find() && child.startsWith(parent))
+                            ? child.substring(parent.length())
+                            : child));
+            this.libraryPath = this.exists() ? this.toURI() : null;
+            this.isLibraryModule = false;
+            this.resolver = fts;
+        }
+
+        public TLAFile(String pathname, URL location, boolean isLibraryModule, FilenameToStream fts) {
+            super(pathname);
+            // Do not check exists here like in the other ctors because it returns false
+            // before the file is actually read in util.SimpleFilenameToStream.read(String,
+            // URL, InputStream). Instead, assume that the file exists if we get here. After
+            // all, we know that its inputstream exists.
+            // If the conversion from URL to URI fails, we continue with null an hope that
+            // everything goes fine.
+            this.libraryPath = toNullOrURI(location);
+            this.isLibraryModule = isLibraryModule;
+            this.resolver = fts;
+        }
+
+        public TLAFile(Path path, URL location, boolean isLibraryModule, FilenameToStream fts) {
+            this(path.toFile().toString(), location, isLibraryModule, fts);
+        }
+
+        private static URI toNullOrURI(URL location) {
+            try {
+                return location.toURI();
+            } catch (URISyntaxException e) {
+                return null;
+            }
+        }
+
+        public boolean isLibraryModule() {
+            return isLibraryModule;
+        }
+
+        /**
+         * @return null if no TLC module override for this module exists.
+         */
+        public File getModuleOverride() {
+            final File moduleOverride = resolver.resolve(getName().replaceAll(".tla$", ".class"), false);
+            if (moduleOverride.exists()) {
+                // resolve(...) return a File instance even if the file doesn't exist.
+                return moduleOverride;
+            }
+            return null;
+        }
+
+        /**
+         * This method enables us to keep track of the original path (or library)
+         * of the module.
+         */
+        public URI getLibraryPath() {
+            return libraryPath;
+        }
+
+        public boolean hasLibraryPath() {
+            return libraryPath != null;
+        }
+    }
 
     /**
-     * Returns true iff moduleName is the name of a standard module, which
-     * is identified by the directory in which its source file resides.
+     * Resolves a logical name to a concrete OS-resource.
      *
-     * @param moduleName
-     * @return
-	 * @see tla2sany.modanalyzer.ParseUnit.isLibraryModule()
-	 * @see StandardModules.isDefinedInStandardModule()
+     * @param filename a logical name (e.g. <code>"Integers"</code>) or path (e.g. <code>"subdir/Integers.tla"</code>)
+     * @param isModule if true and the given filename does not end with {@link TLAConstants.Files#TLA_EXTENSION}, then
+     *                 the extension will be added.  Also, if the extension was already present, it will be converted
+     *                 to the correct case.  If <code>isModule</code> is false, then <code>filename</code> will be
+     *                 resolved unaltered.
+     * @return a local file containing the module source, or a non-null non-existent file if the name was not found
      */
-    public boolean isStandardModule(String moduleName) ;
+    // TODO: improve the performance of this interface.
+    // Returning a File is overly restrictive: some implementations resolve from non-filesystem locations like inside
+    // JARs.  To satisfy this interface, those implementations have to copy the resolved resource to a local file.
+    // That copy could be avoided if this interface returned a less restrictive type like NamedInputStream.
+    File resolve(String filename, boolean isModule);
 
-	default boolean isLibraryModule(String moduleName) {
-		return isStandardModule(moduleName);
-	}
- 	
-	static boolean isInJar(final String aString) {
-		return aString.startsWith("jar:") || aString.endsWith(".jar");
-	}
+    /**
+     * Get a string describing the path locations this resolver will search.  The result has no particular format;
+     * it is intended for user-facing error output and debugging.
+     *
+     * @return a string describing the searched locations
+     * @since August 2014 - TL
+     */
+    String getFullPath();
 
-	static boolean isArchive(String aString) {
-		return isInJar(aString) || aString.endsWith(".zip");
-	}
-	
-	static Path getTempDirectory() {
-  		try {
-			return Files.createTempDirectory("tlc-");
-		} catch (IOException e) {
-			// This is unfortunately how we handle things over here. :-(
-			e.printStackTrace();
-		}
-  		return null;
-	}
+    /**
+     * Returns true iff <code>moduleName</code> resolves to a standard module.  The argument is treated the same as a
+     * call to {@link #resolve(String, boolean)} with <code>isModule=true</code>.
+     *
+     * @param moduleName the name of a module (e.g. <code>"Integers"</code>)
+     * @return true iff <code>moduleName</code> names a standard module
+     * @see tla2sany.modanalyzer.ParseUnit#isLibraryModule()
+     * @see tla2sany.semantic.StandardModules#isDefinedInStandardModule(String)
+     */
+    boolean isStandardModule(String moduleName);
+
+    static Path getTempDirectory() {
+        try {
+            return Files.createTempDirectory("tlc-");
+        } catch (IOException e) {
+            // This is unfortunately how we handle things over here. :-(
+            e.printStackTrace();
+        }
+        return null;
+    }
 }
--- tlatools/org.lamport.tlatools/test/util/SimpleFilenameToStreamTest.java
+++ tlatools/org.lamport.tlatools/test/util/SimpleFilenameToStreamTest.java
@@ -1,26 +1,214 @@
+// Copyright (c) 2023, Oracle and/or its affiliates.
+
 package util;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 import org.junit.Test;
+import tla2sany.semantic.StandardModules;
+import tlc2.tool.impl.Tool;
 
 public class SimpleFilenameToStreamTest {
 
-	/**
-	 * Try to load a standard module
-	 */
-	@Test
-	public void testResolveStandardModule() {
-		final SimpleFilenameToStream sfts = new SimpleFilenameToStream();
-		final File file = sfts.resolve("TLC.tla", true);
-		
+	private void checkResolveStandardModule(FilenameToStream resolver, String path, boolean isModule) {
+		File file = resolver.resolve("TLC.tla", true);
 		assertNotNull(file);
 		assertTrue(file.getAbsolutePath() + " does not exist!", file.exists());
+		assertTrue(file + " should be a standard module but isn't!", resolver.isStandardModule(path));
 	}
-	
+
+	/**
+	 * Try various ways of resolving all the standard modules
+	 */
+	@Test
+	public void testResolveStandardModules() {
+		final SimpleFilenameToStream sfts = new SimpleFilenameToStream();
+		for (String name : new String[] {
+				"Bags", "FiniteSets", "Integers", "Json", "Naturals", "Randomization",
+				"Reals", "RealTime", "Sequences", "TLC", "TLCExt", "Toolbox" }) {
+			checkResolveStandardModule(sfts, name, true);
+			assertFalse("Resolution with isModule=false should fail", sfts.resolve(name, false).exists());
+			checkResolveStandardModule(sfts, name + ".tla", true);
+			checkResolveStandardModule(sfts, name + ".tla", false);
+		}
+	}
+
+	/**
+	 * Test if we can resolve modules from {@link ToolIO#getUserDir()}
+	 */
+	@Test
+	public void testResolveFromUserDir() throws IOException  {
+		// Save the old value so we can restore it at the end of the test
+		String oldUserDir = ToolIO.getUserDir();
+
+		// We'll try to delete these at the end of the test
+		Path d1 = null;
+		Path d2 = null;
+
+		try {
+			d1 = Files.createTempDirectory(null);
+			d2 = Files.createTempDirectory(null);
+
+			ToolIO.setUserDir(d1.toString());
+			File f1 = new SimpleFilenameToStream().resolve("MyModule", true);
+			assertFalse("Resolver should not find nonexistent module MyModule", f1.exists());
+
+			// Create MyModule.tla
+			Files.writeString(d1.resolve("MyModule.tla"), "", StandardCharsets.US_ASCII);
+
+			File f2 = new SimpleFilenameToStream().resolve("MyModule", true);
+			assertTrue("Resolver should find MyModule even though it is empty", f2.exists());
+			assertFalse("Module from userDir is not a standard module", new SimpleFilenameToStream().isStandardModule("MyModule"));
+
+			ToolIO.setUserDir(d2.toString());
+			File f3 = new SimpleFilenameToStream().resolve("MyModule", true);
+			assertFalse("Resolver should not find MyModule in different directory", f3.exists());
+
+			// Create subdir/MyModule.tla
+			Path d2subdir = d2.resolve("subdir");
+			Files.createDirectory(d2subdir);
+			Files.writeString(d2subdir.resolve("MyModule.tla"), "", StandardCharsets.US_ASCII);
+
+			File f4 = new SimpleFilenameToStream().resolve("MyModule", true);
+			assertFalse("Resolver should not find MyModule in subdirectory", f4.exists());
+
+			File f5 = new SimpleFilenameToStream().resolve("subdir/MyModule", true);
+			assertTrue("Resolver should find MyModule in subdirectory", f5.exists());
+
+			File f6 = new SimpleFilenameToStream().resolve("subdir/MyModule.tla", true);
+			assertTrue("Resolver should find MyModule.tla in subdirectory", f6.exists());
+
+			File f7 = new SimpleFilenameToStream().resolve("subdir/MyModule", false);
+			assertFalse("Resolver should not find non-module MyModule in subdirectory", f7.exists());
+
+			File f8 = new SimpleFilenameToStream().resolve("subdir/MyModule.tla", false);
+			assertTrue("Resolver should find MyModule.tla in subdirectory", f8.exists());
+		} finally {
+			ToolIO.setUserDir(oldUserDir);
+			try {
+				if (d1 != null) {
+					FileUtil.deleteDir(d1.toFile(), true);
+				}
+			} finally {
+				if (d2 != null) {
+					FileUtil.deleteDir(d2.toFile(), true);
+				}
+			}
+		}
+	}
+
+	/**
+	 * Test that the library path constructor argument behaves as expected
+	 */
+	@Test
+	public void testResolveWithCustomLibraryPath() throws IOException  {
+		// Save the old value so we can restore it at the end of the test
+		String oldUserDir = ToolIO.getUserDir();
+
+		// We'll try to delete these at the end of the test
+		Path d1 = null;
+		Path d2 = null;
+
+		try {
+			d1 = Files.createTempDirectory(null);
+			d2 = Files.createTempDirectory(null);
+
+			FilenameToStream resolver = new SimpleFilenameToStream(d1.toString());
+
+			File f1 = resolver.resolve("Integers", true);
+			assertTrue("Resolver should still find Integers when library path is empty", f1.exists());
+
+			Files.writeString(d1.resolve("MyModule.tla"), "", StandardCharsets.US_ASCII);
+			Files.writeString(d2.resolve("MyModule.tla"), "", StandardCharsets.US_ASCII);
+
+			File f2 = resolver.resolve("MyModule", true);
+			assertTrue("Resolver should find modules in explicit library dir", f2.exists());
+			assertFalse("Modules in explicit library dir are not library modules", resolver.isStandardModule("MyModule"));
+
+			ToolIO.setUserDir(d2.toString());
+
+			resolver = new SimpleFilenameToStream(d1.toString());
+			File f3 = resolver.resolve("MyModule", true);
+			assertTrue("Resolver should prefer userDir over library dir", f3.toString().startsWith(d2.toString()));
+		} finally {
+			ToolIO.setUserDir(oldUserDir);
+			try {
+				if (d1 != null) {
+					FileUtil.deleteDir(d1.toFile(), true);
+				}
+			} finally {
+				if (d2 != null) {
+					FileUtil.deleteDir(d2.toFile(), true);
+				}
+			}
+		}
+	}
+
+	/**
+	 * Test that the resolver handles the TLA_LIBRARY system property as expected
+	 */
+	@Test
+	public void testTLALibrarySystemProperty() throws IOException  {
+		// Save the old value so we can restore it at the end of the test
+		String oldTLALibrary = System.getProperty(SimpleFilenameToStream.TLA_LIBRARY);
+
+		// We'll try to delete these at the end of the test
+		Path d1 = null;
+		Path d2 = null;
+
+		try {
+			d1 = Files.createTempDirectory(null);
+			d2 = Files.createTempDirectory(null);
+
+			FilenameToStream resolver = new SimpleFilenameToStream();
+
+			System.setProperty(SimpleFilenameToStream.TLA_LIBRARY, d1.toString());
+			File f1 = resolver.resolve("Integers", true);
+			assertTrue("Resolver should still find Integers when library path is empty", f1.exists());
+
+			Files.writeString(d1.resolve("MyModule.tla"), "", StandardCharsets.US_ASCII);
+
+			resolver = new SimpleFilenameToStream();
+			File f2 = resolver.resolve("MyModule", true);
+			assertTrue("Resolver should find modules in TLA_LIBRARY", f2.exists());
+			assertFalse("Modules in TLA_LIBRARY are not library modules", resolver.isStandardModule("MyModule"));
+
+			// TODO: is this next check actually desirable?  Because of this choice:
+			//    - When TLC is invoked on a module like "MyModule.tla", it will also use the TLA_LIBRARY system
+			//      property.
+			//    - But, when TLC is invoked on a module like "some-dir/MyModule.tla", it will NOT use the TLA_LIBRARY
+			//      system property.
+			resolver = new SimpleFilenameToStream(d2.toString());
+			File f3 = resolver.resolve("MyModule", true);
+			assertFalse("Resolver should ignore TLA_LIBRARY system property if an explicit path was given", f3.exists());
+		} finally {
+			if (oldTLALibrary != null) {
+				System.setProperty(SimpleFilenameToStream.TLA_LIBRARY, oldTLALibrary);
+			} else {
+				System.clearProperty(SimpleFilenameToStream.TLA_LIBRARY);
+			}
+			try {
+				if (d1 != null) {
+					FileUtil.deleteDir(d1.toFile(), true);
+				}
+			} finally {
+				if (d2 != null) {
+					FileUtil.deleteDir(d2.toFile(), true);
+				}
+			}
+		}
+	}
+
 	/**
 	 * Test whether the fix for #424 still works
 	 */
