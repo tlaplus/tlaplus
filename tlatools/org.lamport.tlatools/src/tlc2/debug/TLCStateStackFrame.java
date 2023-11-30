503a1e483a1909e15fe027043a4db719c8ad8021
Markus Alexander Kuppe <github.com@lemmster.de>
2023-11-30 12:04:36 -0800

    Variables not working as Watch expression.
    Addresses vscode-tlaplus Github issue #311
    https://github.com/tlaplus/vscode-tlaplus/issues/311
    
    [Feature][TLC][Debugger]
    
    Signed-off-by: Markus Alexander Kuppe <github.com@lemmster.de>

--- tlatools/org.lamport.tlatools/src/tlc2/debug/TLCStateStackFrame.java
+++ tlatools/org.lamport.tlatools/src/tlc2/debug/TLCStateStackFrame.java
@@ -33,6 +33,7 @@ import java.util.Deque;
 import java.util.LinkedList;
 import java.util.List;
 
+import org.eclipse.lsp4j.debug.EvaluateResponse;
 import org.eclipse.lsp4j.debug.Scope;
 import org.eclipse.lsp4j.debug.Variable;
 
@@ -270,6 +271,24 @@ public class TLCStateStackFrame extends TLCStackFrame {
 			return variable;
 		}
 		return super.getVariable(path);
+	}
+	
+	@Override
+	public EvaluateResponse getWatch(final String name) {
+		if (name == null) {
+			return new EvaluateResponse();
+		} 
+
+		final IValue lookup = getT().lookup(name);
+		if (lookup != null) { // null when there is no such variable or the value of the variable has not yet been determined.
+			final Variable variable = getVariable(lookup, name);
+			final EvaluateResponse er = new EvaluateResponse();
+			er.setResult(variable.getValue());
+			er.setVariablesReference(variable.getVariablesReference());
+			return er;
+		}
+		
+		return super.getWatch(name);
 	}
 	
 	protected boolean isPrimeScope(LinkedList<SemanticNode> path) {
