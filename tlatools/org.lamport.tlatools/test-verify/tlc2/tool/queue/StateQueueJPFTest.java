534788509c9501b41ca62e20570f55ebc6aab34c
Federico Ponzi <me@fponzi.me>
2024-06-13 20:53:09 +0100

    Refactor StateQueueJPFTest to remove thread creation outside the critical path
    Signed-off-by: Federico Ponzi <me@fponzi.me>

--- tlatools/org.lamport.tlatools/test-verify/tlc2/tool/queue/StateQueueJPFTest.java
+++ tlatools/org.lamport.tlatools/test-verify/tlc2/tool/queue/StateQueueJPFTest.java
@@ -1,9 +1,9 @@
 /*******************************************************************************
- * Copyright (c) 2015 Microsoft Research. All rights reserved. 
+ * Copyright (c) 2015 Microsoft Research. All rights reserved.
  *
  * The MIT License (MIT)
- * 
- * Permission is hereby granted, free of charge, to any person obtaining a copy 
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
@@ -11,8 +11,8 @@
  * so, subject to the following conditions:
  *
  * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software. 
- * 
+ * copies or substantial portions of the Software.
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
  * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
@@ -23,7 +23,6 @@
  * Contributors:
  *   Markus Alexander Kuppe - initial API and implementation
  ******************************************************************************/
-
 package tlc2.tool.queue;
 
 import java.io.IOException;
@@ -35,76 +34,110 @@ import tlc2.tool.TLCState;
 
 public class StateQueueJPFTest extends TestJPF {
 
-	public static void main(String[] args) {
-		new StateQueueJPFTest().test();
-	}
-	
-	@Test
-	public void test() {
-		if (verifyNoPropertyViolation()) {
-			final StateQueue queue = new DummyStateQueue();
-			final TLCState tlcState = new DummyTLCState();
-			queue.enqueue(tlcState);
-			
-			Thread main = new Thread(new Runnable() {
-				public void run() {
-					queue.suspendAll();
-					// critical section (taking a checkpoint)
-					queue.resumeAll();
-				}
-			}, "Main");
-			main.start();
+    private final StateQueue queue = new DummyStateQueue();
+    private final TLCState tlcState = new DummyTLCState();
+    private final Thread mainThread = new Thread(new MainTask(queue), "Main");
+    private final Thread[] workerThreads = new Thread[3];
 
-			for (int i = 0; i < 3; i++) {
-				Thread worker = new Thread(new Runnable() {
-					public void run() {
-						for (int i = 0; i < 3; i++) {
-							TLCState state = queue.dequeue();
-							if (state == null) {
-								queue.finishAll();
-								return;
-							}
-							queue.enqueue(tlcState);
-						}
-						queue.finishAll();
-					}
-				}, "Worker" + i);
-				worker.start();
-			}
-		}
-	}
-	
-	/*
-	 * Dummy implementation of StateQueue with minimal functionality compared to
-	 * DiskStateQueue or MemStateQueue. After all, we verify the abstract
-	 * StateQueue and not the implementations (which should be done separately).
-	 */
-	private static class DummyStateQueue extends StateQueue {
+    public StateQueueJPFTest() {
+        // Initialize the worker threads
+        for (int i = 0; i < workerThreads.length; i++) {
+            workerThreads[i] = new Thread(new WorkerTask(queue, tlcState), "Worker" + i);
+        }
+    }
 
-		private TLCState state;
+    public static void main(String[] args) {
+        new StateQueueJPFTest().test();
+    }
 
-		void enqueueInner(TLCState state) {
-			this.state = state;
-		}
+    @Test
+    public void test() {
+        if (verifyNoPropertyViolation()) {
+            try {
+                // Start the threads only if they are not already started
+                mainThread.start();
 
-		TLCState dequeueInner() {
-			return state;
-		}
+                for (Thread worker : workerThreads) {
+                    if (!worker.isAlive()) {
+                        worker.start();
+                    }
+                }
 
-		TLCState peekInner() {
-			return state;
-		}
+                // Ensure all threads complete before exiting the test
+                mainThread.join();
+                for (Thread worker : workerThreads) {
+                    worker.join();
+                }
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
+    }
 
-		public void beginChkpt() throws IOException {
-			// checkpointing not being verified
-		}
+    private static class MainTask implements Runnable {
+        private final StateQueue queue;
 
-		public void commitChkpt() throws IOException {
-			// checkpointing not being verified
-		}
+        public MainTask(StateQueue queue) {
+            this.queue = queue;
+        }
 
-		public void recover() throws IOException {
-			// checkpointing not being verified
-		}
-	}
+        @Override
+        public void run() {
+            queue.suspendAll();
+            // critical section (taking a checkpoint)
+            queue.resumeAll();
+        }
+    }
+
+    private static class WorkerTask implements Runnable {
+        private final StateQueue queue;
+        private final TLCState tlcState;
+
+        public WorkerTask(StateQueue queue, TLCState tlcState) {
+            this.queue = queue;
+            this.tlcState = tlcState;
+        }
+
+        @Override
+        public void run() {
+            for (int i = 0; i < 3; i++) {
+                TLCState state = queue.dequeue();
+                if (state == null) {
+                    queue.finishAll();
+                    return;
+                }
+                queue.enqueue(tlcState);
+            }
+            queue.finishAll();
+        }
+    }
+
+    private static class DummyStateQueue extends StateQueue {
+
+        private TLCState state;
+
+        void enqueueInner(TLCState state) {
+            this.state = state;
+        }
+
+        TLCState dequeueInner() {
+            return state;
+        }
+
+        TLCState peekInner() {
+            return state;
+        }
+
+        public void beginChkpt() throws IOException {
+            // checkpointing not being verified
+        }
+
+        public void commitChkpt() throws IOException {
+            // checkpointing not being verified
+        }
+
+        public void recover() throws IOException {
+            // checkpointing not being verified
+        }
+    }
 }
