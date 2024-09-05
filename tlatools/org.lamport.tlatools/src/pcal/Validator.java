fa4a6f64cd14421afae66a83f0897745f0fecbc8
Andrew Helwer <2n8rn1w1f@mozmail.com>
2024-09-05 16:45:35 -0400

    Fix PlusCal Unicode handling on Windows
    Signed-off-by: Andrew Helwer <2n8rn1w1f@mozmail.com>

--- tlatools/org.lamport.tlatools/src/pcal/Validator.java
+++ tlatools/org.lamport.tlatools/src/pcal/Validator.java
@@ -5,6 +5,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
@@ -67,7 +68,7 @@ public class Validator {
 			throws IOException {
 		final BufferedReader reader = new BufferedReader(
 				new InputStreamReader((inputStream instanceof BufferedInputStream) ? (BufferedInputStream) inputStream
-						: new BufferedInputStream(inputStream)));
+						: new BufferedInputStream(inputStream), StandardCharsets.UTF_8));
 		return validate(parseUnit, reader);
 	}
 	
