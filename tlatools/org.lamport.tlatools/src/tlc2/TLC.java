4cb92ac5837fb1051d08f775b155fbcc5c157391
Markus Alexander Kuppe <github.com@lemmster.de>
2024-11-27 10:16:59 -0800

    Add `-dumpTrace tlcaction` (with an optional filename) to dump the sequence of actions and their arguments/parameters leading to the counterexample.
    ```tla
    ---- MODULE TLCActionTrace ----
    EXTENDS Simple
    
    _length == 9
    
    _next(_idx) ==
    \/ 1 = _idx /\ A(23)
    \/ 2 = _idx /\ B(77, 42)
    ...
    \/ 9 = _idx /\ A(87)
    
    _actionConstraint ==
    _next(LET T == INSTANCE TLC IN T!TLCGet("level"))
    
    ====
    ```
    
    ```tla
    ------- MODULE MinimizeSimple -------
    EXTENDS Simple, SimpleTrace
    
    VARIABLE _idx
    
    MinimizeInit ==
        /\ Init \* Simple!Init
        /\ _idx = {}
    
    MinimizeNext ==
        \E i \in 1.._length \ _idx:
               /\ _idx' = _idx \cup {i}
               /\ _next(i)
    
    MinimizeView ==
        x \* Simple!x
    
    =====
    ---- CONFIG MinimizeSimple -----
    INIT MinimizeInit
    NEXT MinimizeNext
    VIEW MinimizeView
    
    INVARIANT Inv
    =====
    ```
    
    ```tla
    ------- MODULE Simple -------
    EXTENDS Naturals
    
    VARIABLES x
    
    Init == x = 0
    
    A(i) == x' = x + i
    
    B(i, n) == x' = x - i
    
    Next == \E i \in 0..100: A(i) \/ B(i, 42)
    
    Inv == x < 2000
    
    ====
    ```
    
    Related to Github issue #400 & #1084
    https://github.com/tlaplus/tlaplus/issues/400
    https://github.com/tlaplus/tlaplus/issues/1084
    
    [Feature][TLC]
    
    Signed-off-by: Markus Alexander Kuppe <github.com@lemmster.de>

--- tlatools/org.lamport.tlatools/src/tlc2/TLC.java
+++ tlatools/org.lamport.tlatools/src/tlc2/TLC.java
@@ -642,6 +642,11 @@ public class TLC {
 						final List<PostCondition> pcs = (List<PostCondition>) params.computeIfAbsent(
 								ParameterizedSpecObj.POST_CONDITIONS, k -> new ArrayList<PostCondition>());
 						pcs.add(new PostCondition("_TLCTracePlain", "_TLCTrace", "_TLCTraceFile", args[index++]));
+					} else if ("tlcaction".equalsIgnoreCase(fmt)) {
+						@SuppressWarnings("unchecked")
+						final List<PostCondition> pcs = (List<PostCondition>) params.computeIfAbsent(
+								ParameterizedSpecObj.POST_CONDITIONS, k -> new ArrayList<PostCondition>());
+						pcs.add(new PostCondition("_TLCActionTrace", "_TLCTrace", "_TLCTraceFile", args[index++]));
 					} else if ("dot".equalsIgnoreCase(fmt)) {
 						@SuppressWarnings("unchecked")
 						final List<PostCondition> pcs = (List<PostCondition>) params.computeIfAbsent(
