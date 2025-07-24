9a15e8d3a0693af5704e1666c9d40a1de2937b7f
Markus Alexander Kuppe <github.com@lemmster.de>
2025-07-24 11:14:51 -0700

    Using `-dump` option to produce state dumps in machine readable format.
    Support passing additional definition overrides to a PostCondition.
    
    Part of Github issue #1216
    https://github.com/tlaplus/tlaplus/issues/1216
    
    [Feature][TLC]
    
    Signed-off-by: Markus Alexander Kuppe <github.com@lemmster.de>

--- tlatools/org.lamport.tlatools/src/tlc2/tool/impl/SpecProcessor.java
+++ tlatools/org.lamport.tlatools/src/tlc2/tool/impl/SpecProcessor.java
@@ -2220,7 +2220,7 @@ public class SpecProcessor implements ValueConstants, ToolGlobals {
 		return defns;
 	}
 
-	public java.util.List<ExprNode> getPostConditionSpecs() {
-		return this.specObj.getPostConditionSpecs();
+	public java.util.List<ExprNode> getPostConditionSpecs(final Map<String, String> params) {
+		return this.specObj.getPostConditionSpecs(params);
 	}
 }
