tlaplus/tlaplus

commit 1004befcfea770f011829b0cef79607671936ee8

Bogus undefined identifier error when fairness condition does not completely specify successor state.
Print TLA+ call stack.

Fixes Github issue #317
https://github.com/tlaplus/tlaplus/issues/317

[Bug][TLC]

tlatools/org.lamport.tlatools/src/tlc2/tool/Worker.java
@@ -15,6 +15,7 @@ import tlc2.TLCGlobals;
 import tlc2.output.EC;
 import tlc2.output.MP;
 import tlc2.tool.fp.FPSet;
+import tlc2.tool.impl.CallStackTool;
 import tlc2.tool.impl.Tool;
 import tlc2.tool.queue.IStateQueue;
 import tlc2.util.BufferedRandomAccessFile;
@@ -23,6 +24,8 @@ import tlc2.util.IdThread;
 import tlc2.util.SetOfStates;
 import tlc2.util.statistics.FixedSizedBucketStatistics;
 import tlc2.util.statistics.IBucketStatistics;
+import util.Assert;
+import util.Assert.TLCRuntimeException;
 import util.FileUtil;
 import util.WrongInvocationException;
 
@@ -160,7 +163,54 @@ public final class Worker extends IdThread implements IWorker, INextStateFunctor
 		liveNextStates.put(curStateFP, curState);
 		this.tlc.allStateWriter.writeState(curState, curState, true, IStateWriter.Visualization.STUTTERING);
 
-		this.tlc.liveCheck.addNextState(tlc.tool.getLiveness(), curState, curStateFP, liveNextStates);
+		// Contrary to exceptions that are thrown during the evaluation of the init
+		// predicate and the next-state relation, the code below causes the call stack
+		// to be printed first and only then the trace.  Ordinary safety checking first
+		// prints the trace followed by the call stack.  The Toolbox is oblivious because
+		// it parses the output.  Command-line users, however, might miss the call stack.
+		// Perhaps, it would be best to attach CallStack to the exception and let the
+		// global exception handling take care of it. Unfortunately, TLC's exception
+		// handling is a mess: a) TLCRuntimException and EvalException overlap b)
+		// CallStackTool cannot always be enabled at the site where exceptions
+		// originate, and c) there is no global exception handling.
+		try {
+			this.tlc.liveCheck.addNextState(tlc.tool.getLiveness(), curState, curStateFP, liveNextStates);
+		} catch (EvalException | TLCRuntimeException origExp) {
+			// liveCheck#addNextState throws an EvalException if, e.g., a Java module overrides throw one:
+			// For example: `Cardinality(S) ~> ...` with `S` a naturals, ...
+
+			// Reset the iterator before using it again after the call to addNextState above.
+			liveNextStates.resetNext();
+
+			// TODO: Try to pass DebugTool instead of tool.getLiveness to enable the TLC
+			// debugger for liveness checking.
+			final CallStackTool cTool = new CallStackTool(tlc.tool.getLiveness());
+			try {
+				this.tlc.liveCheck.addNextState(cTool, curState, curStateFP, liveNextStates);
+				// Regular evaluation with tlc.tool.getLiveness failed but CallStackTool
+				// succeeded. This should never happen!
+				Assert.fail(EC.GENERAL, origExp);
+			} catch (EvalException | TLCRuntimeException rerunExp) {
+				// liveCheck#addNextState is not side-effect free. For example, the behavior
+				// (liveness) graph might have been changed and new GraphNodes been added. If
+				// that's the case, calling addNextStates with the same parameters again causes
+				// different exceptions. Those exception are bogus and should not be shown to the
+				// user.
+				// I don't expect equals to do the right thing(tm) for EvalException and/or
+				// TLCRuntimeException. We effectively expect the same exception gets
+				// throw by calling addNextState again.
+				//assert origExp.getClass().isAssignableFrom(rerunExp.getClass());
+				//assert origException.equals(rerunException);
+				
+				if (cTool.hasCallStack()) {
+					// Do not handle the exception here but send it up the stack.
+					// ModelChecker#doNextFailed puts this exception into context and prints the
+					// current (prefix of) the behavior.
+					MP.printError(EC.TLC_NESTED_EXPRESSION, cTool.toString());
+				}
+			}
+			throw origExp;
+		}
 
 		if (liveNextStates.capacity() > (multiplier * INITIAL_CAPACITY)) {
 			// Increase initial size for as long as the set has to grow

tlatools/org.lamport.tlatools/src/tlc2/tool/impl/CallStackTool.java
@@ -239,4 +239,8 @@ public final class CallStackTool extends Tool {
 		value.setSource(expr);
 		return value;
 	}
+
+	public boolean hasCallStack() {
+		return callStack.size() > 0;
+	}
 }

tlatools/org.lamport.tlatools/test-model/Github317.tla
@@ -1,0 +1,28 @@
+-------------------------------- MODULE Github317 --------------------------------
+EXTENDS Naturals
+
+VARIABLES a, b
+vars == <<a,b>>
+
+Init == /\ a = 0
+        /\ b = 0
+        
+disj == /\ a' = a + 1
+
+Next == /\ disj
+        /\ UNCHANGED b \* Moving UNCHANGED b to disj makes bug disappear (see WF_vars(...) below).
+
+Spec == /\ Init
+        /\ [][Next]_vars
+        /\ WF_vars(disj) \* disj does not define b, yet vars includes b.
+
+Inv == ENABLED disj
+
+Prop == <>FALSE \* Force TLC to run liveness checking.
+
+=============================================================================
+---- CONFIG Github317 ----
+SPECIFICATION Spec
+INVARIANT Inv
+PROPERTY Prop
+====

tlatools/org.lamport.tlatools/test/tlc2/tool/liveness/Github317Test.java
@@ -1,0 +1,61 @@
+/*******************************************************************************
+ * Copyright (c) 2021 Microsoft Research. All rights reserved. 
+ *
+ * The MIT License (MIT)
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy 
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is furnished to do
+ * so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software. 
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN
+ * AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ * Contributors:
+ *   Markus Alexander Kuppe - initial API and implementation
+ ******************************************************************************/
+package tlc2.tool.liveness;
+
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+
+import tlc2.output.EC;
+
+public class Github317Test extends ModelCheckerTestCase {
+
+	public Github317Test() {
+		super("Github317", new String[] { "-config", "Github317.tla"}, EC.ExitStatus.ERROR);
+	}
+
+	@Test
+	public void testSpec() {
+		assertTrue(recorder.recorded(EC.TLC_FINISHED));
+		assertTrue(recorder.recordedWithStringValue(EC.TLC_SEARCH_DEPTH, "2"));
+		assertTrue(recorder.recordedWithStringValues(EC.TLC_STATS, "2", "2", "1"));
+
+		final List<String> expectedTrace = new ArrayList<String>(1);
+		expectedTrace.add("/\\ a = 0\n" + "/\\ b = 0");
+		assertTraceWith(recorder.getRecords(EC.TLC_STATE_PRINT2), expectedTrace);
+		
+		assertTrue(recorder.recordedWithStringValues(EC.TLC_NESTED_EXPRESSION,
+				"0. Line 17, column 20 to line 17, column 23 in Github317\n"
+				+ "1. Line 10, column 9 to line 10, column 21 in Github317\n"
+				+ "2. Line 10, column 12 to line 10, column 21 in Github317\n"
+				+ "3. Line 17, column 15 to line 17, column 18 in Github317\n"
+				+ "4. Line 5, column 9 to line 5, column 15 in Github317\n"
+				+ "5. Line 5, column 13 to line 5, column 13 in Github317\n\n"));
+	}
+}

commit f697811cb0346d1afcdad9f938bfc62d3fbb627d

TLC reports error with trace, but no message about what went wrong
Commit 1004befc introduced support for re-running liveness checking with
CallStackTool to improve error diagnostics (the irony). However,
CallStackTool can throw a FingerprintException because it carries source
information that FastTool does not. The calling code in Worker did not
catch FingerprintException, which caused TLC to report an error trace
without the corresponding error message. Catch FingerprintException and
report the appropriate error message.

Fixes Github issue #763
https://github.com/tlaplus/tlaplus/issues/763

[Bug][TLC]

Signed-off-by: Markus Alexander Kuppe <github.com@lemmster.de>

tlatools/org.lamport.tlatools/src/tlc2/tool/Worker.java
@@ -184,7 +184,7 @@ public final class Worker extends IdThread implements IWorker, INextStateFunctor
 		// originate, and c) there is no global exception handling.
 		try {
 			this.tlc.liveCheck.addNextState(tlc.tool.noDebug(), curState, curStateFP, liveNextStates);
-		} catch (EvalException | TLCRuntimeException origExp) {
+		} catch (EvalException | TLCRuntimeException | FingerprintException origExp) {
 			synchronized (this.tlc) {
 				if (this.tlc.printedLivenessErrorStack) {
 					// Another worker beat us to printing an error trace.
@@ -206,7 +206,7 @@ public final class Worker extends IdThread implements IWorker, INextStateFunctor
 					// Regular evaluation with tlc.tool.getLiveness failed but CallStackTool
 					// succeeded. This should never happen!
 					Assert.fail(EC.GENERAL, origExp);
-				} catch (EvalException | TLCRuntimeException rerunExp) {
+				} catch (EvalException | TLCRuntimeException | FingerprintException rerunExp) {
 					// liveCheck#addNextState is not side-effect free. For example, the behavior
 					// (liveness) graph might have been changed and new GraphNodes been added. If
 					// that's the case, calling addNextStates with the same parameters again causes

tlatools/org.lamport.tlatools/test-model/Github763.tla
@@ -1,0 +1,16 @@
+---- MODULE Github763 ----
+
+CONSTANT MV
+VARIABLE x
+Init == x = {}
+Grow == x' = [val |-> 1]
+Next == Grow \/ UNCHANGED x
+Spec == Init /\ [][Next]_x /\ WF_<<x>>(Grow)
+Live == []<>(x /= MV)
+
+====
+---- CONFIG Github763 ----
+SPECIFICATION Spec
+CONSTANT MV = MV
+PROPERTY Live
+====

tlatools/org.lamport.tlatools/test/tlc2/tool/liveness/Github763Test.java
@@ -1,0 +1,83 @@
+/*******************************************************************************
+ * Copyright (c) 2026 NVIDIA Corp. All rights reserved.
+ *
+ * The MIT License (MIT)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is furnished to do
+ * so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN
+ * AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ * Contributors:
+ *   Markus Alexander Kuppe - initial API and implementation
+ ******************************************************************************/
+package tlc2.tool.liveness;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import tlc2.output.EC;
+
+public class Github763Test extends ModelCheckerTestCase {
+
+	public Github763Test() {
+		super("Github763", new String[] { "-config", "Github763.tla" }, EC.ExitStatus.ERROR);
+	}
+
+	@Override
+	protected boolean runWithDebugger() {
+		return false;
+	}
+
+	@Override
+	protected boolean noGenerateSpec() {
+		return true;
+	}
+
+	@Override
+	protected boolean doCoverage() {
+		return false;
+	}
+
+	@Override
+	protected boolean doDump() {
+		return false;
+	}
+
+	@Override
+	protected boolean doDumpTrace() {
+		return false;
+	}
+
+	@Test
+	public void testSpec() {
+		assertTrue(recorder.recorded(EC.TLC_FINISHED));
+
+		// Assert the error trace is printed.
+		assertFalse(recorder.getRecords(EC.TLC_STATE_PRINT2).isEmpty());
+
+		// Assert the error message is printed (regression: was previously missing).
+		assertTrue(recorder.recordedWithStringValues(EC.GENERAL,
+				"TLC threw an unexpected exception.\n" + "This was probably caused by an error in the spec or model.\n"
+						+ "See the User Output or TLC Console for clues to what happened.\n"
+						+ "The exception was a java.lang.RuntimeException\n"
+						+ ": Attempted to check equality of the set {} with the value:\n" + "[val |-> 1]"));
+
+		// Assert the nested expression (call stack) is printed.
+		assertTrue(recorder.recorded(EC.TLC_NESTED_EXPRESSION));
+	}
+}